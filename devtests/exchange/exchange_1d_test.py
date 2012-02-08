babb5f1e48ac5c5440d4b0e5abc070ed69e6f202
Dmitri Chernyshenko <d.chernyshenko@soton.ac.uk>
2012-02-08 18:39:57 +0000

    Computing the exchange field for a minimal energy configuration of spins in 1d and 3d

---
+++ devtests/exchange/exchange_1d_test.py
@@ -0,0 +1,25 @@
+import dolfin as df
+import numpy as np
+import sys
+
+#M.assign(df.Expression("3 * x[0]"))
+#M.assign(df.Constant(1))
+def compute_b(m):
+    interval = df.UnitInterval(4)
+    V = df.FunctionSpace(interval, "Lagrange", 1)
+    M = df.Function(V)
+    M.vector()[:] = np.array(m)
+    H_ex = df.TrialFunction(V)
+    v = df.TestFunction(V)
+    a = df.inner(H_ex, v) * df.dx
+    L = - df.inner(df.grad(M), df.grad(v)) * df.dx
+    A = df.assemble(a)
+    b = df.assemble(L)
+    H_ex = df.Function(V)
+    df.solve(A, H_ex.vector(), b)
+    return b.array()
+
+for m in np.eye(5):
+    print compute_b(m)
+
+print "\n", compute_b([0,1,2,3,4])
---
+++ devtests/exchange/exchange_3d_test.py
@@ -0,0 +1,54 @@
+import dolfin as df
+import numpy as np
+import math
+
+N = 5
+def compute_b(m):
+    interval = df.UnitInterval(N-1)
+    V = df.VectorFunctionSpace(interval, "Lagrange", 1, dim=3)
+
+    M    = df.Function(V)
+    M.vector()[:] = np.array(m)
+
+    H_ex = df.TrialFunction(V)
+    v    = df.TestFunction(V)
+
+    a = df.inner(H_ex, v) * df.dx
+    A = df.assemble(a)
+
+    L = - df.inner(df.grad(M), df.grad(v)) * df.dx
+    b = df.assemble(L)
+
+    H_ex = df.Function(V)
+    df.solve(A, H_ex.vector(), b)
+    return b.array()
+
+def rotate(v, a):
+    return np.array([
+        v[0]*math.cos(a) + v[1]*math.sin(a),
+        -v[0]*math.sin(a) + v[1]*math.cos(a),
+        0
+    ])
+
+for m in np.eye(N*3):
+    print compute_b(m)
+
+def cross(a, b):
+    assert a.shape[0] == 3
+    assert a.shape == b.shape
+    res = np.empty(a.shape)
+    res[0] = a[1]*b[2] - a[2]*b[1]
+    res[1] = a[2]*b[0] - a[0]*b[2]
+    res[2] = a[0]*b[1] - a[1]*b[0]
+    return res
+
+m = np.zeros(N*3)
+for i in xrange(N):
+    m[i::N] = rotate([1,0,0],math.pi*i/(N-1))
+
+b = compute_b(m)
+
+m.shape = (3, -1)
+b.shape = (3, -1)
+
+print cross(m, b)
