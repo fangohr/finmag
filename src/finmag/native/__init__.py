db85ae6f5fad36159fde1b1fe0b4e36f8103a552
Dmitri Chernyshenko <d.chernyshenko@soton.ac.uk>
2012-02-13 19:22:42 +0000

    Added a blank C++ extension module

---
+++ src/finmag/native/__init__.py
@@ -0,0 +1,17 @@
+# FinMag - a thin layer on top of FEniCS to enable micromagnetic multi-physics simulations
+# Copyright (C) 2012 University of Southampton
+# Do not distribute
+#
+# CONTACT: h.fangohr@soton.ac.uk
+#
+# AUTHOR(S) OF THIS FILE: Dmitri Chernyshenko (d.chernyshenko@soton.ac.uk)
+
+"""
+Provides the symbols from the finmag native extension module.
+
+Importing this module will first compile the finmag extension module, then
+load all of its symbols into this module's namespace.
+"""
+from ..util.native_compiler import make_modules as _make_modules
+_make_modules()
+from ..native.finmag import *
---
+++ src/finmag/util/native_compiler.py
@@ -0,0 +1,59 @@
+# FinMag - a thin layer on top of FEniCS to enable micromagnetic multi-physics simulations
+# Copyright (C) 2012 University of Southampton
+# Do not distribute
+#
+# CONTACT: h.fangohr@soton.ac.uk
+#
+# AUTHOR(S) OF THIS FILE: Dmitri Chernyshenko (d.chernyshenko@soton.ac.uk)
+
+"""
+Automatic compilation of C/C++ extension modules.
+
+Invoking make_modules() will run the Makefile in the native code directory.
+If make returns an error code, an exception will be raised.
+
+Only the first call to make_modules() will invoke make; subsequent calls are ignored.
+
+This module should not be used directly. Use
+
+    from finmag.native import [symbol]
+
+when a native function or class is required.
+"""
+import subprocess
+import sys
+import os
+import re
+
+__all__ = ["make_modules"]
+
+NATIVE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../native")
+MODULES_OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../finmag/native")
+MAKEFILE = os.path.join(NATIVE_DIR, "Makefile")
+
+def replace_c_errors_with_python_errors(s):
+    repl = lambda m: r'File "%s", line %s (%s): ' % (os.path.abspath(os.path.join(NATIVE_DIR, m.group(1))), m.group(2), m.group(3))
+    return re.sub(r"([^\s:]+):(\d+):(\d+): ", repl, s)
+
+def run_make(cmd, **kwargs):
+    try:
+        subprocess.check_output(cmd, stderr=subprocess.STDOUT, **kwargs)
+    except subprocess.CalledProcessError, ex:
+        output = replace_c_errors_with_python_errors(ex.output)
+        sys.stderr.write(output)
+        raise Exception("make_modules: Make failed")
+
+modules_compiled = False
+def make_modules():
+    global modules_compiled
+    if not modules_compiled:
+        if not os.environ.has_key('DISABLE_PYTHON_MAKE') and os.path.exists(MAKEFILE):
+            run_make(["make"], cwd=NATIVE_DIR)
+        modules_compiled = True
+
+def pipe_output(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, bufsize=1)
+    for line in process.stdout:
+        print replace_c_errors_with_python_errors(line),
+    process.communicate()
+    return process.poll()
