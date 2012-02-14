ce6609f73cc5ca744adfa8db843baf8103938773
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-02-14 14:50:51 +0000

    add a test without pinning just in case

--- tests/test_exchange_static.py
+++ tests/test_exchange_static.py
@@ -8,17 +8,25 @@ from ..src.helpers import make_vectors_function,angle
 
 TOLERANCE = 5e-10
 
-def test_all_orientations():
+# define the mesh
+length = 20e-9 #m
+simplexes = 10
+mesh = dolfin.Interval(simplexes, 0, length)
 
-  length = 20e-9 # m
-  simplexes = 10
-  mesh = dolfin.Interval(simplexes, 0, length) 
+# initial configuration of the magnetisation
+left_right = 'MS * (2*x[0]/L - 1)'
+up_down = 'sqrt(MS*MS - MS*MS*(2*x[0]/L - 1)*(2*x[0]/L - 1))'
 
-  def angles_after_a_nanosecond(initial_M):
+possible_orientations = [
+    (left_right, up_down, '0'), # (left_right, '0', up_down),
+    (up_down, '0', left_right), # (up_down, left_right, '0'),
+    ('0', left_right, up_down)] # , ('0', up_down, left_right)]
+
+def angles_after_a_nanosecond(initial_M, pins=[]):
     llg = LLG(mesh)
     llg.initial_M_expr(initial_M, L=length, MS=llg.MS)
     llg.setup()
-    llg.pins = [0, 10]
+    llg.pins = pins 
 
     ts = numpy.linspace(0, 1e-9, 2)
     ys, infodict = odeint(llg.solve_for, llg.M, ts, atol=10, full_output=True)
@@ -28,14 +36,12 @@ def test_all_orientations():
     angles = numpy.array([angle(M[i], M[i+1]) for i in xrange(len(M)-1)])
     return angles
 
-  left_right = 'MS * (2*x[0]/L - 1)'
-  up_down = 'sqrt(MS*MS - MS*MS*(2*x[0]/L - 1)*(2*x[0]/L - 1))'
+def test_all_orientations_without_pinning():
+    for M0 in possible_orientations:
+        angles = angles_after_a_nanosecond(M0)
+        assert angles.max() < TOLERANCE
 
-  possible_orientations = [
-    (left_right, up_down, '0'), (left_right, '0', up_down),
-    (up_down, left_right, '0'), (up_down, '0', left_right),
-    ('0', left_right, up_down), ('0', up_down, left_right)]
-
-  for M0 in possible_orientations:
-    angles = angles_after_a_nanosecond(M0)
-    assert abs(angles.max() - angles.min()) < TOLERANCE
+def test_all_orientations_with_pinning():
+    for M0 in possible_orientations:
+        angles = angles_after_a_nanosecond(M0, [0, 10])
+        assert abs(angles.max() - angles.min()) < TOLERANCE
