c3ac0a037fc5149ce5ca25c6a2148e99dd649d46
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-16 15:02:26 +0000

    Allowing to compute exchange energy (could do with some tests)

--- src/finmag/sim/exchange.py
+++ src/finmag/sim/exchange.py
@@ -25,8 +25,8 @@ class Exchange(object):
         self.method = method
 
         v = df.TestFunction(V)
-        E = self.exchange_factor * df.inner(df.grad(M), df.grad(M)) * df.dx
-        self.dE_dM = df.derivative(E, M, v)
+        self.E = self.exchange_factor * df.inner(df.grad(M), df.grad(M)) * df.dx
+        self.dE_dM = df.derivative(self.E, M, v)
 
         if method=='box':
             self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
@@ -38,11 +38,17 @@ class Exchange(object):
             self.a = df.dot(H_exch_trial, self.v) * df.dx
             self.L = self.dE_dM
             self.H_exch_project = df.Function(self.V)        
-
+            #Note that we could make this 'project' method faster by computing the matrices
+            #that represent a and L, and only to solve the matrix system in 'compute'().
+            #IF this method is actually useful, we can do that. HF 16 Feb 2012
         else:
             NotImplementedError("Only 'box' and 'project' methods are implemented")
 
     def compute(self):
+        print "Consider using compute_field rather than compute - this is to also provide compute_energy"
+        return self.compute_field()
+        
+    def compute_field(self):
         """Assembles vector with H_exchange, and returns effective field as 
         numpy array.
         
@@ -62,3 +68,5 @@ class Exchange(object):
         else: 
             raise NotImplementedError
 
+    def compute_energy(self):
+        return df.assemble(self.E)
