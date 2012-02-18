77cf6f806a5b5eec91cccf247653f87e5b34e5e3
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-18 01:28:30 -0800

    Gathering scripts that help with setting up the right libraries to install finmag on Ubuntu 11.10

---
+++ install/on-fresh-ubuntu-11.10/fenics.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:fenics-packages/fenics
+sudo apt-get update
+sudo apt-get install fenics
+sudo apt-get dist-upgrade
---
+++ install/on-fresh-ubuntu-11.10/finmag-libs.sh
@@ -0,0 +1,7 @@
+#version control
+sudo apt-get install mercurial
+#time integration / c++ 
+sudo apt-get install libboost-python-dev libboost-thread-dev libsundials-serial-dev
+#useful support libraries
+sudo apt-get install python-matplotlib python-visual python-scipy ipython
+
