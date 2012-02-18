d1f73710c4e213a7ee8531f234ab934798a9d583
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-18 23:07:52 +0000

    Adding more tools

--- install/on-fresh-ubuntu-11.10/fangohr-preferred-ubuntu-pkgs.sh
+++ install/on-fresh-ubuntu-11.10/fangohr-preferred-ubuntu-pkgs.sh
@@ -1,1 +1,6 @@
-sudo apt-get install aptitude emacs
+#editors
+sudo apt-get install aptitude emacs gvim-gnome
+#mesh generators
+sudo apt-get install netgen netgen-doc gmsh
+#Plotting
+sudo apt-get install grace gnuplot
