326f2891c0750c42653d40ec9e4120dbc660c039
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-20 14:52:41 +0000

    Some work on Jacobian finite difference test (with Anders)

--- src/finmag/sim/tests/test_jacobian.py
+++ src/finmag/sim/tests/test_jacobian.py
@@ -6,6 +6,7 @@ set_log_level(21)
 
 # Mesh and functions
 m = 1e-5
+m = 1
 mesh = Box(0,m,0,m,0,m,1,1,1)
 V = VectorFunctionSpace(mesh, "CG", 1)
 u = TrialFunction(V)
@@ -26,9 +27,11 @@ M = project(Constant((Ms, 0, 0)), V)
 
 # Variational forms
 a = inner(u, v)*dx
-L = inner((-p*cross(M,H)
-           -p*alpha/Ms*cross(M,cross(M,H))
-           -c*(inner(M,M) - Ms**2)*M/Ms**2), v)*dx
+#L = inner((-p*cross(M,H)
+#           -p*alpha/Ms*cross(M,cross(M,H))
+#           -c*(inner(M,M) - Ms**2)*M/Ms**2), v)*dx
+L = inner(M, v)*dx
+
 
 # Ufl jacobian
 J = derivative(L, M)
@@ -45,7 +48,7 @@ solve(a == L, dM1)
 n = len(dM1.vector())
 J2 = np.zeros((n,n))
 
-h = 1e-10
+h = 1 #small change relative to Ms
 for i in range(n):
     M.vector()[i] += h
 
@@ -54,9 +57,15 @@ for i in range(n):
 
     # Forward differences
     J2[i,:] = (dM2.vector() - dM1.vector())/h
-    #M.vector()[i] -= h
+    #print i, M.vector().array()
+    M.vector()[i] -= h
+    
+
 
 print J[0]
 print ''
 print J2[0]
 
+
+#Observation: J2 is diagonal matrix (as expected for the chosen L), but J shows three submatrices along the diagonal.
+#Dont' know why at the moment.
