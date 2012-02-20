d6f01c4802515625ff9866e1141c5ca2ed41e384
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-20 16:34:19 +0000

    Adding some print statements
    - allows to see actual tolerances if
    - file run on its own or
    - using pytest -s (where -s means not to capture the stdout)
    
    [id 24993289] [id 24993283]

--- devtests/demag/test_solver_nitsche.py
+++ devtests/demag/test_solver_nitsche.py
@@ -21,11 +21,11 @@ class TestNischeSolver(object):
         self.problem3d = MagUnitSphere()
 
     def test_1d(self):
-        self.probtest(self.problem1d)
+        return self.probtest(self.problem1d)
     def test_2d(self):
-        self.probtest(self.problem2d)
+        return self.probtest(self.problem2d)
     def test_3d(self):
-        self.probtest(self.problem3d)
+        return self.probtest(self.problem3d)
 ##        
     def probtest(self,problem):
         solver = NitscheSolver(problem,problem.gamma)
@@ -33,6 +33,7 @@ class TestNischeSolver(object):
         self.dbc_test(problem,solution)
         self.continuity_test(problem,solver,solution)
         self.normalderivativejump_test(problem,solver,solution)
+        return solver,solution
 
     def dbc_test(self,problem, solution):
         #1 Test dirichlet boundary condition on outside
@@ -40,6 +41,7 @@ class TestNischeSolver(object):
         a = abs(solution)*ds
         c = one*ds
         L1error = assemble(a)/assemble(c)
+        print "dbc_test: L1error=",L1error
         errmess = "Error in Nitsche Solver with problem " + problem.desc() + \
         " outer dirichlet BC condition not satisfied, average solution boundary integral is %g"%(L1error)
         assert L1error < TOL,errmess 
@@ -54,6 +56,8 @@ class TestNischeSolver(object):
         c = one('-')*dSC
         L1error1 = assemble(a1,interior_facet_domains = problem.coreboundfunc)/assemble(c,interior_facet_domains = problem.coreboundfunc)
         L1error2 = assemble(a2,interior_facet_domains = problem.coreboundfunc)/assemble(c,interior_facet_domains = problem.coreboundfunc)
+        print "continuity_test: L1error1=",L1error1
+        print "continuity_test: L1error2=",L1error2
         assert L1error1 < TOL,"Error in Nitsche Solver with problem" + problem.desc() + " continuity accross magnetic core boundary not satisfied for phi1 and phi2, \
                                TOL = %g, average L1error = %g"%(TOL,L1error1)
         assert L1error2 < TOL,"Error in Nitsche Solver with 1d problem, continuity accross magnetic core boundary not satisfied for phi total \
@@ -72,9 +76,22 @@ class TestNischeSolver(object):
         c = one('-')*dSC
         L1error1 = assemble(a1,interior_facet_domains = problem.coreboundfunc )/assemble(c,interior_facet_domains = problem.coreboundfunc)
         L1error2 = assemble(a2,interior_facet_domains = problem.coreboundfunc)/assemble(c,interior_facet_domains = problem.coreboundfunc)
+        print "normalderivativejump_test: L1error1=",L1error1
+        print "normalderivativejump_test: L1error2=",L1error2
         assert L1error1 < TOL,"Error in Nitsche Solver with 1d problem, normal derivative jump accross magnetic core boundary not satisfied for phi1 and phi2, \
                                TOL = %g, average L1error = %g"%(TOL,L1error1)
         ##This test is failed since the total function is twice as high as it should be on the boundary. When this is solved I expect this to be passed.
 ##        assert L1error2 < TOL,"Error in Nitsche Solver with 1d problem, normal derivative jump accross magnetic core boundary not satisfied for phi total \
 ##                               TOL = %g, average L1error = %g"%(TOL,L1error2)
 
+
+
+if __name__ == "__main__":
+    t = TestNischeSolver()
+    t.setup_class()
+    print "* Doing test 1d ==========="
+    solver, solution = t.test_1d()
+    print "* Doing test 2d ==========="
+    solver, solution = t.test_2d()
+    print "* Doing test 3d ==========="
+    solver, solution = t.test_3d()
