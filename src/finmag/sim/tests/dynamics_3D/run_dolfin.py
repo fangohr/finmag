f5f6125a671405cab4d8aa9d8b5f695d8a1516dd
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-02-21 18:00:34 +0000

    with exchange (and animation)

--- src/finmag/sim/tests/dynamics_3D/run_dolfin.py
+++ src/finmag/sim/tests/dynamics_3D/run_dolfin.py
@@ -1,5 +1,6 @@
 import dolfin as df
 import numpy as np
+import finmag.sim.helpers as h
 
 from scipy.integrate import ode
 from finmag.sim.llg import LLG
@@ -13,6 +14,16 @@ llg.alpha = 0.1
 llg.MS = 0.86e6 # A/m
 llg.C = 1.3e-11 # J/m
 llg.H_app = (0.43e6, 0, 0) # A/m
+
+# non-uniform starting magnetisation
+nodes = mesh.num_vertices()
+vectors = h.perturbed_vectors(nodes, [1,1,0], llg.MS)
+print [h.norm(v) for v in vectors]
+vectors_for_dolfin = h.rows_to_columns(vectors).flatten()
+llg._M = df.Function(llg.V) # no API for initialising with pre-computed field.
+llg.M = vectors_for_dolfin
+df.plot(llg._M)
+
 llg.initial_M_expr(("MS/sqrt(2)", "MS/sqrt(2)", "0"), MS=llg.MS)
 llg.setup(exchange_flag=False)
 
@@ -21,7 +32,9 @@ llg_wrap = lambda t, y: llg.solve_for(y, t)
 r = ode(llg_wrap).set_integrator("vode", method="bdf", with_jacobian=False)
 r.set_initial_value(llg.M, t0)
 while r.successful() and r.t <= tmax:
+    df.plot(llg._M)
     Mx, My, Mz = llg.average_M()
     fh.write(str(r.t) + " " + str(Mx) + " " + str(My) + " " + str(Mz) + "\n")
     r.integrate(r.t+dt)
 fh.close()
+df.interactive()
