9187e29b65d5fde90c195c8112c7f9c48a2ac26c
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-02-22 12:01:30 +0000

    [completed #24968955] magnetisation aligns with applied field

---
+++ src/finmag/sim/tests/test_applied_field.py
@@ -0,0 +1,21 @@
+import dolfin as df
+from scipy.integrate import odeint
+from finmag.sim.llg import LLG
+
+TOLERANCE = 1e-2
+
+def test_aligns_with_applied_field():
+    llg = LLG(df.UnitCube(2, 2, 2))
+    llg.initial_M((llg.MS, 0, 0))
+    llg.H_app = (0, 0, llg.MS/2)
+    llg.setup(False)
+
+    ts = [0, 1e-9]
+    ys = odeint(llg.solve_for, llg.M, ts, atol=0.1)
+
+    Mx, My, Mz = llg.average_M()
+
+    assert abs(Mx) < TOLERANCE
+    assert abs(My) < TOLERANCE
+    assert llg.MS - Mz < TOLERANCE
+
