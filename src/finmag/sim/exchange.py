c53af570c1a0df6796631470624758bb2762c4bc
Anders Johansen <anders.e.johansen@gmail.com>
2012-02-23 13:36:06 +0000

    Added docstrings to anisotropy and exchange for documentation testing.

--- src/finmag/sim/exchange.py
+++ src/finmag/sim/exchange.py
@@ -2,24 +2,59 @@ import numpy as np
 import dolfin as df
 
 class Exchange(object):
+    """
+    Compute the exchange field.
+
+    .. math::
+        
+        E_{\\text{exch}} = \\int_\\Omega A (\\nabla M)^2  dx
+        
+    *Arguments*
+        V 
+            a dolfin VectorFunctionSpace object.
+        M 
+            the dolfin object representing the magnetisation
+        C 
+            the exchange constant
+        Ms 
+            the saturation magnetisation
+
+    Possible methods are 'box','project'.
+
+    At the moment, we think 'box' works (and is what is used in Magpar and Nmag).
+
+    However, we are curious what happens if we 'properly project' the exchange field
+    into the function space.
+
+    *Example of Usage*
+
+        .. code-block:: python
+
+            >>> m    = 1e-8
+            >>> n    = 5
+            >>> mesh = Box(0, m, 0, m, 0, m, n, n, n)
+            
+            >>> V  = VectorFunctionSpace(mesh, "Lagrange", 1)
+            >>> Ms = 8.6e5 # A/m 
+            >>> C  = 1.3e-11 # J/m exchange constant
+            >>> M  = project((Constant((Ms, 0, 0)), V) # Initial magnetisation
+
+            >>> exchange = Exchange(V, M, C, Ms)
+
+            >>> # Print energy
+            >>> print exchange.compute_energy()
+
+            >>> # Exchange field ('box' method)
+            >>> H_exch = exchange.compute_field_box()
+
+            >>> # Using 'project' method
+            >>> exchange_pro = Exchange(V, M, C, Ms, method='project')
+            >>> H_exch_pro = exchange_pro.compute_field_project()
+            
+    """
+
     def __init__(self, V, M, C, Ms, method='box'):
-        """
-        Compute the exchange field.
         
-        The constructor takes the parameters
-            V a dolfin VectorFunctionSpace
-            M the dolfin object representing the magnetisation
-            C the exchange constant
-            Ms the saturation magnetisation
-
-        Possible methods are 'box','project'.
-
-        At the moment, we think 'box' works (and is what is used in Magpar and Nmag).
-        However, we are curious what happens if we 'properly project' the exchange field
-        into the function space.
-        
-        """
-
         mu0 = 4 * np.pi * 10**-7 # Vs/(Am)
         self.exchange_factor = df.Constant(-2 * C / (mu0 * Ms))
         self.method = method
@@ -47,11 +82,35 @@ class Exchange(object):
             NotImplementedError("Only 'box' and 'project' methods are implemented")
 
     def compute_field_box(self):
+        """
+        Assemble vector with H_exchange using the 'box' method.
+        
+        *Returns*
+            numpy.ndarray
+                The effective field.
+                
+        """
         return df.assemble(self.dE_dM).array() / self.vol
 
     def compute_field_project(self):
+        """
+        Assemble vector with H_exchange using the 'project' method.
+        
+        *Returns*
+            numpy.ndarray
+                The effective field.
+                
+        """
         df.solve(self.a == self.L, self.H_exch_project)
         return self.H_exch_project.vector().array()
 
     def compute_energy(self):
+        """
+        Return the exchange energy.
+
+        *Returns*
+            Float
+                The exchange energy.
+
+        """
         return df.assemble(self.E)
