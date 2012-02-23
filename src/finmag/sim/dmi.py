e9f0623997b214cef854cab36b126fea104e2449
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-23 15:26:31 +0000

    Changes to make basic DMI demo work (fixed field for now) with Marijan
    [#25057631]

--- src/finmag/sim/dmi.py
+++ src/finmag/sim/dmi.py
@@ -3,65 +3,33 @@ import dolfin as df
 
 #This file needs updating for DMI -- based on exchange.py
 class DMI(object):
-    def __init__(self, V, M, C, Ms, method='box'):
+    def __init__(self, V, M, Ms):
         """
         Compute the DMI field 
         
         The constructor takes the parameters
             V a dolfin VectorFunctionSpace
             M the dolfin object representing the magnetisation
-            C the exchange constant
             Ms the saturation magnetisation
 
-        Possible methods are 'box','project'.
-
-        At the moment, we think 'box' works (and is what is used in Magpar and Nmag).
-        However, we are curious what happens if we 'properly project' the exchange field
-        into the function space.
-        
         """
 
-        mu0 = 4 * np.pi * 10**-7 # Vs/(Am)
-        self.exchange_factor = df.Constant(-2 * C / (mu0 * Ms))
-        self.method = method
+        #v = df.TestFunction(V)
 
-        v = df.TestFunction(V)
-        self.E = self.exchange_factor * df.inner(df.grad(M), df.grad(M)) * df.dx
-        self.dE_dM = df.derivative(self.E, M, v)
+        self.M = M   
+        self.V = V
 
-        if method=='box':
-            self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
-        elif method=='project':
-            self.v = v
-            self.V = V
-            
-            H_exch_trial = df.TrialFunction(self.V)
-            self.a = df.dot(H_exch_trial, self.v) * df.dx
-            self.L = self.dE_dM
-            self.H_exch_project = df.Function(self.V)        
-            #Note that we could make this 'project' method faster by computing the matrices
-            #that represent a and L, and only to solve the matrix system in 'compute'().
-            #IF this method is actually useful, we can do that. HF 16 Feb 2012
-        else:
-            NotImplementedError("Only 'box' and 'project' methods are implemented")
-
-    def compute(self):
-        print "Consider using compute_field rather than compute - this is to also provide compute_energy"
-        return self.compute_field()
-        
     def compute_field(self):
-        """Assembles vector with H_exchange, and returns effective field as 
-        numpy array.
-        """
+        """Compute the DMI field, and return it as a numpy array
 
-        if self.method == 'box':
-            box = df.assemble(self.dE_dM).array() / self.vol
-            return box
-        elif self.method == 'project':
-            df.solve( self.a==self.L, self.H_exch_project)
-            return self.H_exch_project.vector().array()
-        else: 
-            raise NotImplementedError
+        Access to the current magnetisation is available through self.M 
+        """
+        H_dmi_tmp = df.Constant((0,0,0.8e6))        
+        H_dmi_dolfin_field = df.project(H_dmi_tmp,self.V)
+        return H_dmi_dolfin_field.vector().array()
+        
 
     def compute_energy(self):
-        return df.assemble(self.E)
+        """Compute the DMI energy, and return as a number """
+        raise NotImplementedError("Compute_energy not implemented yet for DMI term")
+
