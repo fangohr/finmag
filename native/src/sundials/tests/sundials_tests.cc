8948631f5671025500befcb75520467bde6b629a
Dmitri Chernyshenko <d.chernyshenko@soton.ac.uk>
2012-02-23 16:04:05 +0000

    Added nvector_to_array_object and some tests

--- native/src/sundials/tests/sundials_tests.cc
+++ native/src/sundials/tests/sundials_tests.cc
@@ -49,4 +49,19 @@ BOOST_AUTO_TEST_CASE(test_nvector_to_array)
     for (int i = 0; i < N; i+=   N/1000) arr.data()[i] = 17;
 }
 
+BOOST_AUTO_TEST_CASE(test_nvector_to_array_object)
+{
+    N_Vector vec = N_VNew_Serial(N);
+    bp::object obj = finmag::sundials::nvector_to_array_object(vec);
+    np_array<double> arr = bp::extract<np_array<double> >(obj);
+    // assign to numpy array
+    arr.data()[0] = 3.14;
+    // check NVector data
+    BOOST_CHECK_EQUAL(NV_Ith_S(vec, 0), 3.14);
+    // free the original NVector
+    N_VDestroy_Serial(vec);
+    // array data still usable
+    for (int i = 0; i < N; i+=   N/1000) arr.data()[i] = 17;
+}
+
 BOOST_AUTO_TEST_SUITE_END()
