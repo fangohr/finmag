bba5b7b0a8dd339bcbd03ead13a3d079456424f3
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-02-26 17:30:55 +0000

    store less files, compare on the fly

--- src/finmag/sim/tests/dynamics_1D/test_exchange_1d.py
+++ src/finmag/sim/tests/dynamics_1D/test_exchange_1d.py
@@ -1,22 +1,13 @@
-import sys
-import numpy
 import dolfin
-import sys
-
-from scipy.integrate import odeint, ode
-
+import numpy
+import finmag.sim.helpers as h
 from finmag.sim.llg import LLG
-from finmag.sim.helpers import vectors,angle
-
-#
-# TODO: test with and without pinning, build comparison with
-#       dynamics of nmag and then remove the file test_exchange_static
-#
+from scipy.integrate import odeint, ode
 
 TOLERANCE = 1e-7
 
 # define the mesh
-length = 20e-9 #m
+length = 20e-9 # m
 simplexes = 10
 mesh = dolfin.Interval(simplexes, 0, length)
 
@@ -25,57 +16,64 @@ m0_x = '2*x[0]/L - 1'
 m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
 m0_z = '0'
 
-def test_exchange_without_pinning():
-    llg = LLG(mesh)
-    llg.set_m0((m0_x, m0_y, m0_z), L=length)
-    llg.setup(exchange_flag=True)
+llg = LLG(mesh)
+llg.Ms = 0.86e6
+llg.C = 1.3e-11
+llg.alpha = 0.2
+llg.set_m0((m0_x, m0_y, m0_z), L=length)
+llg.setup(exchange_flag=True)
+#llg.H_app = (0, 0, llg.Ms/2)
+llg.pins = [0, 10]
 
-    ts = numpy.linspace(0, 1e-9, 100)
-    ys, infodict = odeint(llg.solve_for, llg.m, ts, full_output=True)
+# ode takes the parameters in the order t, y whereas odeint and we use y, t.
+llg_wrap = lambda t, y: llg.solve_for(y, t)
 
-    m = vectors(ys[-1])
-    angles = numpy.array([angle(m[j], m[j+1]) for j in xrange(len(m)-1)])
-    assert abs(angles.max() - angles.min()) < TOLERANCE
+t0 = 0; t1 = 3.10e-9; dt = 5e-12; # s
+r = ode(llg_wrap).set_integrator("vode", method="bdf")
+r.set_initial_value(llg.m, t0)
 
-def test_exchange_with_pinning():
-    llg = LLG(mesh)
-    llg.set_m0((m0_x, m0_y, m0_z), L=length)
-    llg.setup(exchange_flag=True)
-    llg.pins = [0, 10]
+averages = []
+third_node = []
 
-    ts = numpy.linspace(0, 1e-9, 100)
-    ys, infodict = odeint(llg.solve_for, llg.m, ts, full_output=True)
+while r.successful() and r.t <= t1:
+    mx, my, mz = llg.m_average
+    averages.append([r.t, mx, my, mz])
 
-    m = vectors(ys[-1])
-    angles = numpy.array([angle(m[j], m[j+1]) for j in xrange(len(m)-1)])
+    mx, my, mz = h.components(llg.m)
+    m2x, m2y, m2z = mx[2], my[2], mz[2]
+    third_node.append([r.t, m2x, m2y, m2z])
+
+    r.integrate(r.t + dt)
+
+def test_angles():
+    m = h.vectors(llg.m)
+    angles = numpy.array([h.angle(m[i], m[i+1]) for i in xrange(len(m)-1)])
     assert abs(angles.max() - angles.min()) < TOLERANCE
 
 def _test_compare_averages():
     ref = [line.strip().split() for line in open("averages_ref.txt")]
-    computed = [line.strip().split() for line in open("averages.txt")]
 
     for i in range(len(ref)):
         t_ref, mx_ref, my_ref, mz_ref = ref[i]
-        t, mx, my, mz = computed[i]
+        t, mx, my, mz = averages[i]
         
-        assert t_ref == t
+        assert float(t_ref) == t
         print "t={0}: ref={1}|{2}|{3} computed:{4}|{5}|{6}.".format(
                 t, mx_ref, my_ref, mz_ref, mx, my, mz)
-        assert abs(float(mx_ref) - float(mx)) < TOLERANCE
-        assert abs(float(my_ref) - float(my)) < TOLERANCE
-        assert abs(float(mz_ref) - float(mz)) < TOLERANCE
+        assert abs(float(mx_ref) - mx) < TOLERANCE
+        assert abs(float(my_ref) - my) < TOLERANCE
+        assert abs(float(mz_ref) - mz) < TOLERANCE
 
 def _test_compare_third_node():
     ref = [line.strip().split() for line in open("third_node_ref.txt")]
-    computed = [line.strip().split() for line in open("third_node.txt")]
 
     for i in range(len(ref)):
         t_ref, mx_ref, my_ref, mz_ref = ref[i]
-        t, mx, my, mz = computed[i]
+        t, mx, my, mz = third_node[i]
         
-        assert t_ref == t
+        assert float(t_ref) == t
         print "t={0}: ref={1}|{2}|{3} computed:{4}|{5}|{6}.".format(
                 t, mx_ref, my_ref, mz_ref, mx, my, mz)
-        assert abs(float(mx_ref) - float(mx)) < TOLERANCE
-        assert abs(float(my_ref) - float(my)) < TOLERANCE
-        assert abs(float(mz_ref) - float(mz)) < TOLERANCE
+        assert abs(float(mx_ref) - mx) < TOLERANCE
+        assert abs(float(my_ref) - my) < TOLERANCE
+        assert abs(float(mz_ref) - mz) < TOLERANCE
