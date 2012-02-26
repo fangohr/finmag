38eb16980af27273624c002893bcf10937bdd47b
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-02-26 17:17:08 +0000

    comparison dynamics of averages and one particular node

--- src/finmag/sim/tests/dynamics_1D/test_exchange_1d.py
+++ src/finmag/sim/tests/dynamics_1D/test_exchange_1d.py
@@ -25,11 +25,10 @@ m0_x = '2*x[0]/L - 1'
 m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
 m0_z = '0'
 
-def test_exchange_with_odeint():
+def test_exchange_without_pinning():
     llg = LLG(mesh)
     llg.set_m0((m0_x, m0_y, m0_z), L=length)
     llg.setup(exchange_flag=True)
-    #llg.pins = [0, 10]
 
     ts = numpy.linspace(0, 1e-9, 100)
     ys, infodict = odeint(llg.solve_for, llg.m, ts, full_output=True)
@@ -38,19 +37,45 @@ def test_exchange_with_odeint():
     angles = numpy.array([angle(m[j], m[j+1]) for j in xrange(len(m)-1)])
     assert abs(angles.max() - angles.min()) < TOLERANCE
 
-def test_exchange_with_ode():
+def test_exchange_with_pinning():
     llg = LLG(mesh)
     llg.set_m0((m0_x, m0_y, m0_z), L=length)
     llg.setup(exchange_flag=True)
-    #llg.pins = [0, 10]
+    llg.pins = [0, 10]
 
-    llg_wrap = lambda t, y: llg.solve_for(y, t) # for ode
-    t0 = 0; dt = 1e-12; tmax = 1e-9 # s
-    r = ode(llg_wrap).set_integrator("vode", method="bdf")
-    r.set_initial_value(llg.m, t0)
+    ts = numpy.linspace(0, 1e-9, 100)
+    ys, infodict = odeint(llg.solve_for, llg.m, ts, full_output=True)
 
-    while r.successful() and r.t <= tmax:
-        r.integrate(r.t + dt)
-    m = vectors(llg.m)
+    m = vectors(ys[-1])
     angles = numpy.array([angle(m[j], m[j+1]) for j in xrange(len(m)-1)])
     assert abs(angles.max() - angles.min()) < TOLERANCE
+
+def _test_compare_averages():
+    ref = [line.strip().split() for line in open("averages_ref.txt")]
+    computed = [line.strip().split() for line in open("averages.txt")]
+
+    for i in range(len(ref)):
+        t_ref, mx_ref, my_ref, mz_ref = ref[i]
+        t, mx, my, mz = computed[i]
+        
+        assert t_ref == t
+        print "t={0}: ref={1}|{2}|{3} computed:{4}|{5}|{6}.".format(
+                t, mx_ref, my_ref, mz_ref, mx, my, mz)
+        assert abs(float(mx_ref) - float(mx)) < TOLERANCE
+        assert abs(float(my_ref) - float(my)) < TOLERANCE
+        assert abs(float(mz_ref) - float(mz)) < TOLERANCE
+
+def _test_compare_third_node():
+    ref = [line.strip().split() for line in open("third_node_ref.txt")]
+    computed = [line.strip().split() for line in open("third_node.txt")]
+
+    for i in range(len(ref)):
+        t_ref, mx_ref, my_ref, mz_ref = ref[i]
+        t, mx, my, mz = computed[i]
+        
+        assert t_ref == t
+        print "t={0}: ref={1}|{2}|{3} computed:{4}|{5}|{6}.".format(
+                t, mx_ref, my_ref, mz_ref, mx, my, mz)
+        assert abs(float(mx_ref) - float(mx)) < TOLERANCE
+        assert abs(float(my_ref) - float(my)) < TOLERANCE
+        assert abs(float(mz_ref) - float(mz)) < TOLERANCE
