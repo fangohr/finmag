58cf9ac52d4dff8829a6d972c05decf07ae35d1d
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-02-27 14:26:27 +0000

    Hans changes from changeset 5476b5d6f339

--- src/finmag/sim/tests/macrospin/test_macrospin.py
+++ src/finmag/sim/tests/macrospin/test_macrospin.py
@@ -6,11 +6,13 @@ from finmag.sim.llg import LLG
 
 """
 The analytical solution of the LLG equation for a constant
-applied field, based on Appendix B of Matteo's PhD thesis.
+applied field, based on Appendix B of Matteo's PhD thesis. 
+#TODO
+#Add page number and equation number
 
 """
 
-def make_analytical_solution(H, alpha, gamma):
+def make_analytic_solution(H, alpha, gamma):
 	"""
 	Returns a function with computes the magnetisation vector
 	as a function of time. Takes the following parameters:
@@ -42,76 +44,90 @@ def make_analytical_solution(H, alpha, gamma):
 	def z(t):
 	    return cos_theta(t)
 
-	def M(t):
+	def m(t):
 	    return numpy.array([x(t), y(t), z(t)])
 
-	return M
+	return m
 
-def test_macrospin_default_damping():
+def test_llg_macrospin_analytic(alpha=0.5,max_t=1e-9):
     """
     Compares the C/dolfin/odeint solution to the analytical one defined above.
 
     """
+
+    print "running test_llg_macrospin_analytic with alpha=%g" % alpha
+
     
+    #define 3d mesh
     x0 = y0 = z0 = 0
     x1 = y1 = z1 = 10e-9
     nx = ny = nz = 1
     mesh = dolfin.Box(x0, x1, y0, y1, z0, z1, nx, ny, nz)
     llg = LLG(mesh)
+    llg.alpha = alpha
     llg.set_m0((1, 0, 0))
-    llg.H_app = (0, 0, 1e5)
+    llg.H_app = (0, 0, 1e6)
 
     EXCHANGE = False
     llg.setup(EXCHANGE)
 
-    ts = numpy.linspace(0, 1e-9, num=100)
+    ts = numpy.linspace(0, max_t, num=100)
+    tsfine = numpy.linspace(0, max_t, num=1000)
     ys = odeint(llg.solve_for, llg.m, ts)
+    print ys.shape
+    print llg.gamma
 
-    M_analytical = make_analytical_solution(1e5, llg.alpha, llg.gamma)
+    m_analytical = make_analytic_solution(1e6, alpha, llg.gamma)
 
-    TOLERANCE = 4e-2
-
-    for i in range(len(ts)):
-        
-        M_computed = numpy.mean(ys[i].reshape((3, -1)), 1)
-        print M_computed
-        M_ref = M_analytical(ts[i])
-        print M_ref
-        diff_max = numpy.max(numpy.abs(M_computed - M_ref))
-
-        assert diff_max < TOLERANCE, \
-          "t=%e (i=%d) failed with diff=%e" % (ts[i],i,diff_max)
-
-def test_macrospin_low_damping():
-    """
-    Compares the C/dolfin/odeint solution to the analytical one defined above.
-
-    """
-    
-    x0 = y0 = z0 = 0
-    x1 = y1 = z1 = 10e-9
-    nx = ny = nz = 1
-    mesh = dolfin.Box(x0, x1, y0, y1, z0, z1, nx, ny, nz)
-    llg = LLG(mesh)
-    llg.alpha = 0.02
-    llg.set_m0((1, 0, 0))
-    llg.H_app = (0, 0, 1e5)
-
-    EXCHANGE = False
-    llg.setup(EXCHANGE)
-
-    ts = numpy.linspace(0, 1e-9, num=100)
-    ys = odeint(llg.solve_for, llg.m, ts, rtol=1e-11)
-
-    M_analytical = make_analytical_solution(1e5, llg.alpha, llg.gamma)
-
-    TOLERANCE = 3e-3
+    TOLERANCE = 1e-6 #tolerance on Ubuntu 11.10, VM Hans, 25/02/2012
 
     for i in range(len(ts)):
         
-        M_computed = numpy.mean(ys[i].reshape((3, -1)), 1)
-        M_ref = M_analytical(ts[i])
-        diff_max = numpy.max(numpy.abs(M_computed - M_ref))
-
+        m = numpy.mean(ys[i].reshape((3, -1)), 1)
+        #print m
+        m_ref = m_analytical(ts[i])
+        #print M_ref
+        diff_max = numpy.max(numpy.abs(m - m_ref))
+	print "diff_max (ts=%5g) = %g" % (ts[i],diff_max)
         assert diff_max < TOLERANCE, \
           "t=%e (i=%d) failed with diff=%e" % (ts[i],i,diff_max)
+
+    ys3d = ys.reshape((len(ys),3,8)).mean(-1) 
+    mx = ys3d[:,0]
+    my = ys3d[:,1]
+    mz = ys3d[:,2]
+    print "mx.shape",mx.shape
+    print "m_analytical.shape",m_analytical(ts).shape
+    m_exact = m_analytical(tsfine)
+    mx_exact = m_exact[0,:] 
+    my_exact = m_exact[1,:] 
+    mz_exact = m_exact[2,:] 
+    #make plot
+    import pylab
+    pylab.plot(ts,mx,'o',label='mx')
+    pylab.plot(ts,my,'x',label='my')
+    pylab.plot(ts,mz,'^',label='mz')
+    pylab.plot(tsfine,mx_exact,'-',label='mx (exact)')
+    pylab.plot(tsfine,my_exact,'-',label='my (exact)')
+    pylab.plot(tsfine,mz_exact,'-',label='mz (exact)')
+    pylab.xlabel('t [s]')
+    pylab.ylabel('m=M/Ms')
+    pylab.title('Macro spin behaviour, alpha=%g' % alpha)
+    pylab.grid()
+    pylab.legend()
+    pylab.savefig('alpha-%04.2f.png' % alpha)
+    pylab.savefig('alpha-%04.2f.pdf' % alpha)
+    #pylab.show()
+    pylab.close()
+    return ys 
+
+def test_different_alphas():
+	test_llg_macrospin_analytic(alpha=1)
+	test_llg_macrospin_analytic(alpha=0.1)
+	test_llg_macrospin_analytic(alpha=0.02)
+	
+if __name__=="__main__":
+	ys = test_llg_macrospin_analytic(alpha=1.0,max_t=1e-10)
+	ys = test_llg_macrospin_analytic(alpha=0.5,max_t=1e-10)
+	ys = test_llg_macrospin_analytic(alpha=0.1,max_t=2e-10)
+	ys = test_llg_macrospin_analytic(alpha=0.01,max_t=2e-9)
