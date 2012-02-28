0b918b4b3181ae4e472ca2aea2e8e4b71213dc23
Hans Fangohr <fangohr@soton.ac.uk>
2012-02-28 22:41:50 +0000

    Implemented computation of the matrix g for the exchange, so that the exchange field can be computed via a matrix (g) vector (m) multiplication, i.e. Hex = g*m rather than Hex = assemble(dE_dM).
    I suggest to call the method we used so far 'box-assemble' and the new method 'box-matrix', as
    both use the box method, but different ways of computing H_ex.
    
    The current 'box-matrix' implementation uses a numpy array matrix to store g. This is not ideal
    as it is stored as a full matrix where g is actually sparse. By moving the matrix
    to a linear algebra backend that supports sparse matrices (such as the dolfin-supported
    PETSC), this can be addressed.
    
    The current 'box-matrix' implementation results in a speed up of all regression tests in
    finmag/src from 158 seconds to 96 seconds (on my virtual ubuntu machine with 1GB RAM, running
    on a MacBook Pro). I have thus made this the default method.
    
    I will commit another file in a moment that can be used to test the performance ratio of
    box-matrix over max-assemble.
    
    This commit includes a test (in test_exchange) which checks that the box-assemble and the
    box-matrix method give identical results (apart from numerics).
    
    
    [#25041739]

--- src/finmag/sim/exchange.py
+++ src/finmag/sim/exchange.py
@@ -17,12 +17,22 @@ class Exchange(object):
         C 
             the exchange constant
 
-    Possible methods are 'box','project'.
+    Possible methods are 'box','box-assemble', 'project'.
 
-    At the moment, we think 'box' works (and is what is used in Magpar and Nmag).
+    At the moment, we think 'box' works (and is what is used in Magpar and Nmag)
+    and is fastest.
 
-    However, we are curious what happens if we 'properly project' the exchange field
-    into the function space.
+    - 'box-assemble' is a slower version that assembles the H_ex for a given M in every
+      iteation.
+
+    - 'box' precomputes a matrix g, so that H_ex = g*M
+      (faster). Should be called 'box-matrix', but as this is the
+      default choice 'box' might be appropriate.  [Default choice]
+
+    - 'project': does not use the box method but 'properly project' the exchange field
+      into the function space. Should explore whether this works and/or makes any difference
+      (other than being slow.) Untested.
+
 
     *Example of Usage*
 
@@ -55,18 +65,31 @@ class Exchange(object):
         mu0 = 4 * np.pi * 10**-7 # Vs/(Am)
         self.exchange_factor = df.Constant(-2 * C / (mu0 * Ms))
         self.method = method
+        self.M = M #keep reference to M
 
         v = df.TestFunction(V)
         self.E = self.exchange_factor * df.inner(df.grad(M), df.grad(M)) * df.dx
         self.dE_dM = df.derivative(self.E, M, v)
 
-        if method=='box':
+        if method=='box-assemble':
             self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
-            self.compute_field = self.compute_field_box
+            self.compute_field = self.compute_field_box_assemble
+        elif method == 'box':
+            self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
+            self.compute_field = self.compute_field_box_gmatrix
+            #linearise dE_dM with respect to M. As we know this is
+            #linear ( should add reference to Werner Scholz paper and
+            #relevant equation for g), this creates the right matrix
+            #to compute dE_dM later as dE_dM=g*M.  We essentially
+            #compute a Taylor series of the energy in M, and know that
+            #the first two terms (dE_dM=Hex, and ddE_dMdM=g) are the
+            #only finite ones as we know the expression for the
+            #energy.
+            g_form = df.derivative(self.dE_dM,M)
+            self.g = df.assemble(g_form).array() #store matrix as numpy array
         elif method=='project':
             self.v = v
             self.V = V
-            
             H_exch_trial = df.TrialFunction(self.V)
             self.a = df.dot(H_exch_trial, self.v) * df.dx
             self.L = self.dE_dM
@@ -78,7 +101,7 @@ class Exchange(object):
         else:
             NotImplementedError("Only 'box' and 'project' methods are implemented")
 
-    def compute_field_box(self):
+    def compute_field_box_assemble(self):
         """
         Assemble vector with H_exchange using the 'box' method.
         
@@ -89,9 +112,27 @@ class Exchange(object):
         """
         return df.assemble(self.dE_dM).array() / self.vol
 
+    def compute_field_box_gmatrix(self):
+        """
+        Assemble vector with H_exchange using the 'box' method
+        and the pre-computed matrix g.
+
+        *Returns*
+            numpy.ndarray
+                The effective field.
+                
+        """
+        
+        Mvec = self.M.vector().array()
+        H_ex = np.dot(self.g,Mvec)
+        return H_ex/self.vol
+
+
     def compute_field_project(self):
         """
         Assemble vector with H_exchange using the 'project' method.
+
+        This may not work.
         
         *Returns*
             numpy.ndarray
