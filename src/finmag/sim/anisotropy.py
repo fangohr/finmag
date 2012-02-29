0b459f727dbccc33e1f1d8ecef82375c23938a6f
Anders Johansen <anders.e.johansen@gmail.com>
2012-02-29 15:20:17 +0000

    [#24824353] Anisotropy now has the same methods as Exchange.

--- src/finmag/sim/anisotropy.py
+++ src/finmag/sim/anisotropy.py
@@ -1,3 +1,4 @@
+import numpy as np
 import dolfin as df
 
 class Anisotropy(object):
@@ -49,36 +50,132 @@ class Anisotropy(object):
             
         """   
     
-    def __init__(self, V, M, K, a):
+    def __init__(self, V, M, K, a, method=None):
+        if method == None:
+            method = 'box-matix-petsc'
 
         # Local testfunction
-        w = df.TestFunction(V)
-
+        v = df.TestFunction(V)
+        
         # Convert K1 to dolfin.Constant
         K = df.Constant((K))
         
         # Anisotropy energy
-        self.E_ani = K*(1 - (df.dot(a, M))**2)*df.dx
+        self.E = K*(1 - (df.dot(a, M))**2)*df.dx
 
         # Gradient
-        self.dE_dM = df.derivative(self.E_ani, M)
+        self.dE_dM = df.derivative(self.E, M)
 
-        # Volume
-        self.vol = df.assemble(df.dot(w,
-                     df.Constant((1,1,1)))*df.dx).array()
-
+        # Store for later
         self.V = V
+        self.M = M
 
-    def compute_field(self):
+
+        if method=='box-assemble':
+            self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
+            self.compute_field = self.compute_field_box_assemble
+        
+        elif method == 'box-matrix-numpy':
+            self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
+            self.compute_field = self.compute_field_box_matrix_numpy
+            
+            #linearise dE_dM with respect to M. As we know this is
+            #linear ( should add reference to Werner Scholz paper and
+            #relevant equation for g), this creates the right matrix
+            #to compute dE_dM later as dE_dM=g*M.  We essentially
+            #compute a Taylor series of the energy in M, and know that
+            #the first two terms (dE_dM=Hex, and ddE_dMdM=g) are the
+            #only finite ones as we know the expression for the
+            #energy.
+
+            g_form = df.derivative(self.dE_dM, M)
+            self.g = df.assemble(g_form).array() #store matrix as numpy array
+        
+        elif method == 'box-matrix-petsc':
+            #petsc version of the scheme above.
+            self.vol = df.assemble(df.dot(v, df.Constant([1,1,1])) * df.dx).array()
+            self.compute_field = self.compute_field_box_matrix_petsc
+            g_form = df.derivative(self.dE_dM,M)
+            self.g_petsc = df.PETScMatrix()
+            df.assemble(g_form,tensor=self.g_petsc)
+            #self.g = df.assemble(g_form).array() #store matrix as numpy array
+            self.H_ex_petsc = df.PETScVector()
+
+
+        elif method=='project':
+            self.v = v
+            self.V = V
+            H_exch_trial = df.TrialFunction(self.V)
+            self.a = df.dot(H_exch_trial, self.v) * df.dx
+            self.L = self.dE_dM
+            self.H_exch_project = df.Function(self.V)        
+            #Note that we could make this 'project' method faster by computing the matrices
+            #that represent a and L, and only to solve the matrix system in 'compute'().
+            #IF this method is actually useful, we can do that. HF 16 Feb 2012
+            self.compute_field = self.compute_field_project
+        else:
+            raise NotImplementedError("""Only methods currently implemented are
+                                    * 'box-assemble', 
+                                    * 'box-matrix-numpy',
+                                    * 'box-matrix-petsc'  
+                                    * 'project'""")
+
+    def compute_field_box_assemble(self):
         """
-        Return the gradient divided by volume according to the *box method*.
+        Assemble vector with H_exchange using the 'box' method.
+        
+        *Returns*
+            numpy.ndarray
+                The effective field.
+                
+        """
+        return df.assemble(self.dE_dM).array() / self.vol
+
+    def compute_field_box_matrix_numpy(self):
+        """
+        Assemble vector with H_exchange using the 'box' method
+        and the pre-computed matrix g.
 
         *Returns*
             numpy.ndarray
                 The effective field.
+                
+        """
+        
+        Mvec = self.M.vector().array()
+        H_ex = np.dot(self.g,Mvec)
+        return H_ex/self.vol
+
+    def compute_field_box_matrix_petsc(self):
+        """
+        PETSc version of the function above. Takes advantage of the 
+        sparsity of g.
+        
+        *Returns*
+            numpy.ndarray
+                The effective field
 
         """
-        return df.assemble(self.dE_dM).array() / self.vol
+        
+        print "Check H_ex_petsc_vector"
+        self.g_petsc.mult(self.M.vector(),self.H_ex_petsc)
+        return self.H_ex_petsc.array()/self.vol
+                
+
+    def compute_field_project(self):
+        """
+        Assemble vector with H_exchange using the 'project' method.
+
+        This may not work.
+        
+        *Returns*
+            numpy.ndarray
+                The effective field.
+                
+        """
+        df.solve(self.a == self.L, self.H_exch_project)
+        return self.H_exch_project.vector().array()
+
 
     def compute_energy(self):
         """
