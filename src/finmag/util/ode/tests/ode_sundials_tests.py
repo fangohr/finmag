227b98669d149260122536080c76958121965e1e
Dmitri Chernyshenko <d.chernyshenko@soton.ac.uk>
2012-03-06 20:48:36 +0000

    Fixed issue with advance_time/nvector conversion.

--- src/finmag/util/ode/tests/ode_sundials_tests.py
+++ src/finmag/util/ode/tests/ode_sundials_tests.py
@@ -34,20 +34,24 @@ class OdeSundialsTests(unittest.TestCase):
         ref_ys = np.array([reference(t)  for t in ts])
         assert np.max(np.abs(ys - ref_ys)) < 1e-6
 
-    def atest_simple_1d(self):
+    def test_simple_1d(self):
+        integrator = sundials.cvode(sundials.CV_ADAMS, sundials.CV_FUNCTIONAL)
+        self.run_simple_test(integrator)
+        integrator = sundials.cvode(sundials.CV_BDF, sundials.CV_FUNCTIONAL)
+        self.run_simple_test(integrator)
+
+    def run_simple_test(self, integrator):
         def rhs(t, y, ydot):
             ydot[:] = 0.5 * y
             return 0
 
-        integrator = sundials.cvode(sundials.CV_ADAMS, sundials.CV_FUNCTIONAL)
         integrator.init(rhs, 0, np.array([1.]))
-        integrator.set_scalar_tolerances(1e-8, 1e-8)
+        integrator.set_scalar_tolerances(1e-9, 1e-9)
         reference = lambda t: [math.exp(0.5 * t)]
         yout = np.zeros(1)
         ts = np.linspace(0.001, 3, 100)
-        ys = np.zeros(ts.shape)
+        ys = np.zeros((100, 1))
         for i, t in enumerate(ts):
-            print i, t, yout
             integrator.advance_time(t, yout)
             ys[i] = yout.copy()
         ref_ys = np.array([reference(t)  for t in ts])
