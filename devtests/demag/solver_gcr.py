bce082a6c322b84450c7c9dd61dc7c6fc4d453c5
Gabriel Balaban <G.Balaban@soton.ac.uk>
2012-03-07 13:39:17 +0100

    added module for solver base classes

---
+++ devtests/demag/solver_gcr.py
@@ -0,0 +1,9 @@
+"A Solver for the demagnetization field using the Garcia Cervera Roma approach" 
+#To be implemented# 
+
+__author__ = "Gabriel Balaban"
+__copyright__ = __author__
+__project__ = "Finmag"
+__organisation__ = "University of Southampton"
+
+from dolfin import *
