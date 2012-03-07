37105c9ea930e4c0581174d293b0fe20da7e3029
Gabriel Balaban <G.Balaban@soton.ac.uk>
2012-03-07 21:30:16 +0100

    [#26096577, #25914567,#25914621] Begun implementation of FEMBEM GCR solver

--- devtests/demag/prob_fembem_testcases.py
+++ devtests/demag/prob_fembem_testcases.py
@@ -9,13 +9,13 @@ from dolfin import *
 import numpy as np
 import prob_base as pb
 
-class MagUnitSphereFemBem(pb.TruncDeMagProblem):
+class MagUnitSphere(pb.FemBemDeMagProblem):
     """Uniformly magnetized sphere problem for fembem solvers"""
     def __init__(self):
         mesh = UnitSphere(10)
         M = ("1","0","0")
         #Initialize Base Class
-        super(MagUnitSphereFemBem,self).__init__(mesh,M)
+        super(MagUnitSphere,self).__init__(mesh,M)
         
     def desc(self):
         return "unit sphere demagnetisation test problem fembem"
