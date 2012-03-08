f3d1bb3f805da08e089ada9ab86e116d4e07f8a7
Hans Fangohr <fangohr@soton.ac.uk>
2012-03-08 17:03:42 +0000

    Testing curl operator. Seems that this needs a 3d mesh to work. (with Marijan and Weiwei)

---
+++ devtests/skyrmions/test_curl_operator.py
@@ -0,0 +1,20 @@
+
+from dolfin import *
+mesh=UnitCube(1,1,1)
+V=VectorFunctionSpace(mesh,"CG",1)
+
+M=interpolate(Constant((1,1,1)),V)
+
+#E=dot(M,M)*dx
+E=inner(M,curl(M))*dx
+print "energy=",assemble(E)
+
+dE_dM=derivative(E,M)
+dE_dMvec=assemble(dE_dM)
+print "vector=",dE_dMvec.array()
+
+#curlM=project(somecurlform,V)
+
+#print curlM.vector().array()
+
+
