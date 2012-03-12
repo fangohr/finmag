57b688c83c3b5b3a1a1260b2e851e4d498514968
Anders Johansen <anders.e.johansen@gmail.com>
2012-03-12 15:00:48 +0000

    [#24839761] [#24824353] Fix minus sign bug in anisotropy and corresponing tests. Not really sure if this is the right way to do it, but it seems to work in all our test cases now.

--- devtests/domainwall/uniaxial_anisotropy.py
+++ devtests/domainwall/uniaxial_anisotropy.py
@@ -6,7 +6,7 @@ import pylab
 
 def Mz_exact(x):
     """Analytical solution."""
-    return 1400e3 * np.cos(np.pi/2 + np.arctan(np.sinh((x - 252e-9)/np.sqrt(30e-12/520e3))))
+    return np.cos(np.pi/2 + np.arctan(np.sinh((x - 252e-9)/np.sqrt(30e-12/520e3))))
 
 def M0(r):
     """Return initial magnetisation."""
@@ -18,7 +18,7 @@ def M0(r):
     mz = min(1.0, max(-1.0, rel))
     return 0.0, np.sqrt(1.0 - mz*mz), mz
 
-simplices = 1000
+simplices = 200
 L = 504e-9
 dim = 3
 mesh = Interval(simplices, 0, L)
@@ -45,29 +45,31 @@ for i in range(len(coor)):
 
 # Setup LLG
 llg = LLG(mesh)
-llg.Ms = 1400e3
 
 # TODO: Find out how one are supposed to pin.
 # llg.pins = [0,1,-2,-1] # This is not so good
 
-llg.c = 30e12
+llg.Ms = 1400e3
+llg.C = 30e-12
+llg.K = 520e3
 llg.set_m0(m0)
 llg.setup(exchange_flag=True, anisotropy_flag=True)
 llg_wrap = lambda t, y: llg.solve_for(y, t)
 
 # Time integration
-t0 = 0; dt = 0.05e-12; t1 = 10e-12
+t0 = 0; dt = 10e-12; t1 = 1e-09
 r = ode(llg_wrap).set_integrator("vode", method="bdf", rtol=1e-5, atol=1e-5)
 r.set_initial_value(llg.m, t0)
 
-while r.successful() and r.t <= t1:
+while r.successful() and r.t < t1:
     r.integrate(r.t + dt)
     print "Integrating time: %g" % r.t
 
 # Plot magnetisation in z-direction
 mz = llg.m[zstart:]
 x = np.linspace(0, L, simplices+1)
-# Comment out analytical solution for now, as it has overflow problems.
-#pylab.plot(x,mz,x,Mz_exact(x))
-pylab.plot(x, mz)
+pylab.plot(x,mz,x,Mz_exact(x))
 pylab.show()
+
+diff = abs(mz - Mz_exact(x))
+print max(diff)
