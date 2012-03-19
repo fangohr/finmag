5a54512ea7eda437397306419cddeb5946bf686e
Anders Johansen <anders.e.johansen@gmail.com>
2012-03-19 15:09:17 +0000

    [#26516263] Added HF's tests suggestions.

---
+++ src/finmag/demag/tests/test_demag.py
@@ -0,0 +1,77 @@
+import dolfin as df
+import numpy as np
+from finmag.demag import solver_fk, solver_gcr
+from finmag.demag.problems import prob_fembem_testcases as pft
+
+TOL = 1e-3
+
+problems = [pft.MagUnitSphere(n) for n in (2, 5)]
+#problems.append(pft.MagSphere())
+solvers = [solver_fk.FemBemFKSolver, solver_gcr.GCRFemBemDeMagSolver]
+
+#FIXME: All asserts are commented out for development testing. 
+
+def one_third(problem, grad):
+    """Demag field should be one third of initial magnetisation."""
+    W = df.VectorFunctionSpace(problem.mesh, "DG", 0, dim=3)
+    a = df.interpolate(df.Constant(problem.M), W)
+
+    diff =  np.abs(grad.vector().array() - 1./3*a.vector().array())
+    print "Max difference should be zero, is %g." % max(diff)
+    #assert max(diff) < TOL
+
+def y_z_zero(problem, grad):
+    """Demag field should be zero in y- and z-direction."""
+    y, z = grad.split(True)[1:]
+    y, z = y.vector().array(), z.vector().array()
+    
+    maxy = max(abs(y))
+    print "Max field in y-direction should be zero, is %g." % maxy
+    #assert maxy < TOL
+
+    maxz = max(abs(z))
+    print "Max field in z-direction should be zero, is %g." % maxz
+    #assert maxz < TOL
+
+def x_deviation(problem, grad):
+    """Deviation of demag field in x-direction should be zero."""
+    x = grad.split(True)[0].vector().array()
+    dev = abs(max(x) - min(x))
+    
+    print "Max deviation in x-direction should be zero, is %g." % dev
+    #assert dev < TOL
+
+def norm_x(problem, grad):
+    """Norm of demag field in x-direction minus 1/3 Ms should be zero."""
+    x = grad.split(True)[0].vector().array() - 1./3*problem.Ms
+    normx = np.linalg.norm(x)
+
+    print "L2 norm of the x-component should be zero. Is %g." % normx
+    #assert normx < TOL
+
+def avg_x(problem, grad):
+    """Average of the x-components should be 1/3 of Ms."""
+    x = grad.split(True)[0].vector().array()
+    avg = np.average(x) 
+
+    print "Average of x-component should be %g. Is %g." % (1./3*problem.Ms, avg)
+    #assert abs(avg-1./3*Ms) < TOL
+
+def test_all_tests():
+    for problem in problems:
+        print "\nProblem:", problem.desc()
+        for solver in solvers:
+            solver = solver(problem)
+            print "\nSolver:", solver.__class__.__name__
+            phi = solver.solve()
+            grad = solver.get_demagfield(phi)
+
+            one_third(problem, grad)
+            y_z_zero(problem, grad)
+            x_deviation(problem, grad)
+            norm_x(problem, grad)
+            avg_x(problem, grad)
+            print ''
+
+if __name__ == '__main__':
+    test_all_tests()
