b7906eaceb675c18682813212f1528daf1f4dd22
Dmitri Chernyshenko <d.chernyshenko@soton.ac.uk>
2012-03-20 18:40:48 +0000

    Started the implementation of the C++ Jacobean

---
+++ native/src/llg/py_llg_module.cc
@@ -0,0 +1,21 @@
+/**
+ * FinMag - a thin layer on top of FEniCS to enable micromagnetic multi-physics simulations
+ * Copyright (C) 2012 University of Southampton
+ * Do not distribute
+ *
+ * CONTACT: h.fangohr@soton.ac.uk
+ *
+ * AUTHOR(S) OF THIS FILE: Dmitri Chernyshenko (d.chernyshenko@soton.ac.uk)
+ */
+
+#include "finmag_includes.h"
+
+#include "util/np_array.h"
+
+BOOST_PYTHON_MODULE(llg)
+{
+    initialise_np_array();
+
+    bp::scope().attr("__doc__") = "C++ routines for computing the LLG (dM/dt and the Jacobean)";
+//    finmag::sundials::register_numpy_malloc();
+}
