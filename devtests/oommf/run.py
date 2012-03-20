29799d078b9b5b2745917147b00e1cba251f16c0
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-20 19:43:22 +0000

    transform into 1d example

--- devtests/oommf/run.py
+++ devtests/oommf/run.py
@@ -9,19 +9,20 @@ from finmag.util.oommf import oommf_uniform_exchange, mesh
 finmag code
 
 """
-x0 = y0 = z0 = 0
-x1 = y1 = z1 = 10e-9
-xn = yn = zn = 10 
-msh = df.Box(x0, y0, z0, x1, y1, z1, xn, yn, zn)
+x0 = 0; y0 = z0 = 0
+x1 = 10e-9; y1 = z1 = 1e-10
+xn = 20; yn = zn = 1 
+msh = df.Interval(xn-1, x0, x1)
 
 llg = LLG(msh)
-llg.set_m0(("2*x[0]/L - 1", "2*x[1]/W - 1", "1"), L=x1, W=y1)
-llg.H_app = (llg.Ms/2, 0, 0)
+
+# initial configuration of the magnetisation
+m0_x = '2*x[0]/L - 1'
+m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
+m0_z = '0'
+llg.set_m0((m0_x, m0_y, m0_z), L=x1)
 llg.setup(exchange_flag=True)
 
-print "finmag\n\nm0"
-print llg.m
-print "\nexchange:"
 print components(llg.exchange.compute_field())
 
 """
@@ -29,17 +30,18 @@ oommf code
 
 """
 
-print "\noommf\n\n"
+print "\noommf\n"
 
 msh = mesh.Mesh((xn, yn, zn), size=(x1, y1, z1))
 m0 = msh.new_field(3)
 
 for i, (x, y, z) in enumerate(msh.iter_coords()):
     m0.flat[0,i] = 2 * x/x1 - 1
-    m0.flat[1,i] = 2 * y/y1 -1
-    m0.flat[2,i] = 1
+    m0.flat[1,i] = np.sqrt(1 - (2*x/x1 - 1)*(2*x/x1 - 1))
+    m0.flat[2,i] = 0
 
 # m0.flat.shape == (3, n)
 m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
 
 print oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
+
