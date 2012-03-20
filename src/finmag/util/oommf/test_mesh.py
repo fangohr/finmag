5cb360a41bfea3f858563ed238668aef4af91665
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-20 14:29:10 +0000

    fixed TODO, iter_coords_int now with different array orderings

--- src/finmag/util/oommf/test_mesh.py
+++ src/finmag/util/oommf/test_mesh.py
@@ -1,18 +1,36 @@
+import unittest
 import numpy as np
 from finmag.util.oommf import mesh
 
-def test_iter_coords_int_zyx():
-    m = mesh.Mesh((3, 1, 1), cellsize=(1, 1, 1))
-    indices  = [r for r in m.iter_coords_int()]
-    expected = [[0, 0, 0], [1, 0, 0], [2, 0, 0]]
-    assert np.array_equal(m.mesh_size, [3, 1, 1])
-    assert m.array_order == mesh.Mesh.ZYX
-    assert np.array_equal(expected, indices) 
+class TestIterCoordsInt(unittest.TestCase):
+    def test_zyx_ordering(self):
+        m = mesh.Mesh((3, 1, 1), cellsize=(1, 1, 1))
+        indices  = [r for r in m.iter_coords_int()]
+        expected = [[0, 0, 0], [1, 0, 0], [2, 0, 0]]
+        assert np.array_equal(m.mesh_size, [3, 1, 1])
+        assert m.array_order == mesh.Mesh.ZYX
+        assert np.array_equal(expected, indices) 
 
-    m = mesh.Mesh((2, 2, 2), cellsize=(1, 1, 1))
-    indices  = [r for r in m.iter_coords_int()]
-    expected = [[0,0,0], [1,0,0], [0,1,0], [1,1,0],
-                [0,0,1], [1,0,1], [0,1,1], [1,1,1]]
-    assert np.array_equal(m.mesh_size, [2, 2, 2])
-    assert m.array_order == mesh.Mesh.ZYX
-    assert np.array_equal(expected, indices) 
+        m = mesh.Mesh((2, 2, 2), cellsize=(1, 1, 1))
+        indices  = [r for r in m.iter_coords_int()]
+        expected = [[0,0,0], [1,0,0], [0,1,0], [1,1,0],
+                    [0,0,1], [1,0,1], [0,1,1], [1,1,1]]
+        assert np.array_equal(m.mesh_size, [2, 2, 2])
+        assert m.array_order == mesh.Mesh.ZYX
+        assert np.array_equal(expected, indices) 
+
+    def test_xyz_ordering(self):
+        m = mesh.Mesh((3, 1, 1), cellsize=(1, 1, 1), array_order=mesh.Mesh.XYZ)
+        indices  = [r for r in m.iter_coords_int()]
+        expected = [[0, 0, 0], [1, 0, 0], [2, 0, 0]]
+        assert np.array_equal(m.mesh_size, [3, 1, 1])
+        assert m.array_order == mesh.Mesh.XYZ
+        assert np.array_equal(expected, indices)
+        
+        m = mesh.Mesh((2, 2, 2), cellsize=(1, 1, 1), array_order=mesh.Mesh.XYZ)
+        indices = [r for r in m.iter_coords_int()]
+        expected = [[0,0,0], [0,0,1], [0,1,0], [0,1,1],
+                    [1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+        assert np.array_equal(m.mesh_size, [2, 2, 2])
+        assert m.array_order == mesh.Mesh.XYZ
+        assert np.array_equal(expected, indices)
