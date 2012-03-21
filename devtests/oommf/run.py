415222a9e5362ce1a9496192ddc7d0eae83313b1
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-21 17:32:19 +0000

    difference and relative difference

--- devtests/oommf/run.py
+++ devtests/oommf/run.py
@@ -5,50 +5,83 @@ from finmag.sim.llg import LLG
 from finmag.sim.helpers import components
 from finmag.util.oommf import oommf_uniform_exchange, mesh
 
-"""
-finmag code
+L = 20e-9; W = 10e-9; H = 1e-9;
+nL = 20; nW = 10; nH = 1;
 
-"""
-x0 = 0; y0 = z0 = 0
-x1 = 10e-9; y1 = z1 = 1e-10
-xn = 20; yn = zn = 1 
-#msh = df.Interval(xn-1, x0, x1)
-msh = df.Box(x0, y0, z0, x1, y1, z1, xn, yn, zn)
+def one_dimensional_problem():
+    msh = df.Interval(nL, 0, L)
+    llg = LLG(msh)
+    m0_x = '2 * x[0]/L - 1'
+    m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
+    m0_z = '0'
+    llg.set_m0((m0_x, m0_y, m0_z), L=L)
+    llg.setup(exchange_flag=True)
+    return llg
 
-llg = LLG(msh)
+def three_dimensional_problem():
+    msh = df.Box(0, 0, 0, L, W, H, nL, nW, nH)
+    llg = LLG(msh)
+    m0_x = "pow(sin(x[0]*pow(10, 9)/3), 2)"
+    m0_y = "0"
+    m0_z = "1" # "pow(cos(x[0]*pow(10, 9)/3), 2)" 
+    llg.set_m0((m0_x, m0_y, m0_z))
+    llg.setup(exchange_flag=True)
+    return llg
 
-# initial configuration of the magnetisation
-m0_x = '2*x[0]/L - 1'
-m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
-m0_z = '0'
-llg.set_m0((m0_x, m0_y, m0_z), L=x1)
-llg.setup(exchange_flag=True)
-
-print components(llg.exchange.compute_field())
+# llg = one_dimensional_problem()
+llg = three_dimensional_problem()
 
 exc_finmag = df.Function(llg.V)
 exc_finmag.vector()[:] = llg.exchange.compute_field()
+
 df.plot(llg._m)
 df.plot(exc_finmag)
 df.interactive()
 
-"""
-oommf code
+def one_dimensional_problem_oommf():
+    msh = mesh.Mesh((nL, 1, 1), size=(L, 1e-10, 1e-10))
+    m0 = msh.new_field(3)
 
-"""
+    for i, (x, y, z) in enumerate(msh.iter_coords()):
+        m0.flat[0,i] = 2 * x/L - 1
+        m0.flat[1,i] = np.sqrt(1 - (2*x/L - 1)*(2*x/L - 1))
+        m0.flat[2,i] = 0
 
-print "\noommf\n"
+    # m0.flat.shape == (3, n)
+    m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
 
-msh = mesh.Mesh((xn, yn, zn), size=(x1, y1, z1))
-m0 = msh.new_field(3)
+    return msh, oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
 
+def three_dimensional_problem_oommf():
+    msh = mesh.Mesh((nL, nW, nH), size=(L, W, H))
+    m0 = msh.new_field(3)
+
+    for i, (x, y, z) in enumerate(msh.iter_coords()):
+        m0.flat[0,i] = np.sin(10**9 * x/3)**2
+        m0.flat[1,i] = 0
+        m0.flat[2,i] = 1 # np.cos(10**9 * x/3)
+
+    # m0.flat.shape == (3, n)
+    m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
+
+    return msh, oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
+
+#msh, exc_oommf = one_dimensional_problem_oommf()
+msh, exc_oommf = three_dimensional_problem_oommf()
+
+exc_finmag_like_oommf = msh.new_field(3)
 for i, (x, y, z) in enumerate(msh.iter_coords()):
-    m0.flat[0,i] = 2 * x/x1 - 1
-    m0.flat[1,i] = np.sqrt(1 - (2*x/x1 - 1)*(2*x/x1 - 1))
-    m0.flat[2,i] = 0
+    # E_x, E_y, E_z = exc_finmag(x) # one dimension
+    E_x, E_y, E_z = exc_finmag(x, y, z)
+    exc_finmag_like_oommf.flat[0,i] = E_x
+    exc_finmag_like_oommf.flat[1,i] = E_y
+    exc_finmag_like_oommf.flat[2,i] = E_z
 
-# m0.flat.shape == (3, n)
-m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
+difference = exc_finmag_like_oommf.flat - exc_oommf
+relative_difference = difference / exc_oommf
+print "difference"
+print difference
+print "relative difference"
+print relative_difference # "nan" for y-component because 0/0
 
-print oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
 
