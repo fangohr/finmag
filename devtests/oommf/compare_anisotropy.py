98e91388eee071665e40206cdbedcd3d8f7b45b4
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-21 17:59:21 +0000

    comparison with oommf for anisotropy

---
+++ devtests/oommf/compare_anisotropy.py
@@ -0,0 +1,91 @@
+import dolfin as df
+import numpy as np
+
+from finmag.sim.llg import LLG
+from finmag.util.oommf import oommf_uniaxial_anisotropy, mesh
+
+L = 20e-9; W = 10e-9; H = 1e-9;
+nL = 20; nW = 10; nH = 1;
+
+K1 = 45e4 # J/m^3
+
+def one_dimensional_problem():
+    msh = df.Interval(nL, 0, L)
+    llg = LLG(msh)
+    m0_x = '2 * x[0]/L - 1'
+    m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
+    m0_z = '0'
+    llg.set_m0((m0_x, m0_y, m0_z), L=L)
+    llg.add_uniaxial_anisotropy(K1, df.Constant((0, 0, 1)))
+    llg.setup(exchange_flag=True)
+    return llg
+
+def three_dimensional_problem():
+    msh = df.Box(0, 0, 0, L, W, H, nL, nW, nH)
+    llg = LLG(msh)
+    m0_x = "pow(sin(x[0]*pow(10, 9)/3), 2)"
+    m0_y = "0"
+    m0_z = "1" # "pow(cos(x[0]*pow(10, 9)/3), 2)" 
+    llg.set_m0((m0_x, m0_y, m0_z))
+    llg.add_uniaxial_anisotropy(K1, df.Constant((1, 0, 0)))
+    llg.setup(exchange_flag=True)
+    return llg
+
+# llg = one_dimensional_problem()
+llg = three_dimensional_problem()
+
+anis_finmag = df.Function(llg.V)
+# There is only one anisotropy in our example.
+anis_finmag.vector()[:] = llg._anisotropies[0].compute_field()
+
+df.plot(llg._m)
+df.plot(anis_finmag)
+df.interactive()
+
+def one_dimensional_problem_oommf():
+    msh = mesh.Mesh((nL, 1, 1), size=(L, 1e-10, 1e-10))
+    m0 = msh.new_field(3)
+
+    for i, (x, y, z) in enumerate(msh.iter_coords()):
+        m0.flat[0,i] = 2 * x/L - 1
+        m0.flat[1,i] = np.sqrt(1 - (2*x/L - 1)*(2*x/L - 1))
+        m0.flat[2,i] = 0
+
+    # m0.flat.shape == (3, n)
+    m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
+
+    return msh, oommf_uniaxial_anisotropy(m0, llg.Ms, K1, (0,0,1)).flat
+
+def three_dimensional_problem_oommf():
+    msh = mesh.Mesh((nL, nW, nH), size=(L, W, H))
+    m0 = msh.new_field(3)
+
+    for i, (x, y, z) in enumerate(msh.iter_coords()):
+        m0.flat[0,i] = np.sin(10**9 * x/3)**2
+        m0.flat[1,i] = 0
+        m0.flat[2,i] = 1 # np.cos(10**9 * x/3)
+
+    # m0.flat.shape == (3, n)
+    m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
+
+    return msh, oommf_uniaxial_anisotropy(m0, llg.Ms, K1, (1,0,0)).flat
+
+#msh, anis_oommf = one_dimensional_problem_oommf()
+msh, anis_oommf = three_dimensional_problem_oommf()
+print anis_oommf
+anis_finmag_like_oommf = msh.new_field(3)
+for i, (x, y, z) in enumerate(msh.iter_coords()):
+    # A_x, A_y, A_z = anis_finmag(x) # one dimension
+    A_x, A_y, A_z = anis_finmag(x, y, z)
+    anis_finmag_like_oommf.flat[0,i] = A_x
+    anis_finmag_like_oommf.flat[1,i] = A_y
+    anis_finmag_like_oommf.flat[2,i] = A_z
+
+difference = anis_finmag_like_oommf.flat - anis_oommf
+relative_difference = difference / anis_oommf
+print "difference in the x-component" # rest is 0 (both value and diff)
+print difference[0]
+print "relative difference (x-component)"
+print relative_difference[0]
+
+
