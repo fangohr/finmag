593a7e4ce5e8bd84cc914eb3e7cbca09a7a59cfd
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-22 14:16:03 +0000

    smoother distribution of magnetisation, some statistics

--- devtests/oommf/compare_exchange.py
+++ devtests/oommf/compare_exchange.py
@@ -5,13 +5,14 @@ from finmag.sim.llg import LLG
 from finmag.util.oommf import oommf_uniform_exchange, mesh
 
 L = 20e-9; W = 10e-9; H = 1e-9;
-nL = 20; nW = 10; nH = 1;
+nL = 40; nW = 10; nH = 1;
 
 def one_dimensional_problem():
-    msh = df.Interval(nL, 0, L)
+    #msh = df.Interval(nL, 0, L)
+    msh = df.Box(0, 0, 0, L, 1e-10, 1e-10, nL, 1, 1)
     llg = LLG(msh)
-    m0_x = '2 * x[0]/L - 1'
-    m0_y = 'sqrt(1 - (2*x[0]/L - 1)*(2*x[0]/L - 1))'
+    m0_x = 'sqrt(x[0]/L)'
+    m0_y = 'sqrt(1 - sqrt(x[0]/L)*sqrt(x[0]/L))'
     m0_z = '0'
     llg.set_m0((m0_x, m0_y, m0_z), L=L)
     llg.setup(exchange_flag=True)
@@ -27,8 +28,8 @@ def three_dimensional_problem():
     llg.setup(exchange_flag=True)
     return llg
 
-# llg = one_dimensional_problem()
-llg = three_dimensional_problem()
+llg = one_dimensional_problem()
+#llg = three_dimensional_problem()
 
 exc_finmag = df.Function(llg.V)
 exc_finmag.vector()[:] = llg.exchange.compute_field()
@@ -38,12 +39,12 @@ df.plot(exc_finmag)
 df.interactive()
 
 def one_dimensional_problem_oommf():
-    msh = mesh.Mesh((nL, 1, 1), size=(L, 1e-10, 1e-10))
+    msh = mesh.Mesh((nL, 1, 1), size=(L, 1e-11, 1e-11))
     m0 = msh.new_field(3)
 
     for i, (x, y, z) in enumerate(msh.iter_coords()):
-        m0.flat[0,i] = 2 * x/L - 1
-        m0.flat[1,i] = np.sqrt(1 - (2*x/L - 1)*(2*x/L - 1))
+        m0.flat[0,i] = np.sqrt(x/L)
+        m0.flat[1,i] = np.sqrt(1 - np.sqrt(x/L)*np.sqrt(x/L))
         m0.flat[2,i] = 0
 
     # m0.flat.shape == (3, n)
@@ -65,22 +66,27 @@ def three_dimensional_problem_oommf():
 
     return msh, oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
 
-#msh, exc_oommf = one_dimensional_problem_oommf()
-msh, exc_oommf = three_dimensional_problem_oommf()
+msh, exc_oommf = one_dimensional_problem_oommf()
+#msh, exc_oommf = three_dimensional_problem_oommf()
 
 exc_finmag_like_oommf = msh.new_field(3)
 for i, (x, y, z) in enumerate(msh.iter_coords()):
-    # E_x, E_y, E_z = exc_finmag(x) # one dimension
-    E_x, E_y, E_z = exc_finmag(x, y, z)
+    E_x, E_y, E_z = exc_finmag(x, y, z) # one dimension
+    #E_x, E_y, E_z = exc_finmag(x, y, z)
     exc_finmag_like_oommf.flat[0,i] = E_x
     exc_finmag_like_oommf.flat[1,i] = E_y
     exc_finmag_like_oommf.flat[2,i] = E_z
 
 difference = exc_finmag_like_oommf.flat - exc_oommf
-relative_difference = difference / exc_oommf
+relative_difference = np.abs(difference / exc_oommf)
+
 print "difference"
 print difference
-print "relative difference"
-print relative_difference # "nan" for y-component because 0/0
-
-
+print "absolute relative difference"
+print relative_difference
+print "absolute relative difference:"
+print "  median", np.median(relative_difference, axis=1)
+print "  average", np.mean(relative_difference, axis=1)
+print "  minimum", np.min(relative_difference, axis=1)
+print "  maximum", np.max(relative_difference, axis=1)
+print "  spread", np.std(relative_difference, axis=1)
