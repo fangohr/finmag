060f1e850a7d0db6a9279616b461b6660ad36461
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-22 18:15:44 +0000

    creates /usr/local/bin/oommf with correct location of oommf (can be customised)

--- install/oommf.sh
+++ install/oommf.sh
@@ -1,7 +1,18 @@
+#
+# This script will download and install oommf into a directory
+# called oommf under $PREFIX, which is your user's home per default. It will
+# then create a command called "oommf" in your /usr/local/bin directory.
+#
+# It will also install the needed ubuntu packages.
+#
+
+PREFIX="$HOME" # EDIT HERE.
+
 # install prerequisites
 sudo apt-get install tk-dev tcl-dev
 
 # download and extract oommf
+cd $PREFIX
 if [ ! -e "oommf12a4pre-20100719bis.tar.gz" ]
 then
     wget http://math.nist.gov/oommf/snapshot/oommf12a4pre-20100719bis.tar.gz
@@ -17,3 +28,12 @@ OOMMF_TK_INCLUDE_DIR=/usr/include/tcl8.5/; export OOMMF_TK_INCLUDE_DIR
 ./oommf.tcl pimake upgrade
 ./oommf.tcl pimake
 ./oommf.tcl +platform
+
+# create an executable called 'oommf' to call oommf in /usr/local/bin
+oommf_command=$(cat <<EOF
+#! /bin/bash
+tclsh $PREFIX/oommf/oommf.tcl "\$@"
+EOF
+)
+sudo sh -c "echo '$oommf_command' > '/usr/local/bin/oommf'"
+sudo chmod a+x /usr/local/bin/oommf
