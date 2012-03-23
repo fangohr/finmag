5ceecaede0a6141ba040d6e0a575690cb86cbef8
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-23 21:58:32 +0000

    go cra

--- devtests/oommf/compare_exchange.py
+++ devtests/oommf/compare_exchange.py
@@ -1,11 +1,12 @@
+import os
 import dolfin as df
 import numpy as np
-import matplotlib.pyplot as plt
-from finmag.sim.helpers import quiver
+from finmag.sim.helpers import quiver, boxplot
 from finmag.sim.llg import LLG
 from finmag.util.oommf import oommf_uniform_exchange, mesh
 
 REL_TOLERANCE = 40 # 1e-3
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def test_one_dimensional_problem():
     _, rel_diff = one_dimensional_problem()
@@ -21,7 +22,7 @@ def compute_exc_finmag(mesh, m0, **kwargs):
     llg.setup()
     exc_field = df.Function(llg.V)
     exc_field.vector()[:] = llg.exchange.compute_field()
-    return exc_field, llg.Ms, llg.C
+    return exc_field, llg
 
 def finmag_to_oommf(finmag_exchange, oommf_mesh, dims=1):
     exchange_finmag_for_oommf = oommf_mesh.new_field(3)
@@ -39,14 +40,14 @@ def one_dimensional_problem():
     x_min = 0; x_max = 20e-9; x_n = 40
     
     # compute exchange field with finmag
-    msh = df.Interval(x_n, x_min, x_max)
+    dolfmesh = df.Interval(x_n, x_min, x_max)
     m0_x = 'sqrt(x[0]/L)'
     # workaround floating point precision problems, getting really small
     # but negative values under the square root.
     m0_y_squared = '1 - x[0]/L - pow(sin(4*pi*x[0]/L)/10, 2)'
     m0_y = 'sqrt(fabs({0}) < 1e-15 ? 0: ({0}))'.format(m0_y_squared)
     m0_z = 'sin(4*pi*x[0]/L) / 10'
-    exc_finmag, Ms, C = compute_exc_finmag(msh, (m0_x, m0_y, m0_z), L=x_max)
+    exc_finmag, llg = compute_exc_finmag(dolfmesh, (m0_x, m0_y, m0_z), L=x_max)
 
     # compute exchange field with oommf
     msh = mesh.Mesh((x_n, 1, 1), size=(x_max, 1e-11, 1e-11))
@@ -58,20 +59,23 @@ def one_dimensional_problem():
         m0.flat[2,i] = np.sin(4 * np.pi * x/x_max)/10
     m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] +
                        m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
-    exchange_oommf = oommf_uniform_exchange(m0, Ms, C).flat
+    exchange_oommf = oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
 
     # create data from finmag field for comparison with oommf
     exchange_for_oommf  = finmag_to_oommf(exc_finmag, msh, dims=1)
 
     difference = exchange_for_oommf.flat - exchange_oommf
     relative_difference = np.abs(difference / exchange_oommf)
-    return difference, relative_difference
+
+    return dict(m0=llg.m, mesh=dolfmesh, oommf_mesh=msh,
+            exc=exc_finmag.vector().array(), oommf_exc=exchange_oommf,
+            diff=difference, rel_diff=relative_difference)
 
 def three_dimensional_problem():
     x_max = 20e-9; y_max = 10e-9; z_max = 10e-9;
-    msh = df.Box(0, 0, 0, x_max, y_max, z_max, 20, 10, 10)
+    dolfmesh = df.Box(0, 0, 0, x_max, y_max, z_max, 20, 10, 10)
     m0 = ("pow(sin(x[0] * pow(10, 9) / 3), 2)", "0", "1")
-    exc_finmag, Ms, C = compute_exc_finmag(msh, m0)
+    exc_finmag, llg = compute_exc_finmag(dolfmesh, m0)
 
     msh = mesh.Mesh((20, 10, 10), size=(x_max, y_max, z_max))
     m0 = msh.new_field(3)
@@ -80,15 +84,18 @@ def three_dimensional_problem():
         m0.flat[1,i] = 0
         m0.flat[2,i] = 1
     m0.flat /= np.sqrt(m0.flat[0]*m0.flat[0] + m0.flat[1]*m0.flat[1] + m0.flat[2]*m0.flat[2])
-    exchange_oommf = oommf_uniform_exchange(m0, Ms, C).flat
+    exchange_oommf = oommf_uniform_exchange(m0, llg.Ms, llg.C).flat
 
     exchange_for_oommf = finmag_to_oommf(exc_finmag, msh, dims=3)
 
     difference = exchange_for_oommf.flat - exchange_oommf
     relative_difference = np.abs(difference / exchange_oommf)
-    return difference, relative_difference
 
-def stats_from_array_str(arr):
+    return dict(m0=llg.m, mesh=dolfmesh, oommf_mesh=msh,
+            exc=exc_finmag.vector().array(), oommf_exc=exchange_oommf,
+            diff=difference, rel_diff=relative_difference)
+
+def stats(arr):
     median  = np.median(arr)
     average = np.mean(arr, axis=1)
     minimum = np.nanmin(arr)
@@ -98,13 +105,15 @@ def stats_from_array_str(arr):
             minimum, median, maximum, average, spread)
     return stats
 
-def boxplot(arr, filename):
-    plt.boxplot(arr)
-    plt.show()
-
 if __name__ == '__main__':
-    diff_1d, rel_diff_1d = one_dimensional_problem()
-    print "1D problem, relative difference:\n", stats_from_array_str(rel_diff_1d)
+    res1 = one_dimensional_problem()
+    # images are worthless if the colormap is not shown. How to do that?
+    quiver(res1["m0"], res1["mesh"], MODULE_DIR+"/1d_exchange_m0.png")
+    quiver(res1["exc"], res1["mesh"], MODULE_DIR+"/1d_exchange_finmag.png")
+    quiver(res1["oommf_exc"], res1["oommf_mesh"], MODULE_DIR+"/1d_exchange_oommf.png")
+    quiver(res1["rel_diff"], res1["oommf_mesh"], MODULE_DIR+"/1d_exchange_rel_diff.png")
+    boxplot(list(res1["rel_diff"]), MODULE_DIR+"/1d_exchange_rel_diff_stats.png")
+    print "1D problem, relative difference:\n", stats(res1["rel_diff"])
 
-    diff_3d, rel_diff_3d = three_dimensional_problem()
-    print "3D problem, relative difference:\n", stats_from_array_str(rel_diff_3d)
+    res3 = three_dimensional_problem()
+    print "3D problem, relative difference:\n", stats(res3["rel_diff"])
