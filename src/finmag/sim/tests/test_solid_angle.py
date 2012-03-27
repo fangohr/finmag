6b8e7418825eb1d270649f64647c32807d8e3652
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-03-27 18:09:09 +0100

    test different octants, and the sum of all, probably found bug

--- src/finmag/sim/tests/test_solid_angle.py
+++ src/finmag/sim/tests/test_solid_angle.py
@@ -38,7 +38,7 @@ def solid_angle(triangles, point):
     #print "r",r.shape,
     T = np.zeros((3,3,n))
     T[:,:,0] = np.array(triangles)
-    #print "T",T.shape,"\n",T
+    print "T",T.shape,"\n",T
     a = np.zeros(1)
     csa(r,T,a)
     return a
@@ -48,11 +48,11 @@ def solid_angle(triangles, point):
 #print solid_angle([[1.,0,0],[-0.5,-0.5,0],[-0.5,0.5,0]],[0,0,-1e-1])
 #print solid_angle([[1.,0,0],[-0.5,-0.5,0],[-0.5,0.5,0]],[1,0,0])
 
-print "the following triangle is big, so it should cover 'half the sky', i.e. 2*pi=",math.pi*2
-a=1000.
-print solid_angle([[a,0,0],[-a,-a,0],[-a,a,0]],[0,0,1e-5])
-print "looking from the other side should give us the solid same angle?"
-print solid_angle([[a,0,0],[-a,-a,0],[-a,a,0]],[0,0,-1e-5])
+#print "the following triangle is big, so it should cover 'half the sky', i.e. 2*pi=",math.pi*2
+#a=1000.
+#print solid_angle([[a,-a,-a],[0,-a,a],[0,0,0]],[0,0,1e-5])
+#print "looking from the other side should give us the solid same angle?"
+#print solid_angle([[a,-a,-a],[0,-a,a],[0,0,0]],[0,0,-1e-5])
 
 def test_solid_angle():
 
@@ -79,34 +79,76 @@ def test_solid_angle():
     csa(r,T,a)
     print a
 
-def test_octants_solid_angle():
+def test_solid_angle_first_octant():
     """
     In spherical coordinates, the solid angle is defined as
     :math:`\\d\\Omega=\\sin\\theta\\,d\\theta\\,d\\varphi`.
-    
-    This gives 4PI for the solid angle of a sphere measured
-    from a point in its interior.
 
     From the point (0, 0, 0), the triangle defined by the vertices
-    (1, 0, 0), (0, 1, 0) and (0, 0, 1) can be identified with the azimuth
-    and zenith angles PI/2 and the integral yields PI/2.
-
-    Another way of getting to the same result is considering we are looking
-    at one of the 8 divisions of the euclidean three-dimensional
-    coordinate system (the first octant to be exact), divide the solid angle
-    of a sphere by 8 and get PI/2 again.
+    (1, 0, 0), (0, 1, 0) and (0, 0, 1) in cartesian coordinates
+    can be identified with the azimuth and zenith angles PI/2
+    and the integral yields PI/2.
 
     """
     origin = np.zeros((3, 1))
+
+    # first octant (+, +, +) front-right-top
     triangle = np.array([[[1.],[0.],[0.]], [[0.],[1.],[0.]], [[0.],[0.],[1.]]])
     assert triangle.shape == (3, 3, 1)
     angle = np.zeros(1)
-
     csa(origin, triangle, angle)
-    assert angle[0] - np.pi / 2 < TOLERANCE
+    assert abs(angle[0] - np.pi / 2) < TOLERANCE, \
+            "The solid angle is {0}, but should be PI/2={1}.".format(angle[0], np.pi/2)
+    
+def test_solid_angle_one_minus_sign():
+    origin = np.zeros((3, 1))
+
+    # (-, +, +) back-right-top
+    triangle = np.array([[[-1.],[0.],[0.]], [[0.],[1.],[0.]], [[0.],[0.],[1.]]])
+    assert triangle.shape == (3, 3, 1)
+    angle = np.zeros(1)
+    csa(origin, triangle, angle)
+    assert abs(angle[0] - np.pi / 2) < TOLERANCE, \
+            "The solid angle is {0}, but should be PI/2={1}.".format(angle[0], np.pi/2)
+
+def test_solid_angle_two_minus_signs():
+    origin = np.zeros((3, 1))
+
+    # (-, -, +) back-left-top
+    triangle = np.array([[[-1.],[0.],[0.]], [[0.],[-1.],[0.]], [[0.],[0.],[1.]]])
+    assert triangle.shape == (3, 3, 1)
+    angle = np.zeros(1)
+    csa(origin, triangle, angle)
+    assert abs(angle[0] - np.pi / 2) < TOLERANCE, \
+            "The solid angle is {0}, but should be PI/2={1}.".format(angle[0], np.pi/2)
+
+def test_octants_solid_angle():
+    """
+    By the same reasing as above, we get 4PI for the solid angle of a sphere
+    as seen by one point inside of it.
+
+    That means that another way of getting the solid angle of a single octant
+    is considering we are looking at one of the 8 divisions of the
+    euclidean three-dimensional coordinate system, divide the solid
+    angle of a sphere by 8 and get PI/2.
+
+    """
+    origin = np.zeros((3, 1))
+
+    TRIANGLES = 8
+    triangles = np.zeros((3, 3, TRIANGLES))
+    # (3, 3, 8) three components, three vertices per triangle, 8 triangles
+
+    triangles[0,0,] = np.array([1., -1., 1., -1., 1., -1., 1., -1.]) # X, V1, 1-8
+    triangles[1,1,] = np.array([1., 1., -1., -1., 1., 1., -1., -1.]) # Y, V2, 1-8
+    triangles[2,2,] = np.array([1., 1., 1., 1., -1., -1., -1., -1.]) # Z, V3, 1-8
+
+    angle = np.zeros(1)
+    csa(origin, triangles, angle)
+    assert abs(angle[0] - 4*np.pi) < TOLERANCE, \
+        "The solid angle is {0}, but should be 4PI={1}.".format(angle[0], 4*np.pi)
 
 if __name__=="__main__":
     test_octants_solid_angle()
-    #test_solid_angle()
     pass
 
