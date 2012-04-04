b33bb6a857c6198dbc1083992319f0cec855b2a8
Hans Fangohr <fangohr@soton.ac.uk>
2012-04-04 23:05:43 +0100

    Made REL_TOLERANCE values stricter, and used different ones for every test.
    I don't understand why the 3d example shows relatively poor accuracy.
    
    I also observer that -- for exampel in the 1d example -- if we increase
    the OOMMF mesh resolution, the relative error increases. This seems odd
    as well. On the positive side, by increasing the number of FE nodes to 100,000,
    we get a relative error of less that 2e-9.
    
    As the agreement with Magpar and Weiwei's FE code is good for the anisotropy
    this is more likely to be a matter of either the code we use to drive OOMMF
    or the particular configurations we test. However, that is just guess work
    for now.

--- src/finmag/sim/tests/test_anisotropy_with_oommf.py
+++ src/finmag/sim/tests/test_anisotropy_with_oommf.py
@@ -4,20 +4,26 @@ from finmag.util.oommf import mesh
 from finmag.util.oommf.comparison import compare_anisotropy
 from finmag.sim.helpers import stats
 
-K1 = 45e4 # J/m^3
-
-REL_TOLERANCE = 5e-3 
+K1 = 45e4 # J/m^31
 
 def test_small_problem():
     results = small_problem()
+    REL_TOLERANCE = 1e-15
+    print "0d: rel_diff_max:",np.nanmax(results["rel_diff"])
     assert np.nanmax(results["rel_diff"]) < REL_TOLERANCE
 
 def test_one_dimensional_problem():
     results = one_dimensional_problem()
+    REL_TOLERANCE = 0.0006 #for 200 nodes
+    REL_TOLERANCE = 2e-9 #for 100,000 FE nodes
+
+    print "1d: rel_diff_max:",np.nanmax(results["rel_diff"])
     assert np.nanmax(results["rel_diff"]) < REL_TOLERANCE
 
 def test_three_dimensional_problem():
     results = three_dimensional_problem()
+    REL_TOLERANCE = 0.003
+    print "3d: rel_diff_max:",np.nanmax(results["rel_diff"])
     assert np.nanmax(results["rel_diff"]) < REL_TOLERANCE
 
 def small_problem():
@@ -34,7 +40,7 @@ def small_problem():
     return compare_anisotropy(m_gen, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, name="single")
 
 def one_dimensional_problem():
-    x_min = 0; x_max = 10e-9; x_n = 200;
+    x_min = 0; x_max = 100e-9; x_n = 100000;
     dolfin_mesh = df.Interval(x_n, x_min, x_max)
     oommf_mesh = mesh.Mesh((20, 1, 1), size=(x_max, 1e-12, 1e-12))
 
@@ -46,12 +52,19 @@ def one_dimensional_problem():
 
 def three_dimensional_problem():
     x_max = 20e-9; y_max = 10e-9; z_max = 10e-9;
-    dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, 100, 10, 10)
-    oommf_mesh = mesh.Mesh((20, 10, 10), size=(x_max, y_max, z_max))
+    x_n=20; y_n=10; z_n=10
+    dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, x_n*5, y_n, z_n)
+    oommf_mesh = mesh.Mesh((x_n, y_n, z_n), size=(x_max, y_max, z_max))
+
+    #TODO: HF: This tests seems fishy. If I change either the anisotropy directior
+    #or the m_gen function, the max error increases from the already large 
+    #value of 0.003. Finer meshes do not really help, and 
+    #we also use a finer mesh for dolfin here than we do for OOMMF.
+    #This should not be necessary.
 
     def m_gen(rs):
       xs = rs[0]
-      return np.array([np.sin(1e9 * xs/3)**2, np.zeros(len(xs)), np.ones(len(xs))])
+      return np.array([np.sin(1e9 * xs/3.)**2, np.zeros(len(xs)), np.ones(len(xs))])
 
     from finmag.util.oommf.comparison import compare_anisotropy
     return compare_anisotropy(m_gen, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, dims=3, name="3D")
