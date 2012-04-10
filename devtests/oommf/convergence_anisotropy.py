e1e2e964ff6ffa8ca67668726f81c0f9c384c934
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-04-10 09:58:34 +0100

    example for gabriel

--- devtests/oommf/convergence_anisotropy.py
+++ devtests/oommf/convergence_anisotropy.py
@@ -14,8 +14,8 @@ MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
 max_rdiffs = [[],[]]
 vertices = [[],[]]
 
-x_min = 0; x_max = 100e-9;
 for x_n in [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]:
+    x_min = 0; x_max = 100e-9;
     dolfin_mesh = df.Interval(int(x_n), x_min, x_max)
     oommf_mesh = mesh.Mesh((20, 1, 1), size=(x_max, 1e-12, 1e-12))
 
@@ -28,6 +28,20 @@ for x_n in [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]:
     vertices[0].append(dolfin_mesh.num_vertices())  
     max_rdiffs[0].append(np.max(res["rel_diff"]))
 
+for n in [1e1, 1e2, 1e3]:
+    x_max = y_max = z_max = 100e-9;
+    dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, int(n), int(n), int(n))
+    oommf_mesh = mesh.Mesh((20, 20, 20), size=(x_max, y_max, z_max))
+
+    def m_gen(rs):
+      xs = rs[0]
+      return np.array([np.ones(len(xs)), np.zeros(len(xs)), np.zeros(len(xs))])
+
+    res = compare_anisotropy(m_gen, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, dims=3, name="3D")
+ 
+    vertices[1].append(dolfin_mesh.num_vertices())  
+    max_rdiffs[1].append(np.max(res["rel_diff"]))
+
 print vertices
 print max_rdiffs
 
@@ -35,6 +49,8 @@ plt.xlabel("vertices")
 plt.ylabel("rel. max diff.")
 plt.loglog(vertices[0], max_rdiffs[0], "--")
 plt.loglog(vertices[0], max_rdiffs[0], "o", label="1d problem")
+plt.loglog(vertices[1], max_rdiffs[1], "--")
+plt.loglog(vertices[1], max_rdiffs[1], "o", label="3d problem")
 plt.legend()
 plt.savefig(MODULE_DIR + "anis_convergence.png")
 plt.show()
