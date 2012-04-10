3a5bdf72e17db5957f2ecc1732d4bd8a6fe29e3c
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-04-10 11:23:19 +0100

    anisotropy convergence 3D added to plot

--- devtests/oommf/convergence_anisotropy.py
+++ devtests/oommf/convergence_anisotropy.py
@@ -28,14 +28,15 @@ for x_n in [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]:
     vertices[0].append(dolfin_mesh.num_vertices())  
     max_rdiffs[0].append(np.max(res["rel_diff"]))
 
-for n in [1e1, 1e2, 1e3]:
-    x_max = y_max = z_max = 100e-9;
-    dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, int(n), int(n), int(n))
-    oommf_mesh = mesh.Mesh((20, 20, 20), size=(x_max, y_max, z_max))
+for x_n in [10, 80, 350]:
+    x_max = 100e-9; y_max = z_max = x_max/10;
+    y_n = z_n = x_n/10;
+    dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, x_n, y_n, z_n)
+    oommf_mesh = mesh.Mesh((50, 5, 5), size=(x_max, y_max, z_max))
 
     def m_gen(rs):
       xs = rs[0]
-      return np.array([np.ones(len(xs)), np.zeros(len(xs)), np.zeros(len(xs))])
+      return np.array([xs/x_max, np.sqrt(1 - (xs/x_max)**2), np.zeros(len(xs))])
 
     res = compare_anisotropy(m_gen, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, dims=3, name="3D")
  
