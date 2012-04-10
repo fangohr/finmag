1c02097324b71acb58c61f6967622ad6093b086d
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-04-10 12:04:09 +0100

    convergence exchange plot

--- devtests/oommf/compare_exchange.py
+++ devtests/oommf/compare_exchange.py
@@ -1,24 +1,23 @@
+import os
 import numpy as np
 import dolfin as df
+import matplotlib.pyplot as plt
 from finmag.sim.helpers import stats 
 from finmag.util.oommf import mesh
 from finmag.util.oommf.comparison import compare_exchange 
 
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
+
 def test_one_dimensional_problem():
-    REL_TOLERANCE = 2
-    results = one_dimensional_problem()
-    assert np.nanmax(results["rel_diff"]) < REL_TOLERANCE
+    REL_TOLERANCE = 1e-3
+    results = one_dimensional_problem(5000)
+    assert np.nanmax(np.mean(results["rel_diff"], axis=1)) < REL_TOLERANCE
 
-def test_three_dimensional_problem():
-    REL_TOLERANCE = 7
-    results = three_dimensional_problem()
-    assert np.nanmax(results["rel_diff"]) < REL_TOLERANCE
+def one_dimensional_problem(vertices):
+    x_min = 0; x_max = 100e-9;
 
-def one_dimensional_problem():
-    x_min = 0; x_max = 100e-9; x_n = 50;
-
-    dolfin_mesh = df.Interval(x_n, x_min, x_max)
-    oommf_mesh = mesh.Mesh((x_n, 1, 1), size=(x_max, 1e-12, 1e-12))
+    dolfin_mesh = df.Interval(vertices, x_min, x_max)
+    oommf_mesh = mesh.Mesh((vertices, 1, 1), size=(x_max, 1e-12, 1e-12))
 
     def m_gen(coords):
         xs = coords[0]
@@ -26,21 +25,21 @@ def one_dimensional_problem():
 
     return compare_exchange(m_gen, dolfin_mesh, oommf_mesh, dims=1, name="1d")
 
-def three_dimensional_problem():
-    x_max = 100e-9; y_max = z_max = 10e-9;
-    x_n = 50; y_n = z_n = x_n/10;
-
-    dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, x_n, x_n, x_n)
-    oommf_mesh = mesh.Mesh((x_n, y_n, z_n), size=(x_max, y_max, z_max))
-
-    def m_gen(coords):
-        xs = coords[0]
-        return np.array([np.sqrt(xs/x_max), np.sqrt(1 - xs/x_max), np.zeros(len(xs))])
-
-    return compare_exchange(m_gen, dolfin_mesh, oommf_mesh, name="3d")
-
 if __name__ == '__main__':
-    res1 = one_dimensional_problem()
-    print "1D problem, relative difference:\n", stats(res1["rel_diff"])
-    res3 = three_dimensional_problem()
-    print "3D problem, relative difference:\n", stats(res3["rel_diff"])
+
+    vertices = []
+    mean_diffs = []
+
+    for n in [10, 1e2, 1e3, 1e4, 1e5]:
+      res = one_dimensional_problem(int(n))
+      print "1D problem ({} nodes) relative difference:".format(n)
+      print stats(res["rel_diff"])
+      vertices.append(int(n))
+      mean_diffs.append(np.nanmax(np.mean(res["rel_diff"], axis=1)))
+
+    plt.xlabel("vertices")
+    plt.ylabel("rel. mean diff.")
+    plt.loglog(vertices, mean_diffs, "--")
+    plt.loglog(vertices, mean_diffs, "o", label="1d problem")
+    plt.legend()
+    plt.savefig(MODULE_DIR + "exchange_convergence.png")
