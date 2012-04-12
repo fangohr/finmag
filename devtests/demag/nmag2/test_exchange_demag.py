afc43be27163fee3adf17d3aaa7c428a90235875
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-04-12 17:26:35 +0100

    modify test
    check for time
    skip if finmag wasn't run. We will do this as long as we can't run this example automatically.

--- devtests/demag/nmag2/test_exchange_demag.py
+++ devtests/demag/nmag2/test_exchange_demag.py
@@ -1,11 +1,11 @@
 import os
-import cProfile
-import pstats
+import pytest
 import finmag.sim.helpers as h
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 TOLERANCE = 1e-3
 
+@pytest.mark.skipif("not os.path.exists(MODULE_DIR+ '/averages.txt')")
 def test_compare_averages():
     ref = h.read_float_data(MODULE_DIR + "/averages_ref.txt")
     computed = h.read_float_data(MODULE_DIR + "/averages.txt")
@@ -13,6 +13,8 @@ def test_compare_averages():
     for i in range(len(computed)):
         t_ref, mx_ref, my_ref, mz_ref = ref[i]
         t, mx, my, mz = computed[i]
+        
+        assert abs(t  - t_ref)  < 1e-15
 
         assert abs(mx - mx_ref) < TOLERANCE
         assert abs(my - my_ref) < TOLERANCE
