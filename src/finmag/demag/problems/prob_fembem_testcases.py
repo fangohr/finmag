b4221d71c2c5a1a8660f2da5d5010adad31430aa
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-12 11:55:12 +0100

    [#27830521] Activated the demag tests using the Weiwei code. Tests pass, but still with a (relative) high tolerance. Can get the tolerance lower, but that will require more jenkins time.

--- src/finmag/demag/problems/prob_fembem_testcases.py
+++ src/finmag/demag/problems/prob_fembem_testcases.py
@@ -89,13 +89,14 @@ class MagSphereBase(pb.FemBemDeMagProblem,cm.MeshGenerator):
         self.M = (str(self.Ms), "0.0", "0.0")
         self.V = VectorFunctionSpace(self.mesh, "CG", 1)
         self.m = interpolate(Expression(self.M), self.V)
+        self.r = radius        
         self.maxh = maxh
 
         #Initialize Base pb.FemBemDeMagProblem
         pb.FemBemDeMagProblem.__init__(self,self.mesh,self.M)
         
     def desc(self):
-        return "Sphere demagnetisation test problem fembem, Ms=%g, maxh=%g" %(self.Ms,self.maxh)
+        return "Sphere demag test problem, Ms=%g, radius=%g, maxh=%g" %(self.Ms, self.r, self.maxh)
 
 #Note python doesn't allow :." in class names so the Sphere1.0 is now Sphere10 etc...        
 
--- src/finmag/demag/tests/test_demag.py
+++ src/finmag/demag/tests/test_demag.py
@@ -1,14 +1,17 @@
 import pytest
 import dolfin as df
 import numpy as np
-#from finmag.demag import solver_fk, solver_gcr
 from finmag.demag.solver_fk_test import SimpleFKSolver
 from finmag.demag.problems.prob_fembem_testcases import MagSphere
 
-TOL = 3e-1
+# Can get smaller tolerance if we use finer mesh, but that will
+# delay the jenkins build with minutes.
+TOL = 5e-3
 
-problems = [MagSphere(5,3), MagSphere(5,2), MagSphere(5,1)]
+# Feel free to add more (interesting) problems
+problems = [MagSphere(5, 0.75), MagSphere(1, 0.15)]
 
+# Please feel free to also add more (working) solvers...
 solvers = [SimpleFKSolver]
 
 fields = []
@@ -20,29 +23,23 @@ for problem in problems:
         cases.append(case)
         fields.append(grad)
 
-
-
 @pytest.mark.parametrize(("case", "grad"), zip(cases, fields))
 def test_one_third(case, grad):
     """Demag field should be one third of initial magnetisation."""
-    W = df.VectorFunctionSpace(case.V.mesh(), "CG", 1, dim=3)
-    a = df.interpolate(df.Expression(problem.M), W)
+    m = case.m
+    assert len(grad) == len(m.vector().array())
 
-    #Note to self check the array's 
-    assert len(grad) == len(a.vector().array())
-
-    diff =  np.abs(grad + 1./3*a.vector().array())
+    diff =  np.abs(grad + 1./3*m.vector().array())
     print "Max difference should be zero, is %g." % max(diff)
     assert max(diff) < TOL
-
-
 
 @pytest.mark.parametrize(("case", "grad"), zip(cases, fields))
 def test_y_z_zero(case, grad):
     """Demag field should be zero in y- and z-direction."""
-    y_start = len(grad)//3
-    y = grad[y_start:2*y_start]
-    z = grad[2*y_start:]
+    grad = np.hsplit(grad, 3)
+    y = grad[1]
+    z = grad[2]
+
     maxy = max(abs(y))
     print "Max field in y-direction should be zero, is %g." % maxy
     assert maxy < TOL
@@ -51,42 +48,36 @@ def test_y_z_zero(case, grad):
     print "Max field in z-direction should be zero, is %g." % maxz
     assert maxz < TOL
 
-
-
 @pytest.mark.parametrize(("case", "grad"), zip(cases, fields))
 def test_x_deviation(case, grad):
     """Deviation of demag field in x-direction should be zero."""
-    x = grad[:len(grad)//3]
+    grad = np.hsplit(grad, 3)
+    x = grad[0]
     dev = abs(max(x) - min(x))
     
     print "Max deviation in x-direction should be zero, is %g." % dev
     assert dev < TOL
 
-
-
 @pytest.mark.parametrize(("case", "grad"), zip(cases, fields))
 def test_norm_x(case, grad):
     """Norm of demag field in x-direction minus 1/3 Ms should be zero."""
-    x = grad[:len(grad)//3] #+ 1./3*case.Ms
-    print len(x), len(grad)
-    
-    y = grad.reshape((
-    return
-    normx = np.linalg.norm(x)
+    grad = np.hsplit(grad, 3)
+    x = grad[0]
+    normx = np.linalg.norm(x + 1./3*case.Ms)
 
     print "L2 norm of the x-component + 1/3 Ms should be zero. Is %g." % normx
-    assert normx < TOL
-
-
+    # This test needs a higher tolerance to pass.
+    assert normx < TOL*14
 
 @pytest.mark.parametrize(("case", "grad"), zip(cases, fields))
 def test_avg_x(case, grad):
     """Average of the x-components should be 1/3 of Ms."""
-    x = grad[:len(grad)//3]
+    grad = np.hsplit(grad, 3)
+    x = grad[0] 
     avg = np.average(x) 
 
-    print "Average of x-component should be -%g. Is %g." % (1./3*problem.Ms, avg)
-    assert abs(avg+1./3*problem.Ms) < TOL
+    print "Average of x-component should be -%g. Is %g." % (1./3*case.Ms, avg)
+    assert abs(avg+1./3*case.Ms) < TOL
 
 
 if __name__ == '__main__':
@@ -95,10 +86,10 @@ if __name__ == '__main__':
     for case, grad in zip(cases, fields):
         print "Test case %d: %s" % (i+1, problems[i].desc())
         print "Number of vertices:", case.V.mesh().num_vertices()
-        #test_one_third(case, grad)
-        #test_y_z_zero(case, grad)
-        #test_x_deviation(case, grad)
+        test_one_third(case, grad)
+        test_y_z_zero(case, grad)
+        test_x_deviation(case, grad)
         test_norm_x(case, grad)
-        #test_avg_x(case, grad)
+        test_avg_x(case, grad)
         print ""
         i += 1
