c7ead1fe07941ac16c27f907fe657a6ccbb85058
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-13 09:27:32 +0100

    Debugging a weird problem

--- devtests/demag/nmag2/test_exchange_demag.py
+++ devtests/demag/nmag2/test_exchange_demag.py
@@ -10,6 +10,9 @@ def test_compare_averages():
     ref = h.read_float_data(MODULE_DIR + "/averages_ref.txt")
     computed = h.read_float_data(MODULE_DIR + "/averages.txt")
 
+    #TODO: Remove this
+    ERR = 0
+
     for i in range(len(computed)):
         t_ref, mx_ref, my_ref, mz_ref = ref[i]
         t, mx, my, mz = computed[i]
@@ -19,3 +22,16 @@ def test_compare_averages():
         assert abs(mx - mx_ref) < TOLERANCE
         assert abs(my - my_ref) < TOLERANCE
         assert abs(mz - mz_ref) < TOLERANCE
+
+        ####
+        # DEBUGGING, TODO: REMOVE THE REST OF THE FILE
+        ####
+
+        err = max(abs(mx-mx_ref), abs(my-my_ref), abs(mz-mz_ref))
+        if err > ERR:
+            ERR = err
+    print ERR
+
+if __name__ == '__main__':
+    test_compare_averages()
+
