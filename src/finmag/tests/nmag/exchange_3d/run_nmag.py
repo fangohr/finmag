c25a7f785e7d14a46459b34ce657cd8f4f517d6c
Hans Fangohr <fangohr@soton.ac.uk>
2012-04-14 07:39:18 +0100

    Partial backout of changset 5f6554186bb3 which removed some desired files by accident.
    Command used:
    
    fangohr@ubuntu:~/hg/finmag/src/finmag$ hg backout -I tests -r 5f6554186bb3

---
+++ src/finmag/tests/nmag/exchange_3d/run_nmag.py
@@ -0,0 +1,28 @@
+import nmag
+from nmag import SI, every
+from nsim.si_units.si import degrees_per_ns
+
+# example 2 of the nmag documentation
+# without demag but with external field
+
+mat_Py = nmag.MagMaterial(
+    name="Py",
+    Ms=SI(0.86e6, "A/m"),
+    exchange_coupling=SI(13.0e-12, "J/m"),
+    llg_damping=0.1)
+
+L = 30.0e-9; H = 10.0e-9; W = 10.0e-9
+def m0(r):
+    mx = 2*r[0]/L - 1
+    my = 2*r[1]/W - 1
+    mz = 1
+    return [mx, my, mz]
+
+sim = nmag.Simulation("bar", do_demag=False)
+sim.load_mesh("bar.nmesh.h5", [("Py", mat_Py)], unit_length=SI(1e-9, "m"))
+sim.set_H_ext([1, 0, 0], SI(0.43e6, "A/m"))
+sim.set_m(m0)
+
+sim.set_params(stopping_dm_dt=1*degrees_per_ns,
+        ts_rel_tol=1e-6, ts_abs_tol=1e-6)
+sim.relax(save=[('averages', every('time', SI(1e-11, "s")))])
---
+++ src/finmag/tests/nmag/spinwaves/run_finmag.py
@@ -0,0 +1,60 @@
+import os
+import dolfin as df
+from scipy.integrate import ode
+from finmag.sim.llg import LLG
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def run_simulation():
+    """
+    Translation of the nmag code.
+    Mesh generated on the fly.
+
+    """
+
+    x0 = 0; x1 = 15e-9; nx = 30;
+    y0 = -4.5e-9; y1 = 4.5e-9; ny = 18;
+    z0 = -0.1e-9; z1 = 0.1e-9; nz = 1;
+    mesh = df.Box(x0, y0, z0, x1, y1, z1, nx, ny, nz) 
+    nb_nodes = len(mesh.coordinates())
+
+    llg = LLG(mesh)
+    llg.Ms = 1e6
+    llg.A = 1.3e-11
+    #llg.c = 1e11
+    llg.alpha = 0.02
+
+    llg.set_m(("1",
+        "5 * pow(cos(pi * (x[0] * pow(10, 9) - 11) / 6), 3) \
+           * pow(cos(pi * x[1] * pow(10, 9) / 6), 3)",
+        "0"))
+
+    m = llg.m
+    for i in xrange(nb_nodes):
+        x, y, z = mesh.coordinates()[i]
+        mx = 1; my = 0; mz = 0;
+        if 8e-9 < x < 14e-9 and -3e-9 < y < 3e-9:
+            pass
+        else:
+            m[i] = mx; m[i+nb_nodes] = my; m[i+2*nb_nodes] = mz;
+    llg.m = m
+    llg.setup(use_exchange=True)
+
+    llg_wrap = lambda t, y: llg.solve_for(y, t)
+    t0 = 0; dt = 0.05e-12; t1 = 10e-12
+    r = ode(llg_wrap).set_integrator("vode", method="bdf", rtol=1e-5, atol=1e-5)
+    r.set_initial_value(llg.m, t0)
+
+    fh = open(MODULE_DIR + "/averages.txt", "w")
+    while r.successful() and r.t <= t1:
+        print "Integrating time = %gs" % (r.t)
+        mx, my, mz = llg.m_average
+        fh.write(str(r.t) + " " + str(mx) + " " + str(my) + " " + str(mz) + "\n")
+        r.integrate(r.t + dt)
+        #df.plot(llg._m)
+    fh.close()
+    print "Done"
+    #df.interactive()
+
+if __name__ == "__main__":
+    run_simulation()
