4d308900341723fdecbb0832a038ac5d3b9bcb74
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-17 09:49:34 +0100

    Made the trunk solver default and added timings.

--- src/finmag/demag/solver_fk.py
+++ src/finmag/demag/solver_fk.py
@@ -20,14 +20,198 @@ import finmag.util.solid_angle_magpar as solid_angle_solver
 compute_belement=belement_magpar.return_bele_magpar()
 compute_solid_angle=solid_angle_solver.return_csa_magpar()
 import logging
+from finmag.util.timings import timings
 
 logger = logging.getLogger(name='finmag')
 
 class FemBemFKSolver(sb.FemBemDeMagSolver):
+    def __init__(self, problem, degree=1, element="CG"):
+        super(FemBemFKSolver, self).__init__(problem, degree, element=element)
+        self.m = problem.M
+        self.Ms = problem.Ms
+        self.mesh = problem.mesh
+        self.W = df.VectorFunctionSpace(self.mesh, element, degree, dim=3)
+        self.w = df.TrialFunction(self.W)
+        self.phi1 = df.Function(self.V)
+        self.phi2 = df.Function(self.V)
+
+        self.bdofs = self.doftionary.keys()
+        self.nodes_number = self.mesh.num_vertices()
+        self.__build_mapping()
+
+    def solve(self):
+        # Compute phi1 on the whole domain
+        timings.start("Compute phi1")
+        self.__compute_phi1()
+        timings.stop("Compute phi1")
+
+        # Restrict phi1 to the boundary
+        timings.start("Restrict phi1 to boundary")
+        # FIXME: Why doesn't this work?
+        # phi1_bnd = self.restrict_to(self.phi1.vector())
+        phi1_bnd = self.__restrict_phi1_to_bnd()
+        timings.stop("Restrict phi1 to boundary")
+
+        # Compute boundary element matrix
+        if self.bem is None:
+            timings.start("Build boundary element matrix")
+            self.bem = self.__compute_bem()
+            timings.stop("Build boundary element matrix")
+
+
+        # Compute phi2 on the boundary as a df.dot product
+        # between the boundary element matrix and
+        # phi1 on the boundary
+        timings.start("Compute phi2 on boundary")
+        phi2_bnd = np.dot(self.bem, phi1_bnd)
+        self.phi2.vector()[self.bdofs[:]] = phi2_bnd[:]
+        timings.stop("Compute phi2 on boundary")
+
+        # Compute Laplace's equation inside the domain
+        timings.start("Compute phi2 inside")
+        self.phi2 = self.solve_laplace_inside(self.phi2)
+        timings.stop("Compute phi2 inside")
+
+        # phi = phi1 + phi2
+        timings.start("Add phi1 and phi2")
+        self.phi = self.calc_phitot(self.phi1, self.phi2)
+        timings.stop("Add phi1 and phi2")
+        return self.phi
+
+    def __compute_phi1(self):
+        """Compute phi1."""
+        Ms, m, u, v, w = self.Ms, self.m, self.u, self.v, self.w
+
+        # These statements are equivalent
+        timings.start("phi1 compute b")
+        b = Ms*df.inner(w, df.grad(v))*df.dx
+        timings.stop("phi1 compute b")
+        timings.start("phi1 assemble g1")
+        g1 = df.assemble(b)*m.vector()
+        timings.stop("phi1 assemble g1")
+        # and:
+        #n = df.FacetNormal(self.mesh)
+        #b = df.dot(n, m)*v*df.ds - df.div(m)*v*df.dx
+        #g1 = Ms*df.assemble(b)
+
+        if not hasattr(self, "poisson_matrix"):
+            timings.start("Build poisson matrix")
+            self.build_poisson_matrix()
+            timings.stop("Build poisson matrix")
+        timings.start("Solve for phi1")
+        df.solve(self.poisson_matrix, self.phi1.vector(), g1)
+        timings.stop("Solve for phi1")
+
+    def __restrict_phi1_to_bnd(self):
+        """Restrict phi1 to boundary."""
+        U1 = sp.lil_matrix((self.bnd_nodes_number,
+                                self.nodes_number),
+                                dtype='float32')
+
+        g2b = self.gnodes_to_bnodes
+
+        for i in range(self.nodes_number):
+            if g2b[i] >= 0:
+                U1[g2b[i], i] = 1
+        return U1*self.phi1.vector()
+
+    def compute_field(self):
+        # Should instead use self.get_demagfield() when this
+        # is renamed (?) and projected onto CG1 elements.
+        self.solve()
+        demag = df.project(-df.grad(self.phi), self.W)
+        return demag.vector().array()
+
+    def __build_mapping(self):
+        self.bnd_face_nodes,\
+        self.gnodes_to_bnodes,\
+        self.bnd_faces_number,\
+        self.bnd_nodes_number = \
+                belement.compute_bnd_mapping(self.mesh)
+
+    def __compute_bem(self):
+        mesh = self.mesh
+        xyz = mesh.coordinates()
+        bfn = self.bnd_face_nodes
+        g2b = self.gnodes_to_bnodes
+
+        nodes_number = self.nodes_number
+
+        n = self.bnd_nodes_number
+        B = np.zeros((n,n))
+
+        tmp_bele = np.zeros(3)
+
+        # Progressbar..
+        loops = (nodes_number - sum(g2b<0))*len(bfn) + mesh.num_cells()*4
+        loop_ctr = 0
+        bar = pb.ProgressBar(maxval=loops, \
+                widgets=[pb.ETA(), pb.Bar('=', '[', ']'), ' ', pb.Percentage()])
+        logger.info("Building Boundary Element Matrix")
+
+        for i in range(nodes_number):
+            #skip the node not at the boundary
+            if g2b[i]<0:
+                continue
+
+            for j in range(self.bnd_faces_number):
+
+                # Progressbar data
+                loop_ctr += 1
+                bar.update(loop_ctr)
+
+                #skip the node in the face
+                if i in set(bfn[j]):
+                    continue
+
+                compute_belement(
+                    xyz[i],
+                    xyz[bfn[j][0]],
+                    xyz[bfn[j][1]],
+                    xyz[bfn[j][2]],
+                tmp_bele)
+
+                for k in range(3):
+                    ti=g2b[i]
+                    tj=g2b[bfn[j][k]]
+                    B[ti][tj]+=tmp_bele[k]
+
+
+        #the solid angle term ...
+        solid_angle = np.zeros(nodes_number)
+
+        cells = mesh.cells()
+        for i in range(mesh.num_cells()):
+            for j in range(4):
+                omega = compute_solid_angle(
+                    xyz[cells[i][j]],
+                    xyz[cells[i][(j+1)%4]],
+                    xyz[cells[i][(j+2)%4]],
+                    xyz[cells[i][(j+3)%4]])
+
+                solid_angle[cells[i][j]] += omega
+
+                # Progressbar data
+                loop_ctr += 1
+                bar.update(loop_ctr)
+
+        for i in range(nodes_number):
+            j = g2b[i]
+            if j < 0:
+                continue
+
+            B[j][j] += solid_angle[i]/(4*np.pi) - 1
+
+        return B
+
+
+
+class FemBemFKSolverOld(sb.FemBemDeMagSolver):
+    # Very deprecated, should be removed soon.
     """FemBem solver for Demag Problems using the Fredkin-Koehler approach."""
 
     def __init__(self, problem, degree=1):
-        super(FemBemFKSolver,self).__init__(problem, degree)
+        super(FemBemFKSolverOld,self).__init__(problem, degree)
         #Default linalg solver parameters
         self.phi1solverparams = {"linear_solver":"lu"}
         self.phi2solverparams = {"linear_solver":"lu"}
@@ -181,164 +365,6 @@ class FemBemFKSolver(sb.FemBemDeMagSolver):
 
         return SA
 
-
-class FemBemFKSolverTrunc2(sb.FemBemDeMagSolver):
-    def __init__(self, problem, degree=1, element="CG"):
-        super(FemBemFKSolverTrunc2, self).__init__(problem, degree, element=element)
-        self.m = problem.M
-        self.Ms = problem.Ms
-        self.mesh = problem.mesh
-        self.W = df.VectorFunctionSpace(self.mesh, element, degree, dim=3)
-        self.w = df.TrialFunction(self.W)
-        self.phi1 = df.Function(self.V)
-        self.phi2 = df.Function(self.V)
-
-        self.bdofs = self.doftionary.keys()
-        self.nodes_number = self.mesh.num_vertices()
-        self.__build_mapping()
-
-    def __solve(self):
-        # Compute phi1 on the whole domain
-        self.__compute_phi1()
-
-        # Restrict phi1 to the boundary
-        # FIXME: Why doesn't this work?
-        # phi1_bnd = self.restrict_to(self.phi1.vector())
-        phi1_bnd = self.__restrict_phi1_to_bnd()
-
-        # Compute boundary element matrix
-        B = self.__compute_bem()
-
-        # Compute phi2 on the boundary as a df.dot product
-        # between the boundary element matrix and
-        # phi1 on the boundary
-        phi2_bnd = np.dot(B, phi1_bnd)
-        self.phi2.vector()[self.bdofs[:]] = phi2_bnd[:]
-
-        # Compute Laplace's equation inside the domain
-        self.phi2 = self.solve_laplace_inside(self.phi2)
-
-        # phi = phi1 = phi2
-        self.phi = self.calc_phitot(self.phi1, self.phi2)
-
-    def __compute_phi1(self):
-        """Compute phi1."""
-        Ms, m, u, v, w = self.Ms, self.m, self.u, self.v, self.w
-
-        # These statements are equivalent
-        b = Ms*df.inner(w, df.grad(v))*df.dx
-        g1 = df.assemble(b)*m.vector()
-        # and:
-        #n = df.FacetNormal(self.mesh)
-        #b = df.dot(n, m)*v*df.ds - df.div(m)*v*df.dx
-        #g1 = Ms*df.assemble(b)
-
-        if not hasattr(self, "poisson_matrix"):
-            self.build_poisson_matrix()
-        df.solve(self.poisson_matrix, self.phi1.vector(), g1)
-
-    def __restrict_phi1_to_bnd(self):
-        """Restrict phi1 to boundary."""
-        U1 = sp.lil_matrix((self.bnd_nodes_number,
-                                self.nodes_number),
-                                dtype='float32')
-
-        g2b = self.gnodes_to_bnodes
-
-        for i in range(self.nodes_number):
-            if g2b[i] >= 0:
-                U1[g2b[i], i] = 1
-        return U1*self.phi1.vector()
-
-    def compute_field(self):
-        # Should instead use self.get_demagfield() when this
-        # is renamed (?) and projected onto CG1 elements.
-        self.__solve()
-        demag = df.project(-df.grad(self.phi), self.W)
-        return demag.vector().array()
-
-    def __build_mapping(self):
-        self.bnd_face_nodes,\
-        self.gnodes_to_bnodes,\
-        self.bnd_faces_number,\
-        self.bnd_nodes_number = \
-                belement.compute_bnd_mapping(self.mesh)
-
-    def __compute_bem(self):
-        mesh = self.mesh
-        xyz = mesh.coordinates()
-        bfn = self.bnd_face_nodes
-        g2b = self.gnodes_to_bnodes
-
-        nodes_number = self.nodes_number
-
-        n = self.bnd_nodes_number
-        B = np.zeros((n,n))
-
-        tmp_bele = np.zeros(3)
-
-        # Progressbar..
-        loops = (nodes_number - sum(g2b<0))*len(bfn) + mesh.num_cells()*4
-        loop_ctr = 0
-        bar = pb.ProgressBar(maxval=loops, \
-                widgets=[pb.ETA(), pb.Bar('=', '[', ']'), ' ', pb.Percentage()])
-        logger.info("Building Boundary Element Matrix")
-
-        for i in range(nodes_number):
-            #skip the node not at the boundary
-            if g2b[i]<0:
-                continue
-
-            for j in range(self.bnd_faces_number):
-
-                # Progressbar data
-                loop_ctr += 1
-                bar.update(loop_ctr)
-
-                #skip the node in the face
-                if i in set(bfn[j]):
-                    continue
-
-                compute_belement(
-                    xyz[i],
-                    xyz[bfn[j][0]],
-                    xyz[bfn[j][1]],
-                    xyz[bfn[j][2]],
-                tmp_bele)
-
-                for k in range(3):
-                    ti=g2b[i]
-                    tj=g2b[bfn[j][k]]
-                    B[ti][tj]+=tmp_bele[k]
-
-
-        #the solid angle term ...
-        solid_angle = np.zeros(nodes_number)
-
-        cells = mesh.cells()
-        for i in range(mesh.num_cells()):
-            for j in range(4):
-                omega = compute_solid_angle(
-                    xyz[cells[i][j]],
-                    xyz[cells[i][(j+1)%4]],
-                    xyz[cells[i][(j+2)%4]],
-                    xyz[cells[i][(j+3)%4]])
-
-                solid_angle[cells[i][j]] += omega
-
-                # Progressbar data
-                loop_ctr += 1
-                bar.update(loop_ctr)
-
-        for i in range(nodes_number):
-            j = g2b[i]
-            if j < 0:
-                continue
-
-            B[j][j] += solid_angle[i]/(4*np.pi) - 1
-
-        return B
-
 if __name__ == "__main__":
 
     from finmag.demag.problems.prob_fembem_testcases import MagSphere
@@ -351,8 +377,8 @@ if __name__ == "__main__":
     problem.Ms = Ms
     problem.M = m
 
-    demag = FemBemFKSolverTrunc2(problem)
+    demag = FemBemFKSolver(problem)
     print demag.compute_field()
-    #demag = FemBemFKSolver(problem)
+    #demag = FemBemFKSolverOld(problem)
     #phi = demag.solve()
     #print demag.get_demagfield(phi).vector().array()
