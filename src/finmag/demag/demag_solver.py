06526c9ba5a9bde0a8782bade46c1087f556ac58
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-17 09:53:06 +0100

    Timings and small change to the temporary link solver.

--- src/finmag/demag/demag_solver.py
+++ src/finmag/demag/demag_solver.py
@@ -5,13 +5,14 @@ from finmag.demag.solver_gcr import FemBemGCRSolver
 from finmag.util.timings import timings
 
 class Demag(object):
-    def __init__(self, V, m, Ms, method="GCR"):
+    def __init__(self, V, m, Ms, method="FK"):
         timings.start("Demag-init")
         self.V = V
         mesh = V.mesh()
         self.Ms = Ms
         timings.startnext("Demag-init-PBProblem")
         self.problem = FBProblem(mesh, m)
+        self.problem.Ms = Ms
         timings.startnext("Demag-init-FemBemConstructorCall")
         if method == "FK":
             self.solver = FemBemFKSolver(self.problem)
@@ -21,14 +22,17 @@ class Demag(object):
             raise NotImplementedError("""Only methods currently implemented are
                                     * 'FK'
                                     * 'GCR'""")
-
+        self.method = method
         timings.stop("Demag-init-FemBemConstructorCall")
 
     def compute_field(self):
-        timings.start("Demag-computephi")
+        #timings.start("Demag-computephi")
         phi = self.solver.solve()
-        timings.startnext("Demag-computefield")
-        demag_field = df.project(-df.grad(phi), self.V)
-        H = demag_field.vector().array()*self.Ms
-        timings.stop("Demag-computefield")
-        return H
+        #timings.startnext("Demag-computefield")
+        timings.start("Project demag field")
+        demag_field = df.project(-df.grad(phi), self.V).vector().array()
+        timings.stop("Project demag field")
+        if self.method == "GCR":
+            demag_field *= self.Ms
+        #timings.stop("Demag-computefield")
+        return demag_field
