e453bc4a93f4e00bdce670401f8b61ec89b031fe
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-19 10:21:13 +0100

    Added a short script that demonstrates use of array after assemble.

---
+++ devtests/dolfin/assemble_array.py
@@ -0,0 +1,38 @@
+from dolfin import *
+
+mesh = UnitCube(30,30,30)
+V = FunctionSpace(mesh, "CG", 1)
+v = TestFunction(V)
+F = Function(V)
+
+b = v*dx
+L = assemble(b)
+
+print \
+"""
+In this test case, F is a dolfin.Function, and L is computed as
+
+\tL = assemble(v*dx),
+
+where v is a dolfin.TestFunction. Say we want to divide F by L.
+It we simply write
+
+\tA1 = F.vector().array()/L.
+
+This has proved to be much slower than
+
+\tA2 = F.vector().array()/L.array().
+
+Timings that computes A1 and A2 on a 30 x 30 x 30 unit cube
+20 times each, show the issue:
+"""
+
+tic()
+for i in range(20):
+    A1 = F.vector().array()/L
+print "A1: %.4f sec." % toc()
+
+tic()
+for i in range(20):
+    A2 = F.vector().array()/L.array()
+print "A2: %.4f sec." % toc()
