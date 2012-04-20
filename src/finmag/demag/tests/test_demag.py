53fcbf1a5bed0bd2f3ff96a9c7ef7e6862a2dfd6
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-20 09:55:34 +0100

    Added the now working FK solver (dolfin version) to the test file.

--- src/finmag/demag/tests/test_demag.py
+++ src/finmag/demag/tests/test_demag.py
@@ -2,23 +2,33 @@ import pytest
 import dolfin as df
 import numpy as np
 from finmag.demag.solver_fk_test import SimpleFKSolver
+from finmag.demag.solver_fk import FemBemFKSolver as FKSolver
 from finmag.demag.problems.prob_fembem_testcases import MagSphere
 
 # Can get smaller tolerance if we use finer mesh, but that will
 # delay the jenkins build with minutes.
-TOL = 5e-3
+TOL = 1e-2
 
 # Feel free to add more (interesting) problems
-problems = [MagSphere(5, 0.75), MagSphere(1, 0.15)]
+# NOTE: Should increase the number of vertices
+# when the lindholm formulation makes the build
+# of the boundary element matrix faster.
+problems = [MagSphere(5, 1), MagSphere(1, 0.25)]
 
 # Please feel free to also add more (working) solvers...
-solvers = [SimpleFKSolver]
+solvers = [SimpleFKSolver, FKSolver]
 
 fields = []
 cases = []
 for problem in problems:
     for solver in solvers:
-        case = solver(problem.V, problem.m, problem.Ms)
+        if solver.__name__ == "SimpleFKSolver":
+            case = solver(problem.V, problem.m, problem.Ms)
+        else:
+            # Temporary change because GB uses DG and not CG
+            # in solver base
+            problem.M = problem.m
+            case = solver(problem)
         grad = case.compute_field()
         cases.append(case)
         fields.append(grad)
@@ -54,7 +64,7 @@ def test_x_deviation(case, grad):
     grad = np.hsplit(grad, 3)
     x = grad[0]
     dev = abs(max(x) - min(x))
-    
+
     print "Max deviation in x-direction should be zero, is %g." % dev
     assert dev < TOL
 
@@ -67,14 +77,14 @@ def test_norm_x(case, grad):
 
     print "L2 norm of the x-component + 1/3 Ms should be zero. Is %g." % normx
     # This test needs a higher tolerance to pass.
-    assert normx < TOL*14
+    assert normx < TOL*10
 
 @pytest.mark.parametrize(("case", "grad"), zip(cases, fields))
 def test_avg_x(case, grad):
     """Average of the x-components should be 1/3 of Ms."""
     grad = np.hsplit(grad, 3)
-    x = grad[0] 
-    avg = np.average(x) 
+    x = grad[0]
+    avg = np.average(x)
 
     print "Average of x-component should be -%g. Is %g." % (1./3*case.Ms, avg)
     assert abs(avg+1./3*case.Ms) < TOL
@@ -84,7 +94,8 @@ if __name__ == '__main__':
     print ""
     i = 0
     for case, grad in zip(cases, fields):
-        print "Test case %d: %s" % (i+1, problems[i].desc())
+        print "Test case %d: %s" % (i+1, problems[i//len(solvers)].desc())
+        print "Solver: %s" % case.__class__.__name__
         print "Number of vertices:", case.V.mesh().num_vertices()
         test_one_third(case, grad)
         test_y_z_zero(case, grad)
