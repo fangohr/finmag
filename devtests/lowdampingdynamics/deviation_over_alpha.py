3d66db570b924c360f84e5e6b90624eb73672b94
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-04-21 12:53:54 +0100

    update old macrospin file and transform into test

--- devtests/lowdampingdynamics/deviation_over_alpha.py
+++ devtests/lowdampingdynamics/deviation_over_alpha.py
@@ -1,3 +1,4 @@
+import os
 import dolfin
 import numpy
 import matplotlib.pyplot as plt
@@ -7,62 +8,71 @@ from finmag.sim.tests.test_macrospin import make_analytical_solution
 from scipy.integrate import odeint
 
 """
-We gather the deviation between the analytical solution and the computed one
-for some values of the tolerance of the time integrator and several values
-for the damping term alpha.
+We gather the deviation between the analytical solution of the macrospin problem
+and the computed one for some values of the tolerance of the time integrator
+and an alpha ranging from 0.01 to 0.99.
 
 """
 
-rtols_powers_of_ten = [-7, -8, -9, -10, -11] # easier LaTeX formatting
-alphas = numpy.linspace(0.01, 0.99, 100)
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+TOLERANCE = 2e-6
 
+rtols_powers_of_ten = [-7, -8, -9, -10, -11] # easier LaTeX formatting
 mesh = dolfin.Box(0,1, 0,1, 0,1, 1,1,1)
 
-mean_deviationss = []
-for rtol_power_of_ten in rtols_powers_of_ten:
-    rtol = pow(10, rtol_power_of_ten)
-    print "#### New series for rtol={0}. ####".format(rtol)
+def test_deviations_over_alpha_and_tol(number_of_alphas=5, do_plot=False):
+    alphas = numpy.linspace(0.01, 0.99, number_of_alphas)
 
-    # One entry in this array corresponds to the average deviation between
-    # the analytical solution and the computed solution for one value of alpha.
-    mean_deviations = []
-    for alpha in alphas:
-        print "Solving for alpha={0}.".format(alpha)
+    max_deviationss = []
+    for rtol_power_of_ten in rtols_powers_of_ten:
+        rtol = pow(10, rtol_power_of_ten)
+        print "#### New series for rtol={0}. ####".format(rtol)
 
-        llg = LLG(mesh)
-        llg.alpha = alpha
-        llg.initial_M((llg.Ms, 0, 0))
-        llg.H_app = (0, 0, 1e5)
-        llg.setup(False)
+        # One entry in this array corresponds to the maximum deviation between
+        # the analytical solution and the computed solution for one value of alpha.
+        max_deviations = []
+        for alpha in alphas:
+            print "Solving for alpha={0}.".format(alpha)
 
-        M_analytical = make_analytical_solution(llg.Ms, 1e5, llg.alpha, llg.gamma) 
-    
-        ts = numpy.linspace(0, 1e-9, num=100)
-        ys = odeint(llg.solve_for, llg.M, ts, rtol=rtol)
+            llg = LLG(mesh)
+            llg.alpha = alpha
+            llg.set_m((1, 0, 0))
+            llg.H_app = (0, 0, 1e5)
+            llg.setup(use_exchange=False)
 
-        # One entry in this array corresponds to the deviation between the two
-        # solutions for one particular moment during the simulation.
-        deviations = []
-        for i in range(len(ts)):
-            M_computed = numpy.mean(ys[i].reshape((3, -1)), 1)
-            M_ref = M_analytical(ts[i])
-            # The difference of the two vectors has 3 components. The
-            # deviation is the average over these components.
-            deviations.append(numpy.mean(numpy.abs(M_computed - M_ref)))
+            M_analytical = make_analytical_solution(1, 1e5, llg.alpha, llg.gamma) 
+        
+            ts = numpy.linspace(0, 1e-9, num=50)
+            ys = odeint(llg.solve_for, llg.m, ts, rtol=rtol, atol=rtol)
 
-        # This represents the addition of one point to the graph.
-        mean_deviations.append(numpy.mean(deviations))
+            # One entry in this array corresponds to the deviation between the two
+            # solutions for one particular moment during the simulation.
+            deviations = []
+            for i in range(len(ts)):
+                M_computed = numpy.mean(ys[i].reshape((3, -1)), 1)
+                M_ref = M_analytical(ts[i])
+                # The difference of the two vectors has 3 components. The
+                # deviation is the average over these components.
+                deviation = numpy.mean(numpy.abs(M_computed - M_ref))
+                assert deviation < TOLERANCE
+                deviations.append(deviation)
 
-    # This represents one additional series in the graph.
-    mean_deviationss.append(mean_deviations)
+            # This represents the addition of one point to the graph.
+            max_deviations.append(numpy.max(deviations))
 
-for i in range(len(rtols_powers_of_ten)):
-    label = r"$rtol=1\cdot 10^{" + str(rtols_powers_of_ten[i]) + r"}$"
-    plt.plot(alphas, mean_deviationss[i], ".", label=label)
-plt.legend()
-plt.title(r"Influence of $\alpha$ and rtol on the Deviation")
-plt.ylabel("deviation")
-plt.xlabel(r"$\alpha$")
-plt.ylim((0, 0.1))
-plt.savefig("deviation_over_alpha_rtols.pdf")
+        # This represents one additional series in the graph.
+        max_deviationss.append(max_deviations)
 
+    if do_plot:
+        for i in range(len(rtols_powers_of_ten)):
+            label = r"$rtol=1\cdot 10^{" + str(rtols_powers_of_ten[i]) + r"}$"
+            plt.plot(alphas, max_deviationss[i], ".", label=label)
+        plt.legend()
+        plt.title(r"Influence of $\alpha$ and rtol on the Deviation")
+        plt.ylabel("deviation")
+        plt.xlabel(r"$\alpha$")
+        plt.ylim((0, 1e-6))
+        plt.savefig(MODULE_DIR+"/deviation_over_alpha_rtols.pdf")
+
+if __name__ == '__main__':
+    test_deviations_over_alpha_and_tol(100, do_plot=True)
