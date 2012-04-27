04634a455394134e976045ef1b825f08c26d2ff3
Anders Johansen <anders.e.johansen@gmail.com>
2012-04-27 12:18:57 +0100

    Split the demag example into two files.

---
+++ examples/demag/sphere1.geo
@@ -0,0 +1,8 @@
+algebraic3d
+
+solid main = sphere (0, 0, 0; 10)-maxh=1.1 ;
+
+tlo main;
+
+
+
---
+++ examples/demag/test_field.py
@@ -0,0 +1,50 @@
+from dolfin import Mesh
+from numpy import average
+from finmag.sim.llg import LLG
+from finmag.util.convert_mesh import convert_mesh
+
+TOL = 1e-3
+
+# Using mesh with radius 10 nm (nmag ex. 1)
+mesh = Mesh(convert_mesh("sphere1.geo"))
+mesh_units = 1e-9
+llg = LLG(mesh, mesh_units=mesh_units)
+llg.set_m((1, 0, 0))
+llg.Ms = 1e6
+llg.setup(use_demag=True)
+
+def test_field():
+    """
+    Test the demag field.
+
+    H_demag should be equal to -1/3 M, and with m = (1, 0 ,0)
+    and Ms = 1,this should give H_demag = (-1/3, 0, 0).
+
+    """
+    # Compute demag field
+    H_demag = llg.demag.compute_field()
+    H_demag.shape = (3, -1)
+    x, y, z = H_demag[0], H_demag[1], H_demag[2]
+
+    print "Max values in direction:"
+    print "x: %g,  y: %g,  z: %g" % (max(x), max(y), max(z))
+    print "Min values in direction:"
+    print "x: %g,  y: %g,  z: %g" % (min(x), min(y), min(z))
+
+    x, y, z = average(x), average(y), average(z)
+    print "Average values in direction"
+    print "x: %g,  y: %g,  z: %g" % (x, y, z)
+
+    # Compute relative erros
+    x = abs((x + 1./3*llg.Ms)/llg.Ms)
+    y = abs(y/llg.Ms)
+    z = abs(z/llg.Ms)
+
+    print "Relative error:"
+    print "x: %g,  y: %g,  z: %g" % (x, y, z)
+    assert x < TOL, "x-average is %g, should be -1/3." % x
+    assert y < TOL, "y-average is %g, should be zero." % y
+    assert z < TOL, "z-average is %g, should be zero." % z
+
+if __name__ == '__main__':
+    test_field()
