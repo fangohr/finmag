2508741a98dfda1dfe2da9f0f823edfb76c463c3
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-01 21:32:28 +0100

    exchange density finmag/nmag better plot, refactoring

--- devtests/energy_density/simple_1D_finmag.py
+++ devtests/energy_density/simple_1D_finmag.py
@@ -1,37 +1,51 @@
-from dolfin import *
+from dolfin import Interval
 from finmag.sim.llg import LLG
 import numpy as np
-import pylab as p
+import matplotlib.pylab as plt
 import commands
 
+# run nmag
 commands.getstatusoutput("nsim simple_1D_nmag.py --clean")
+nd = np.load("nmag_hansconf.npy")
 
+# run finmag
 mesh = Interval(100, 0, 10e-9)
 llg = LLG(mesh)
 llg.Ms = 1
 llg.set_m(("cos(x[0]*pi/10e-9)", "sin(x[0]*pi/10e-9)", "0"))
 llg.setup(use_exchange=True, use_dmi=False, use_demag=False)
+fd = llg.exchange.energy_density()
 
-finmag_data = llg.exchange.energy_density()
-nmag_data = np.load("nmag_hansconf.npy")
+# draw an ASCII table of the findings
+table_border    = "+" + "-" * 8 + "+" + "-" * 64 + "+"
+table_entries   = "| {:<6} | {:<20} {:<20} {:<20} |"
+table_entries_f = "| {:<6} | {:<20.8f} {:<20.8f} {:<20g} |"
+print table_border
+print table_entries.format(" ", "min", "max", "delta")
+print table_border
+print table_entries_f.format("finmag", min(fd), max(fd), max(fd)-min(fd))
+print table_entries_f.format("nmag", min(nd), max(nd), max(nd)-min(nd))
+print table_border
 
+# draw a plot of the two exchange energy densities
+xs = mesh.coordinates().flatten()
+figure, (upper_axis, lower_axis) = plt.subplots(2, 1, sharex=True)
 
-print "Finmag:"
-print finmag_data
-print "Nmag:"
-print nmag_data
+upper_axis.plot(xs, fd, "b-", label="finmag")
+lower_axis.plot(xs, nd, "r-", label="nmag")
+upper_axis.legend().draw_frame(False)
+lower_axis.legend().draw_frame(False)
 
-print "Finmag max-min:", max(finmag_data) - min(finmag_data)
-print "Nmag max-min:", max(nmag_data) - min(nmag_data)
+EPSILON = 1e-6 # to make sure the same zoom is used on both parts of the figure
+upper_axis.set_ylim(np.mean(fd) - EPSILON, np.mean(fd) + EPSILON)
+lower_axis.set_ylim(np.mean(nd) - EPSILON, np.mean(nd) + EPSILON)
 
-x = np.linspace(0, 10e-9, 101)
-p.plot(x, finmag_data, x, nmag_data)
-p.legend(["Finmag", "Nmag"])
-p.figure()
-p.plot(x, finmag_data)
-p.legend(["Finmag"])
-p.figure()
-p.plot(x, nmag_data, label="Nmag")
-p.legend(["Nmag"])
-p.show()
+# make the plot prettier
+# from https://github.com/matplotlib/matplotlib/blob/master/examples/pylab_examples/broken_axis.py
+upper_axis.spines['bottom'].set_visible(False)
+lower_axis.spines['top'].set_visible(False)
+upper_axis.xaxis.tick_top()
+upper_axis.tick_params(labeltop="off")
+lower_axis.xaxis.tick_bottom()
 
+plt.show()
