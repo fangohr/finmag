9b95455e3e4087aaaf54eb00deb222772291a63c
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-01 18:42:29 +0100

    add oommf results for energy density to plots comparing finmag and nmag

--- devtests/energy_density/run_finmag.py
+++ devtests/energy_density/run_finmag.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pylab as p
 from dolfin import Mesh
 from finmag.sim.llg import LLG
@@ -44,19 +45,24 @@ for i in R:
 nmag_exch = [float(i) for i in open("nmag_exch_Edensity.txt", "r").read().split()]
 nmag_demag = [float(i) for i in open("nmag_demag_Edensity.txt", "r").read().split()]
 
+# Read oommf data
+oommf_exch = np.genfromtxt("oommf_exch_Edensity.txt")
+oommf_demag = np.genfromtxt("oommf_demag_Edensity.txt")
+oommf_coords = np.genfromtxt("oommf_coords_z_axis.txt") * 1e9
+
 # Plot exchange energy density
-p.plot(R, finmag_exch, 'o-', R, nmag_exch, 'x-')
+p.plot(R, finmag_exch, 'o-', R, nmag_exch, 'x-', oommf_coords, oommf_exch, "+-")
 p.xlabel("nm")
 p.title("Exchange energy density")
-p.legend(["Finmag", "Nmag"])
+p.legend(["Finmag", "Nmag", "oommf"])
 p.savefig("exch.png")
 
 # Plot demag energy density
 p.figure()
-p.plot(R, finmag_demag, 'o-', R, nmag_demag, 'x-')
+p.plot(R, finmag_demag, 'o-', R, nmag_demag, 'x-', oommf_coords, oommf_demag, "+-")
 p.xlabel("nm")
 p.title("Demag energy density")
-p.legend(["Finmag", "Nmag"])
+p.legend(["Finmag", "Nmag", "oommf"])
 p.savefig("demag.png")
 
 p.show()
