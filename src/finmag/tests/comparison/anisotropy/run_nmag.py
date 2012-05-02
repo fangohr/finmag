3f010a6f017a306499de8a3cfd2c277854443406
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-02 18:05:20 +0100

    table for anisotropy from live data as well

--- src/finmag/tests/comparison/anisotropy/run_nmag.py
+++ src/finmag/tests/comparison/anisotropy/run_nmag.py
@@ -6,16 +6,13 @@ from nmag import SI
 from test_anisotropy_field import m_gen
 
 # Create the material
-mat_Py = nmag.MagMaterial(name="Py",
+mat_Py = nmag.MagMaterial(
+    name="Py",
     Ms=SI(0.86e6, "A/m"),
-    exchange_coupling=SI(13.0e-12, "J/m"),
-    llg_gamma_G=SI(0.2211e6, "m/A s"),
-    anisotropy=nmag.uniaxial_anisotropy(axis=[1, 0, 0], K1=SI(520e3, "J/m^3")),
-    llg_damping=SI(0.2),
-    llg_normalisationfactor=SI(0.001e12, "1/s"))
+    anisotropy=nmag.uniaxial_anisotropy(axis=[1, 0, 0], K1=SI(520e3, "J/m^3")))
 
 # Create the simulation object
-sim = nmag.Simulation("1d", do_demag=False)
+sim = nmag.Simulation("anis", do_demag=False)
 
 # Load the mesh
 sim.load_mesh("bar_5_5_5.nmesh.h5", [("Py", mat_Py)], unit_length=SI(1e-9, "m"))
--- src/finmag/tests/comparison/anisotropy/test_anisotropy_field.py
+++ src/finmag/tests/comparison/anisotropy/test_anisotropy_field.py
@@ -3,13 +3,15 @@ import dolfin as df
 import numpy as np
 from finmag.util.convert_mesh import convert_mesh
 from finmag.sim.llg import LLG
-from finmag.sim.helpers import quiver, vectors, norm, stats
+from finmag.sim.helpers import vectors, norm, stats, sphinx_sci as s
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
 
 Ms = 0.86e6; K1 = 520e3; a = (1, 0, 0);
-
 x1 = y1 = z1 = 5; # same as in bar_5_5_5.geo file
+
+table_delim   = "    " + "=" * 10 + (" " + "=" * 30) * 4 + "\n"
+table_entries = "    {:<10} {:<30} {:<30} {:<30} {:<30}\n"
 
 def m_gen(r):
     x = np.maximum(np.minimum(r[0]/x1, 1.0), 0.0) # x, y and z as a fraction
@@ -20,87 +22,112 @@ def m_gen(r):
     my = np.sqrt(1 - mx**2 - mz**2)
     return np.array([mx, my, mz])
 
-class TestAnisotropy():
-    def setup_class(self, plot=False):
-        mesh = df.Mesh(convert_mesh(MODULE_DIR + "/bar_5_5_5.geo"))
-        llg = LLG(mesh, mesh_units=1e-9)
-        coords = np.array(zip(* mesh.coordinates()))
-        m0 = m_gen(coords).flatten()
-        if plot:
-            quiver(m0, mesh, MODULE_DIR + "m0_finmag.png")
+def start_table():
+    table  = ".. _anis_table:\n\n"
+    table += ".. table:: Comparison of the anisotropy field computed with finmag against nmag, oommf and magpar\n\n"
+    table += table_delim 
+    table += table_entries.format(
+        ":math:`\,`", # hack because sphinx light table syntax does not allow an empty header
+        ":math:`\\subn{\\Delta}{test}`",
+        ":math:`\\subn{\\Delta}{max}`",
+        ":math:`\\bar{\\Delta}`",
+        ":math:`\\sigma`")
+    table += table_delim
+    return table
 
-        llg.set_m(m0)
-        llg.Ms = Ms
-        llg.add_uniaxial_anisotropy(K1, df.Constant(a))
-        llg.setup(use_exchange=False)
+def setup_finmag():
+    mesh = df.Mesh(convert_mesh(MODULE_DIR + "/bar_5_5_5.geo"))
+    llg = LLG(mesh, mesh_units=1e-9)
+    coords = np.array(zip(* mesh.coordinates()))
+    m0 = m_gen(coords).flatten()
 
-        self.mesh = mesh
-        self.V = llg.V
-        self.m = llg.m
-        self._m = llg._m
-        H_anis = df.Function(llg.V)
-        H_anis.vector()[:] = llg._anisotropies[0].compute_field()
-        if plot:
-            quiver(H_anis.vector().array(), mesh, MODULE_DIR + "anis_finmag.png")
-        self.H_anis = H_anis
+    llg.set_m(m0)
+    llg.Ms = Ms
+    llg.add_uniaxial_anisotropy(K1, df.Constant(a))
+    llg.setup(use_exchange=False)
 
-    def test_nmag(self):
-        REL_TOLERANCE = 0.4 # ???
+    H_anis = df.Function(llg.V)
+    H_anis.vector()[:] = llg._anisotropies[0].compute_field()
+    return dict(m=llg.m, H=H_anis, table=start_table(), llg=llg)
 
-        m_ref = np.genfromtxt(MODULE_DIR + "m0_nmag.txt")
-        m_computed = vectors(self.m)
-        assert m_ref.shape == m_computed.shape
+def teardown_finmag(finmag):
+    finmag["table"] += table_delim
+    with open(MODULE_DIR + "table.rst", "w") as f:
+        f.write(finmag["table"])
 
-        H_ref = np.genfromtxt(MODULE_DIR + "H_anis_nmag.txt")
-        H_computed = vectors(self.H_anis.vector().array())
-        assert H_ref.shape == H_computed.shape
+def pytest_funcarg__finmag(request):
+    finmag = request.cached_setup(setup=setup_finmag,
+            teardown=teardown_finmag, scope="module")
+    return finmag
 
-        assert m_ref.shape == H_ref.shape
-        m_cross_H_ref = np.cross(m_ref, H_ref)
-        m_cross_H_computed = np.cross(m_computed, H_computed)
+def test_nmag(finmag):
+    REL_TOLERANCE = 0.4 # ???
+
+    m_ref = np.genfromtxt(MODULE_DIR + "m0_nmag.txt")
+    m_computed = vectors(finmag["m"])
+    assert m_ref.shape == m_computed.shape
+
+    H_ref = np.genfromtxt(MODULE_DIR + "H_anis_nmag.txt")
+    H_computed = vectors(finmag["H"].vector().array())
+    assert H_ref.shape == H_computed.shape
+
+    assert m_ref.shape == H_ref.shape
+    m_cross_H_ref = np.cross(m_ref, H_ref)
+    m_cross_H_computed = np.cross(m_computed, H_computed)
 
 
-        diff = np.abs(m_cross_H_ref - m_cross_H_computed)
-        rel_diff = diff/max([norm(v) for v in m_cross_H_ref])
-      
-        print "comparison with nmag, m x H, relative difference:"
-        print stats(rel_diff)
-        assert np.max(rel_diff) < REL_TOLERANCE
+    diff = np.abs(m_cross_H_ref - m_cross_H_computed)
+    rel_diff = diff/max([norm(v) for v in m_cross_H_ref])
 
-    def test_oommf(self):
-        from finmag.util.oommf import mesh, oommf_uniaxial_anisotropy
-        from finmag.util.oommf.comparison import oommf_m0, finmag_to_oommf
+    finmag["table"] += table_entries.format(
+        "nmag", s(REL_TOLERANCE, 0), s(np.max(rel_diff)), s(np.mean(rel_diff)), s(np.std(rel_diff)))
 
-        REL_TOLERANCE = 2e-5
+    print "comparison with nmag, m x H, relative difference:"
+    print stats(rel_diff)
+    assert np.max(rel_diff) < REL_TOLERANCE
 
-        oommf_mesh = mesh.Mesh((2, 2, 2), size=(5e-9, 5e-9, 5e-9))
-        oommf_anis  = oommf_uniaxial_anisotropy(oommf_m0(lambda r: m_gen(np.array(r)*1e9), oommf_mesh), Ms, K1, a).flat
-        finmag_anis = finmag_to_oommf(self.H_anis, oommf_mesh, dims=3)
+def test_oommf(finmag):
+    from finmag.util.oommf import mesh, oommf_uniaxial_anisotropy
+    from finmag.util.oommf.comparison import oommf_m0, finmag_to_oommf
 
-        assert oommf_anis.shape == finmag_anis.shape
-        diff = np.abs(oommf_anis - finmag_anis)
-        rel_diff = diff / np.max(oommf_anis[0]**2 + oommf_anis[1]**2 + oommf_anis[2]**2)
+    REL_TOLERANCE = 2e-5
 
-        print "comparison with oommf, H, relative_difference:"
-        print stats(rel_diff)
-        assert np.max(rel_diff) < REL_TOLERANCE
+    oommf_mesh = mesh.Mesh((2, 2, 2), size=(5e-9, 5e-9, 5e-9))
+    oommf_anis  = oommf_uniaxial_anisotropy(oommf_m0(lambda r: m_gen(np.array(r)*1e9), oommf_mesh), Ms, K1, a).flat
+    finmag_anis = finmag_to_oommf(finmag["H"], oommf_mesh, dims=3)
 
-    def test_magpar(self):
-        from finmag.tests.magpar.magpar import compute_anis_magpar, compare_field_directly
+    assert oommf_anis.shape == finmag_anis.shape
+    diff = np.abs(oommf_anis - finmag_anis)
+    rel_diff = diff / np.max(oommf_anis[0]**2 + oommf_anis[1]**2 + oommf_anis[2]**2)
 
-        REL_TOLERANCE = 8e-6
+    finmag["table"] += table_entries.format(
+        "oommf", s(REL_TOLERANCE, 0), s(np.max(rel_diff)), s(np.mean(rel_diff)), s(np.std(rel_diff)))
 
-        magpar_nodes, magpar_anis = compute_anis_magpar(self.V, self._m, K1, a, Ms, mesh_units=1)
-        _, _, diff, rel_diff = compare_field_directly(
-                self.mesh.coordinates(), self.H_anis.vector().array(),
-                magpar_nodes, magpar_anis)
-        print "comparison with magpar, H, relative_difference:"
-        print stats(rel_diff)
-        assert np.max(rel_diff) < REL_TOLERANCE
+    print "comparison with oommf, H, relative_difference:"
+    print stats(rel_diff)
+    assert np.max(rel_diff) < REL_TOLERANCE
+
+def test_magpar(finmag):
+    from finmag.tests.magpar.magpar import compute_anis_magpar, compare_field_directly
+
+    REL_TOLERANCE = 8e-6
+
+    llg = finmag["llg"]
+    magpar_nodes, magpar_anis = compute_anis_magpar(llg.V, llg._m, K1, a, Ms, mesh_units=1)
+    _, _, diff, rel_diff = compare_field_directly(
+            llg.mesh.coordinates(), finmag["H"].vector().array(),
+            magpar_nodes, magpar_anis)
+
+    finmag["table"] += table_entries.format(
+        "magpar", s(REL_TOLERANCE, 0), s(np.max(rel_diff)), s(np.mean(rel_diff)), s(np.std(rel_diff)))
+    print "comparison with magpar, H, relative_difference:"
+
+    print stats(rel_diff)
+    assert np.max(rel_diff) < REL_TOLERANCE
 
 if __name__ == '__main__':
-    t = TestAnisotropy()
-    t.setup_class(plot=False)
-    t.test_nmag()
-    t.test_oommf()
-    t.test_magpar()
+    f = setup_finmag()
+    test_nmag(f)
+    test_oommf(f)
+    test_magpar(f)
+    teardown_finmag(f)
