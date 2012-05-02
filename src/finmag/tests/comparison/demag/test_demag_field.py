de472783c21a405b1a2457036ace1cc78cc01f03
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-02 22:40:31 +0100

    draw sphinx-table for comparison with magpar and compare magpar with analytical solution

--- src/finmag/tests/comparison/demag/test_demag_field.py
+++ src/finmag/tests/comparison/demag/test_demag_field.py
@@ -3,10 +3,13 @@ import numpy as np
 from dolfin import Mesh
 from finmag.sim.llg import LLG
 from finmag.util.convert_mesh import convert_mesh
-from finmag.sim.helpers import stats
+from finmag.sim.helpers import stats, sphinx_sci as s
 from finmag.tests.magpar.magpar import compare_field_directly, compute_demag_magpar
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+table_delim   = "    " + "=" * 10 + (" " + "=" * 30) * 4 + "\n"
+table_entries = "    {:<10} {:<30} {:<30} {:<30} {:<30}\n"
 
 def setup_finmag():
     mesh = Mesh(convert_mesh(MODULE_DIR + "sphere.geo"))
@@ -14,11 +17,25 @@ def setup_finmag():
     llg.set_m((1, 0, 0))
     llg.Ms = 1
     llg.setup(use_demag=True)
-    return dict(H=llg.demag.compute_field().reshape((3, -1)), llg=llg)
+    return dict(H=llg.demag.compute_field().reshape((3, -1)), llg=llg, table=start_table())
 
 def teardown_finmag(finmag):
-    pass
+    finmag["table"] += table_delim
+    with open(MODULE_DIR + "table.rst", "w") as f:
+        f.write(finmag["table"])
 
+def start_table():
+    table  = ".. _anis_table:\n\n"
+    table += ".. table:: Comparison of the anisotropy field computed with finmag against nmag, oommf and magpar\n\n"
+    table += table_delim 
+    table += table_entries.format(
+        ":math:`\,`", # hack because sphinx light table syntax does not allow an empty header
+        ":math:`\\subn{\\Delta}{test}`",
+        ":math:`\\subn{\\Delta}{max}`",
+        ":math:`\\bar{\\Delta}`",
+        ":math:`\\sigma`")
+    table += table_delim
+    return table
 def pytest_funcarg__finmag(request):
     finmag = request.cached_setup(setup=setup_finmag, teardown=teardown_finmag,
             scope="module")
@@ -34,6 +51,9 @@ def test_using_analytical_solution(finmag):
     diff = np.abs(finmag["H"] - H_ref)
     rel_diff = diff / np.sqrt(np.max(H_ref[0]**2 + H_ref[1]**2 + H_ref[2]**2))
 
+    finmag["table"] += table_entries.format(
+        "analytical", s(REL_TOLERANCE, 0), s(np.max(rel_diff)), s(np.mean(rel_diff)), s(np.std(rel_diff)))
+
     print "comparison with analytical results, H, relative_difference:"
     print stats(rel_diff)
     assert np.max(rel_diff) < REL_TOLERANCE
@@ -42,8 +62,12 @@ def test_using_nmag(finmag):
     REL_TOLERANCE = 3e-5
 
     H_ref = np.array(zip(* np.genfromtxt(MODULE_DIR + "H_demag_nmag.txt")))
+    assert H_ref.shape == finmag["H"].shape
     diff = np.abs(finmag["H"] - H_ref)
     rel_diff = diff / np.sqrt(np.max(H_ref[0]**2 + H_ref[1]**2 + H_ref[2]**2))
+
+    finmag["table"] += table_entries.format(
+        "nmag", s(REL_TOLERANCE, 0), s(np.max(rel_diff)), s(np.mean(rel_diff)), s(np.std(rel_diff)))
 
     print "comparison with nmag, H, relative_difference:"
     print stats(rel_diff)
@@ -58,8 +82,24 @@ def test_using_magpar(finmag):
             llg.mesh.coordinates(), finmag["H"].flatten(),
             magpar_nodes, magpar_H)
 
+    finmag["table"] += table_entries.format(
+        "magpar", s(REL_TOLERANCE, 0), s(np.max(rel_diff)), s(np.mean(rel_diff)), s(np.std(rel_diff)))
     print "comparison with magpar, H, relative_difference:"
     print stats(rel_diff)
+
+    # Compare magpar with analytical solution
+    H_magpar = magpar_H.reshape((3, -1))
+    H_ref = np.zeros(H_magpar.shape) 
+    H_ref[0] -= 1.0/3.0
+
+    magpar_diff = np.abs(H_magpar - H_ref)
+    magpar_rel_diff = magpar_diff / np.sqrt(np.max(H_ref[0]**2 + H_ref[1]**2 + H_ref[2]**2))
+
+    finmag["table"] += table_entries.format(
+        "magpar/an.", "", s(np.max(magpar_rel_diff)), s(np.mean(magpar_rel_diff)), s(np.std(magpar_rel_diff)))
+    print "comparison beetween magpar and analytical solution, H, relative_difference:"
+    print stats(magpar_rel_diff)
+
     assert np.max(rel_diff) < REL_TOLERANCE
 
 if __name__ == "__main__":
