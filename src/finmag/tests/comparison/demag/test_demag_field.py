fef169fc336168e244b8f3fe8870388fc9abc76a
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-02 22:18:21 +0100

    compare demag to magpar

--- src/finmag/tests/comparison/demag/test_demag_field.py
+++ src/finmag/tests/comparison/demag/test_demag_field.py
@@ -4,6 +4,7 @@ from dolfin import Mesh
 from finmag.sim.llg import LLG
 from finmag.util.convert_mesh import convert_mesh
 from finmag.sim.helpers import stats
+from finmag.tests.magpar.magpar import compare_field_directly, compute_demag_magpar
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
 
@@ -13,52 +14,63 @@ def setup_finmag():
     llg.set_m((1, 0, 0))
     llg.Ms = 1
     llg.setup(use_demag=True)
-    return llg.demag.compute_field()
+    return dict(H=llg.demag.compute_field().reshape((3, -1)), llg=llg)
 
-def teardown_finmag(H):
+def teardown_finmag(finmag):
     pass
 
-def pytest_funcarg__H(request):
-    H = request.cached_setup(setup=setup_finmag, teardown=teardown_finmag,
+def pytest_funcarg__finmag(request):
+    finmag = request.cached_setup(setup=setup_finmag, teardown=teardown_finmag,
             scope="module")
-    return H
+    return finmag
 
-def test_using_analytical_solution(H):
+def test_using_analytical_solution(finmag):
     """ Expecting (-1/3, 0, 0) as a result. """
-    REL_TOLERANCE = 1e-2
+    REL_TOLERANCE = 2e-2
 
-    H_ref = np.zeros(len(H)).reshape((3, -1)) 
+    H_ref = np.zeros(finmag["H"].shape) 
     H_ref[0] -= 1.0/3.0
-    H_ref = H_ref.flatten()
 
-    diff = np.abs(H - H_ref)
+    diff = np.abs(finmag["H"] - H_ref)
     rel_diff = diff / np.sqrt(np.max(H_ref[0]**2 + H_ref[1]**2 + H_ref[2]**2))
 
     print "comparison with analytical results, H, relative_difference:"
     print stats(rel_diff)
     assert np.max(rel_diff) < REL_TOLERANCE
 
-def test_using_nmag(H):
+def test_using_nmag(finmag):
     REL_TOLERANCE = 3e-5
 
-    H = H.reshape((3, -1))
     H_ref = np.array(zip(* np.genfromtxt(MODULE_DIR + "H_demag_nmag.txt")))
-
-    diff = np.abs(H - H_ref)
+    diff = np.abs(finmag["H"] - H_ref)
     rel_diff = diff / np.sqrt(np.max(H_ref[0]**2 + H_ref[1]**2 + H_ref[2]**2))
 
     print "comparison with nmag, H, relative_difference:"
     print stats(rel_diff)
     assert np.max(rel_diff) < REL_TOLERANCE
 
-if __name__ == "__main__":
-    H = setup_finmag()
+def test_using_magpar(finmag):
+    REL_TOLERANCE = 3e-1
 
-    Hx, Hy, Hz = H.reshape((3, -1))
+    llg = finmag["llg"]
+    magpar_nodes, magpar_H = compute_demag_magpar(llg.V, llg._m, llg.Ms)
+    _, _, diff, rel_diff = compare_field_directly(
+            llg.mesh.coordinates(), finmag["H"].flatten(),
+            magpar_nodes, magpar_H)
+
+    print "comparison with magpar, H, relative_difference:"
+    print stats(rel_diff)
+    assert np.max(rel_diff) < REL_TOLERANCE
+
+if __name__ == "__main__":
+    f = setup_finmag()
+
+    Hx, Hy, Hz = f["H"] 
     print "Expecting (Hx, Hy, Hz) = (-1/3, 0, 0)."
     print "demag field x-component:\n", stats(Hx)
     print "demag field y-component:\n", stats(Hy)
     print "demag field z-component:\n", stats(Hz)
 
-    test_using_analytical_solution(H)
-    test_using_nmag(H)
+    test_using_analytical_solution(f)
+    test_using_nmag(f)
+    test_using_magpar(f)
