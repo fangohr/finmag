First attempt at implementing the GCR method. The BEM computation seems correct but GCR produces incorrect results. Also, added a config option dolfin_log_level in [logging] to set the dolfin log level - set to WARN to get rid of the annoying "Solving linear variational problem" message

src/finmag/demag/tests/bem_computation_tests.py
@@ -3,11 +3,13 @@ import numpy as np
 import math
 import dolfin as df
 import os
-from finmag.native.llg import compute_bem_element, compute_bem, OrientedBoundaryMesh
+from finmag.native.llg import compute_lindholm_L, compute_lindholm_K, compute_bem_fk, compute_bem_gcr, OrientedBoundaryMesh
 from finmag.util import time_counter
 from finmag.sim import helpers
 from finmag.demag import belement_magpar
 from finmag.sim.llg import LLG
+from finmag.tests.solid_angle_invariance_tests import random_3d_rotation_matrix
+from finmag.demag.problems.prob_fembem_testcases import MagSphere
 
 compute_belement = belement_magpar.return_bele_magpar()
 
@@ -35,13 +37,18 @@ def compute_scalar_potential_llg(mesh, m_expr=df.Constant([1, 0, 0]), Ms=1.):
     normalise_phi(llg.demag.phi, mesh)
     return llg.demag.phi
 
+# Computes the derivative f'(x) using the finite difference approximation
+def differentiate_fd(f, x, eps=1e-4, offsets=(-2,-1,1,2), weights=(1./12.,-2./3.,2./3.,-1./12.)):
+    return sum(f(x+eps*dx)*weights[i] for i, dx in enumerate(offsets))/eps
+
 PHI1_SOLVER_PARAMS = PHI2_SOLVER_PARAMS = {
     "linear_solver": "gmres",
     "preconditioner": "ilu",
-    }
+}
 
+# Solves the demag problem for phi using the FK method and the native BEM matrix
 def compute_scalar_potential_native_fk(mesh, m_expr=df.Constant([1, 0, 0]), Ms=1.):
-# Set up the FE problems
+    # Set up the FE problems
     V_m = df.VectorFunctionSpace(mesh, 'Lagrange', 1, dim=3)
     V_phi = df.FunctionSpace(mesh, 'Lagrange', 1)
     u = df.TrialFunction(V_phi)
@@ -57,10 +64,14 @@ def compute_scalar_potential_native_fk(mesh, m_expr=df.Constant([1, 0, 0]), Ms=1
     # Solve the variational problem for phi1
     a = df.dot(df.grad(u), df.grad(v)) * df.dx
     L = -Ms * df.div(m) * v * df.dx + Ms * df.dot(n, m) * v * df.ds
+    # The problem for phi1 is underconstrained since the solution is defined up to a constant.
+    # However, the Krylov subspace methods are supposed to be regularising, therefore
+    # as long as we use a fixed initial guess, or renormalise the initial guess
+    # periodically during time integration, we should be fine
     df.solve(a == L, phi1, solver_parameters=PHI1_SOLVER_PARAMS)
     # Compute the BEM
     boundary_mesh = OrientedBoundaryMesh(mesh)
-    bem, b2g = compute_bem(boundary_mesh)
+    bem, b2g = compute_bem_fk(boundary_mesh)
     # Restrict phi1 to boundary
     phi1_boundary = phi1.vector().array()[b2g]
     # Compute phi2 on boundary using the BEM matrix
@@ -70,7 +81,51 @@ def compute_scalar_potential_native_fk(mesh, m_expr=df.Constant([1, 0, 0]), Ms=1
     # Solve the laplace equation for phi2
     a = df.dot(df.grad(u), df.grad(v)) * df.dx
     bc = df.DirichletBC(V_phi, phi2_bc, lambda x, on_boundary: on_boundary)
-    # Erm how do I solve the Laplace equation with
+    L = df.Constant(0) * v * df.dx
+    df.solve(a == L, phi2, bc, solver_parameters=PHI2_SOLVER_PARAMS)
+    # Add phi2 back to phi1
+    phi1.vector()[:] += phi2.vector().array()
+    normalise_phi(phi1, mesh)
+    return phi1
+
+# Solves the demag problem for phi using the GCR method and the native BEM matrix
+def compute_scalar_potential_native_gcr(mesh, m_expr=df.Constant([1, 0, 0]), Ms=1.):
+    # Set up the FE problems
+    V_m = df.VectorFunctionSpace(mesh, 'Lagrange', 1, dim=3)
+    V_phi = df.FunctionSpace(mesh, 'Lagrange', 1)
+    u = df.TrialFunction(V_phi)
+    v = df.TestFunction(V_phi)
+    n = df.FacetNormal(mesh)
+    fa = df.FacetArea(mesh)
+    m = df.interpolate(m_expr, V_m)
+    m.vector()[:] = helpers.fnormalise(m.vector().array())
+
+    phi1 = df.Function(V_phi)
+    phi2_bc = df.Function(V_phi)
+    phi2 = df.Function(V_phi)
+
+    # Solve the variational problem for phi1
+    a = df.dot(df.grad(u), df.grad(v)) * df.dx
+    L = -Ms * df.div(m) * v * df.dx
+    phi1_bc = df.DirichletBC(V_phi, df.Constant(0), lambda x, on_boundary: on_boundary)
+    df.solve(a == L, phi1, phi1_bc, solver_parameters=PHI1_SOLVER_PARAMS)
+    # Compute the BEM
+    boundary_mesh = OrientedBoundaryMesh(mesh)
+    bem, b2g = compute_bem_gcr(boundary_mesh)
+    # Compute the surface monopole density with something like the box method
+    # This is might be wildly inaccurate for geometries with corners,
+    # but what else can we do apart from making BEM depend both on M
+    # and phi thus using 4x as much RAM
+    # P.S. looks like this is inaccurate since this does not work...
+    # How can I test this?
+    q = df.assemble(Ms*df.dot(n, -m + df.grad(phi1))*v/fa*df.ds, mesh=mesh).array()
+    # Compute phi2 on boundary using the BEM matrix
+    phi2_boundary = np.dot(bem, q[b2g])
+    # Map phi2 back to global space
+    phi2_bc.vector()[b2g] = phi2_boundary
+    # Solve the laplace equation for phi2
+    a = df.dot(df.grad(u), df.grad(v)) * df.dx
+    bc = df.DirichletBC(V_phi, phi2_bc, lambda x, on_boundary: on_boundary)
     L = df.Constant(0) * v * df.dx
     df.solve(a == L, phi2, bc, solver_parameters=PHI2_SOLVER_PARAMS)
     # Add phi2 back to phi1
@@ -84,7 +139,7 @@ class BemComputationTests(unittest.TestCase):
         r2 = np.array([2., 1., 3.])
         r3 = np.array([5., 0., 1.])
         be_magpar = compute_belement_magpar(r1, r2, r3)
-        be_native = compute_bem_element(r1, r2, r3)
+        be_native = compute_lindholm_L(np.zeros(3), r1, r2, r3)
         print "Magpar: ", be_magpar
         print "Native C++: ", be_native
         self.assertAlmostEqual(np.max(np.abs(be_magpar - be_native)), 0, delta=1e-12)
@@ -109,7 +164,7 @@ class BemComputationTests(unittest.TestCase):
         llg.setup(use_demag=True)
         bem_finmag = llg.demag.bem
         bem_native = np.zeros(bem_finmag.shape)
-        bem, b2g = compute_bem(OrientedBoundaryMesh(mesh))
+        bem, b2g = compute_bem_fk(OrientedBoundaryMesh(mesh))
         g2finmag = llg.demag.gnodes_to_bnodes
         for i_dolfin in xrange(bem.shape[0]):
             i_finmag = g2finmag[b2g[i_dolfin]]
@@ -140,26 +195,91 @@ class BemComputationTests(unittest.TestCase):
         print "Boundary mesh computation for %s: %s" % (mesh, c)
         c = time_counter.counter()
         while c.next():
-            bem, _ = compute_bem(boundary_mesh)
+            bem, _ = compute_bem_fk(boundary_mesh)
             n = bem.shape[0]
-        print "BEM computation for %dx%d (%.2f Mnodes/sec): %s" % (n, n, c.calls_per_sec(n*n/1e6), c)
+        print "FK BEM computation for %dx%d (%.2f Mnodes/sec): %s" % (n, n, c.calls_per_sec(n*n/1e6), c)
+        c = time_counter.counter()
+        while c.next():
+            bem, _ = compute_bem_gcr(boundary_mesh)
+        print "GCR BEM computation for %dx%d (%.2f Mnodes/sec): %s" % (n, n, c.calls_per_sec(n*n/1e6), c)
 
     def test_bem_netgen(self):
         module_dir = os.path.dirname(os.path.abspath(__file__))
         netgen_mesh = df.Mesh(os.path.join(module_dir, "bem_netgen_test_mesh.xml.gz"))
-        bem, b2g_map = compute_bem(OrientedBoundaryMesh(netgen_mesh))
+        bem, b2g_map = compute_bem_fk(OrientedBoundaryMesh(netgen_mesh))
 
-    def run_demag_computation_test(self, mesh, m_expr, compute_func, method_name, tol=1e-10):
-        phi_a = compute_scalar_potential_llg(mesh, m_expr)
-        phi_b = compute_scalar_potential_native_fk(mesh, m_expr)
+    def run_demag_computation_test(self, mesh, m_expr, compute_func, method_name, tol=1e-10, ref=compute_scalar_potential_llg):
+        phi_a = ref(mesh, m_expr)
+        phi_b = compute_func(mesh, m_expr)
         error = df.errornorm(phi_a, phi_b, mesh=mesh)
         message = "Method: %s, mesh: %s, m: %s, error: %8g" % (method_name, mesh, m_expr, error)
         print message
+#        print phi_a.vector().array()
+#        print phi_b.vector().array()
         self.assertAlmostEqual(error, 0, delta=tol, msg="Error is above threshold %g, %s" % (tol, message))
 
-    def test_compute_phi_fk(self):
+    def test_compute_scalar_potential_fk(self):
         m1 = df.Constant([1, 0, 0])
         m2 = df.Expression(["x[0]*x[1]+3", "x[2]+5", "x[1]+7"])
         for k in xrange(1,5+1):
             self.run_demag_computation_test(df.UnitSphere(k), m1, compute_scalar_potential_native_fk, "native, FK")
             self.run_demag_computation_test(df.UnitSphere(k), m2, compute_scalar_potential_native_fk, "native, FK")
+        self.run_demag_computation_test(MagSphere(1, 0.25).mesh, m1, compute_scalar_potential_native_fk, "native, FK")
+        self.run_demag_computation_test(MagSphere(1, 0.25).mesh, m2, compute_scalar_potential_native_fk, "native, FK")
+
+    def atest_compute_scalar_potential_gcr(self):
+        m1 = df.Constant([1, 0, 0])
+        m2 = df.Expression(["x[0]*x[1]+3", "x[2]+5", "x[1]+7"])
+        self.run_demag_computation_test(MagSphere(1, 0.1).mesh, m1, compute_scalar_potential_native_gcr, "native, GCR", ref=compute_scalar_potential_native_fk)
+        for k in xrange(5,10+1):
+#            self.run_demag_computation_test(df.UnitCube(k,k,k), m1, compute_scalar_potential_native_gcr, "native, GCR", ref=compute_scalar_potential_native_fk)
+            self.run_demag_computation_test(df.UnitSphere(k), m1, compute_scalar_potential_native_gcr, "native, GCR", ref=compute_scalar_potential_native_fk)
+#            self.run_demag_computation_test(df.UnitSphere(k), m2, compute_scalar_potential_native_gcr, "native, GCR")
+
+    def run_symmetry_test(self, formula):
+        func = globals()[formula]
+        np.random.seed(1)
+        for i in xrange(100):
+            r = np.random.rand(3)*2-1
+            r1 = np.random.rand(3)*2-1
+            r2 = np.random.rand(3)*2-1
+            r3 = np.random.rand(3)*2-1
+            a = np.random.rand(3)*2-1
+            b = random_3d_rotation_matrix(1)[0]
+            # Compute the formula
+            v1 = func(r, r1, r2, r3)
+            # The formula is invariant under rotations via b
+            v2 = func(np.dot(b, r), np.dot(b, r1), np.dot(b, r2), np.dot(b, r3))
+            self.assertAlmostEqual(np.max(np.abs(v1 - v2)), 0)
+            # and under translations via a
+            v3 = func(r+a, r1+a, r2+a, r3+a)
+            self.assertAlmostEqual(np.max(np.abs(v1 - v3)), 0)
+            # and under permutations of r1-r2-r3
+            v4 = func(r, r2, r3, r1)
+            self.assertAlmostEqual(np.max(np.abs(v1[[1,2,0]] - v4)), 0)
+
+    def test_lindholm_L_symmetry(self):
+        # Lindholm formulas should be invariant under rotations and translations
+        self.run_symmetry_test("compute_lindholm_L")
+
+    def test_lindholm_K_symmetry(self):
+        self.run_symmetry_test("compute_lindholm_K")
+
+    def test_lindholm_derivative(self):
+        # the double layer potential is the derivative of the single layer potential
+        # with respect to displacements of the triangle in the normal direction
+        np.random.seed(1)
+        for i in xrange(100):
+            r = np.random.rand(3) * 2 - 1
+            r1 = np.random.rand(3) * 2 - 1
+            r2 = np.random.rand(3) * 2 - 1
+            r3 = np.random.rand(3) * 2 - 1
+            zeta  = np.cross(r2-r1, r3-r1)
+            zeta /= np.linalg.norm(zeta)
+            def f(x):
+                return compute_lindholm_K(r, r1+x*zeta, r2+x*zeta, r3+x*zeta)
+            L1 = compute_lindholm_L(r, r1, r2, r3)
+            L2 = differentiate_fd(f, 0)
+            self.assertAlmostEqual(np.max(np.abs(L1 - L2)), 0, delta=1e-10)
+
+