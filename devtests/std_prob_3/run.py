253f43393e69a66bc5efb3e1fb63fce82c3dddc8
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-07 19:25:21 +0100

    start Sim class and impl. of std. prob. 3 with it

---
+++ devtests/std_prob_3/run.py
@@ -0,0 +1,34 @@
+import numpy as np
+import dolfin as df
+
+from finmag.sim.sim import Sim
+from finmag.energies.anisotropy import UniaxialAnisotropy
+from finmag.energies.exchange import Exchange
+
+"""
+Micromag Standard Problem #3
+
+specification:
+    http://www.ctcms.nist.gov/~rdm/mumag.org.html
+solution with nmag:
+    http://magnonics.ex.ac.uk:3000/wiki/dynamag/Mumag3_nmag
+a good write-up (Rave 1998):
+    http://www.sciencedirect.com/science/article/pii/S030488539800328X
+
+"""
+
+mu0   = 4.0 * np.pi * 10**-7  # vacuum permeability             N/A^2
+Ms    = 1.0e6                 # saturation magnetisation        A/m
+A     = 13.0e-12              # exchange coupling strength      J/m
+Km    = 0.5 * mu0 * Ms**2     # magnetostatic energy density    kg/ms^2
+lexch = (A/Km)**0.5           # exchange length                 m
+K1    = 0.1 * Km
+
+for lfactor in range(1, 10):
+    L = lfactor * lexch       # cube length                     m
+    divisions = lfactor * 2
+    mesh = df.Box(0, 0, 0, L, L, L, divisions, divisions, divisions)
+
+    sim = Sim(mesh)
+    sim.add(UniaxialAnisotropy(K1, [0, 0, 1], Ms))
+    sim.add(Exchange(A, Ms))
---
+++ src/finmag/sim/sim.py
@@ -0,0 +1,44 @@
+import time
+import logging
+import dolfin as df
+import numpy as np
+from finmag.util.timings import timings
+
+log = logging.getLogger(name="finmag")
+
+class Sim(object):
+    def __init__(self, mesh, unit_length=1):
+        timings.reset()
+        timings.start("Sim-init")
+
+        log.info("Creating Sim object (rank={}/{}) [{}].".format(
+            df.MPI.process_number(), df.MPI.num_processes(), time.asctime()))
+        log.debug(mesh)
+
+        self.mesh = mesh
+        self.unit_length = unit_length
+        self.S1 = df.FunctionSpace(mesh, "Lagrange", 1)
+        self.S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1, dim=3)
+        self.Volume = df.assemble(df.Constant(1) * df.dx, mesh=mesh)
+        self._m = df.Function(self.S3)
+        self.t = 0
+
+        self.interactions = []
+
+        timings.stop("Sim-init")
+
+
+    def add(self, interaction):
+        interaction.setup(self.S3, self._m, self.unit_length)
+        self.interactions.append(interaction)
+
+    def compute_effective_field(self):
+        H_eff = np.array(self._m.shape)
+        
+        for interaction in self.interactions:
+            H_eff += interaction.compute_field(self.m, self.t)
+    
+        return H_eff
+
+    
+
