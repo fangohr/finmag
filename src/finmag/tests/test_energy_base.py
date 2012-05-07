aa915c736ec3790400208a3f67ae5c1e01a561b4
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-07 11:14:41 +0100

    abstract base class for energies, with implementation example as test

---
+++ src/finmag/tests/test_energy_base.py
@@ -0,0 +1,58 @@
+from finmag.energies.energy_base import EnergyBase
+
+"""
+Shows how to conform to the abstract base class
+for the energies through subclassing.
+
+"""
+
+class MyEnergy(EnergyBase):
+    def __init__(self, myConstant1, myConstant2):
+        """
+        The __init__ method is not mandated by
+        the abstract base class and can take
+        any parameters needed.
+
+        """
+        pass
+
+    def setup(self, S3, m, unit_length):
+        """
+        This method is required.
+
+        The setup will most likely be called
+        by the new Sim class. We should agree
+        on a common set of parameters for all
+        energies.
+
+        """
+        pass
+
+    def compute_field(self):
+        """
+        This method is required.
+
+        We could think about passing t to
+        compute_field for time-dependent
+        external fields, or m, if we want
+        to refactor the whole field computation
+        process.
+
+        """
+        pass
+
+    def compute_energy(self):
+        """
+        This method is required.
+
+        """
+        pass
+
+def test_my_energy():
+    try:
+        my_energy = MyEnergy(1, 2)
+        assert True
+    except Exception, ex:
+        print "Problem initialising MyEnergy."
+        print ex
+        assert False
