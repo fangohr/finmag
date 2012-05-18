6aa4cc587f1b9716954370559829057356edbd9b
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-18 15:55:58 +0100

    reorganise anisotropy tests
    ... and discover a surprising amount of bugs
    
    nmag bug with the max norm in the rel_diff calculation
    oommf forgot square root in rel_diff, wrong mesh size and
        coordinates in wrong units

---
+++ src/finmag/tests/comparison/anisotropy/test_anis_magpar.py
@@ -0,0 +1,24 @@
+import numpy as np
+import conftest as test
+import finmag.util.magpar as magpar
+from finmag.sim.helpers import stats
+
+REL_TOLERANCE = 5e-7
+
+def test_against_magpar(finmag):
+    magpar_nodes, magpar_anis = magpar.compute_anis_magpar(finmag["m"],
+            K1=test.K1, a=test.a, Ms=test.Ms)
+    _, _, diff, rel_diff = magpar.compare_field_directly(
+            finmag["S3"].mesh().coordinates(), finmag["H"].vector().array(),
+            magpar_nodes, magpar_anis)
+
+    print "comparison with magpar, H, relative_difference:"
+    print stats(rel_diff)
+
+    finmag["table"] += test.table_entry("magpar", REL_TOLERANCE, rel_diff)
+    assert np.max(rel_diff) < REL_TOLERANCE
+
+if __name__ == "__main__":
+    finmag = test.setup()
+    test_against_magpar(finmag)
+    test.teardown(finmag)
