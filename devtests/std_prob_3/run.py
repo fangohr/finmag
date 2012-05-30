daf6d4e4b0dc15d42907f6c548213c9b5c87a992
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-05-30 22:09:27 +0100

    [completes #28854153] finish mumag 3
    Add energies and average magnetisation.
    Good agreement with nmag!
    May still investigate how mesh fineness influences single domain limit, if wanted.

--- devtests/std_prob_3/run.py
+++ devtests/std_prob_3/run.py
@@ -1,3 +1,5 @@
+import os
+import time
 import logging
 import numpy as np
 import dolfin as df
@@ -7,6 +9,8 @@ from finmag.energies import UniaxialAnisotropy, Exchange, Demag
 
 log = logging.getLogger(name="finmag")
 log.setLevel(logging.INFO)
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
 
 """
 Micromag Standard Problem #3
@@ -43,28 +47,51 @@ def vortex_init(rs):
     m_phi = np.sin(2 * np.arctan(rho/b))
     return np.array([np.sqrt(1.0 - m_phi**2), m_phi*np.cos(phi), -m_phi*np.sin(phi)])
 
-def run_simulation(lfactor, m_init):
+def run_simulation(lfactor, m_init, m_init_name=""):
     L = lfactor * lexch
     divisions = int(round(lfactor * 1.5)) # that magic number influences L
     mesh = df.Box(0, 0, 0, L, L, L, divisions, divisions, divisions)
 
+    exchange = Exchange(A)
+    anisotropy = UniaxialAnisotropy(K1, [0, 0, 1])
+    demag = Demag()
+
     sim = Simulation(mesh, Ms)
     sim.set_m(m_init)
-    sim.add(Exchange(A))
-    sim.add(UniaxialAnisotropy(K1, [0, 0, 1]))
-    sim.add(Demag())
+    sim.add(exchange)
+    sim.add(anisotropy)
+    sim.add(demag)
     sim.relax()
 
-    total_energy_density = sim.total_energy() / sim.Volume
-    relative_total_energy_density = total_energy_density / Km
-    return relative_total_energy_density
+    # Save average magnetisation.
+    mx, my, mz = sim.llg.m_average
+    with open(MODULE_DIR + "data_m.txt", "a") as f:
+        t = time.asctime()
+        f.write("{} {} {} {} {} {}\n".format(m_init_name, lfactor, mx, my, mz, t))
+
+    # Save energies.
+    # We could call sim.total_energy, but we want the individual contributions.
+    e_exc   = exchange.compute_energy() / (sim.Volume * Km)
+    e_anis  = anisotropy.compute_energy() / (sim.Volume * Km)
+    e_demag = demag.compute_energy() / (sim.Volume * Km)
+    e_total = e_exc + e_anis + e_demag # relative total energy density
+
+    with open(MODULE_DIR + "data_energies.txt", "a") as f:
+        t = time.asctime()
+        f.write("{} {} {} {} {} {} {}\n".format(m_init_name,
+            lfactor, e_total, e_exc, e_anis, e_demag, t))
+
+    return e_total
 
 def energy_difference(lfactor):
     print "Running the two simulations for lfactor={}.".format(lfactor)
-    e_vortex = run_simulation(lfactor, vortex_init)
-    e_flower = run_simulation(lfactor, flower_init)
+    e_vortex = run_simulation(lfactor, vortex_init, "vortex")
+    e_flower = run_simulation(lfactor, flower_init, "flower")
     diff = e_vortex - e_flower
+    with open(MODULE_DIR + "data_diffs.txt", "a") as f:
+        t = time.asctime()
+        f.write("{} {} {} {} {}\n".format(lfactor, e_vortex, e_flower, diff, t))
     return diff
 
 single_domain_limit = bisect(energy_difference, 8, 8.5, xtol=0.1)
-print "L = " + single_domain_limit + "."
+print "L = " + str(single_domain_limit) + "."
