a0ca54bc18156bced301ed2d8d79297e47356016
Anders Johansen <anders.e.johansen@gmail.com>
2012-05-31 08:58:48 +0100

    Tried run to relax on the skyrmion timings.

--- devtests/skyrmions/skyrmion_timings.py
+++ devtests/skyrmions/skyrmion_timings.py
@@ -1,7 +1,7 @@
 import dolfin as df
 from finmag import Simulation
 from finmag.energies import Exchange, DMI
-
+output = open("timings.txt", "w")
 mesh = df.Box(0, 0, 0, 30e-9, 30e-9, 3e-9, 10, 10, 1)
 
 def run_sim(exc_jac, dmi_jac):
@@ -16,10 +16,17 @@ def run_sim(exc_jac, dmi_jac):
     dmi.in_jacobian = dmi_jac
     sim.add(dmi)
 
-    sim.run_until(1e-9)
-    print sim.timings(5)
+    #sim.run_until(1e-9)
+    sim.relax()
+    output.write(sim.timings(5))
 
+output.write("Neither in Jacobian:\n")
 run_sim(exc_jac=False, dmi_jac=False)
+#output.write("\nOnly DMI in Jacobian:\n")
 #run_sim(exc_jac=False, dmi_jac=True) # CVODE fails with CV_TOO_MUCH_WORK
+output.write("\nOnly Exchange in Jacobian:\n")
 run_sim(exc_jac=True, dmi_jac=False)
+output.write("\nBoth in Jacobian:\n")
 run_sim(exc_jac=True, dmi_jac=True)
+output.close()
+print "Timings written to timings.txt"
