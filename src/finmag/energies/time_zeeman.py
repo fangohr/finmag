a9d2b64537b221971f5649fdf62044f2f44c1e49
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-06-03 15:15:00 +0100

    Split external field classes into continuous and discrete updates.

--- src/finmag/energies/time_zeeman.py
+++ src/finmag/energies/time_zeeman.py
@@ -8,45 +8,21 @@ log = logging.getLogger("finmag")
 mu0 = 4 * np.pi * 1e-7
 
 class TimeZeeman(EnergyBase):
-    def __init__(self, expression, update_times):
+    def __init__(self, field_expression):
         self.in_jacobian = False
-
-        self.expression = expression
-        self.update_times = update_times
-        self.current_t_index = 0
-        self.switched_off = False
+        self.f_expr = field_expression
 
     def setup(self, S3, m, Ms, unit_length=1):
         self.m = m
         self.Ms = Ms
         self.S3 = S3
 
-        self.H = df.interpolate(self.expression, self.S3)
+        self.H = df.interpolate(self.f_expr, self.S3)
         self.E = - mu0 * self.Ms * df.dot(self.m, self.H) * df.dx
 
     def update(self, t):
-        current_t = self.update_times[self.current_t_index]
-        if t >= current_t:
-            self.expression.t = t
-            self.H = df.interpolate(self.expression, self.S3)
-            log.debug("At t={}, passed update value {} for H_ext(t). {} points remaining.".format(
-                t, current_t, len(self.update_times)- (self.current_t_index + 1)))
-            self.current_t_index += 1
-            if t > self.update_times[-1] or len(self.update_times) == 0:
-                self.switch_off()
-
-    def switch_off(self):
-        print "Switching off field."
-        """
-        Careful: Not reversible.
-
-        """
-        self.H = df.Function(self.S3)
-
-        def new_update(self, t):
-            pass # Just in case it does get called again.
-        self.update = new_update
-        self.switched_off = True
+        self.f_expr.t = t
+        self.H = df.interpolate(self.f_expr, self.S3)
 
     def compute_field(self):
         return self.H.vector().array()
@@ -54,6 +30,31 @@ class TimeZeeman(EnergyBase):
     def compute_energy(self):
         E = df.assemble(self.E)
         return E
+
+class DiscreteTimeZeeman(TimeZeeman):
+    def __init__(self, field_expression, t_off, dt_update):
+        super(DiscreteTimeZeeman, self).__init__(field_expression)
+        self.t_off = t_off
+        self.dt_update = dt_update
+        self.t_last_update = 0.0
+        self.switched_off = False
+
+    def update(self, t):
+        if not self.switched_off:
+            if t >= self.t_off:
+                self.switch_off()
+                return
+                
+            dt_since_last_update = t - self.t_last_update
+            if dt_since_last_update >= self.dt_update:
+                self.H = df.interpolate(self.f_expr, self.S3)
+                log.debug("At t={}, after dt={}, update external field again.".format(
+                    t, dt_since_last_update))
+
+    def switch_off(self):
+        log.debug("Switching external field off.")
+        self.H = df.Function(self.S3)
+        self.switched_off = True
 
         
 
