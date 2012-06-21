179bf9d1c83d7ab4951ff2fae01c93a5042f9ca6
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-06-21 23:23:47 +0100

    update test, extra parameter for oommf compare anisotropy

--- src/finmag/tests/oommf/convergence_anis_oommf.py
+++ src/finmag/tests/oommf/convergence_anis_oommf.py
@@ -26,7 +26,7 @@ def test_1d():
     for x_n in [1e1, 1e2, 1e3, 1e4, 1e5, 1e6]:
         dolfin_mesh = df.Interval(int(x_n), 0, x_max)
         oommf_mesh = mesh.Mesh((int(x_n), 1, 1), size=(x_max, 1e-12, 1e-12))
-        res = compare_anisotropy(m_gen, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, dims=1, name="1D")
+        res = compare_anisotropy(m_gen, 1.0, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, dims=1, name="1D")
 
         vertices[0].append(dolfin_mesh.num_vertices())  
         mean_rdiffs[0].append(np.mean(res["rel_diff"]))
@@ -39,33 +39,15 @@ def test_1d():
     popt, pcov = curve_fit(linear, xs, ys)
     assert popt[0] < - 1.0
 
-def do_3d():
-    print "3D problem..."
-    for x_n in [10, 100, 200, 300]:
-        y_max = z_max = 1e-9;
-        y_n = z_n = x_n/10;
-        dolfin_mesh = df.Box(0, 0, 0, x_max, y_max, z_max, x_n, y_n, z_n)
-        oommf_mesh = mesh.Mesh((x_n, y_n, z_n), size=(x_max, y_max, z_max))
-        res = compare_anisotropy(m_gen, K1, (1, 1, 1), dolfin_mesh, oommf_mesh, dims=3, name="3D")
-     
-        vertices[1].append(dolfin_mesh.num_vertices())  
-        mean_rdiffs[1].append(np.mean(res["rel_diff"]))
-        max_rdiffs[1].append(np.max(res["rel_diff"]))
-
 if __name__ == '__main__':
     test_1d()
-    do_3d()
 
     plt.xlabel("number of vertices")
     plt.ylabel("relative difference")
     plt.loglog(vertices[0], mean_rdiffs[0], "b--")
-    plt.loglog(vertices[0], mean_rdiffs[0], "bo", label="1d mean")
-    plt.loglog(vertices[1], mean_rdiffs[1], "r--")
-    plt.loglog(vertices[1], mean_rdiffs[1], "ro", label="3d mean")
-    #plt.loglog(vertices[0], max_rdiffs[0], "b-.")
-    #plt.loglog(vertices[0], max_rdiffs[0], "bs", label="1d max")
-    #plt.loglog(vertices[1], max_rdiffs[1], "r-.")
-    #plt.loglog(vertices[1], max_rdiffs[1], "rs", label="3d max")
+    plt.loglog(vertices[0], mean_rdiffs[0], "bo", label="average")
+    plt.loglog(vertices[0], max_rdiffs[0], "b-.")
+    plt.loglog(vertices[0], max_rdiffs[0], "bs", label="maximum")
     plt.legend()
     plt.savefig(MODULE_DIR + "anis_convergence.png")
     plt.show()
