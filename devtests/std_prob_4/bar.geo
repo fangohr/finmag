24d8d87c9c5c757046aa7784859aa3531faccbc6
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-06-21 23:15:34 +0100

    std. prob. 4 with graphs

---
+++ devtests/std_prob_4/bar.geo
@@ -0,0 +1,3 @@
+algebraic3d
+solid cube = orthobrick( 0, 0, 0 ; 500, 125, 3 ) -maxh = 5.0;
+tlo cube;
---
+++ devtests/std_prob_4/timeseries_to_vtk.py
@@ -0,0 +1,22 @@
+import os
+import dolfin as df
+from finmag.util.convert_mesh import convert_mesh
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+mesh_file = MODULE_DIR + "bar.geo"
+mesh = df.Mesh(convert_mesh(mesh_file))
+
+S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1)
+m = df.Function(S3)
+
+series = df.TimeSeries("solution/m")
+times = series.vector_times()
+
+vtk_files = df.File("vtk-files/m.pvd")
+for t in times:
+    vector = df.Vector()
+    series.retrieve(vector, t)
+    m.vector()[:] = vector
+    
+    vtk_files << m
