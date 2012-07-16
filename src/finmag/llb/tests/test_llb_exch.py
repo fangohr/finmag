f6ff5ba38c64f4498b462b61bc7b39da39bf6041
Hans Fangohr <fangohr@soton.ac.uk>
2012-07-16 20:53:27 +0100

    minor tidying up of tests

--- src/finmag/llb/tests/test_llb_exch.py
+++ src/finmag/llb/tests/test_llb_exch.py
@@ -3,11 +3,8 @@ import dolfin as df
 import numpy as np
 
 
-from finmag.llb.material import Material
-
-from finmag.energies.exchange import Exchange 
+from finmag.energies.exchange import Exchange
 import finmag.llb.exchange as llb
-
 
 
 class LLBTestExch(unittest.TestCase):
@@ -18,26 +15,12 @@ class LLBTestExch(unittest.TestCase):
         expr = df.Expression(('sin(x[0])', 'cos(x[0])'))
         Ms = 1
         M = df.project(expr, S3)
-    
         exch = Exchange(C)
         exch.setup(S3, M, Ms)
-        H1=exch.compute_field()
+        H1 = exch.compute_field()
 
-        llb_exch=llb.Exchange(C)
-        llb_exch.setup(S3, M, Ms,1)
-        H2=llb_exch.compute_field()
-        
-        assert(np.max(H2-H1)==0)
-        
-        
-        
-        
-        
-        
-        
-
-    
-
-
-
-            
+        llb_exch = llb.Exchange(C)
+        llb_exch.setup(S3, M, Ms, 1)
+        H2 = llb_exch.compute_field()
+        print("max(H1-H2)=%g" % np.max(H2 - H1))
+        assert(np.max(H2 - H1) == 0)
--- src/finmag/llb/tests/test_llb_sim.py
+++ src/finmag/llb/tests/test_llb_sim.py
@@ -5,39 +5,27 @@ import numpy as np
 
 from finmag.llb.material import Material
 import finmag.native.llb as native_llb
-from finmag.llb.llb import LLB
-
-
 
 
 class LLBSimTest(unittest.TestCase):
     def test_materials(self):
         mesh = df.UnitCube(1, 1, 1)
-        mat = Material(mesh,name='FePt')
+        mat = Material(mesh, name='FePt')
         mat.T = 600
-        
+
         print mat.T
         print mat.alpha
         print mat.inv_chi_par
-        print 'inv_chi_perp:',mat.inv_chi_perp
+        print 'inv_chi_perp:', mat.inv_chi_perp
         print mat.A
         print mat.m_e
         print mat.Ms0
         print mat.gamma_LL
-    
+
     def test_native_llb(self):
-        m=np.array([1.,2.,3.])
+        m = np.array([1., 2., 3.])
         native_llb.test_numpy(m)
         print m
-        
-    def test_exampe(self):
+
+    def test_example(self):
         pass
-        
-        
-        
-
-    
-
-
-
-            
