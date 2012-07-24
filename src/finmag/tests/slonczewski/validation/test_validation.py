b916b5f461574ec540f7639c0fd5501e13dfab73
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-07-24 12:36:25 +0100

    another disabled test for Slonczewski

---
+++ src/finmag/tests/slonczewski/validation/test_validation.py
@@ -0,0 +1,18 @@
+import os
+import run as sim
+import numpy as np
+from finmag.util import helpers
+
+epsilon = 1e-16
+tolerance = 1e-3
+nmag_file = sim.MODULE_DIR + "averages_nmag5.txt"
+
+def _test_oscillator():
+    sim.run_simulation()
+
+    averages = helpers.read_float_data(sim.averages_file)
+    nmag_avg = helpers.read_float_data(nmag_file)
+   
+    diff = np.abs(np.array(averages) - np.array(nmag_avg))
+    assert np.max(diff[:,0]) < epsilon # compare times
+    assert np.max(diff[:,1:]) < tolerance
