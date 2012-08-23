3c0f9da38b81684002e3c5555d2b89b56a4a09ea
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-08-23 23:15:41 +0100

    nit

--- src/finmag/util/consts.py
+++ src/finmag/util/consts.py
@@ -3,7 +3,7 @@ from math import pi
 #a collection of constants we use frequently.
 #(in SI units)
 
-mu0 = 4 * pi * 10 ** -7  # Vs/(Am)
+mu0 = 4 * pi * 1e-7  # Vs/(Am)
 k_B = 1.3806488e-23 # Boltzmann constant in J / K
 h_bar = 1.054571726e-34 # reduced Plank constant in Js
 e = 1.602176565e-19 # elementary charge in As
