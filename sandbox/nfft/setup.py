5671b26c96cd0f554c9774813a0e35d2cab26cef
Weiwei Wang <w.wang@soton.ac.uk>
2012-08-26 22:52:27 +0100

    better to submit, add treecode

--- sandbox/nfft/setup.py
+++ sandbox/nfft/setup.py
@@ -7,15 +7,14 @@ import os
 #python setup.py build_ext --inplace
 #NFFT_DIR = os.path.expanduser('~/nfft-3.2.0')
 
-# Install into /usr/local by default, if the environment variable NFFT_DIR is not set
-NFFT_DIR = os.environ.get('NFFT_DIR', '/usr/local')
 
 ext_modules = [
     Extension("demag_nfft_lib",
-              sources = ['demag_nfft.c','demag_nfft_lib.pyx'],
-              include_dirs = ['%s/include'%NFFT_DIR,numpy.get_include()],
-              library_dirs = ['%s/lib'%NFFT_DIR],
-              libraries=['m','fftw3','nfft3'],
+              sources = ['fast_sum.c','demag_nfft_lib.pyx'],
+              include_dirs = [numpy.get_include()],
+              libraries=['m'],
+              #extra_compile_args=["-g"],
+              #extra_link_args=["-g"],
         )
     ]
 
