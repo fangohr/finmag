d3b1eda33cffb49c297feeedc86f9fdc4c883c57
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-08-27 14:27:56 +0200

    reproducing paper: figure 5 done, found bug which misplaced spin currents

--- sandbox/interference_patterns/run.py
+++ sandbox/interference_patterns/run.py
@@ -4,7 +4,7 @@ import math
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from finmag import Simulation
-from finmag.util.consts import mu0, gamma
+from finmag.util.consts import mu0
 from finmag.energies import Zeeman, Exchange, ThinFilmDemag
 from point_contacts import point_contacts
 
@@ -26,6 +26,7 @@ def figure_5_6(mesh, m, p, t):
     print "Saved."
 
 alpha = 0.012 # dimensionless
+gamma = 2.210173e5 # m/(As), our value
 
 # Permalloy.
 Ms = 860e3 # A/m
@@ -43,40 +44,45 @@ dL = L/2; dW = W/2; dH = H/2;
 
 # Use exchange length to figure out discretisation.
 l_ex = np.sqrt(2 * A / (mu0 * Ms**2))
-print "The exchange length is l_ex = {:.2g} m.".format(l_ex) # >5 nm
+print "The exchange length is l_ex = {:.2} m.".format(l_ex) # >5 nm
 
 discretisation = math.floor(l_ex*1e9)*1e-9 
 nx, ny, nz = (L/discretisation, W/discretisation, 1)
 #mesh = df.Rectangle(-dL, -dW, dL, dW, int(nx), int(ny)
 mesh = df.Box(-dL, -dW, -dH, dL, dW, dH, int(nx), int(ny), int(nz))
 
-sim = Simulation(mesh, Ms)
-sim.set_m((0, 0, 1))
-sim.add(Zeeman((0, 0, 1.1 * Ms))) # section 3 and end of section 5
-sim.add(Exchange(A))
-sim.add(ThinFilmDemag())
+# p = (0, 0, 1) doesn't make a lot of sense here, just adding 
+# it to rule out errors in setting up the other params
+for p in [(1, 0, 0), (0, 1, 0), (1, 1, 0), (0, 0, 1)]:
+    sim = Simulation(mesh, Ms)
+    sim.set_m((0, 0, 1))
+    sim.add(Zeeman((0, 0, 1.1 * Ms))) # section 3 and end of section 5
+    sim.add(Exchange(A))
+    sim.add(ThinFilmDemag())
 
-# Spin-Torque
-I = 10e-3 # A
-point_contact_radius = 10e-9
-point_contact_area = math.pi * point_contact_radius ** 2
-J = I / point_contact_area
-print "Current density is J = {:.2g} A/m^2.".format(J)
-J_expr = point_contacts([(L/3, W/2), (2*L/3, W/2)],
-        radius=point_contact_radius, J=J)
-P = 0.4
-d = H
-p = (1, 0, 0)
-sim.set_stt(J_expr, P, d, p)
-pulse_time = 50e-12
-snapshot_times = np.array([15e-12, 175e-12, 265e-12])
+    # Spin-Torque
+    I = 10e-3 # A
+    point_contact_radius = 10e-9
+    point_contact_area = math.pi * point_contact_radius ** 2
+    J = I / point_contact_area
+    print "Current density is J = {:.2} A/m^2.".format(J)
+    J_expr = point_contacts([(L/6, 0), (-L/6, 0)],
+            radius=point_contact_radius, J=J) 
+    #J_visu = df.interpolate(J_expr, sim.S1)
+    #df.plot(J_visu)
+    #df.interactive()
+    P = 0.4
+    d = H
+    sim.set_stt(J_expr, P, d, p)
+    pulse_time = 50e-12
+    snapshot_times = np.array([0, 50e-12, 175e-12, 265e-12])
 
-t = 0; dt = 1e-12; t_max = 1e-9;
-while t <= t_max:
-    if abs(t - 50e-12) < 1e-16:
-        print "Switching spin current off at {}.".format(t)
-        sim.toggle_stt(False)
-    if np.min(np.abs(t - snapshot_times)) < 1e-16:
-        figure_5_6(mesh, sim.m, "100", t)
-    t += dt
-    sim.run_until(t)
+    t = 0; dt = 1e-12; t_max = 1e-9;
+    while t <= t_max:
+        if abs(t - 50e-12) < 1e-16:
+            print "Switching spin current off at {}.".format(t)
+            sim.toggle_stt(False)
+        if np.min(np.abs(t - snapshot_times)) < 1e-16:
+            figure_5_6(mesh, sim.m, "".join(map(str, p)), t)
+        t += dt
+        sim.run_until(t)
