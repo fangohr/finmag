09ba60717363791726e1ee96eb895915803834b4
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-08-29 17:28:43 +0200

    point contacts now more circular using netgen mesh, but still dolfin expression for the current

--- sandbox/interference_patterns/point_contacts.py
+++ sandbox/interference_patterns/point_contacts.py
@@ -1,4 +1,5 @@
 import dolfin as df
+from finmag.util.meshes import from_geofile
 
 def point_contacts(origins, radius, J, debug=False):
     """
@@ -10,6 +11,11 @@ def point_contacts(origins, radius, J, debug=False):
     specified *origins*.
 
     """
+    if radius > 1e-6:
+        # Assuming this is a macroscopic mesh coming from netgen, with the
+        # circular part cut out at the right position.
+        # Need to smooth the boundary.
+        radius += 1e-4
     distance_to_origin = "sqrt(pow(x[0] - {0}, 2) + pow(x[1] - {1}, 2))"
     point_contact_conditions = ["(" + distance_to_origin.format(pos[0], pos[1]) + " <= r)" for pos in origins]
     expr_str = " || ".join(point_contact_conditions) + " ? J : 0"
@@ -18,10 +24,11 @@ def point_contacts(origins, radius, J, debug=False):
     return df.Expression(expr_str, r=radius, J=J)
 
 if __name__ == "__main__":
-    mesh = df.Box(0, 0, 0, 70e-9, 50e-9, 1e-9, 240, 200, 1)
+    mesh = from_geofile("film.geo")
     S1 = df.FunctionSpace(mesh, "Lagrange", 1)
-    pc_expr = point_contacts([(20e-9, 25e-9), (50e-9, 25e-9)], radius=10e-9, J=1e10, debug=True)
+    pc_expr = point_contacts([(-62.5, 0), (62.5, 0)], radius=10, J=1e10, debug=True)
     f = df.project(pc_expr, S1)
 
+    df.plot(mesh)
     df.plot(f)
     df.interactive()
