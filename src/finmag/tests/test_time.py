f7b5501d17fc15f37184229667369cd0c9e35347
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-08-30 11:57:38 +0200

    test that shows that cur_t now gets updated
    was fixed in my last commit. Note that cur_t is the current time in the integrator
    and does not correspond to the current simulation time.

---
+++ src/finmag/tests/test_time.py
@@ -0,0 +1,23 @@
+import dolfin as df
+from finmag import Simulation as Sim
+from finmag.energies import Exchange, Demag
+
+def test_current_time():
+    size = 20e-9
+    simplices = 4
+    mesh = df.Box(0, 0, 0, size, size, size, simplices, simplices, simplices)
+
+    Ms = 860e3
+    A = 13.0e-12
+
+    sim = Sim(mesh, Ms)
+    sim.set_m((1, 0, 0))
+    sim.add(Exchange(A))
+    sim.add(Demag())
+
+    t = 0.0; t_max = 1e-10; dt = 1e-12;
+
+    while t <= t_max:
+        t += dt
+        sim.run_until(t)
+        assert not sim.integrator.cur_t == 0.0
