e737086f40eb4a5d2409a60e9d01bdc52cf9c352
Hans Fangohr <fangohr@soton.ac.uk>
2012-09-12 09:24:08 +0100

    Added brief explanation of test (guessing from reading the code)

--- src/finmag/llb/tests/test_llb_exch.py
+++ src/finmag/llb/tests/test_llb_exch.py
@@ -6,9 +6,23 @@ import numpy as np
 from finmag.energies.exchange import Exchange
 import finmag.llb.exchange as llb
 
+#from IPython import embed
 
 class LLBTestExch(unittest.TestCase):
     def test_exch(self):
+        """Test case that compares the LLB exchange
+        with the normal exchange class for a system with
+        preserved magnitude.
+
+        It does this for a two-dimensional magnetisation
+        vector.
+
+        There was a change on 11 Sept 2012 in the repository
+        that required to change the tolerance in the assert
+        statement below from 0 to 2e-8. It was probably 
+        coincidence that the test passed with deviation 0.0 so
+        far.
+        """
         mesh = df.Interval(10, 0, 5)
         S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1, dim=2)
         C = 1
@@ -24,3 +38,5 @@ class LLBTestExch(unittest.TestCase):
         H2 = llb_exch.compute_field()
         print("max(H1-H2)=%g" % np.max(H2 - H1))
         assert(np.max(H2 - H1) <  2e-8)
+        #embed()
+
