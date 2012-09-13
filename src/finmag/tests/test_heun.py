0760546b0cc40c9ce1b60c93258540f6d0d741f1
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-09-13 21:53:50 +0100

    continue Stochastic Heun integrator API

--- src/finmag/tests/test_heun.py
+++ src/finmag/tests/test_heun.py
@@ -1,6 +1,8 @@
-from finmag.native.llg import helloWorld, StochasticHeunIntegrator
+import numpy as np
+from finmag.native.llg import StochasticHeunIntegrator
 
-def test_file_was_built():
-    helloWorld()
-    shi = StochasticHeunIntegrator()
-    shi.helloWorld()
+def test_file_builds():
+    drift = lambda y, t: 2 * y
+    diffusion = lambda y, t: y + 0.1
+    integrator = StochasticHeunIntegrator(np.zeros(1), drift, diffusion, 1e-12)
+    integrator.helloWorld()
