a581f4b5cfeddff3ab9a065c8971db399820a4c4
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-09-27 13:50:59 +0200

    interference patterns version used for report

--- sandbox/interference_patterns/run.py
+++ sandbox/interference_patterns/run.py
@@ -10,19 +10,19 @@ def point_contacts(fig=5, debug=False):
     assert fig == 5 or fig == 6
     if fig == 5: # Two point contacts, on a horizontal line.
         distance = 125.0
-        expr = point_contacts_gen([(-distance/2, 0), (distance/2, 0)], radius=pc_radius, J=J)
+        expr = point_contacts_gen([(-distance/2, 0), (distance/2, 0)], radius=pc_radius, J=J/2)
     if fig == 6: # Three point contacts, arranged on a circle around the center of the mesh.
         distance = 65.0
         r = distance * sqrt(3)/3
         coords = [(r * cos(theta), r * sin(theta)) for theta in [0, 2*pi/3, 4*pi/3]]
-        expr = point_contacts_gen(coords, radius=pc_radius, J=J)
+        expr = point_contacts_gen(coords, radius=pc_radius, J=J/3)
     if debug:
         df.plot(df.interpolate(expr, sim.S1))
         df.interactive()
         import sys; sys.exit()
     return expr
 
-mesh = from_geofile("film.geo"); unit_length = 1e-9;
+unit_length = 1e-9;
 Ms = 860e3
 A = 17e-12 # J/m, computed from the exchange length, is meant to be Permalloy
 
@@ -33,20 +33,24 @@ I = 10e-3 # Section 5, page 8.
 J = I / pc_area
 print "Current density is J = {:.2} A/m^2.".format(J)
 
-for figure in [5]:
+for figure in [5, 6]:
+    if figure == 5:
+        mesh = from_geofile("film.geo")
+    else:
+        mesh = df.Rectangle(-220, -220, 220, 220, 400, 400)
+
     sim = Simulation(mesh, Ms, unit_length)
     sim.alpha = 0.012 # Section 5 page 8
     sim.set_m((0, 0, 1))
     sim.add(Zeeman((0, 0, 1.1 * Ms))) # Section 3, end of Section 5
     sim.add(Exchange(A))
     sim.add(ThinFilmDemag())
-    sim.set_stt(point_contacts(figure), polarisation=0.4, thickness=5e-9, direction=(1,0,0))
+    sim.set_stt(point_contacts(figure), polarisation=1.0, thickness=5e-9, direction=(1,1,0))
     
-    m_abs = df.Function(sim.S3)
-
+    m = df.Function(sim.S3)
     m_archive = df.File("figure_{}/m.pvd".format(figure))
 
-    t = 0; dt = 5e-12; pulse_time = 50e-12; t_max = 65e-12;
+    t = 0; dt = 5e-12; pulse_time = 10e-12; t_max = 100e-12;
     while t <= t_max:
         print "Running until {}s.".format(t)
         sim.run_until(t)
@@ -54,8 +58,8 @@ for figure in [5]:
         if abs(t - pulse_time) < 1e-15:
             print "Switching spin current off at {}s.".format(sim.t)
             sim.toggle_stt(False)
-
-        m_abs.vector()[:] = np.abs(sim.m)
-        m_archive << m_abs 
+    
+        m.vector()[:] = np.sqrt(1 - sim.m**2)
+        m_archive << m
         t += dt;
     del(sim)
