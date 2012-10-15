e9d54455f6a9e233cfdaa0046b560efcffbb539b
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-10-15 13:50:03 +0100

    Having an analytical expression for a precessing macrospin is generally useful, so let's move the 'make_analytical_solution()' function to the utils directory.

---
+++ src/finmag/util/macrospin.py
@@ -0,0 +1,45 @@
+import numpy
+
+def make_analytic_solution(H, alpha, gamma):
+    """
+    Returns a function `m` which computes the magnetisation vector as
+    a function of time, with the "initial condition" m(0) = (1,0,0).
+
+    Arguments:
+
+        - H: the magnitude of the applied field (in A/m)
+
+        - alpha: Gilbert damping factor (dimensionless)
+
+        - gamma (alias OOMMF's gamma_G): gyromagnetic ratio (in m/A*s)
+
+    """
+    p = float(gamma) / (1 + alpha ** 2)
+    theta0 = numpy.pi / 2
+    t0 = numpy.log(numpy.sin(theta0) / (1 + numpy.cos(theta0))) / (p * alpha * H)
+
+    # Matteo uses spherical coordinates,
+    # which have to be converted to cartesian coordinates.
+
+    def phi(t):
+        return p * H * t
+
+    def cos_theta(t):
+        return numpy.tanh(p * alpha * H * (t - t0))
+
+    def sin_theta(t):
+        return 1 / (numpy.cosh(p * alpha * H * (t - t0)))
+
+    def x(t):
+        return sin_theta(t) * numpy.cos(phi(t))
+
+    def y(t):
+        return sin_theta(t) * numpy.sin(phi(t))
+
+    def z(t):
+        return cos_theta(t)
+
+    def m(t):
+        return numpy.array([x(t), y(t), z(t)])
+
+    return m
