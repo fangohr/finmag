a376436a48715d61dc7dd2aa3cfa3a1492d362ab
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-10-15 14:07:35 +0100

    Add constant ONE_DEGREE_PER_NS to util/consts.py

--- src/finmag/util/consts.py
+++ src/finmag/util/consts.py
@@ -8,6 +8,7 @@ k_B = 1.3806488e-23 # Boltzmann constant in J / K
 h_bar = 1.054571726e-34 # reduced Plank constant in Js
 e = 1.602176565e-19 # elementary charge in As
 gamma = 2.210173e5 # m/(As) (source:  OOMMF manual, and in Werner Scholz thesis, after (3.7), llg_gamma_G = m/(As))
+ONE_DEGREE_PER_NS = 17453292.52 # rad/s
 
 def exchange_length(A, Ms):
     """
