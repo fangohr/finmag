ccffe682fe9f2bd65c187edbf6cb5ef402bb8c28
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-11-06 12:45:09 +0000

    deprecate helpers.norm because np.linalg.norm does the exact same thing

--- src/finmag/tests/test_helpers.py
+++ src/finmag/tests/test_helpers.py
@@ -18,14 +18,6 @@ def test_vectors():
     y2 = np.array([[0, 2, 4], [1, 3, 5], [1, 3, 5], [0, 2, 4]])
     assert np.array_equal(y2, vectors(x2))
 
-def test_norm():
-    assert norm([0, 0, 0]) == 0
-    assert norm([1, 0, 0]) == 1
-    assert abs(norm([1, 1, 0]) - np.sqrt(2)) < TOLERANCE
-    assert abs(norm([1, 1, 1]) - np.sqrt(3)) < TOLERANCE
-    assert abs(norm([-1, 0, 0]) - norm([1, 0, 0])) < TOLERANCE 
-    assert abs(3*norm([1, 1, 1]) - norm(3*np.array([1, 1, 1]))) < TOLERANCE
-
 def test_fnormalise():
     a = np.array([1., 1., 2., 2., 0., 0.])
     norm = np.sqrt(1+2**2+0**2)
@@ -153,19 +145,6 @@ def test_rows_to_columns():
     x = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
     y = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3]])
     assert np.array_equal(y, rows_to_columns(x))
-
-def test_perturbed_vectors():
-    quantity = 10; direction = [1, 0, 0]; length = 5
-    # I could pass a fake random function to perturbed_vectors for testing,
-    # but the exact behaviour of perturbed_vectors is unspecified except
-    # for the number of vectors returned and their length anyways.
-
-    vector_field = perturbed_vectors(quantity, direction, length)
-    assert len(vector_field) == 10
-
-    for v in vector_field:
-        assert len(v) == 3
-        assert abs(norm(v) - 5) < TOLERANCE
 
 def test_cartesian_to_spherical():
     hapi = np.pi / 2
