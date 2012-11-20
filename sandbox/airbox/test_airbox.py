5037d1d678fdc9500261612161b6b92e9b7f5ff1
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-11-20 13:12:24 +0000

    fix bug: plot now shows midpoint of cells.
    The region information is stored per cell, the plotting looked up vertices
    using the cell data.

--- sandbox/airbox/test_airbox.py
+++ sandbox/airbox/test_airbox.py
@@ -1,10 +1,10 @@
 import os
 import dolfin as df
+import matplotlib.pyplot as plt
 from finmag.util.meshes import from_geofile
 from finmag.util.helpers import piecewise_on_subdomains
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
-mesh = from_geofile(os.path.join(MODULE_DIR, "cube.geo"))
 
 def test_airbox_method():
     """
@@ -17,21 +17,16 @@ def test_airbox_method():
     field of a dipole.
     """
     mesh = from_geofile("mesh.geo")
-    # This holds the region number for each cell.
-    mesh_function = df.MeshFunction("uint", mesh, "cube_mat.xml")
-    print "TODO: Change the .geo file so that we define a sphere instead of a cube."
+    mesh_region = df.MeshFunction("uint", mesh, "mesh_mat.xml")
+    midpoints = [[c.midpoint() for c in df.cells(mesh) if mesh_region[c.index()] == region]
+            for region in (1, 2)]
 
-    # XXX Plot the regions for debugging
-    print "TODO: Are the regions correctly assigned? Plotting this seems to indicate that region 1 has some stray points around the edge of the mesh"
-    import matplotlib.pyplot as plt
-    coords = mesh.coordinates()
-    region1 = [i for i in xrange(len(coords)) if mesh_function.array()[i] == 1]
-    region2 = [i for i in xrange(len(coords)) if mesh_function.array()[i] == 2]
-    pts1 = [coords[i] for i in region1]
-    pts2 = [coords[i] for i in region2]
+    pts1 = [(m.x(), m.y(), m.z()) for m in midpoints[0]]
+    pts2 = [(m.x(), m.y(), m.z()) for m in midpoints[1]]
+
     ax = plt.gca(projection='3d')
     ax.scatter3D(*zip(*pts1), color="green")
-    #ax.scatter3D(*zip(*pts2), color="red")
+    ax.scatter3D(*zip(*pts2), color="red", alpha=0.3)
     plt.show()
 
     # Define different values for the saturation magnetisation on each subdomain
@@ -39,4 +34,5 @@ def test_airbox_method():
         1: 1e6,  # region 1 = Permalloy
         2: 0,    # region 2 = "air"
         }
-    Ms = piecewise_on_subdomains(mesh, mesh_function, Ms_vals)
+    Ms = piecewise_on_subdomains(mesh, mesh_region, Ms_vals)
+
