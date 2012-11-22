12594f8f88c008fcf4aeb2489b204e03b7a06d99
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-11-22 16:17:57 +0000

    In airbox test, try to initialise a simulation with varying Ms (fails at the moment).

--- sandbox/airbox/test_airbox.py
+++ sandbox/airbox/test_airbox.py
@@ -3,6 +3,7 @@ import dolfin as df
 import matplotlib.pyplot as plt
 from finmag.util.meshes import from_geofile, plot_mesh_regions
 from finmag.util.helpers import piecewise_on_subdomains
+from finmag import sim_with
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -22,13 +23,16 @@ def test_airbox_method():
 
     # Visualise mesh regions to check they look right (this will be
     # removed in the final test).
-    plot_mesh_regions(mesh_region, regions=[1, 2], colors=["green", "red"],
-                      alphas=[1.0, 0.25])
-    plt.show()
+    #plot_mesh_regions(mesh_region, regions=[1, 2], colors=["green", "red"],
+    #                  alphas=[1.0, 0.25])
+    #plt.show()
 
     # Define different values for the saturation magnetisation on each subdomain
     Ms_vals = (8.6e5, 0)
     Ms = piecewise_on_subdomains(mesh, mesh_region, Ms_vals)
+
+    sim = sim_with(mesh, Ms=Ms, m_init=(1.0, 0.0, 0), alpha=1.0,
+                   unit_length=1e-9, A=13.0e-12, demag_solver='FK')
 
 if __name__ == '__main__':
     test_airbox_method()
