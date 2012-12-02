24756ece03f11ed2ad12b428486d3d022ca3d51a
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-02 13:12:31 +0000

    Stub for more basic test with a two-region mesh:
    Set the same value for Ms in both regions and check that the result
    is the same as with a single-region mesh. This test is not implemented
    yet because the explorations in the .ipynb file still seem to indicate
    unresolved issues.

--- sandbox/airbox/test_airbox.py
+++ sandbox/airbox/test_airbox.py
@@ -8,6 +8,18 @@ from finmag import sim_with
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
+def test_two_regions_with_same_Ms():
+    """
+    Define a mesh with two regions but set the same value of Ms in
+    each of them and let the system relax. Then do the same with a
+    single-region mesh and check that the results are the same.
+    """
+    raise NotImplementedError(
+        "This is being tested in the .ipynb file, but there are still "
+        "unresolved issues because the results don't look similar enough, "
+        "so this needs more investigation before it can be included here.")
+
+
 def test_airbox_method():
     """
     Define a mesh with two regions (Permalloy and 'air'), where
