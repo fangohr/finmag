84b9e0e5ce6fa25d11cfcd6a87316b352b22f588
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-07 13:41:10 +0000

    Simple test for (yet-to-be-implemented) automatic saving of .ndt files during simulation runs/time integration.

---
+++ src/finmag/tests/test_writing_data.py
@@ -0,0 +1,11 @@
+import finmag
+import filecmp
+from numpy import linspace
+
+def test_write_ndt_file():
+    sim = finmag.example.barmini(name="barmini_test.ndt")
+    for time in linspace(0, 1e-10, 20):
+        print("Integrating towards t = %gs" % time)
+        sim.run_until(time)
+    print("Done")
+    assert(filecmp.cmp("barmini_test.ndt", "barmini_test.ndt.ref", shallow=False))
