b96878748e7b2882963ec9612b4a1d9ba0b560db
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-07 16:48:10 +0000

    Attempt to fix Jenkins build (reference .ndt file was not picked up, probably because the test was executed in a different directory).

--- src/finmag/tests/test_writing_data.py
+++ src/finmag/tests/test_writing_data.py
@@ -1,8 +1,16 @@
 import finmag
 import filecmp
+import os
 from numpy import linspace
 
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def test_write_ndt_file():
+    # Change into the directory of the test so that the .ndt file is saved there
+    # and the reference file is found.
+    # TODO: This might not be the best solution, should revise this at some point.
+    os.chdir(MODULE_DIR)
+
     sim = finmag.example.barmini(name="barmini_test")
     for time in linspace(0, 1e-10, 20):
         print("Integrating towards t = %gs" % time)
