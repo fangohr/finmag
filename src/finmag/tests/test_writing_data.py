8f5fc4d7b302353c5c1a2f79f5549ef48b7cd473
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-07 15:54:25 +0000

    By default we automatically save averages in the Simulation.run_until() method.

--- src/finmag/tests/test_writing_data.py
+++ src/finmag/tests/test_writing_data.py
@@ -6,7 +6,7 @@ def test_write_ndt_file():
     sim = finmag.example.barmini(name="barmini_test")
     for time in linspace(0, 1e-10, 20):
         print("Integrating towards t = %gs" % time)
-        sim.run_until(time)
-        sim.save_averages()
+        sim.run_until(time, save_averages=True)  # True is the default for save_averages
+                                                 # but we provide it for clarity.
     print("Done")
     assert(filecmp.cmp("barmini_test.ndt", "barmini_test.ndt.ref", shallow=False))
