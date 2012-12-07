7a964d6441cbbdc7d6f83c9a80d8ff5284a3347e
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-07 14:06:15 +0000

    We need to explicitly call sim.save_averages() in the test because the run_until() method doesn't save the averages for us (but perhaps it would be nice to allow this?).

--- src/finmag/tests/test_writing_data.py
+++ src/finmag/tests/test_writing_data.py
@@ -7,5 +7,6 @@ def test_write_ndt_file():
     for time in linspace(0, 1e-10, 20):
         print("Integrating towards t = %gs" % time)
         sim.run_until(time)
+        sim.save_averages()
     print("Done")
     assert(filecmp.cmp("barmini_test.ndt", "barmini_test.ndt.ref", shallow=False))
