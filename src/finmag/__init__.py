b7c97ef16df1099ff80c2bf59962bf4d71083852
Hans Fangohr <fangohr@soton.ac.uk>
2012-12-10 13:16:04 +0000

    Trying to complete the systematic move of python code from __init__.py to init.py - had previsouly forgotten the toplevel.

--- src/finmag/__init__.py
+++ src/finmag/__init__.py
@@ -1,15 +1,7 @@
-# FinMag - a thin layer on top of FEniCS to enable micromagnetic multi-physics simulations
+# FinMag 
 # Copyright (C) 2012 University of Southampton
 # Do not distribute
 #
 # CONTACT: h.fangohr@soton.ac.uk
 #
-# AUTHOR(S) OF THIS FILE: Dmitri Chernyshenko (d.chernyshenko@soton.ac.uk)
-
-import logging
-from finmag.sim.sim import Simulation, sim_with
-from __version__ import __version__
-import example
-
-logger = logging.getLogger("finmag")
-logger.debug("This is Finmag version {}".format(__version__))
+from init import *
---
+++ src/finmag/init.py
@@ -0,0 +1,14 @@
+# FinMag - a thin layer on top of FEniCS to enable micromagnetic multi-physics simulations
+# Copyright (C) 2012 University of Southampton
+# Do not distribute
+#
+# CONTACT: h.fangohr@soton.ac.uk
+
+
+import logging
+from finmag.sim.sim import Simulation, sim_with
+from __version__ import __version__
+import example
+
+logger = logging.getLogger("finmag")
+logger.debug("This is Finmag version {}".format(__version__))
