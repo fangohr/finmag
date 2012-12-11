99e54af253aef824ca203f64663111958237c092
Hans Fangohr <fangohr@soton.ac.uk>
2012-12-11 13:08:16 +0000

    Adding version information - about third party libraries when starting up - comparing linux version from creating the binary build with the one we executed on [needs testing]

--- src/distcp.py
+++ src/distcp.py
@@ -70,6 +70,23 @@ def distcp(targetdir):
     print scandir('finmag', targetdir)
 
 
+def storeversions(targetfile):
+    """Target file should be something like 'finmag/util/binary.py'
+    The data in the file is used to store which version of software we 
+    had when the binary distribution was created."""
+    if os.path.exists(targetfile):
+        print("This is odd: the file '%s' exists already, but is only" % targefile)
+        print("meant to be created now (in function storeversions() in distcp.py)")
+        raise RuntimeError("odd error when running %s" % __file__)
+
+    f = open(targetfile,'w')
+
+    import finmag.util.versions
+    
+    f.write("buildlinux = '%s'\n" % finmag.util.versions.get_linux_issue())
+    f.close()
+
+
 if __name__ == '__main__':
     descr = 'Copy FinMag files to alternative location'
     parser = argparse.ArgumentParser(description=descr)
@@ -82,3 +99,6 @@ if __name__ == '__main__':
     if not os.path.exists(targetdir):
         os.makedirs(targetdir)
     distcp(targetdir)
+
+    storeversions(os.path.join(targetdir,"finmag/util/binary.py"))
+
