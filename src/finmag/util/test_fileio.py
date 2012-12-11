1766c7efd9cc509259f7a892f99dabf661e6630c
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-11 15:05:32 +0000

    Since py.test actually provides us with a temporary directory for each test in a convenient way, let's use that for creating temporary files.

--- src/finmag/util/test_fileio.py
+++ src/finmag/util/test_fileio.py
@@ -3,10 +3,10 @@ import numpy
 from fileio import Tablewriter, Tablereader
 
 
-def test_Table_writer_and_reader():
-    import numpy as np
+def test_Table_writer_and_reader(tmpdir):
     import finmag
     import dolfin as df
+
     xmin, ymin, zmin = 0, 0, 0    # one corner of cuboid
     xmax, ymax, zmax = 6, 6, 11   # other corner of cuboid
     nx, ny, nz = 3, 3, 6         # number of subdivisions (use ~2nm edgelength)
@@ -15,9 +15,7 @@ def test_Table_writer_and_reader():
     sim = finmag.sim_with(mesh, Ms=0.86e6, alpha=0.5, unit_length=1e-9,
                           A=13e-12, m_init=(1, 0, 1))
 
-    filename = 'tmp-test-save_averages-data.ndt'
-    if os.path.exists(filename):
-        os.unlink(filename)
+    filename = os.path.join(tmpdir.dirname, 'test-save_averages-data.ndt')
     ndt = Tablewriter(filename, sim)
     times = np.linspace(0, 3.0e-11, 6 + 1)
     for i, time in enumerate(times):
@@ -35,7 +33,6 @@ def test_Table_writer_and_reader():
     assert abs(data['m_y'][-1] - my) < 5e-7
     assert abs(data['m_z'][-1] - mz) < 5e-7
 
-    #os.unlink(filename)
 
 if __name__ == "__main__":
     test_Table_writer_and_reader()
