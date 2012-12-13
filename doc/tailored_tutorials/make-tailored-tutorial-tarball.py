201567f4603339347d54e25e1f26d7451eca3f77
Maximilian Albert <maximilian.albert@soton.ac.uk>
2012-12-13 23:17:47 +0000

    Add command line flag which allows to optionally build the tailored tutorials by directly converting the .ipynb files to .html.
    This is just a proof of concept and needs further work. The resulting notebook .html
    look much nicer, but there are still a number of shortcomings. In particular:
    
      (i) It doesn't include the custom CSS style file yet;
      (ii) The index.html file only contains links to the individual notebook
           html files but not their contents;
      (iii) These links are only via the .ipynb filenames, not the notebook
            titles (and thus the tabel of contents does not list subsections either).

--- doc/tailored_tutorials/make-tailored-tutorial-tarball.py
+++ doc/tailored_tutorials/make-tailored-tutorial-tarball.py
@@ -13,9 +13,11 @@ import logging
 logging.basicConfig(level = logging.DEBUG)
 import tempfile
 import shutil
-import os.path
+import os
+import sys
 import time
 import subprocess
+import textwrap
 from argparse import ArgumentParser
 
 def targetdirectoryname(conf):
@@ -34,6 +36,41 @@ def targetdirectoryname(conf):
         os.mkdir(targetdirname)
 
     return targetdirname
+
+
+def create_index_html(conf):
+    """
+    Create an index.html file which contains a title and links to all
+    the individual notebook html files.
+    """
+    # XXX TODO: This is just a quick hack for proof-of-concept. The
+    #           resulting html could be made much nicer using
+    #           nbconvert's internal python functions directly, but it
+    #           would probably need a bit of tweaking for that.
+    targetdirname = targetdirectoryname(conf)
+    partner_str = ", ".join(conf['names'])
+
+    logging.debug("[DDD] lst: {} (type: {})".format(conf['tutorials'], type(conf['tutorials'])))
+    contents_str = \
+        "<br>\n".join(map(lambda name: "   <li><a href='{f}'>{f}</a></li>".format(f=name+'.html'),
+                          list(conf['tutorials'])))
+
+    with open(os.path.join(targetdirname, 'index.html'), 'w') as f:
+        f.write(textwrap.dedent("""\
+            <h2>Finmag Manual for {partner_str}</h2>
+            <br>
+            Tutorial compiled on {date_str}.
+            <br><br>
+            Contents:<br>
+            <ul>
+               {contents_str}
+            </ul>
+            <br><br>
+            The raw ipython notebooks are <a href='ipynb/'>available here</a>
+            """.format(partner_str=partner_str,
+                       date_str=time.asctime(),
+                       contents_str=contents_str)))
+
 
 def assemble_rst(conf):
 
@@ -88,6 +125,33 @@ def compile_rst2html(conf):
     output = subprocess.check_output(cmd, shell=True)
     logging.debug("Output was %s" % output)
 
+
+def compile_ipynb2html(conf):
+    try:
+        NBCONVERT = os.environ['NBCONVERT']
+    except KeyError:
+        print("Please set the environment variable NBCONVERT so that it "
+              "points to the location of the script nbconvert.py.")
+        sys.exit(1)
+    # XXX TODO: It would be much nicer to use the python functions in
+    #           nbconvert directly rather than calling it via
+    #           'subprocess'. Once the --output-dest flag (or a
+    #           similar one) has been adopted into the main repo we
+    #           should revisit this.
+
+    targetdir = targetdirectoryname(conf)
+    ipynbdir = os.path.join(targetdir, 'ipynb')
+    if not os.path.exists(ipynbdir):
+        os.mkdir(ipynbdir)
+    for fileroot in conf['tutorials']:
+        shutil.copy("../ipython_notebooks_src/{}.ipynb".format(fileroot), ipynbdir)
+        cmd = "cd {}; python {} --format=html {}.ipynb ../{}.html".format(
+            ipynbdir, NBCONVERT, fileroot, fileroot)
+        logging.debug("Running cmd '%s'" % cmd)
+        output = subprocess.check_output(cmd, shell=True)
+        logging.debug("Output was %s" % output)
+
+
 def assemble_tarballs(conf):
     targetdirname = targetdirectoryname(conf) 
     cmd = "cd %s; tar cfvz finmag-tutorial-%s.tgz finmag-tutorial-%s" % \
@@ -112,6 +176,8 @@ if __name__ == '__main__':
                          help='partner[s] to process (names of subdirectories)')
     parser.add_argument('--keepbuild', action='store_true', default=False, \
                          help='Keep directory in which tar ball is built (for debugging).')
+    parser.add_argument('--use-ipynb2html', action='store_true', default=False, \
+                         help='Use nbconvert directly to convert .ipynb files to .html.')
 
     args = vars(parser.parse_args())
     
@@ -130,13 +196,20 @@ if __name__ == '__main__':
         conf['header'] = ".. contents::\n\n"
         conf['ipynbdir']  = "The raw ipython notebooks are `available here <ipynb>`__.\n\n" 
 
-        # do the work
-        logging.info("Assembling rst file for %s" % partner)
-        assemble_rst(conf)
-        logging.info("Compiling rst file for %s" % partner)
-        compile_rst2html(conf)
-        logging.info("Creating tar ball for  %s" % partner)
+        # Do the work
+        if args['use_ipynb2html']:
+            logging.info("Creating index.html for %s" % partner)
+            create_index_html(conf)
+            logging.info("Converting .ipynb files to .html for %s" % partner)
+            compile_ipynb2html(conf)
+        else:
+            logging.info("Assembling rst file for %s" % partner)
+            assemble_rst(conf)
+            logging.info("Compiling rst file for %s" % partner)
+            compile_rst2html(conf)
 
+        logging.info("Creating tar ball for  %s" % partner)
         assemble_tarballs(conf)
+
         if not args['keepbuild']:
             remove_builddir(conf)
