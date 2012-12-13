2d58c127432b7c76607debd6e8ea54eeb0d537c0
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-12-13 16:57:03 +0100

    replace test that would run test during py.test collection phase

---
+++ src/finmag/tests/demag/test_demag_sphere.py
@@ -0,0 +1,44 @@
+import pytest
+import numpy as np
+import dolfin as df
+from finmag.util.meshes import sphere
+from finmag.energies.demag.solver_fk_alt import SimpleFKSolver
+from finmag.energies.demag.solver_fk import FemBemFKSolver as FKSolver
+from finmag.energies.demag.solver_gcr import FemBemGCRSolver as GCRSolver
+
+solvers = [SimpleFKSolver, FKSolver, GCRSolver]
+TOL = 1e-2
+
+@pytest.fixture(scope="module")
+def uniformly_magnetised_sphere():
+    Ms = 1
+    mesh = sphere(r=1, maxh=0.25)
+    S3 = df.VectorFunctionSpace(mesh, "CG", 1)
+    m = df.Function(S3)
+    m.assign(df.Constant((1, 0, 0)))
+
+    solutions = []
+    for solver in solvers:
+        if solver.__name__ == "SimpleFKSolver":
+            solution = solver(S3, m, Ms)
+        else:
+            solution = solver(mesh, m, Ms=Ms)
+        solution.H = solution.compute_field()
+        solutions.append(solution)
+    return solutions
+
+def test_H_demag(uniformly_magnetised_sphere):
+    for solution in uniformly_magnetised_sphere:
+        H = solution.H.reshape((3, -1)).mean(1)
+        H_expected = np.array([-1.0/3.0, 0, 0])
+        print "{}: Hx = {}, should be {}.".format(
+                solution.__class__.__name__, H, H_expected)
+        diff = np.max(np.abs(H - H_expected))
+        assert diff < TOL
+
+def test_H_demag_deviation(uniformly_magnetised_sphere):
+    for solution in uniformly_magnetised_sphere:
+        H = solution.H.reshape((3, -1))
+        delta = np.max(np.abs(H.max(axis=1) - H.min(axis=1)))
+        assert delta < TOL
+
