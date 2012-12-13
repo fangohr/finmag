07823d88f98d5f64bd2f63ee86ea599516cbd882
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-12-13 15:59:45 +0100

    add test for unit length

--- src/finmag/tests/test_unit_length.py
+++ src/finmag/tests/test_unit_length.py
@@ -5,49 +5,41 @@ from finmag.util.helpers import fnormalise
 from finmag.energies import Exchange
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+n = 20
+Ms = 8.6e5
+A = 1
 
-def exchange(mesh, cube_length, unit_length):
-    print mesh
-    A = 13.0e-12
+def exchange(mesh, unit_length):
+    S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1)
+    m_expr = df.Expression(("x[1]*u", "0", "sqrt(1-pow(x[1]*u, 2))"), u=unit_length)
+    m = df.interpolate(m_expr, S3)
     exch = Exchange(A)
-
-    S3 = df.VectorFunctionSpace(mesh, "CG", 1)
-    m = df.project(df.Expression(("0", "sin(x[0]*pi/(2*L))", "cos(x[0]*pi/(2*L))"), L=cube_length), S3)
-    df.plot(m, interactive=True)
-    Ms = 8.6e5
     exch.setup(S3, m, Ms, unit_length=unit_length)
-
-    H = df.Function(S3)
-    H.vector().set_local(exch.compute_field())
+    H = exch.compute_field()
     E = exch.compute_energy()
+    return m.vector().array(), H, E
 
-    return H, E
-
-def _test_compare_with_dolfin_mesh():
+def test_compare_exchange_for_two_dolfin_meshes():
     """
-    Check that a netgen mesh expressed in nanometers gives the same results
-    as a dolfin mesh expressed in meters for the exchange interaction.
+    Check that a mesh expressed in nanometers gives the same results
+    as a mesh expressed in meters for the exchange interaction.
 
     """
+    mesh_nm = df.Box(0, 0, 0, 1, 1, 1, n, n, n) # in nm
+    m_nm, H_nm, E_nm = exchange(mesh_nm, unit_length=1e-9)
 
-    L_nm = 10 # nm
-    mesh_nm = df.Box(0, 0, 0, L_nm, L_nm, L_nm, 20, 20, 20)
-    H_nm, E_nm = exchange(mesh_nm, L_nm, unit_length=1e-9)
+    mesh = df.Box(0, 0, 0, 1e-9, 1e-9, 1e-9, n, n, n)
+    m, H, E = exchange(mesh, unit_length=1)
 
-    L_m = 10e-9
-    mesh = df.Box(0, 0, 0, L_m, L_m, L_m, 20, 20, 20)
-    H, E = exchange(mesh, L_m, unit_length=1)
+    rel_diff_m = np.max(np.abs(m_nm - m)) # norm m = 1
+    print "Difference of magnetisation is {:.2f}%.".format(100 * rel_diff_m)
+    assert rel_diff_m < 3.0/100
 
-    print "Comparing E = {} with netgen and E = {} with dolfin.".format(E_nm, E)
-    assert abs(E_nm - E) < 1e-15
+    rel_diff_E = abs((E_nm - E) / E)
+    print "Relative difference between E_nm = {:.5g} and E_m = {:.5g} is d = {:.2f}%.".format(E_nm, E, 100*rel_diff_E)
+    assert rel_diff_E < 1e-3
 
-    rs = np.linspace(5, L_nm, 5)
-    for r_nm in rs:
-        # probe along cube diagonal
-        r_m = r_nm * 1e-9
-        print r_nm
-        print H_nm(r_nm, r_nm, r_nm)
-        print r_m
-        print H(r_m, r_m, r_m)
-        assert abs(H_nm(r_nm, r_nm, r_nm) - H(r_m, r_m, r_m)) < 1e-14
-    
+    max_diff_H = np.max(np.abs(H_nm - H)/np.max(H))
+    print "Maximum of relative difference between the two fields is d = {:.2f}%.".format(100*max_diff_H)
+    assert np.max(np.abs(H)) > 0
+    assert max_diff_H < 1e-3
