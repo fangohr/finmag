5250bc5295bdf2a5ea8c4ace97da02d6c9b84747
Hans Fangohr <fangohr@soton.ac.uk>
2012-12-13 16:25:04 +0000

    Adding introduction tutorial.

--- doc/tailored_tutorials/hesjedahl.py
+++ doc/tailored_tutorials/hesjedahl.py
@@ -1,4 +1,4 @@
 
 conf = {'nameshort':'Hesjedahl',
         'names': ('Thorsten Hesjedahl','Shilei Zhang'),
-        'tutorials' : ['tutorial-example2','tutorial-saving-averages-demo','tutorial-use-of-logging','tutorial-running-simulations-with-dmi']}
+        'tutorials' : ['tutorial-using-ipython-notebook','tutorial-example2','tutorial-saving-averages-demo','tutorial-use-of-logging','tutorial-running-simulations-with-dmi']}
