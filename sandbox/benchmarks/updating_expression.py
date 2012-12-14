0108c80e29e3a738c51ec09f1157ab16e2b52422
Hans Fangohr <fangohr@soton.ac.uk>
2012-12-14 22:26:57 +0000

    Changed names a little to make it (at least for me) clearer what happens.
    Looking at the results, it seems the dolfin expression for no
    spatial dependence are much slower than the numpy expression. This could come through the use of the fill() method in 'numpy_vectorised_no_x_dependence()'
    which exploits our extra knowledge about the lack of x-dependence and
    thus is fast.
    
    It would be nice to compare a vectorised numpy version that includes the x-dependence with the spatially varying x. Can this be done with numpy?

--- sandbox/benchmarks/updating_expression.py
+++ sandbox/benchmarks/updating_expression.py
@@ -26,12 +26,14 @@ sinc_expr = df.Expression((
     "f_0"
     " * (fabs(x[0]) < eps ? 1 : sin(x[0])/x[0])"
     " * (fabs(t) < eps ? 1 : sin(t)/t)"), f_0 = f_0, t = 0.0, eps=eps)
-def dolfin_against_numpy_loop(t):
+
+#def dolfin_against_numpy_loop(t):
+def dolfin_expression(t):
     sinc_expr.t = t
     f = df.interpolate(sinc_expr, S)
     return f.vector().array()
 
-def numpy_vectorised(t):
+def numpy_vectorised_no_x_dependence(t):
     """
     function depends only on time
 
@@ -42,13 +44,13 @@ def numpy_vectorised(t):
     return f
 
 simple_expr = df.Expression("sin(t)", t=0)
-def dolfin_against_numpy_vectorised(t):
+def dolfin_expression_no_x_dependence(t):
     simple_expr.t = t
     f = df.interpolate(simple_expr, S)
     return f.vector().array()
 
 lines = ["b", "r", "b--", "r--"]
-for i, method in enumerate([numpy_loop, dolfin_against_numpy_loop, numpy_vectorised, dolfin_against_numpy_vectorised]):
+for i, method in enumerate([numpy_loop, dolfin_expression, numpy_vectorised_no_x_dependence, dolfin_expression_no_x_dependence]):
     vertices = []
     times = []
     for dL in [1, 2, 5, 7, 10, 12, 15, 17, 20]:
