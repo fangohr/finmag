573708e0ec669b1793e8d257e4124367b3e9f9bd
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-12-14 10:10:57 +0100

    reworked dispersion.py indeed produces same plot, overwrite old version

--- examples/dispersion_curves/dispersion.py
+++ examples/dispersion_curves/dispersion.py
@@ -2,121 +2,108 @@ import os
 import subprocess
 import numpy as np
 import dolfin as df
-
+from numpy import fft
 from finmag import Simulation as Sim
-from finmag.energies import Exchange
-from finmag.energies import Demag
-from finmag.energies import TimeZeeman
-
+from finmag.energies import Exchange, Demag, TimeZeeman
 from finmag.util.meshes import from_geofile
-            
 
-def relax_system(geo_file):
-    mesh = from_geofile(geo_file)
-    sim = Sim(mesh, 8.6e5, unit_length=1e-9)
-    sim.alpha = 0.5
+meshfile = "width_modulated_bar.geo"
+mesh = from_geofile(meshfile)
+x0 = -540; x1 = -x0; dx_high_alpha = 10; xn_probe = x1 + 1;
+Ms = 8.6e5
+A = 13.0e-12
+initial_m_file = "mxyz_0.npy"
+m_for_fourier_analysis_file = "my_t.npy"
+dispersion_data_file = "dispersion.dat"
+dispersion_plot_file = "dispersion.png"
+
+def relax_system():
+    """
+    Relax the system to obtain the initial magnetisation for the subsequent simulation.
+
+    """
+    sim = Sim(mesh, Ms, unit_length=1e-9)
     sim.set_m((1, 0, 0))
-
-    exchange = Exchange(13.0e-12)
-    sim.add(exchange)
-    
-    demag = Demag(solver="FK")
-    sim.add(demag)
-    
+    sim.alpha = 1
+    sim.llg.do_precession = False
+    sim.add(Exchange(A))
+    sim.add(Demag(solver="FK")) 
     sim.relax()
-    np.save('relaxed', sim.llg._m.vector().array())  
+    np.save(initial_m_file, sim.m)  
     
-    
-def excite_system(geo_file):
-    mesh = from_geofile(geo_file)
-    sim = Sim(mesh, 8.6e5, unit_length=1e-9)
-    sim.alpha = 0.01
-    init_m=np.load('relaxed.npy')
-    sim.set_m(init_m)
+def excite_system():
+    """
+    Excite the relaxed system with the sinc pulse and save m_y to a file.
 
-    exchange = Exchange(13.0e-12)
-    sim.add(exchange)
+    """
+    sim = Sim(mesh, Ms, unit_length=1e-9)
+    alpha_mult = df.interpolate(df.Expression(
+        "(x[0] < x_left || x[0] > x_right) ? 100.0 : 1.0",
+        x_left=x0+dx_high_alpha, x_right=x1-dx_high_alpha), sim.llg.S1)
+    sim.spatial_alpha(0.01, alpha_mult)
+    sim.set_m(np.load(initial_m_file))
+    sim.add(Exchange(A))
+    sim.add(Demag(solver="FK"))
     
-    demag = Demag(solver="FK")
-    sim.add(demag)
-    
-    llg=sim.llg
-    
-    mult = df.Function(llg.S1)
-    mult.assign(df.Expression("(x[0]>x_limit||x[0]<-x_limit) ? 100.0 : 1.0", x_limit=530))
-    sim.spatial_alpha(0.01,mult)
-    
-    GHz=1e9
-    omega= 50*2*np.pi*GHz
-    
-    field_expr="".join(["(x[0]==0) ?",
-              "(t==0?H0",
-              ":H0*sin(omega*t)/(omega*t))",
-              ":(t==0?H0*sin(kc*x[0])/(kc*x[0])",
-              ":H0*sin(kc*x[0])/(kc*x[0])*sin(omega*t)/(omega*t))"])
-    
-    H = df.Expression(("0.0", field_expr,"0.0"), H0=1e5,kc=1.0,omega=omega, t=0.0)
-     
-    H_app = TimeZeeman(H)
-    H_app.setup(llg.S3, llg._m, Ms=8.6e5)
-    
-    t0=50e-12
-    def update_H_ext(t):
-        H_app.update(t-t0)
-         
-    llg.effective_field.add(H_app, with_time_update=update_H_ext)
-    
-    xs=np.linspace(-540+1e-8, 540-1e-8, 541)
-    times = np.linspace(0, 2e-9, 2001)
-    data=[]
-    for t in times:
+    GHz = 1e9
+    omega = 50 * 2 * np.pi * GHz
+    sinc = (
+        "H_0"
+        " * (t == 0 ? 1 : sin(omega * t)/(omega * t))"
+        " * (x[0] == 0 ? 1 : sin(k_c * x[0])/(k_c * x[0]))")
+    H = df.Expression(("0.0", sinc, "0.0"), H_0=1e5, k_c=1.0, omega=omega, t=0.0)
+    pulse = TimeZeeman(H) 
+    t_0 = 50e-12
+    def update_pulse(t):
+        pulse.update(t - t_0)
+    sim.add(pulse, with_time_update=update_pulse) 
+   
+    xs = np.linspace(x0 + 1e-8, x1 - 1e-8, xn_probe)
+    ts = np.linspace(0, 2e-9, 2001)
+    my_along_x_axis_over_time = []
+    for t in ts:
         sim.run_until(t)
-        my=np.array([llg._m(x,0,0)[1] for x in xs])
-        data.append(my)
-        print 'run time  t=%g'%t
-    
-    np.save('data', np.array(data))
+        my = np.array([sim.llg._m(x, 0, 0)[1] for x in xs])
+        my_along_x_axis_over_time.append(my)
+        print "Simulation t = {:.2}.".format(t)
+    np.save(m_for_fourier_analysis_file, np.array(my_along_x_axis_over_time))
 
+def compute_dispersion(dx, dt):
+    """
+    Compute the dispersion relation, where *dx* is distance between points
+    where m_y was probed in nm, and *dt* is the length of time between
+    measurements in ns.
 
-def compute_dispersion(dx,dt,file_name):
-    
-    data=np.load('data.npy')
-
-    res=np.fft.fft2(data)
-    res=np.fft.fftshift(res)
-    res=np.abs(res)
-    res=np.power(res,2)
-    res=np.log10(res)
-    m,n=res.shape
+    """
+    my = np.load(m_for_fourier_analysis_file)
+    transformed = np.log10(np.power(fft.fftshift(fft.fft2(my)), 2))
+    m, n = transformed.shape
     print m,n
     
-    freq=np.fft.fftfreq(m,d=dt)
-    kx=np.fft.fftfreq(n,d=dx/(2.0*np.pi))
-    freq=np.fft.fftshift(freq)
-    kx=np.fft.fftshift(kx)
+    freq = fft.fftshift(fft.fftfreq(m, dt))
+    kx = fft.fftshift(fft.fftfreq(n, dx/(2.0*np.pi)))
 
-    f=open(file_name,"w")
-    f.write('# kx (nm^-1)        frequency (GHz)        FFT_Power (arb. unit)\n')
-    
-    for j in range(n):
-        for i in range(m):
-            f.write("%15g      %15g      %15g\n" % (kx[n-j-1], freq[i], res[i][j]))
-        f.write('\n')
-    f.close()
-
-
-
+    with open(dispersion_data_file, "w") as f:
+        f.write('# kx (nm^-1)        frequency (GHz)        FFT_Power (arb. unit)\n')
+        for j in range(n):
+            for i in range(m):
+                f.write("%15g      %15g      %15g\n" % (kx[n-j-1], freq[i], transformed[i][j]))
+            f.write('\n')
 
 if __name__ == '__main__':
-    if not os.path.exists('relaxed.npy'):
-        relax_system('width-modulated_bar.geo')
+    if not os.path.exists(initial_m_file):
+        print "Creating initial magnetisation."
+        relax_system()
     
-    if not os.path.exists('data.npy'):
-        excite_system('width-modulated_bar.geo')
+    if not os.path.exists(m_for_fourier_analysis_file):
+        print "Running simulation with excitation."
+        excite_system()
     
-    if not os.path.exists('dispersion.dat'):
-        compute_dispersion(2,1e-3,"dispersion.dat")
+    if not os.path.exists(dispersion_data_file):
+        "Computing dispersion relation."
+        compute_dispersion(2, 1e-3)
     
-    if not os.path.exists('dispersion.png'):
-        cmd=('gnuplot','plot.gnu')
+    if not os.path.exists(dispersion_plot_file):
+        print "Calling gnuplot to create dispersion plot."
+        cmd = ('gnuplot', 'plot.gnu')
         subprocess.check_call(cmd)
