4c27c94060f32434fa117509d75e93fa8bd43aef
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2012-12-17 11:09:16 +0100

    add diagnostics to failing test, produces plot that highlights the problem

--- src/finmag/tests/test_sim_ode.py
+++ src/finmag/tests/test_sim_ode.py
@@ -1,28 +1,40 @@
+import os
 import dolfin as df
 import numpy as np
+import matplotlib.pyplot as plt
 from finmag import Simulation as Sim
 from finmag.energies import Zeeman
 
-def test_sim_ode():
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+def test_sim_ode(do_plot=False):
     mesh = df.Box(0, 0, 0, 2, 2, 2, 1, 1, 1)
-
     sim = Sim(mesh, 8.6e5, unit_length=1e-9)
     sim.alpha = 0.1
     sim.set_m((1, 0, 0))
 
-    H0=1e5
-    zeeman=Zeeman((0,0,H0))
-    sim.add(zeeman)
+    H0 = 1e5
+    sim.add(Zeeman((0, 0, H0)))
 
-    times = np.linspace(0, 1e-11, 100)
-    g=sim.gamma/(1+sim.alpha**2)*sim.alpha*H0
+    ts = np.linspace(0, 1e-11, 100)
 
-    for t in times:
-        sim.run_until(t)
-        m=sim.llg._m.vector().array()
-        mz=m[-1]
-        assert abs(mz-np.tanh(g*t))<1e-8
+    precession_coeff = sim.gamma / (1 + sim.alpha ** 2)
+    mz_ref = np.tanh(precession_coeff * sim.alpha * H0 * ts)
 
-if __name__=="__main__":
-     test_sim_ode()
+    mz = []
+    for t in ts:
+        sim.run_until(t, save_averages=False)
+        mz.append(sim.m[-1]) # same as m_average for this macrospin problem
+
+    if do_plot:
+        ts_ns = ts * 1e9
+        plt.plot(ts_ns, mz, "b.", label="computed") 
+        plt.plot(ts_ns, mz_ref, "r-", label="analytical") 
+        plt.xlabel("time (ns)")
+        plt.ylabel("mz")
+        plt.title("integrating a macrospin")
+        plt.legend()
+        plt.savefig(os.path.join(MODULE_DIR, "test_sim_ode.png"))
+
+if __name__ == "__main__":
+    test_sim_ode(do_plot=True)
