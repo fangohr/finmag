8585c1841a4ddbd625d40139e4de9e4b08d038e0
Hans Fangohr <fangohr@soton.ac.uk>
2012-12-18 07:26:39 +0000

    Trying to make clearer that the second version's speed has nothing to do with numpy, but is an explicit (Python) loop over all nodes.

--- sandbox/benchmarks/updating_expression_simple.py
+++ sandbox/benchmarks/updating_expression_simple.py
@@ -9,11 +9,11 @@ different points in time. The cost of updating f a couple of times is measured.
 
 """
 
-L = math.pi / 2; dL = 20;
-mesh = df.Box(0, 0, 0, L, L, L, dL, dL, dL)
+L = math.pi / 2; n = 20;
+mesh = df.Box(0, 0, 0, L, L, L, n, n, n)
 ts = np.linspace(0, math.pi / 2, 100)
 
-# dolfin code
+# dolfin expression code
 
 S = df.FunctionSpace(mesh, "CG", 1)
 expr = df.Expression("sin(x[0]) * sin(t)", t = 0.0)
@@ -22,16 +22,16 @@ start = time.time()
 for t in ts:
     expr.t = t
     f_dolfin = df.interpolate(expr, S)
-print "Time needed for dolfin: {:.2g}.".format(time.time() - start)
+print "Time needed for dolfin expression: {:.2g}.".format(time.time() - start)
 
-# numpy code
+# explicit loop code
 
-f_numpy = np.empty(mesh.num_vertices())
+f_loop = np.empty(mesh.num_vertices())
 
 start = time.time()
 for t in ts:
     for i, (x, y, z) in enumerate(mesh.coordinates()):
-        f_numpy[i] = math.sin(x) * math.sin(t)
-print "Time needed for numpy: {:.2g}.".format(time.time() - start)
+        f_loop[i] = math.sin(x) * math.sin(t)
+print "Time needed for loop: {:.2g}.".format(time.time() - start)
 
-assert np.max(np.abs(f_dolfin.vector().array() - f_numpy)) < 1e-14
+assert np.max(np.abs(f_dolfin.vector().array() - f_loop)) < 1e-14
