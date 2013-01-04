aa981b4c43dec440990eab3311d0e3453e369af8
Weiwei Wang <w.wang@soton.ac.uk>
2013-01-04 19:10:32 +0000

    add treecode bem to main directory

---
+++ native/src/tree_code_bem/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+import os
+
+#python setup.py build_ext --inplace
+
+ext_modules = [
+    Extension("treecode_bem",
+              sources = ['treecode_bem.c','treecode_bem_lib.pyx'],
+              include_dirs = [numpy.get_include()],
+	      libraries=['m'],
+              #libraries=['m','gomp'],
+              #extra_compile_args=["-fopenmp"],
+              #extra_link_args=["-g"],
+        )
+    ]
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = ext_modules
+)
+
