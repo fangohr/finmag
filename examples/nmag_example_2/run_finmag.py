fdd01f8c1c4c81a46759666b62ea504657fcdb8e
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-01-17 13:33:24 +0100

    let's not care about spatially resolved fields for now

--- examples/nmag_example_2/run_finmag.py
+++ examples/nmag_example_2/run_finmag.py
@@ -3,12 +3,13 @@ from finmag import Simulation
 from finmag.energies import Exchange, Demag
 
 mesh = df.Mesh("bar.xml.gz")
-Ms = 0.86e6
 
-sim = Simulation(mesh, Ms, unit_length=1e-9, name="finmag_bar")
+sim = Simulation(mesh, Ms=0.86e6, unit_length=1e-9, name="finmag_bar")
 sim.set_m((1, 0, 1))
 sim.alpha = 0.5
+
 sim.add(Exchange(13.0e-12))
 sim.add(Demag())
+
 sim.schedule(Simulation.save_averages, every=5e-12)
 sim.run_until(3e-10)
