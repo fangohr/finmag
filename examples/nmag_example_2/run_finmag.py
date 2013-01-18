912989e01e5a6f2289310f30b008d61505417a56
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-01-18 19:09:58 +0100

    add test for nmag example 2

--- examples/nmag_example_2/run_finmag.py
+++ examples/nmag_example_2/run_finmag.py
@@ -3,19 +3,23 @@ from finmag import Simulation
 from finmag.energies import Exchange, Demag
 from finmag.util.timings import timings
 
-mesh = df.Mesh("bar.xml.gz")
+def run_simulation():
+    mesh = df.Mesh("bar.xml.gz")
 
-sim = Simulation(mesh, Ms=0.86e6, unit_length=1e-9, name="finmag_bar")
-sim.set_m((1, 0, 1))
-sim.alpha = 0.5
+    sim = Simulation(mesh, Ms=0.86e6, unit_length=1e-9, name="finmag_bar")
+    sim.set_m((1, 0, 1))
+    sim.alpha = 0.5
 
-sim.add(Exchange(13.0e-12))
-sim.add(Demag())
+    sim.add(Exchange(13.0e-12))
+    sim.add(Demag())
 
-sim.schedule(Simulation.save_averages, every=5e-12)
-sim.run_until(3e-10)
+    sim.schedule(Simulation.save_averages, every=5e-12)
+    sim.run_until(3e-10, save_averages=False)
 
-print timings
-print "The RHS was evaluated {} times, while the Jacobian was computed {} times.".format(
-        sim.integrator.stats()['nfevals'],
-        timings._timings['LLG::sundials_jtimes'].calls)
+    print timings
+    print "The RHS was evaluated {} times, while the Jacobian was computed {} times.".format(
+            sim.integrator.stats()['nfevals'],
+            timings._timings['LLG::sundials_jtimes'].calls)
+
+if __name__ == "__main__":
+    run_simulation()
