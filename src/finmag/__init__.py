d00a8be4e696528f58420d31552e83826e949727
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-01-23 15:00:09 +0000

    set reorder_dofs_serial to False to fix failing tests for now

--- src/finmag/__init__.py
+++ src/finmag/__init__.py
@@ -5,3 +5,13 @@
 # CONTACT: h.fangohr@soton.ac.uk
 #
 from init import *
+
+# dolfin 1.1.0 re-orders the degrees of freedom
+# to speed up some iterative algorithms. However,
+# this means that the ordering doesn't correspond
+# to geometrical structures (such as vertices)
+# anymore. For now, we disable the re-ordering.
+# In the future, we should enable it as it will
+# help with parallel execution.
+import dolfin as df
+df.parameters.reorder_dofs_serial = False
