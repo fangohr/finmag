Fix an extremely subtle bug where probing fields would return wrong results if the probing point coordinates are integers.
This is because np.emtpy_like() uses the dtype of the input array,
so whenever any elements in 'res' are set in the loop, these are
automatically cast to integer values.

Phew, this one has cost me a good few hours...

src/finmag/util/helpers.py
@@ -921,9 +921,8 @@ def probe(dolfin_function, points):
         except RuntimeError:
             return np.array([np.NaN, np.NaN, np.NaN])
 
-    res = np.empty_like(points)
+    res = np.empty(points.shape)
     loop_indices = itertools.product(*map(xrange, points.shape[:-1]))
     for idx in loop_indices:
         res[idx] = _safe_single_probe(points[idx])
     return res
-