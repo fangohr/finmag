07bc3871854d562dd43c074a60ec1042ddbe268c
Hans Fangohr <fangohr@soton.ac.uk>
2013-02-01 16:52:18 +0000

    adding restarting tutorial

--- doc/tailored_tutorials/hesjedahl.py
+++ doc/tailored_tutorials/hesjedahl.py
@@ -6,4 +6,5 @@ conf = {'nameshort': 'Hesjedahl',
                        'tutorial-use-of-logging',
                        'tutorial-running-simulations-with-dmi',
                        'tutorial-sampling-m-at-arbitrary-positions',
+                       'ref-restarting-simulations'
                        ]}
