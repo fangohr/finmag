2e10828395ced0e18e03bbd0ab05a49deadb2dc2
Weiwei Wang <w.wang@soton.ac.uk>
2013-02-03 13:41:46 +0000

    update

--- src/finmag/llb/sllg_test.py
+++ src/finmag/llb/sllg_test.py
@@ -159,14 +159,44 @@ def test_sllg_save_data():
         sim.run_until(t)
         
     
+def plot_random_number():
+    from finmag.native.llb import RandomMT19937
+    mt=RandomMT19937() 
+    mt.initial_random(422353390)
+    
+    x=np.zeros(10000000,dtype=np.float)
+    for i in range(100):    
+        mt.gaussian_random_np(x)
+        if i>80:
+            plt.cla()
+            plt.hist(x, 100, normed=1, facecolor='green', alpha=0.75)
+            plt.grid(True)
+            plt.savefig(os.path.join(MODULE_DIR, "test_mt19937_%d.png"%i))
+        
+        print 'step=',i
+        
 
+def plot_random_number_np():
+    
+    np.random.seed(422353390)
+    
+    for i in range(100):
+        x=np.random.randn(10000000)    
+    
+        plt.cla()
+        plt.hist(x, 100, normed=1, facecolor='green', alpha=0.75)
+        plt.grid(True)
+        plt.savefig(os.path.join(MODULE_DIR, "test_np_%d.png"%i))
+    
     
 
 if __name__ == "__main__":
     #test_sllg_zero_temperature(do_plot=True)
     #test_sllg_100(do_plot=True)
     #test_sllg_time()
-    test_sllg_save_data()
+    #test_sllg_save_data()
+    plot_random_number()
+    #plot_random_number_np()
     
 
 
