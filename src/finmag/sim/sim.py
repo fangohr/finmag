481b2a52553222727dfc8a0adf19c114a76d5b92
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-02-05 14:03:59 +0000

    Move sim_with() back into sim.py (to avoid circular imports, because sim_helpers.py should be importable from sim.py which it currently isn't).

--- src/finmag/sim/sim.py
+++ src/finmag/sim/sim.py
@@ -10,6 +10,7 @@ from finmag.util.fileio import Tablewriter
 from finmag.util import helpers
 from finmag.util.vtk_saver import VTKSaver
 from finmag.sim.hysteresis import hysteresis as hyst, hysteresis_loop as hyst_loop
+from finmag.energies import Exchange, Zeeman, Demag, UniaxialAnisotropy, DMI
 from finmag.integrators.llg_integrator import llg_integrator
 from finmag.integrators import scheduler, relaxation
 from finmag.util import restart
@@ -498,3 +499,50 @@ class Simulation(object):
                 info_string += added_info(l_bloch, 'Bloch parameter', 'l_bloch')
 
         return info_string
+
+
+def sim_with(mesh, Ms, m_init, alpha=0.5, unit_length=1, integrator_backend="sundials",
+             A=None, K1=None, K1_axis=None, H_ext=None, demag_solver='FK',
+             D=None, name="unnamed"):
+    """
+    Create a Simulation instance based on the given parameters.
+
+    This is a convenience function which allows quick creation of a
+    common simulation type where at most one exchange/anisotropy/demag
+    interaction is present and the initial magnetisation is known.
+
+    If a value for any of the optional arguments A, K1 (and K1_axis),
+    or demag_solver are provided then the corresponding exchange /
+    anisotropy / demag interaction is created automatically and added
+    to the simulation. For example, providing the value A=13.0e-12 in
+    the function call is equivalent to:
+
+       exchange = Exchange(A)
+       sim.add(exchange)
+    """
+    sim = Simulation(mesh, Ms, unit_length=unit_length,
+                     integrator_backend=integrator_backend,
+                     name=name)
+
+    sim.set_m(m_init)
+    sim.alpha = alpha
+
+    # If any of the optional arguments are provided, initialise
+    # the corresponding interactions here:
+    if A is not None:
+        sim.add(Exchange(A))
+    if (K1 != None and K1_axis is None) or (K1 is None and K1_axis != None):
+        log.warning(
+            "Not initialising uniaxial anisotropy because only one of K1, "
+            "K1_axis was specified (values given: K1={}, K1_axis={}).".format(
+                K1, K1_axis))
+    if K1 != None and K1_axis != None:
+        sim.add(UniaxialAnisotropy(K1, K1_axis))
+    if H_ext != None:
+        sim.add(Zeeman(H_ext))
+    if D != None:
+        sim.add(DMI(D))
+    if demag_solver != None:
+        sim.add(Demag(solver=demag_solver))
+
+    return sim
