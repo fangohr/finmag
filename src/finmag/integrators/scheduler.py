0e3f66a51bbc56c891cedf265846e0069885ff9c
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-02-05 14:25:49 +0000

    Move save_ndt() into sim_helpers.py

--- src/finmag/integrators/scheduler.py
+++ src/finmag/integrators/scheduler.py
@@ -312,12 +312,3 @@ class Scheduler(object):
         self.items = []
         self.stop_realtime_jobs()
         self.realtime_items = []
-
-def save_ndt(sim):
-    """Given the simulation object, saves one line to the ndt finalise
-    (through the TableWriter object)."""
-    if sim.driver == 'cvode':
-        log.debug("Saving data to ndt file at t={} (sim.name={}).".format(sim.t, sim.name))
-    else:
-        raise NotImplementedError("Only cvode driver known.")
-    sim.tablewriter.save()
