a7e4b5525f6b54c10a5a8e6fb938d5e636ecce59
Weiwei Wang <w.wang@soton.ac.uk>
2013-02-11 09:50:08 +0000

    modify pbc for DMI

--- src/finmag/energies/dmi.py
+++ src/finmag/energies/dmi.py
@@ -214,8 +214,8 @@ class DMI(EnergyBase):
             H_dmi_np = dmi_np.compute_field()
     """
 
-    def __init__(self, D, method="box-matrix-petsc"):
-        super(DMI, self).__init__(method, in_jacobian=True)
+    def __init__(self, D, method="box-matrix-petsc",pbc2d=None):
+        super(DMI, self).__init__(method, in_jacobian=True,pbc2d=pbc2d)
         self.D = D
 
     @mtimed
