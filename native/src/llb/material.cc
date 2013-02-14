54651dfd35d2b5ab2acc511e34286c3e1e9770f2
Weiwei Wang <w.wang@soton.ac.uk>
2013-02-14 23:00:07 +0000

    forgot to add source file

---
+++ native/src/llb/material.cc
@@ -0,0 +1,213 @@
+#include "finmag_includes.h"
+
+#include "util/np_array.h"
+
+#include "llb.h"
+
+
+namespace finmag { namespace llb {
+
+	static const double const_MU0 = M_PI*4e-7; // T m/A
+	static const double const_K_B = 1.3806488e-23; // J/K
+
+
+    class Materials {
+
+    	private:
+        	double Ms0;
+        	double Tc;
+        	double A0;
+        	double K0;
+        	double mu_a;//atomistic magnetic moment
+
+
+    	public:
+        	Materials(double Ms,double Tc,double A,double K, double mu_a){
+        		this->Ms0=Ms;
+        		this->Tc=Tc;
+        		this->A0=A;
+        		this->K0=K;
+        		this->mu_a=mu_a;
+        	};
+        	double m_e(double T);
+        	double Ms(double T);
+        	double A(double T);
+        	double chi_par(double T);
+        	double chi_perp(double T);
+        	double inv_chi_par(double T);
+        	double inv_chi_perp(double T);
+        	void compute_relaxation_field(
+ 	           	   const np_array<double> & T_arr,
+ 	           	   const np_array<double> & M,
+ 	           	   const np_array<double> & H);
+
+    };
+
+    double Materials::m_e(double T){
+
+    	double a0=1.313707215324465;
+    	double a1=-0.159267621771943;
+    	double a2=-0.325981045855364;
+    	double a3=0.172988576034952;
+
+    	double t=1.0-T/Tc;
+    	if (t<=0){
+    		return 0;
+    	}else if (t>=1){
+    		return 1.0;
+    	}
+
+    	double res=a0*sqrt(t)+t*(a1+t*a2+a3*t*t);
+
+    	return res;
+    }
+
+    double Materials::Ms(double T){
+    	return Ms0*m_e(T);
+    }
+
+    double Materials::A(double T){
+    	double a0=0.021566904000209;
+    	double a1=2.428983868562720;
+    	double a2=-3.565579644828024;
+    	double a3=3.247355368671055;
+    	double a4=-1.140849683260966;
+
+    	double t=1.0-T/Tc;
+
+    	if (t<=0){
+    		return 0;
+    	}else if (t>=1){
+    		return A0;
+    	}
+
+    	double res=a0 + t * (a1 + t * (a2 + t * (a3 + t*a4)));
+
+    	return res*A0;
+    }
+
+
+    double Materials::chi_par(double T){
+
+    			double eps=1e-3;
+    			double x=T/Tc;
+    			double res=0;
+
+    			if (x<1){
+    				double a0=0.153298742094880;
+    				double a1=-1.249707138220772;
+    				double a2=2.809708026822461;
+    				double a3=-2.537192200789637;
+    				double a4=0.824002473372640;
+
+    				if (x<eps){
+    					x=eps;
+    				}else if (x>1-eps){
+    					x=1-eps;
+    				}
+
+    				double t=1-x;
+
+    				res=a0/t+t*(a1+t*(a2+a3*t+a4*t*t));
+
+    			}else{
+    				if (x<1+eps){
+    					x=1+eps;
+    				}
+    				res=0.333333333333333/(x-1.0);
+    			}
+
+    			return res*(mu_a/(const_K_B*Tc));
+
+            }
+
+
+    double Materials::chi_perp(double T){
+
+    			double x=T/Tc;
+
+    			if (x<1){
+    				double a0=0.002759963555555556;
+
+    				if(x<0){
+    					x=0;
+    				}
+
+    				double res=1+a0*x;
+
+    				return Ms0/(2*K0)*res;
+
+    			}else{
+
+    				return chi_par(T);
+    			}
+
+            }
+
+    double Materials::inv_chi_par(double T){
+    	return 1.0/chi_par(T);
+    }
+
+    double Materials::inv_chi_perp(double T){
+    	return 1.0/chi_perp(T);
+    }
+
+
+    void Materials::compute_relaxation_field(
+    	           	   const np_array<double> & T_arr, //input
+    	           	   const np_array<double> & M,
+    	           	   const np_array<double> & H){ //output
+
+
+    	            	int length=T_arr.size();
+    	            	assert(length*3==M.size());
+
+    	            	double *T = T_arr.data();
+    	            	double *m=M.data();
+    	            	double *h=H.data();
+
+    	            	int i2,i3;
+    	            	for (int i1 = 0; i1 < length; i1++) {
+    	            		i2=length+i1;
+    	            		i3=length+i2;
+
+    	            		double temp = T[i1];
+    	            		// calculate the relaxation coefficient
+    	            		double me = this->m_e(temp);
+    	            		double me_sq = me * me;
+    	            		double r;
+    	            		double m_sq = m[i1]*m[i1] + m[i2]*m[i2] + m[i3]*m[i3];
+
+    	            		if (temp <= Tc) {
+    	            			r = 0.5 * (1. - m_sq/me_sq);
+    	            		} else {
+    	            			r = -1. - 0.6 * Tc / (temp - Tc) * m_sq;
+    	            		}
+
+    	            		double coeff = r * inv_chi_par(temp);
+
+    	            		h[i1] = coeff * m[i1];
+    	            		h[i2] = coeff * m[i2];
+    	            		h[i3] = coeff * m[i3];
+
+    	            	}
+    	         }
+
+
+    void register_llb_material() {
+        using namespace bp;
+
+        class_<Materials>("Materials", init<
+        		double,double,double,double,double>())
+        		.def("m_e", &Materials::m_e)
+        		.def("Ms", &Materials::Ms)
+        		.def("A", &Materials::A)
+        		.def("chi_par", &Materials::chi_par)
+        		.def("chi_perp", &Materials::chi_perp)
+        		.def("inv_chi_par", &Materials::inv_chi_par)
+        		.def("inv_chi_perp", &Materials::inv_chi_perp)
+        		.def("compute_relaxation_field",&Materials::compute_relaxation_field);
+    }
+
+
+}}
