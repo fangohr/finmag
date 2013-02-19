94052a3aaccd6590fa8f86c58f15022899db1f78
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-02-19 02:01:09 +0000

    Bug fix: name and group parameters in timings were inverted in changeset 3024:820bf7533b82.

--- examples/demag/short_nmag_comp_tests2.py
+++ examples/demag/short_nmag_comp_tests2.py
@@ -136,7 +136,7 @@ for i,maxh in enumerate(meshsizes):
         endtime = time.time()
 
         #Store the times
-        runtimes["bem"][demagtype].append(sb.demag_timings.time(finmagsolvers[demagtype].__name__, "build BEM"))
+        runtimes["bem"][demagtype].append(sb.demag_timings.time("build BEM", finmagsolvers[demagtype].__name__))
         runtimes["solve"][demagtype].append(endtime - starttime)
         
 
