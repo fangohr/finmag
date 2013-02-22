f0eaf66c2478bec5e0a7e89fac26942954df3879
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-02-22 18:16:32 +0000

    Let's be venturous and test all ipython notebooks. This also serves as a good reminder for which ones we still need to deal with.

--- bin/reproduce_ipython_notebooks.py
+++ bin/reproduce_ipython_notebooks.py
@@ -2,11 +2,16 @@
 
 from IPython.nbformat.current import reads
 from ipnbdoctest import test_notebook as reproduce_notebook
+from glob import glob
 import pytest
 
-ipynb_files = ['doc/ipython_notebooks_src/tutorial-using-ipython-notebook.ipynb',  # passes
-               #'doc/ipython_notebooks_src/tutorial-use-of-logging.ipynb',  # fails
-              ]
+#ipynb_files = ['doc/ipython_notebooks_src/tutorial-using-ipython-notebook.ipynb',  # passes
+#               #'doc/ipython_notebooks_src/tutorial-use-of-logging.ipynb',  # fails
+#              ]
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ipynb_files = sorted(glob(os.path.join(MODULE_DIR, '../doc/ipython_notebooks_src/*.ipynb'))
 
 @pytest.mark.parametrize("ipynb", ipynb_files)
 def test_reproduce_ipython_notebook(ipynb):
