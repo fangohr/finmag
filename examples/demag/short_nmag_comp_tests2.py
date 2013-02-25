829853793b0e8fdf3b7ed693d8b6e1ed6754c2e2
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-02-25 13:49:58 +0000

    failing doc build: Running out of commit messages. Show nmag logfile, call make filename of mesh only, not full path

--- examples/demag/short_nmag_comp_tests2.py
+++ examples/demag/short_nmag_comp_tests2.py
@@ -201,7 +201,7 @@ for i,maxh in enumerate(meshsizes):
     print "Will need meshfile '{}'. Calling makefile now.".format(nmag_meshfile)
 
     try:
-        print subprocess.check_output(["make", nmag_meshfile], stderr=subprocess.STDOUT)
+        print subprocess.check_output(["make", os.path.split(nmag_meshfile)[1]], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         print "Failed with returncode {}, output:\n{}".format(e.returncode, e.output)
         raise
@@ -221,6 +221,8 @@ for i,maxh in enumerate(meshsizes):
         print subprocess.check_output(["make", nmagoutput, "MESH={}".format(nmag_meshfile)], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as e:
         print "Failed with returncode {}, output:\n{}".format(e.returncode, e.output)
+        with open("run_nmag_log.log", "r") as f:
+            print "Contents of nmag logfile:\n{}.".format(f.read())
         raise
     endtime = time.time()
 
