b631edd1837798205cd11e8459ccd7def7795567
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-03-07 16:52:30 +0000

    Minor cleanup in test: remove duplicate definition of 'tolerance', move variable definitions inside the test_validation() function.

--- src/finmag/tests/slonczewski/validation/test_validation.py
+++ src/finmag/tests/slonczewski/validation/test_validation.py
@@ -2,13 +2,12 @@ import os
 import run_validation_sim as sim
 import numpy as np
 
-epsilon = 1e-16
-tolerance = 1e-4
-nmag_file = os.path.join(sim.MODULE_DIR, "averages_nmag5.txt")
-
 def test_validation():
+    nmag_file = os.path.join(sim.MODULE_DIR, "averages_nmag5.txt")
+    epsilon = 1e-16
+
     #short run:
-    tmax = 0.05e-9; tolerance = 5e-6
+    tmax = 0.05e-9; tolerance = 2e-6
 
     #long run
     #tmax = 10e-9; tolerance = 1e-4
@@ -16,12 +15,12 @@ def test_validation():
     n = sim.run_simulation(t_max=tmax)
 
     averages = np.loadtxt(sim.averages_file)
-    nmag_avg = np.loadtxt(nmag_file)[:n,:]
+    nmag_avg = np.loadtxt(nmag_file)[:n, :]
 
     diff = np.abs(np.array(averages) - np.array(nmag_avg))
     print("Deviation is %s" % (np.max(diff)))
-    assert np.max(diff[:,0]) < epsilon # compare times
-    assert np.max(diff[:,1:]) < tolerance
+    assert np.max(diff[:, 0]) < epsilon # compare times
+    assert np.max(diff[:, 1:]) < tolerance
 
 
 # Note: On osiris, the time integration in this example seems to get
