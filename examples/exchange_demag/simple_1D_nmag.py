a9032e6d7edb76fad8f36de6588343d9ea6a2a3b
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-03-08 13:52:52 +0000

    Cosmetics: wrap up functionality of the script in a main() function.

--- examples/exchange_demag/simple_1D_nmag.py
+++ examples/exchange_demag/simple_1D_nmag.py
@@ -3,31 +3,37 @@ import nmag, os
 from nmag import SI
 import nmeshlib.unidmesher as unidmesher
 
-MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-L = 10
-mesh_unit = SI(1e-9, "m")   # mesh unit (1 nm)
-layers = [(0.0, L)]         # the mesh
-discretization = 0.1        # discretization
+def main():
+    MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Initial magnetization
-xfactor = float(SI("m")/(L*mesh_unit))
+    L = 10
+    mesh_unit = SI(1e-9, "m")   # mesh unit (1 nm)
+    layers = [(0.0, L)]         # the mesh
+    discretization = 0.1        # discretization
 
-def m0(r):
-    return [np.cos(r[0]*np.pi*xfactor), np.sin(r[0]*np.pi*xfactor), 0]
+    # Initial magnetization
+    xfactor = float(SI("m")/(L*mesh_unit))
 
-mat_Py = nmag.MagMaterial(name="Py",
-                          Ms=SI(1,"A/m"))
+    def m0(r):
+        return [np.cos(r[0]*np.pi*xfactor), np.sin(r[0]*np.pi*xfactor), 0]
 
-sim = nmag.Simulation("Hans' configuration", do_demag=False)
+    mat_Py = nmag.MagMaterial(name="Py",
+                              Ms=SI(1,"A/m"))
 
-mesh_file_name = '1d.nmesh'
-mesh_lists = unidmesher.mesh_1d(layers, discretization)
-unidmesher.write_mesh(mesh_lists, out=mesh_file_name)
+    sim = nmag.Simulation("Hans' configuration", do_demag=False)
 
-sim.load_mesh(mesh_file_name,
-              [("Py", mat_Py)],
-              unit_length=mesh_unit)
-sim.set_m(m0)
+    mesh_file_name = '1d.nmesh'
+    mesh_lists = unidmesher.mesh_1d(layers, discretization)
+    unidmesher.write_mesh(mesh_lists, out=mesh_file_name)
 
-np.save(os.path.join(MODULE_DIR, "nmag_hansconf.npy"), sim.get_subfield("E_exch_Py"))
+    sim.load_mesh(mesh_file_name,
+                  [("Py", mat_Py)],
+                  unit_length=mesh_unit)
+    sim.set_m(m0)
+
+    np.save(os.path.join(MODULE_DIR, "nmag_hansconf.npy"), sim.get_subfield("E_exch_Py"))
+
+
+if __name__ == '__main__':
+    main()
