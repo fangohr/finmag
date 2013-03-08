0c2190a4f84a3bb8583ccad2162d0f60083c188c
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-03-08 08:53:31 +0000

    Restructure and simplify test script. Mainly, we try to avoid calling 'make' via a subprocess because this seems to behave weirdly in Jenkins.

--- examples/nmag_example_2/test_nmag_example_2.py
+++ examples/nmag_example_2/test_nmag_example_2.py
@@ -1,6 +1,7 @@
 import os
 import subprocess as sp
 import numpy as np
+import run_finmag
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,25 +10,29 @@ def test_against_nmag():
     os.chdir(MODULE_DIR)
 
     try:
-        try:
-            sp.check_output(['make', 'clean'], stderr=sp.STDOUT)
+        cmd = ['make', 'clean']
+        sp.call(cmd)
 
-            # the nmag file should be in version control. However, it is
-            # convenient that the test can recompute it if needed.
-            filename = "averages_ref.txt"
-            sp.check_output(['make', filename], stderr=sp.STDOUT)
-            m_nmag = np.genfromtxt(os.path.join(MODULE_DIR, filename))
+        # the nmag file should be in version control. However, it is
+        # convenient that the test can recompute it if needed.
+        filename = 'averages_ref.txt'
+        cmd = ['make', filename]
+        sp.call(cmd)
+        m_nmag = np.genfromtxt(os.path.join(MODULE_DIR, filename))
 
-            filename = "averages.txt"
-            sp.check_output(['make', filename], stderr=sp.STDOUT)
-            m_finmag = np.genfromtxt(os.path.join(MODULE_DIR, filename))
-        except sp.CalledProcessError as ex:
-            print("Running 'make {}' was unsuccessful. The error "
-                  "message was: {}".format(filename, ex.output))
-            raise
+        if not os.path.exists("averages.txt"):
+            run_finmag.run_simulation()
+            cmd = 'ncol finmag_bar.ndt time m_x m_y m_z > averages.txt'
+            sp.call(cmd, shell=True)
+        m_finmag = np.genfromtxt(os.path.join(MODULE_DIR, 'averages.txt'))
+
+    except sp.CalledProcessError as ex:
+        print("Running command '{}' was unsuccessful. The error "
+              "message was: {}".format(cmd, ex.output))
+        raise
 
     finally:
         os.chdir(cwd_backup)
 
-    np.testing.assert_allclose(m_nmag, m_finmag, rtol=1e-4, atol=1e-4)
+    assert max(map(np.linalg.norm, m_nmag - m_finmag)) < 3e-5
     # atol is 0 by default when using assert_allclose
