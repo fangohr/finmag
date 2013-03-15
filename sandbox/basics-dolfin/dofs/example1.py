d941008b838b8b0d3460f17935a77dfc0fa3fc1f
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-03-15 16:18:44 +0000

    Extend dof example to illustrate what happens in parallel (which makes the dofmap appear a lot more meaningful).

--- sandbox/basics-dolfin/dofs/example1.py
+++ sandbox/basics-dolfin/dofs/example1.py
@@ -7,15 +7,33 @@
 # I am gathering this here, to push forward the whole dofmap question
 # that was introduced with dolfin 1.1 (HF, Feb 2013)
 
+# Addendum: The main (whole?) point of the dofmap seems to be
+# for bookkeeping purposes when dolfin is running in parallel.
+# Thus we're now also printing the process number and cell id
+# in each process. It is instructive to look at the output of
+# a command like this:
+#
+#    mpirun -n 6 python example1.py | grep "cell #" | sort
+#
+# So it seems that cell id's are local to each process, and
+# that V.dofmap().cell_dofs() gives a mapping from the local
+# degrees of freedom of the cell to the global degrees of freedom.
+#
+# (Max, 15.3.2013)
+
 
 import dolfin as df
+from mpi4py import MPI
 
-mesh = df.UnitSquareMesh(10,10)
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-V = df.FunctionSpace(mesh, 'CG', 1)
+mesh = df.UnitSquareMesh(4, 4)
+
+V = df.VectorFunctionSpace(mesh, 'CG', 1, dim=3)
 
 x = V.dofmap()
 
 for cell in df.cells(mesh):
-    print cell, V.dofmap().cell_dofs(cell.index())
-	
+    print "Process {}/{}, cell #{}:  {}, {}".format(rank, size, cell.index(), cell, V.dofmap().cell_dofs(cell.index()))
