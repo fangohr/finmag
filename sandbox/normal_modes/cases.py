9d3a61a89ffbb811f1c7fe625415d533d7c56c1a
Dmitri Chernyshenko <d.chernyshenko@soton.ac.uk>
2013-03-21 20:29:12 +0000

    Implemented the correct eigenvalue problem for the normal modes. Unfortunately, the numerical iteration does not converge, probably due to a bug somewhere

--- sandbox/normal_modes/cases.py
+++ sandbox/normal_modes/cases.py
@@ -1,4 +1,8 @@
+import dolfin as df
+
 from finmag import Simulation
+from finmag.energies import Demag, Exchange, Zeeman
+from finmag.util.consts import Oersted_to_SI
 
 class StdProblem(object):
     def __init__(self):
@@ -27,3 +31,27 @@ class StdProblem(object):
 
     def name(self):
         return "stdproblem-%dx%dx%d" % tuple(self.box_divisions)
+
+class Grimsditch2004(object):
+
+    def setup_sim(self, m0):
+        SX, SY, SZ = 116, 60, 20
+        nx, ny, nz = 29, 15, 5
+        # Fe, ref: PRB 69, 174428 (2004)
+        # A = 2.5e-6 erg/cm^3
+        # M_s = 1700 emu/cm^3
+        # gamma = 2.93 GHz/kOe
+        Ms = 1700e3
+        A = 2.5e-6*1e-5
+        gamma_wrong = 2.93*1e6/Oersted_to_SI(1.) # wrong by a factor of 6 (?)
+        Hzeeman = [10e3*Oersted_to_SI(1.), 0, 0]
+
+        mesh = df.BoxMesh(0, 0, 0, SX, SY, SZ, nx, ny, nz)
+
+        sim = Simulation(mesh, Ms)
+        sim.set_m(m0)
+        sim.add(Demag())
+        sim.add(Exchange(A))
+        sim.add(Zeeman(Hzeeman))
+
+        return sim
