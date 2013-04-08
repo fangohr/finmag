519fcd3c4a969739028fd09964642e9a65ff2847
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-04-08 12:17:20 +0100

    More modular tolerance settings in thin film demag test (allows us to use absolute tolerance for zero_thin_film_demag and relative tolerances for the other tests).

--- src/finmag/energies/thin_film_demag_test.py
+++ src/finmag/energies/thin_film_demag_test.py
@@ -3,10 +3,9 @@ import dolfin as df
 from finmag import Simulation as Sim
 from finmag.energies import ThinFilmDemag, Demag
 
-TOLERANCE = 1e-8
 Ms = 8.6e5
 
-def compare_with_demag_from_initial_m(H_gen, m_init):
+def compare_with_demag_from_initial_m(H_gen, m_init, atol=0, rtol=0):
     sim = Sim(df.UnitCubeMesh(2, 2, 2), Ms)
     sim.set_m(m_init)
 
@@ -19,14 +18,17 @@ def compare_with_demag_from_initial_m(H_gen, m_init):
     print "Expected, with shape {}:\n".format(H_expected.shape), H_expected
     print "Got, with shape {}:\n".format(H_computed.shape), H_computed
     print "Difference:\n", diff
-    assert np.max(diff) < TOLERANCE
+    assert np.allclose(H_computed, H_expected, atol=atol, rtol=rtol)
+
 
 def test_zero_thin_film_demag():
-    compare_with_demag_from_initial_m(lambda m: np.zeros(m.shape), (1, 0, 0))
-    compare_with_demag_from_initial_m(lambda m: np.zeros(m.shape), (1, 1, 0))
+    compare_with_demag_from_initial_m(lambda m: np.zeros(m.shape), (1, 0, 0), atol=1e-14)
+    compare_with_demag_from_initial_m(lambda m: np.zeros(m.shape), (1, 1, 0), atol=1e-14)
+
 
 def test_thin_film_demag():
-    compare_with_demag_from_initial_m(lambda m: -Ms * m, (0, 0, 1))
+    compare_with_demag_from_initial_m(lambda m: -Ms * m, (0, 0, 1), rtol=1e-14)
+
 
 def test_thin_film_demag_against_real_demag():
     sim = Sim(df.BoxMesh(0, 0, 0, 500e-9, 500e-9, 1e-9, 50, 50, 1), Ms)
@@ -43,7 +45,7 @@ def test_thin_film_demag_against_real_demag():
     print "Standard Demag:\n", H_demag
     print "ThinFilmDemag:\n", H_tfdemag
     print "Difference relative to Ms:\n", diff
-    assert np.max(diff) < 5e-2 # 5%
+    assert np.allclose(H_tfdemag, H_demag, atol=0.05*Ms)  # 5% of Ms
 
     sim.set_m((1, 0, 0))
     H_tfdemag = tfdemag.compute_field().view().reshape((3, -1)).mean(1)
@@ -54,4 +56,4 @@ def test_thin_film_demag_against_real_demag():
     print "Standard Demag:\n", H_demag
     print "ThinFilmDemag:\n", H_tfdemag
     print "Difference relative to Ms:\n", diff
-    assert np.max(diff) < 5e-2 # 5%
+    assert np.allclose(H_tfdemag, H_demag, atol=0.005*Ms)  # 0.5% of Ms
