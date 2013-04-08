1a13db399d3f326fb214bfefbf6817d7bbaee052
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-04-08 19:23:10 +0100

    remove demag.py (moved content to __init__.py)

---
+++ src/finmag/energies/demag/__init__.py
@@ -0,0 +1,17 @@
+import logging
+from fk_demag import FKDemag
+from solver_gcr import FemBemGCRSolver
+from treecode_bem import TreecodeBEM
+
+log = logging.getLogger("finmag")
+KNOWN_SOLVERS = {'FK': FKDemag, 'GCR': FemBemGCRSolver, 'Treecode': TreecodeBEM}
+
+
+def Demag(solver='FK', *args, **kwargs):
+    if not solver in KNOWN_SOLVERS:
+        log.error("Tried to create a Demag object with unknown solver '{}'".format(solver))
+        raise NotImplementedError(
+            "Solver '{}' not implemented. Valid choices: one of '{}'.".format(solver, KNOWN_SOLVERS.keys()))
+
+    log.debug("Creating Demag object with solver '{}'.".format(solver))
+    return KNOWN_SOLVERS[solver](*args, **kwargs)
