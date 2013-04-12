e4d894a5c4a65916cce23c8f85f29996581d545c
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-04-12 22:40:40 +0100

    for film, optimised FK settings against default FK settings against system size

--- src/finmag/energies/demag/benchmark_fk.py
+++ src/finmag/energies/demag/benchmark_fk.py
@@ -1,72 +1,88 @@
 import time
+import pickle
 import numpy as np
 import dolfin as df
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
 from finmag.energies import Demag
 from finmag.util.meshes import box
-import matplotlib.pyplot as plt
+from finmag.native.llg import compute_bem_fk
 
-maxhs = [1.5, 2.0, 3.0, 3.5]
-unit_length = 1e-9
-m_0 = (1, 0, 0)
+now = time.time
+create_mesh = lambda maxh: box(0, 0, 0, 500, 25, 1, maxh, directory="meshes")
+maxhs = np.arange(1.0, 3.7, 0.2)
 Ms = 1
+m_0 = (1, 0, 0)
+unit_length = 1e-9
+default_params = ("default", "default", "default", "default")
+opt_params = ("cg", "ilu", "cg", "ilu")
+repetitions = 10
+results_file = "results_fk_benchmark.txt"
 
-vertices = []
-solvers = ["FK", "FK"]
-solvers_label = ["FK", "FK opt"]
-timings = [[], []]
-errors = [[], []]
 
-for maxh in maxhs:
-    mesh = box(0, 0, 0, 500, 25, 1, maxh=maxh, directory="meshes")
-    vertices.append(mesh.num_vertices())
-    S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1)
-    m = df.Function(S3)
-    m.assign(df.Constant(m_0))
+def run_demag(repetitions, params, S3, m, Ms, unit_length, bem=None, b2g_map=None):
+    demag = Demag("FK")
+    demag.parameters["phi_1_solver"] = params[0]
+    demag.parameters["phi_1_preconditioner"] = params[1]
+    demag.parameters["phi_2_solver"] = params[2]
+    demag.parameters["phi_2_preconditioner"] = params[3]
+    if bem is not None:
+        demag.precomputed_bem(bem, b2g_map)
+    demag.setup(S3, m, Ms, unit_length)
+    start = now()
+    for j in xrange(repetitions):
+        H = demag.compute_field()
+    runtime = (now() - start) / repetitions
+    return H, runtime
 
-    H_ref = None
+try:
+    results = np.loadtxt(results_file)
+except IOError:
+    results = np.zeros((len(maxhs), 4))
+    for i, maxh in enumerate(maxhs):
+        print "Mesh {}/{} with maxh = {:.3}.".format(i + 1, len(maxhs), maxh)
+        mesh = create_mesh(maxh)
+        S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1)
+        m = df.Function(S3)
+        m.assign(df.Constant(m_0))
 
-    for i, solver in enumerate(solvers):
-        demag = Demag(solver)
-        if i == 0:
-            demag.parameters["phi_1_solver"] = "default"
-            demag.parameters["phi_1_preconditioner"] = "default"
-            demag.parameters["phi_2_solver"] = "default"
-            demag.parameters["phi_2_preconditioner"] = "default"
-        if i == 1:
-            demag.parameters["phi_1_solver"] = "cg"
-            demag.parameters["phi_1_preconditioner"] = "ilu"
-            demag.parameters["phi_2_solver"] = "cg"
-            demag.parameters["phi_2_preconditioner"] = "ilu"
+        # Pre-compute BEM to save time.
+        bem, b2g_map = compute_bem_fk(df.BoundaryMesh(mesh, 'exterior', False))
 
-        demag.setup(S3, m, Ms, unit_length)
+        print "Computing demagnetising field with default solver parameters..."
+        H_default, runtime_default = run_demag(
+            repetitions, default_params, S3, m, Ms, unit_length, bem, b2g_map)
 
-        start = time.time()
-        for j in xrange(10):
-            H = demag.compute_field()
-        elapsed = (time.time() - start) / 10.0
+        print "Computing demagnetising field with optimised solver parameters..."
+        H_opt, runtime_opt = run_demag(
+            repetitions, opt_params, S3, m, Ms, unit_length, bem, b2g_map)
 
-        if H_ref is None:
-            H_ref = H
-        error = np.max(np.abs(H - H_ref))
-        timings[i].append(elapsed)
-        errors[i].append(error)
+        results[i, 0] = mesh.num_vertices()
+        results[i, 1] = runtime_default
+        results[i, 2] = runtime_opt
+        results[i, 3] = np.max(np.abs(H_default - H_opt))
+        np.savetxt(results_file, results)  # Save results after every step.
+
+vertices = results[:,0]
+runtimes_default = results[:,1]
+runtimes_opt = results[:,2]
+deviation = results[:,3]
 
 fig = plt.figure()
 ax = fig.add_subplot(211)
-ax.set_title("Runtime")
-for i, solver in enumerate(solvers):
-    ax.plot(vertices, timings[i], label=solvers[i])
+ax.set_title("Runtime Comparison")
+ax.plot(vertices, runtimes_default, label="default FK settings")
+ax.plot(vertices, runtimes_opt, label="optimised FK settings")
 ax.legend(loc=2)
 ax.set_xlabel("vertices")
 ax.set_ylabel("time (s)")
 
 ax = fig.add_subplot(212)
-ax.set_title("Deviation from Default Parameters")
-for i, solver in enumerate(solvers):
-    ax.plot(vertices, errors[i], label=solvers[i])
-ax.legend(loc=2)
+ax.set_title("Deviation from Solution Obtained with Default Settings")
+ax.plot(vertices, deviation)
 ax.set_xlabel("vertices")
-ax.set_ylabel("deviation")
+ax.set_ylabel("max. deviation")
 
 fig.tight_layout()
-fig.savefig("benchmark_fk.png")
+fig.savefig("results_fk_benchmark.png")
