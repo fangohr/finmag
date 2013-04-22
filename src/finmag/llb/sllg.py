2851b080ae183cc1216d6323e2f5f675451e753f
Weiwei Wang <w.wang@soton.ac.uk>
2013-04-22 18:38:09 +0100

    merge sllg into simulation class

--- src/finmag/llb/sllg.py
+++ src/finmag/llb/sllg.py
@@ -18,25 +18,18 @@ import logging
 log = logging.getLogger(name="finmag")
 
 class SLLG(object):
-    def __init__(self,mesh,Ms=8.6e5,unit_length=1.0,name='unnamed',auto_save_data=True,method='RK2b',checking_length=False, pbc=None):
+    def __init__(self, S1, S3, method='RK2b',checking_length=False, unit_length=1):
+        
+        self.S1 = S1
+        self.S3 = S3
+        self.mesh=S1.mesh()
+        
         self._t=0
         self.time_scale=1e-9
-        self.mesh=mesh
-        self.domains =  df.CellFunction("uint", mesh)
-        self.domains.set_all(0)
-        self.dx = df.Measure("dx")[self.domains]
-        self.region_id=0
         
-        self.pbc = pbc
-        if pbc == '2d':
-            self.pbc = PeriodicBoundary2D(mesh)
-        
-        self.S1 = df.FunctionSpace(mesh, "Lagrange", 1, constrained_domain=self.pbc)
-        self.S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1, dim=3, constrained_domain=self.pbc)
         self._m = df.Function(self.S3)
-        self.nxyz=mesh.num_vertices()
-        
-        self.unit_length=unit_length
+        self._m.rename("m", "magnetisation")
+        self.nxyz = self._m.vector().size()/3
         
         self._T = np.zeros(self.nxyz)
         self._alpha = np.zeros(self.nxyz)
@@ -46,33 +39,15 @@ class SLLG(object):
         self._Ms = np.zeros(3*self.nxyz) #Note: nxyz for Ms length is more suitable? 
         self.effective_field = EffectiveField(self.S3)
         
-
         self.pin_fun=None
-        self.method=method
-        self.checking_length=checking_length
+        self.method = method
+        self.checking_length = checking_length
+        self.unit_length = unit_length 
 
         self.set_default_values()
-        self.auto_save_data=auto_save_data
-        self.name = name
-        self.sanitized_name = helpers.clean_filename(name)
-        self.logfilename = self.sanitized_name + '.log'
-        helpers.start_logging_to_file(self.logfilename, mode='w')
-        log.info("Creating Sim object '{}' (rank={}/{}) [{}].".format(
-            self.name, df.MPI.process_number(),
-            df.MPI.num_processes(), time.asctime()))
-        log.info(mesh)
-        
-        
-        
-        self.Ms=Ms
-        
-        if self.auto_save_data:
-            self.ndtfilename = self.sanitized_name + '.ndt'
-            self.tablewriter = Tablewriter(self.ndtfilename, self, override=True)
 
     def set_default_values(self):
-        #self.Ms = 8.6e5  # A/m saturation magnetisation
-        
+        #self.Ms = 8.6e5  # A/m saturation magnetisation        
         self._pins = np.array([], dtype="int")
         self.volumes = df.assemble(df.dot(df.TestFunction(self.S3), df.Constant([1, 1, 1])) * df.dx).array()
         self.Volume = mesh_volume(self.mesh)
@@ -93,16 +68,16 @@ class SLLG(object):
         self.alpha = 0.5  
         self._gamma = consts.gamma
         self._seed=np.random.random_integers(4294967295)
-        self.dt=1e-13
-        self.T=0
+        self.dt = 1e-13
+        self.T = 0
         
   
     @property
-    def t(self):
+    def cur_t(self):
         return self._t*self.time_scale
     
-    @t.setter
-    def t(self,value):
+    @cur_t.setter
+    def cur_t(self,value):
         self._t=value/self.time_scale 
            
     @property
@@ -138,28 +113,14 @@ class SLLG(object):
         log.info("seed=%d."%self.seed)
         log.info("dt=%g."%self.dt)
         log.info("gamma=%g."%self.gamma)
-        log.info("checking_length: "+str(self.checking_length))
+        #log.info("checking_length: "+str(self.checking_length))
                 
     def set_m(self,value):
         self._m.vector().set_local(helpers.vector_valued_function(value, self.S3, normalise=True).vector().array())
         self.m[:]=self._m.vector().array()[:]
 
-    def add(self,interaction,with_time_update=None):
-        log.debug("Adding interaction %s to simulation '%s'" % (str(interaction),self.name))
-        interaction.setup(self.S3, self._m, self._Ms_dg, self.unit_length)
-        self.effective_field.add(interaction, with_time_update)
-        
-        if interaction.__class__.__name__=='Zeeman':
-            self.zeeman_interation=interaction
-            self.tablewriter.entities['zeeman']={
-                        'unit': '<A/m>',
-                        'get': lambda sim: sim.zeeman_interation.average_field(),
-                        'header': ('h_x', 'h_y', 'h_z')}
-        
-            self.tablewriter.update_entity_order()
 
-
-    def run_until(self,t):
+    def advance_time(self,t):
         tp=t/self.time_scale
         
         if tp <= self._t:
@@ -170,29 +131,20 @@ class SLLG(object):
                 self._m.vector().set_local(self.m)
 
                 self._t+=self._dt
-            
-            
         except Exception,error:
             log.info(error)
             raise Exception(error)
             
-            
         if abs(tp-self._t)<1e-12:
-            self._t=tp
-        log.debug("Integrating dynamics up to t = %g" % t)
-            
-        if self.auto_save_data:
-            self.save_data()
+            self._t = tp
 
     def stochastic_update_field(self,y):
                 
         self._m.vector().set_local(y)
         
-        self.field[:] = self.effective_field.compute(self.t)[:]
+        self.field[:] = self.effective_field.compute(self.cur_t)[:]
         
-        #print self.field[:]
 
-    
     @property
     def T(self):
         return self._T
@@ -222,42 +174,6 @@ class SLLG(object):
         tmp=tmp/self.volumes
         self._Ms[:]=tmp[:]
         
-    def m_average_fun(self,dx=df.dx):
-        """
-        Compute and return the average polarisation according to the formula
-        :math:`\\langle m \\rangle = \\frac{1}{V} \int m \: \mathrm{d}V`
-
-        """ 
-        
-        mx = df.assemble(self._Ms_dg*df.dot(self._m, df.Constant([1, 0, 0])) * dx)
-        my = df.assemble(self._Ms_dg*df.dot(self._m, df.Constant([0, 1, 0])) * dx)
-        mz = df.assemble(self._Ms_dg*df.dot(self._m, df.Constant([0, 0, 1])) * dx)
-        volume = df.assemble(self._Ms_dg*dx,mesh=self.mesh)
-                        
-        return np.array([mx, my, mz]) / volume
-    m_average=property(m_average_fun)
-    
-        
-    def save_m_in_region(self,region,name='unnamed'):
-        
-        self.region_id+=1
-        helpers.mark_subdomain_by_function(region, self.mesh, self.region_id,self.domains)
-        self.dx = df.Measure("dx")[self.domains]
-        
-        if name=='unnamed':
-            name='region_'+str(self.region_id)
-        
-        region_id=self.region_id
-        self.tablewriter.entities[name]={
-                        'unit': '<>',
-                        'get': lambda sim: sim.m_average_fun(dx=self.dx(region_id)),
-                        'header': (name+'_m_x', name+'_m_y', name+'_m_z')}
-        
-        self.tablewriter.update_entity_order()
-    
-    def save_data(self):
-        log.debug("Saving average field values for simulation '{}'.".format(self.name))
-        self.tablewriter.save()
         
 if __name__ == "__main__":
     mesh = df.Box(0, 0, 0, 5, 5, 5, 1, 1, 1)
--- src/finmag/sim/sim_test.py
+++ src/finmag/sim/sim_test.py
@@ -603,3 +603,70 @@ class TestSimulation(object):
         sim.run_until(7.5e-12)
         assert(len(glob('barmini_m_[0-9]*.npy')) == 6)
         assert(len(glob('mag_[0-9]*.npy')) == 3)
+
+    def test_sim_sllg(self, do_plot=False):
+        mesh = df.BoxMesh(0, 0, 0, 2, 2, 2, 1, 1, 1)
+        sim = Simulation(mesh, 8.6e5, unit_length=1e-9, kernal='sllg')
+        alpha=0.1
+        sim.alpha = alpha
+        sim.set_m((1, 0, 0))
+        sim.T = 0
+    
+
+        H0 = 1e5
+        sim.add(Zeeman((0, 0, H0)))
+
+        dt = 1e-12; ts = np.linspace(0, 500 * dt, 100)
+
+        precession_coeff = sim.gamma / (1 + alpha ** 2)
+        mz_ref = []
+    
+        mz = []
+        real_ts=[]
+        for t in ts:
+            sim.run_until(t)
+            real_ts.append(sim.t)
+            mz_ref.append(np.tanh(precession_coeff * alpha * H0 * sim.t))
+            mz.append(sim.m[-1]) # same as m_average for this macrospin problem
+    
+        mz=np.array(mz)
+
+        if do_plot:
+            ts_ns = np.array(real_ts) * 1e9
+            plt.plot(ts_ns, mz, "b.", label="computed") 
+            plt.plot(ts_ns, mz_ref, "r-", label="analytical") 
+            plt.xlabel("time (ns)")
+            plt.ylabel("mz")
+            plt.title("integrating a macrospin")
+            plt.legend()
+            plt.savefig(os.path.join(MODULE_DIR, "test_sllg.png"))
+
+        print("Deviation = {}, total value={}".format(
+            np.max(np.abs(mz - mz_ref)),
+            mz_ref))
+    
+        assert np.max(np.abs(mz - mz_ref)) < 8e-7
+        
+    def test_sim_sllg_time(self):
+        mesh = df.BoxMesh(0, 0, 0, 5, 5, 5, 1, 1, 1)
+        sim = Simulation(mesh, 8.6e5, unit_length=1e-9, kernal='sllg')
+        sim.alpha = 0.1
+        sim.set_m((1, 0, 0))
+        sim.T = 10
+        assert np.max(sim.T) == 10
+        
+        ts = np.linspace(0, 1e-9, 1001)
+    
+        H0 = 1e5
+        sim.add(Zeeman((0, 0, H0)))
+    
+        real_ts=[]
+        for t in ts:
+            sim.run_until(t)
+            real_ts.append(sim.t)
+    
+        print("Max Deviation = {}".format(
+            np.max(np.abs(ts - real_ts))))
+    
+        assert np.max(np.abs(ts - real_ts)) < 1e-24
+        
