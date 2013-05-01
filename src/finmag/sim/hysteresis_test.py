92a3e119de2f88358e73d75c24cc9fef3a128ebd
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-05-01 19:39:06 +0100

    Reduce code duplication in hysteresis test.

--- src/finmag/sim/hysteresis_test.py
+++ src/finmag/sim/hysteresis_test.py
@@ -7,6 +7,20 @@ from finmag.util.helpers import plot_hysteresis_loop
 
 ONE_DEGREE_PER_NS = 17453292.5 # in rad/s
 
+H = 0.2e6  # maximum external field strength in A/m
+initial_direction = np.array([1.0, 0.01, 0.0])
+N = 5
+
+def run_hysteresis_loop(save_every, save_at_stage_end, filename):
+    mesh = df.BoxMesh(0, 0, 0, 1, 1, 1, 1, 1, 1)
+    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0,
+                   unit_length=1e-9, A=None, demag_solver=None)
+    H_vals, m_vals = \
+        sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10,
+                            save_every=save_every,
+                            save_at_stage_end=save_at_stage_end,
+                            filename=filename)
+    return H_vals, m_vals
 
 def test_hysteresis_loop_and_plotting(tmpdir):
     """
@@ -17,25 +31,14 @@ def test_hysteresis_loop_and_plotting(tmpdir):
 
     """
     os.chdir(str(tmpdir))
-    mesh = df.BoxMesh(0, 0, 0, 1, 1, 1, 1, 1, 1)
-
-    H = 0.2e6  # maximum external field strength in A/m
-    initial_direction = np.array([1.0, 0.01, 0.0])
-    N = 5
 
     # 1) Don't save any snapshots. No files should be created.
-    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0, unit_length=1e-9, A=None, demag_solver=None)#'FK')
-    sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10,
-                        save_every=None, save_at_stage_end=False,
-                        filename='test_01/no_snapshots.pvd')
+    run_hysteresis_loop(save_every=None, save_at_stage_end=False, filename='test_01/no_snapshots.pvd')
     assert(not os.path.exists('test_01'))  # without saving, no files should exist
 
     # 2) Only save snapshots at the end of stages. This should result
     #    in one snapshot for each of the 2*N stages.
-    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0, unit_length=1e-9, A=None, demag_solver=None)#'FK')
-    sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10,
-                        save_every=None, save_at_stage_end=True,
-                        filename='test_02/snapshots.pvd')
+    run_hysteresis_loop(save_every=None, save_at_stage_end=True, filename='test_02/snapshots.pvd')
     vtu_files = sorted(glob('test_02/*.vtu'))
     vtu_files_expected = ['test_02/snapshots__stage_{:03d}__000000.vtu'.format(i) for i in xrange(2*N)]
     assert(vtu_files == vtu_files_expected)
@@ -48,33 +51,27 @@ def test_hysteresis_loop_and_plotting(tmpdir):
     #    ns, so this should result in three .vtu snapshots for the
     #    first stage. Since nothing happens at subsequent stages,
     #    there should be a single file for each of the remaining ones.
-    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0, unit_length=1e-9, A=None, demag_solver=None)#'FK')
-    sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10,
-                        save_every=1e-10, save_at_stage_end=False,
-                        filename='test_03/snapshots.pvd')
+    run_hysteresis_loop(save_every=1e-10, save_at_stage_end=False, filename='test_03/snapshots.pvd')
     vtu_files = sorted(glob('test_03/*.vtu'))
     vtu_files_expected = sorted(['test_03/snapshots__stage_{:03d}__000000.vtu'.format(i) for i in xrange(2*N)] +
                                 ['test_03/snapshots__stage_000__000001.vtu',
                                  'test_03/snapshots__stage_000__000002.vtu'])
     assert(vtu_files == vtu_files_expected)
     pvd_files = sorted(glob('test_03/*.pvd'))
-    pvd_files_expected = ['test_03/snapshots.pvd'] + ['test_03/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)]
+    pvd_files_expected = sorted(['test_03/snapshots.pvd'] + ['test_03/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)])
     assert(pvd_files == pvd_files_expected)
 
     # 4) Save snapshots at regular intervals *and* at the end of each
     #    relaxation. This should add one snapshot per stage as
     #    compared to the previous test.
-    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0, unit_length=1e-9, A=None, demag_solver=None)#'FK')
-    (H_vals, m_vals) = sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10,
-                                           save_every=1e-10, save_at_stage_end=True,
-                                           filename='test_04/snapshots.pvd')
+    H_vals, m_vals = run_hysteresis_loop(save_every=1e-10, save_at_stage_end=True, filename='test_04/snapshots.pvd')
     vtu_files = sorted(glob('test_04/*.vtu'))
     vtu_files_expected = sorted(['test_04/snapshots__stage_{:03d}__00000{}.vtu'.format(i, j) for i in xrange(2*N) for j in [0,1]] +
                                 ['test_04/snapshots__stage_000__000002.vtu',
                                  'test_04/snapshots__stage_000__000003.vtu'])
     assert(vtu_files == vtu_files_expected)
     pvd_files = sorted(glob('test_04/*.pvd'))
-    pvd_files_expected = ['test_04/snapshots.pvd'] + ['test_04/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)]
+    pvd_files_expected = sorted(['test_04/snapshots.pvd'] + ['test_04/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)])
     assert(pvd_files == pvd_files_expected)
 
     # Check that the magnetisation values are as trivial as we expect
