7671b11a1c01cad3c9918b8d33d92f87f68cbc3b
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-05-02 22:54:10 +0100

    Simplify test a little by performing the action in a temporary directory (which avoids the need for cleanup).

--- src/finmag/tests/test_writing_data.py
+++ src/finmag/tests/test_writing_data.py
@@ -4,15 +4,11 @@ import numpy as np
 from finmag.util.fileio import Tablereader
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
-output_file = os.path.join(MODULE_DIR, "barmini_test.ndt")
 reference_file = os.path.join(MODULE_DIR, "barmini_test.ndt.ref")
 
 
-def test_write_ndt_file():
-    cwd_backup = os.getcwd()
-    # Change into directory of the test so that the .ndt file is saved
-    # there and the output file is found.
-    os.chdir(MODULE_DIR)
+def test_write_ndt_file(tmpdir):
+    os.chdir(str(tmpdir))
 
     sim = finmag.example.barmini(name="barmini_test")
     for time in np.linspace(0, 1e-10, 21):
@@ -25,7 +21,7 @@ def test_write_ndt_file():
     # Sundials are slightly different for each run (which might be
     # worth investigating, since it means that our simulations runs
     # are not 100% reproducible)
-    f_out = Tablereader(output_file)
+    f_out = Tablereader("barmini_test.ndt")
     f_ref = Tablereader(reference_file)
     a_out = np.array(f_out['time', 'm_x', 'm_y', 'm_z'])
     a_ref = np.array(f_ref['time', 'm_x', 'm_y', 'm_z'])
@@ -33,5 +29,3 @@ def test_write_ndt_file():
     diff = np.abs(a_out - a_ref)
     print "Maximum difference: {}.".format(np.max(diff))
     assert np.allclose(a_out, a_ref, atol=5e-6, rtol=1e-8)
-
-    os.chdir(cwd_backup)
