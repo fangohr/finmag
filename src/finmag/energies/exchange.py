Fix computation of exchange length (and add test).

src/finmag/energies/exchange.py
@@ -5,6 +5,7 @@ from finmag.util.timings import mtimed
 from energy_base import EnergyBase
 from finmag.util.consts import exchange_length
 from finmag.util import helpers
+from numbers import Number
 
 logger = logging.getLogger('finmag')
 
@@ -65,7 +66,26 @@ class Exchange(EnergyBase):
         super(Exchange, self).__init__(method, in_jacobian=True)
 
     def exchange_length(self):
-        return exchange_length(self.A.vector().array(), self.Ms.vector().array())
+        def convert_to_number(x):
+            if isinstance(x, Number):
+                res = x
+            else:
+                # In this case we assume that x is a Constant,
+                # Function, or some other object which supports
+                # compute_vertex_values(). TODO: make sure that there
+                # are no other cases.
+                x_vec = x.compute_vertex_values(self.S3.mesh())
+                if not np.allclose(x_vec, x_vec[0], atol=0):
+                    raise ValueError(
+                        "Exchange constant A and/or saturation magnetisation Ms "
+                        "are spatially non-uniform, but they must be constant to "
+                        "compute the exchange length.")
+                res = x_vec[0]
+            return res
+
+        A = convert_to_number(self.A)
+        Ms = convert_to_number(self.Ms)
+        return exchange_length(A, Ms)
 
     @mtimed
     def setup(self, S3, m, Ms, unit_length=1):