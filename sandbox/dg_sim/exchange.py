5bd2e5bfa8486b13bc367e13c44b101b2047f578
Weiwei Wang <w.wang@soton.ac.uk>
2013-05-28 14:58:59 +0100

    have no idea to speed up the exchange solver

--- sandbox/dg_sim/exchange.py
+++ sandbox/dg_sim/exchange.py
@@ -12,7 +12,183 @@ logger=logging.getLogger('finmag')
 """
 Compute the exchange field in DG0 space with the help of BDM1 space.
 """ 
+
 class ExchangeDG(object):
+    def __init__(self, C, in_jacobian = True, name='ExchangeDG'):
+        self.C = C
+        self.in_jacobian=in_jacobian
+        self.name = name
+   
+    @mtimed
+    def setup(self, DG3, m, Ms, unit_length=1.0): 
+        self.DG3 = DG3
+        self.m = m
+        self.Ms = Ms
+        self.unit_length = unit_length
+        
+        mesh = DG3.mesh()
+        self.mesh = mesh
+        
+        DG = df.FunctionSpace(mesh, "DG", 0)
+        BDM = df.FunctionSpace(mesh, "BDM", 1)
+        
+        #deal with three components simultaneously, each represents a vector
+       
+        sigma = df.TrialFunction(BDM)
+        tau = df.TestFunction(BDM)
+        
+        
+        u = df.TrialFunction(DG)
+        v = df.TestFunction(DG)
+        
+        # what we need is A x = K1 m 
+        #a0 = (df.dot(sigma0, tau0) + df.dot(sigma1, tau1) + df.dot(sigma2, tau2)) * df.dx
+        a0 = df.dot(sigma, tau) * df.dx
+        self.A = df.assemble(a0)
+         
+        a1 = - (df.div(tau) * u) * df.dx
+        self.K1 = df.assemble(a1)
+    
+        
+        def boundary(x, on_boundary):
+            return on_boundary
+        
+        # actually, we need to apply the Neumann boundary conditions.
+        
+        zero = df.Constant((0,0,0))
+        self.bc = df.DirichletBC(BDM, zero, boundary)
+        self.bc.apply(self.A)
+        
+        AA = sp.lil_matrix(self.A.array())
+        
+        self.solver = sp.linalg.factorized(AA.tocsc())
+        
+        #LU = sp.linalg.spilu(AA)
+        #self.solver = LU.solve
+        
+        a2 = (df.div(sigma) * v) * df.dx
+        self.K2 = df.assemble(a2)
+        self.L = df.assemble(v * df.dx).array()
+    
+        self.mu0 = mu0
+        self.exchange_factor = 2.0 * self.C / (self.mu0 * Ms * self.unit_length**2)
+
+        self.coeff = self.exchange_factor/self.L
+        
+        B = sp.lil_matrix(self.K2.array())
+        self.K2 = B.tocsr()
+        
+        # b = K m
+        self.b = df.PETScVector()        
+        # the vector in BDM space
+        self.sigma_v = df.PETScVector()
+        
+        # to store the exchange fields
+        #self.H = df.PETScVector()
+        self.H_eff = m.vector().array()
+        
+        self.m_x = df.PETScVector(self.m.vector().size()/3)
+    
+    @mtimed
+    def compute_field(self):
+        
+        mm = self.m.vector().array()
+        mm.shape = (3,-1)
+        self.H_eff.shape=(3,-1)
+        
+        for i in range(3):
+            self.m_x.set_local(mm[i])
+            self.K1.mult(self.m_x, self.b)
+            self.bc.apply(self.b)
+            
+            H = self.solver(self.b.array())
+            #df.solve(self.A, self.sigma_v, self.b)
+            
+            self.H_eff[i][:] = (self.K2*H)*self.coeff
+        
+        mm.shape = (-1,)
+        self.H_eff.shape=(-1,)
+        
+        return self.H_eff
+        
+    
+    def average_field(self):
+        """
+        Compute the average field.
+        """
+        return helpers.average_field(self.compute_field())
+    
+
+
+class ExchangeDG2(object):
+    def __init__(self, C, in_jacobian = False, name='ExchangeDG'):
+        self.C = C
+        self.in_jacobian=in_jacobian
+        self.name = name
+   
+    #@mtimed
+    def setup(self, DG3, m, Ms, unit_length=1.0): 
+        self.DG3 = DG3
+        self.m = m
+        self.Ms = Ms
+        self.unit_length = unit_length
+        
+        mesh = DG3.mesh()
+        
+        DG = df.FunctionSpace(mesh, "DG", 0)
+        CG3 = df.FunctionSpace(mesh, "BDM", 1)
+    
+        n = df.FacetNormal(mesh)
+    
+        u_dg = df.TrialFunction(DG)
+        v_dg = df.TestFunction(DG)
+    
+        u3 = df.TrialFunction(CG3)
+        v3 = df.TestFunction(CG3)
+    
+        #a1 = u_dg * df.inner(v3, n) * df.ds - u_dg * df.div(v3) * df.dx
+        a1 = u_dg * df.inner(v3, n) * df.ds - u_dg * df.div(v3) * df.dx
+        self.K1 = sp.csr_matrix(df.assemble(a1).array())
+        self.L3 = df.assemble(df.dot(v3, df.Constant([1,1,1])) * df.dx).array()
+    
+        a2 = df.div(u3) * v_dg * df.dx
+        self.K2 = sp.csr_matrix(df.assemble(a2).array())
+        self.L = df.assemble(v_dg * df.dx).array()
+    
+        self.mu0 = mu0
+        self.exchange_factor = 2.0 * self.C / (self.mu0 * Ms * self.unit_length**2)
+        
+        # coeff1 should multiply something, I have no idea so far ....
+        self.coeff1 = 1.0
+        self.coeff2 = self.exchange_factor/self.L
+        
+        self.H = m.vector().array()
+    
+    def compute_field(self):
+        mm = self.m.vector().array()
+        mm.shape = (3,-1)
+        self.H.shape=(3,-1)
+
+        for i in range(3):
+            f = self.coeff1*(self.K1*mm[i])
+            self.H[i][:] = self.coeff2 * (self.K2 * f)
+        
+        mm.shape = (-1,)
+        self.H.shape=(-1,)
+        
+        return self.H
+    
+    def average_field(self):
+        """
+        Compute the average field.
+        """
+        return helpers.average_field(self.compute_field())
+
+
+"""
+Compute the exchange field in DG0 space with the help of BDM1 space.
+""" 
+class ExchangeDG_bak(object):
     def __init__(self, C, in_jacobian = False, name='ExchangeDG'):
         self.C = C
         self.in_jacobian=in_jacobian
@@ -100,11 +276,4 @@ class ExchangeDG(object):
         Compute the average field.
         """
         return helpers.average_field(self.compute_field())
-    
-
-    
-    
-    
-    
-
-    
+ 
