607b454a39c340121523960eedd49b3c2b9b3f9d
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-07-10 16:55:40 +0100

    Remove all the arguments from the sim.hysteresis() command related to VTK saving etc., since this can now be much more conveniently achieved by using the scheduler system.
    It may actually be nice to re-introduce a way of saving snapshots for each hysteresis stage separately, but we can always add this again when it is needed.

--- src/finmag/sim/hysteresis_test.py
+++ src/finmag/sim/hysteresis_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from glob import glob
 from finmag import sim_with
+from finmag.example import barmini
 from finmag.util.helpers import plot_hysteresis_loop
 
 ONE_DEGREE_PER_NS = 17453292.5 # in rad/s
@@ -11,16 +12,31 @@ H = 0.2e6  # maximum external field strength in A/m
 initial_direction = np.array([1.0, 0.01, 0.0])
 N = 5
 
-def run_hysteresis_loop(save_every, save_at_stage_end, filename):
+
+def test_hysteresis(tmpdir):
+    os.chdir(str(tmpdir))
+    sim = barmini()
     mesh = df.BoxMesh(0, 0, 0, 1, 1, 1, 1, 1, 1)
-    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0,
-                   unit_length=1e-9, A=None, demag_solver=None)
-    H_vals, m_vals = \
-        sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10,
-                            save_every=save_every,
-                            save_at_stage_end=save_at_stage_end,
-                            filename=filename)
-    return H_vals, m_vals
+    H_ext_list = [(1, 0, 0), (2, 0, 0), (3, 0, 0), (4, 0, 0)]
+    N = len(H_ext_list)
+
+    # Run a relaxation and save a vtk snapshot at the end of each stage;
+    # this should result in three .vtu files (one for each stage).
+    sim1 = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0,
+                    unit_length=1e-9, A=None, demag_solver=None)
+    sim1.schedule('save_vtk', at_end=True, filename='barmini_hysteresis.pvd')
+    res1 = sim1.hysteresis(H_ext_list=H_ext_list)
+    assert(len(glob('barmini_hysteresis*.vtu')) == N)
+    assert(res1 == None)
+
+    # Run a relaxation with a non-trivial `fun` argument and check
+    # that we get a list of return values.
+    sim2 = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0,
+                    unit_length=1e-9, A=None, demag_solver=None)
+    res2 = sim2.hysteresis(H_ext_list=H_ext_list,
+                           fun=lambda sim: sim.m_average[0])
+    assert(len(res2) == N)
+
 
 def test_hysteresis_loop_and_plotting(tmpdir):
     """
@@ -32,51 +48,11 @@ def test_hysteresis_loop_and_plotting(tmpdir):
     """
     os.chdir(str(tmpdir))
 
-    # 1) Don't save any snapshots. No files should be created.
-    run_hysteresis_loop(save_every=None, save_at_stage_end=False, filename='test_01/no_snapshots.pvd')
-    assert(not os.path.exists('test_01'))  # without saving, no files should exist
-
-    # 2) Try saving snapshots but omit filename. Still no files should be created.
-    run_hysteresis_loop(save_every=1e-10, save_at_stage_end=True, filename='test_02/no_snapshots.pvd')
-    assert(not os.path.exists('test_01'))  # without saving, no files should exist
-
-    # 3) Only save snapshots at the end of stages. This should result
-    #    in one snapshot for each of the 2*N stages.
-    run_hysteresis_loop(save_every=None, save_at_stage_end=True, filename='test_03/snapshots.pvd')
-    vtu_files = sorted(glob('test_03/*.vtu'))
-    vtu_files_expected = ['test_03/snapshots__stage_{:03d}__000000.vtu'.format(i) for i in xrange(2*N)]
-    assert(vtu_files == vtu_files_expected)
-    pvd_files = sorted(glob('test_03/*.pvd'))
-    pvd_files_expected = ['test_03/snapshots.pvd'] + ['test_03/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)]
-    assert(pvd_files == pvd_files_expected)
-
-    # 4) Save snapshots at regular intervals (every 10 ns) during the
-    #    simulation. Currently the first relaxation finishes at ca. 22
-    #    ns, so this should result in three .vtu snapshots for the
-    #    first stage. Since nothing happens at subsequent stages,
-    #    there should be a single file for each of the remaining ones.
-    run_hysteresis_loop(save_every=1e-10, save_at_stage_end=False, filename='test_04/snapshots.pvd')
-    vtu_files = sorted(glob('test_04/*.vtu'))
-    vtu_files_expected = sorted(['test_04/snapshots__stage_{:03d}__000000.vtu'.format(i) for i in xrange(2*N)] +
-                                ['test_04/snapshots__stage_000__000001.vtu',
-                                 'test_04/snapshots__stage_000__000002.vtu'])
-    assert(vtu_files == vtu_files_expected)
-    pvd_files = sorted(glob('test_04/*.pvd'))
-    pvd_files_expected = sorted(['test_04/snapshots_all.pvd'] + ['test_04/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)])
-    assert(pvd_files == pvd_files_expected)
-
-    # 4) Save snapshots at regular intervals *and* at the end of each
-    #    relaxation. This should add one snapshot per stage as
-    #    compared to the previous test.
-    H_vals, m_vals = run_hysteresis_loop(save_every=1e-10, save_at_stage_end=True, filename='test_05/snapshots.pvd')
-    vtu_files = sorted(glob('test_05/*.vtu'))
-    vtu_files_expected = sorted(['test_05/snapshots__stage_{:03d}__00000{}.vtu'.format(i, j) for i in xrange(2*N) for j in [0,1]] +
-                                ['test_05/snapshots__stage_000__000002.vtu',
-                                 'test_05/snapshots__stage_000__000003.vtu'])
-    assert(vtu_files == vtu_files_expected)
-    pvd_files = sorted(glob('test_05/*.pvd'))
-    pvd_files_expected = sorted(['test_05/snapshots.pvd', 'test_05/snapshots_all.pvd'] + ['test_05/snapshots__stage_{:03d}.pvd'.format(i) for i in xrange(2*N)])
-    assert(pvd_files == pvd_files_expected)
+    mesh = df.BoxMesh(0, 0, 0, 1, 1, 1, 1, 1, 1)
+    sim = sim_with(mesh, Ms=1e6, m_init=(0.8, 0.2, 0), alpha=1.0,
+                   unit_length=1e-9, A=None, demag_solver=None)
+    H_vals, m_vals = \
+        sim.hysteresis_loop(H, initial_direction, N, stopping_dmdt=10)
 
     # Check that the magnetisation values are as trivial as we expect
     # them to be ;-)
