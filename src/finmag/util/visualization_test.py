e3c2eb04fdef9cce8d29dc28dd84587fe18df6a6
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-08-02 11:26:34 +0100

    Try to run the visualization test using 'xpra' (found in this ipython cookbook recipe: https://github.com/ipython/ipython/wiki/Cookbook:-Running-screen).

--- src/finmag/util/visualization_test.py
+++ src/finmag/util/visualization_test.py
@@ -1,7 +1,10 @@
 import finmag
 import pytest
+import subprocess
 import os
+import sys
 from finmag.util.visualization import render_paraview_scene
+from finmag.util.visualization_impl import find_unused_X_display
 
 # Skipping this test for now because it still doesn't work on aleph0
 # (although it works on my machine) -- Max, 7.6.2013
@@ -13,13 +16,28 @@ def test_render_paraview_scene(tmpdir):
     """
     tmpdir = str(tmpdir)
     os.chdir(tmpdir)
+
+    # Check whether 'xpra' is installed
+    try:
+        subprocess.check_call(['xpra', 'list'])
+    except OSError:
+        finmag.logger.error("Could not find the 'xpra' executable, but it is needed to run this test. "
+                            "Please install it using: 'sudo apt-get install xpra' (on Debian/Ubuntu-based systems).")
+        sys.exit(1)
+
     sim = finmag.example.barmini()
     sim.save_vtk('initial_state.pvd')
+
+    display = find_unused_X_display()
+    finmag.logger.debug("Rendering Paraview scene on display :{} for test.".format(display))
+    subprocess.check_call(['xpra', 'start', ':{}'.format(display)])
 
     # XXX TODO: Maybe call this function a few times with varying parameters?
     render_paraview_scene(
         'initial_state000000.vtu', 'initial_state.png',
-        color_by_axis='Z', rescale_colormap_to_data_range=False, debugging=False)
+        color_by_axis='Z', rescale_colormap_to_data_range=False, debugging=False, use_display=display)
+
+    subprocess.check_call(['xpra', 'stop', ':{}'.format(display)])
 
     # Check that the expected output file exists
     assert(os.path.exists('initial_state.png'))
