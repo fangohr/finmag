4cf0266299fa6cc93d74d15b1490095fcb4e5a92
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-09-03 09:08:16 +0100

    Deactivate test again as indeed it doesn't work when run with Jenkins.

--- src/finmag/sim/sim_test.py
+++ src/finmag/sim/sim_test.py
@@ -867,6 +867,7 @@ def test_removing_logger_handlers_allows_to_create_many_simulation_objects(tmpdi
     resource.setrlimit(resource.RLIMIT_NOFILE, (soft_limit, hard_limit))
 
 
+@pytest.skip
 def test_schedule_render_scene(tmpdir):
     """
     Check that scheduling 'render_scene' will create incremental snapshots.
