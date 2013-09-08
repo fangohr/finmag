bf7bfc6103f663c155093888de27b2dfaf769430
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-09-09 00:07:38 +0100

    Move most of the complexity of the test out into a separate class which can deal with subdomains intelligently internally.

--- src/finmag/energies/test_energies_in_regions.py
+++ src/finmag/energies/test_energies_in_regions.py
@@ -1,9 +1,9 @@
 from __future__ import division
 import numpy as np
 import dolfin as df
-#import pytest
+import pytest
 import os
-#import finmag
+import finmag
 #from finmag.energies import Zeeman, TimeZeeman, DiscreteTimeZeeman, OscillatingZeeman
 from finmag.energies import Zeeman
 #from finmag.util.consts import mu0
@@ -12,7 +12,66 @@ from finmag.util.helpers import vector_valued_function
 #from math import sqrt, pi, cos, sin
 
 
-def test_energies_in_subdomains(tmpdir):
+class MultiDomainTest(object):
+    def __init__(self, mesh, get_domain_id, m_vals, Ms, unit_length=1e-9):
+        """
+        `get_domain_id` is a function of the form (x, y, z) -> id which maps
+        some point coordinates in the mesh to an integer identifying the domain
+        which the point belongs to.
+
+        """
+        self.mesh = mesh
+        self.get_domain_id = get_domain_id
+        self.domain_ids = [get_domain_id(pt) for pt in mesh.coordinates()]
+        self.Ms = Ms
+        self.unit_length = unit_length
+        #self.rtol = rtol
+
+        domain_classes = {}
+        for k in self.domain_ids:
+            class DomainK(df.SubDomain):
+                def inside(self, pt, on_boundary):
+                    return get_domain_id(pt) == k
+            domain_classes[k] = DomainK()
+        domains = df.CellFunction("size_t", mesh)
+        domains.set_all(0)
+        for k, d in domain_classes.items():
+            d.mark(domains, k)
+
+        self.submeshes = [df.SubMesh(mesh, domains, i) for i in self.domain_ids]
+        self.dx = df.Measure("dx")[domains]
+
+        def m_init(pt):
+            return m_vals[self.get_domain_id(pt)]
+
+        self.V = df.VectorFunctionSpace(mesh, 'CG', 1, dim=3)
+        self.m = vector_valued_function(m_init, self.V, normalise=True)
+
+    def compute_energies_on_subdomains(self, interaction):
+        """
+        Given some interaction (such as Zeeman, Demag, Exchange, etc.),
+        compute the associated energies on each subdomain as well as the
+        total energy.
+
+        *Returns*
+
+        A pair (E_subdmns, E_total), where E_subdmns is a dictionary of
+        energies indexed by the subdomain indices, and E_total is the total
+        energy of the interaction.
+
+        """
+        interaction.setup(self.V, self.m, self.Ms, unit_length=self.unit_length)
+        return {k: interaction.compute_energy(dx=self.dx(k)) for k in self.domain_ids},\
+            interaction.compute_energy(df.dx)
+
+    def check_energy_consistency(self, interaction):
+        E_domains, E_total = self.compute_energies_on_subdomains(interaction)
+        finmag.logger.debug("Energies on subdomains: {}".format(E_domains))
+        finmag.logger.debug("Sum of energies on subdomains: {}; total energy: {}".format(sum(E_domains.values()), E_total))
+        assert np.allclose(sum(E_domains.values()), E_total, atol=0, rtol=1e-12)
+
+
+def test_energies_in_separated_subdomains(tmpdir):
     """
     Create a mesh with two subdomains. For each energy class compute the energy
     on each subdomain and compare with the total energy on the whole mesh. Also
@@ -31,53 +90,26 @@ def test_energies_in_subdomains(tmpdir):
     unit_length = 1e-9
     RTOL = 5e-3  # achievable relative tolerance depends on maxh
 
+    zeeman = Zeeman(1e6 * np.array([1, 0, 0]))
+
     mesh = pair_of_disks(d, d, h1, h2, sep, theta=0, maxh=maxh)
-    V = df.VectorFunctionSpace(mesh, "CG", 1, dim=3)
-
-    # Create a mesh function for the two domains (each representing one disk),
-    # where the regions are marked with '0' (first disk) and '1' (second disk).
-    class Disk1(df.SubDomain):
-        def inside(self, pt, on_boundary):
-            x, y, z = pt
-            return np.linalg.norm([x, y]) < 0.5 * (d + sep)
-
-    class Disk2(df.SubDomain):
-        def inside(self, pt, on_boundary):
-            x, y, z = pt
-            return np.linalg.norm([x, y, z]) > 0.5 * (d + sep)
-
-    disk1 = Disk1()
-    disk2 = Disk2()
-    domains = df.CellFunction("size_t", mesh)
-    domains.set_all(0)
-    disk1.mark(domains, 1)
-    disk2.mark(domains, 2)
-    dx = df.Measure("dx")[domains]
-    dx_disk_1 = dx(1)
-    dx_disk_2 = dx(2)
-
-    def m_init_1(pt):
-        return [1, 0, 0]
-
-    def m_init_2(pt):
-        return [0.5, -0.8, 0]
-
-    def m_init(pt):
+    def get_domain_id(pt):
         x, y, z = pt
-        if np.linalg.norm([x, y]) < 0.5 * (d + sep):
-            res = m_init_1(pt)
-        else:
-            res = m_init_2(pt)
-        return res
+        return 1 if (np.linalg.norm([x, y]) < 0.5 * (d + sep)) else 2
 
-    m = vector_valued_function(m_init, V, normalise=True)
+    m_vals = {1: [1, 0, 0],
+              2: [0.5, -0.8, 0]}
+    multi_domain_test = MultiDomainTest(mesh, get_domain_id, m_vals, Ms, unit_length=unit_length)
+    multi_domain_test.check_energy_consistency(zeeman)
 
-    interaction = Zeeman(1e6 * np.array([1, 0, 0]))
-    interaction.setup(V, m, Ms, unit_length=unit_length)
 
-    E_computed_1 = interaction.compute_energy(dx=dx_disk_1)
-    E_computed_2 = interaction.compute_energy(dx=dx_disk_2)
-    E_computed_total = interaction.compute_energy(dx=df.dx)
-
-    # Check that the sum of the computed energies for disk #1 and #2 equals the total computed energy
-    assert np.allclose(E_computed_1 + E_computed_2, E_computed_total, atol=0, rtol=1e-12)
+# The same test for a mesh with subdomains that touch will fail for some reason.
+# XXX TODO: need to investigate this.
+@pytest.mark.xfail
+def test_energies_in_touching_subdomains():
+    box_mesh = df.BoxMesh(-50, -20, 0, 50, 20, 5, 30, 10, 2)
+    def get_domain_id2(pt):
+        return 1 if (pt[0] < 0) else 2
+    multi_domain_test = MultiDomainTest(box_mesh, get_domain_id, m_vals, Ms, unit_length=unit_length)
+    # The next line fails for touching subdomains. Need to investigate this.
+    multi_domain_test.check_energy_consistency(zeeman)
