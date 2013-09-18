98a42a24b9b4501316063a6cd0b673683bfdc7bf
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-09-18 10:23:41 +0100

    Refactoring to allow different mesh discretisations for different mesh primitives.

--- src/finmag/util/mesh_prototypes.py
+++ src/finmag/util/mesh_prototypes.py
@@ -7,57 +7,86 @@ from finmag.util.helpers import vec2str
 
 class MeshPrototype(object):
     def __init__(self, name=None, csg_string=None):
+        self.name = name
         self._csg_stub = csg_string
-        if isinstance(name, list):
-            self.names = name
-        else:
-            self.names = name or []
 
     def __add__(self, other):
-        csg_string_combined = self._csg_stub + other._csg_stub
-        duplicate_names = [n for n in self.names if n in other.names]
-        if duplicate_names != []:
-            raise ValueError(
-                "Mesh prototypes to be added must not contain duplicate names. "
-                "Please name the individual prototypes explicitly (using the "
-                "'name' argument in the constructor). "
-                "Duplicate names found: {}".format(duplicate_names))
-        names = self.names + other.names
-        return MeshPrototype(name=names, csg_string=csg_string_combined)
+        return MeshSum(self, other)
 
-    def generic_filename(self, maxh):
-        filename = "generic_mesh"
-        for n in self.names:
-            filename += ("_" + n)
-        return filename
+    def generic_filename(self, maxh, **kwargs):
+        raise NotImplementedError("Generic mesh prototyp does not provide a filename. Please build a mesh by combining mesh primitives.")
 
-    def csg_string(self):
-        if not self._csg_stub:
-            raise NotImplementedError
-
-        csg = textwrap.dedent("""\
+    def csg_string(self, maxh=None, **kwargs):
+        csg_string = textwrap.dedent("""\
             algebraic3d
             {}
-            """).format(self._csg_stub)
-        for name in self.names:
-            csg += "tlo {};\n".format(name)
+            """).format(self.csg_stub(maxh, **kwargs))
+        return csg_string
 
-        return csg
-
-    def create_mesh(self, maxh, save_result=True, filename='', directory=''):
+    def create_mesh(self, maxh=None, save_result=True, filename='', directory='', **kwargs):
         if save_result == True and filename == '':
-            filename = self.generic_filename(maxh)
+            filename = self.generic_filename(maxh, **kwargs)
 
-        return from_csg(self.csg_string().format(maxh=maxh), save_result=save_result, filename=filename, directory=directory)
+        csg_string = self.csg_string(maxh, **kwargs)
+        return from_csg(csg_string, save_result=save_result, filename=filename, directory=directory)
 
 
-class Sphere(MeshPrototype):
+class MeshSum(MeshPrototype):
+    def __init__(self, mesh1, mesh2, name=None):
+        if mesh1.name == mesh2.name:
+            raise ValueError(
+                "Cannot add mesh prototypes with the same name ('{}'). Please explicitly "
+                "rename one or both of them (either by using the 'name' argument in the "
+                "constructor or by setting their 'name' attribute).".format(mesh1.name))
+        if name is None:
+            name = 'mesh_sum__{}__{}'.format(mesh1.name, mesh2.name)
+        self.name = name
+        self.mesh1 = mesh1
+        self.mesh2 = mesh2
+
+    def csg_stub(self, maxh=None, **kwargs):
+        csg_stub = self.mesh1.csg_stub(maxh, **kwargs) + self.mesh2.csg_stub(maxh, **kwargs)
+        return csg_stub
+
+    def generic_filename(self, maxh, **kwargs):
+        filename = "mesh_sum__{}__{}".format(self.mesh1.generic_filename(maxh, **kwargs), self.mesh2.generic_filename(maxh, **kwargs))
+        return filename
+
+
+class MeshPrimitive(MeshPrototype):
+    def _get_maxh(self, maxh=None, **kwargs):
+        """
+        If `kwargs` contains an item with key 'maxh_NAME' (where NAME
+        is equal to self.name), returns this value and the associated key.
+        Otherwise returns the value associated with the key 'maxh'.
+
+        """
+        try:
+            key = 'maxh_' + self.name
+            maxh = kwargs[key]
+        except KeyError:
+            if maxh == None:
+                raise ValueError("Please provide a valid value for 'maxh' (got: None).")
+        return maxh
+
+    def csg_stub(self, maxh=None, **kwargs):
+        maxh = self._get_maxh(maxh, **kwargs)
+        key = 'maxh_{}'.format(self.name)
+        fmtdict = {key: maxh}
+        return self._csg_stub.format(**fmtdict)
+
+
+class Sphere(MeshPrimitive):
     def __init__(self, r, center=(0, 0, 0), name='Sphere'):
         self.r = r
         self.center = center
-        self.names = [name]
-        self._csg_stub = "solid {name} = sphere ( {center}; {r} ) -maxh = {{maxh}};\n".format(name=name, center=vec2str(center, delims=''), r=r)
+        self.name = name
+        self._csg_stub = textwrap.dedent("""\
+            solid {name} = sphere ( {center}; {r} ) -maxh = {{maxh_{name}}};
+            tlo {name};
+            """.format(name=name, center=vec2str(center, delims=''), r=r))
 
-    def generic_filename(self, maxh):
+    def generic_filename(self, maxh, **kwargs):
+        maxh = self._get_maxh(maxh, **kwargs)
         return "sphere__center_{}__r_{:.1f}__maxh_{:.1f}".format(
             vec2str(self.center, fmt='{:.1f}', delims='', sep='_'), self.r, maxh).replace(".", "_")
--- src/finmag/util/mesh_prototypes_test.py
+++ src/finmag/util/mesh_prototypes_test.py
@@ -54,11 +54,73 @@ def test_combining_meshes(tmpdir):
     sphere3 = Sphere(r3, center=(0, 10, 0), name='sphere_3')
     three_spheres = sphere1 + sphere2 + sphere3
 
-    #mesh = three_spheres.create_mesh(maxh=2.5, save_result=False)
     mesh = three_spheres.create_mesh(maxh=2.0, save_result=True, directory=str(tmpdir))
-    assert(os.path.exists(os.path.join(str(tmpdir), 'generic_mesh_sphere_1_sphere_2_sphere_3.xml.gz')))
+    meshfilename = "mesh_sum__mesh_sum__sphere__center_-30_0_0_0_0_0__r_10_0__maxh_2_0__sphere__center_30_0_0_0_0_0__r_20_0__maxh_2_0__sphere__center_0_0_10_0_0_0__r_15_0__maxh_2_0.xml.gz"
+    assert(os.path.exists(os.path.join(str(tmpdir), meshfilename)))
 
     vol1 = 4./3 * pi * r1**3
     vol2 = 4./3 * pi * r2**3
     vol3 = 4./3 * pi * r3**3
     check_mesh_volume(mesh, vol1 + vol2 + vol3)
+
+
+def test_maxh_with_mesh_primitive(tmpdir):
+    os.chdir(str(tmpdir))
+
+    prim = MeshPrimitive(name='foo')
+    assert(prim._get_maxh(maxh=2.0, maxh_foo=5.0) == 5.0)
+    assert(prim._get_maxh(maxh=2.0, maxh_bar=5.0) == 2.0)
+    with pytest.raises(ValueError):
+        prim._get_maxh(random_arg=42)
+
+    # We use non-valid CSG strings here because we only want to test the maxh functionality
+    prim = MeshPrimitive(name='foo', csg_string='-maxh = {maxh_foo}')
+    assert(prim.csg_string(maxh=2.0) == '-maxh = 2.0')
+    assert(prim.csg_string(maxh_foo=3.0) == '-maxh = 3.0')
+    assert(prim.csg_string(maxh=2.0, maxh_foo=3.0) == '-maxh = 3.0')  # 'personal' value of maxh should take precedence over generic one
+    with pytest.raises(ValueError):
+        prim.csg_string(maxh_bar=4.0)
+
+    s = Sphere(r=10.0)
+    s.csg_string(maxh=2.0)
+
+    s = Sphere(r=5.0, name='my_sphere')
+    s.csg_string(maxh_my_sphere=3.0)
+
+
+def test_mesh_specific_maxh(tmpdir):
+    """
+    Check that we can pass in mesh-specific values of maxh by
+    providing a keyword argument of the form 'maxh_NAME', where
+    NAME is the name of the MeshPrototype.
+    """
+    os.chdir(str(tmpdir))
+    sphere = Sphere(r=10.0, name='foobar')
+    mesh1 = sphere.create_mesh(maxh=5.0)
+    mesh2 = sphere.create_mesh(maxh_foobar=5.0)
+    with pytest.raises(ValueError):
+        sphere.create_mesh(maxh_quux=5.0)
+
+
+def test_different_mesh_discretisations_for_combined_meshes(tmpdir):
+    """
+    Check that we can create a mesh consisting of two spheres for which
+    we provide a generic value of maxh as well as a specific value for
+    the second spheres.
+    """
+    os.chdir(str(tmpdir))
+    r1 = 10.0
+    r2 = 20.0
+
+    sphere1 = Sphere(r1, center=(-30, 0, 0), name='sphere1')
+    sphere2 = Sphere(r2, center=(+30, 0, 0), name='sphere2')
+
+    two_spheres = sphere1 + sphere2
+
+    # This should render the two spheres with different mesh discretisations.
+    # XXX TODO: How to best check that this worked correctly?!? Currently my best idea is
+    #           to create the mesh twice, once with a fine and once with a coarse discretisation
+    #           for the second sphere, and to check that the second mesh has fewer vertices.
+    mesh1 = two_spheres.create_mesh(maxh=5.0, maxh_sphere2=8.0, save_result=True, directory=str(tmpdir))
+    mesh2 = two_spheres.create_mesh(maxh=5.0, maxh_sphere2=10.0, save_result=True, directory=str(tmpdir))
+    assert(mesh1.num_vertices() > mesh2.num_vertices())
