defa7a9e9d1f2074f0f59c8be284d3620590d8f8
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-09-18 08:03:48 +0100

    Add support for combining mesh prototypes to create meshes containing multiple entities.

--- src/finmag/util/mesh_prototypes.py
+++ src/finmag/util/mesh_prototypes.py
@@ -6,32 +6,58 @@ from finmag.util.helpers import vec2str
 
 
 class MeshPrototype(object):
-    def __init__(self):
-        self.csg_string = None
+    def __init__(self, name=None, csg_string=None):
+        self._csg_stub = csg_string
+        if isinstance(name, list):
+            self.names = name
+        else:
+            self.names = name or []
 
-    def generic_filename(self):
-        raise NotImplementedError("MeshPrototype does not provide a generic filename. Please use one of the specific mesh prototype classes instead (e.g. Sphere, Nanodisk)")
+    def __add__(self, other):
+        csg_string_combined = self._csg_stub + other._csg_stub
+        duplicate_names = [n for n in self.names if n in other.names]
+        if duplicate_names != []:
+            raise ValueError(
+                "Mesh prototypes to be added must not contain duplicate names. "
+                "Please name the individual prototypes explicitly (using the "
+                "'name' argument in the constructor). "
+                "Duplicate names found: {}".format(duplicate_names))
+        names = self.names + other.names
+        return MeshPrototype(name=names, csg_string=csg_string_combined)
 
-    def create_mesh(self, maxh, save_result=True, filename='', directory=''):
-        if not self.csg_string:
+    def generic_filename(self, maxh):
+        filename = "generic_mesh"
+        for n in self.names:
+            filename += ("_" + n)
+        return filename
+
+    def csg_string(self):
+        if not self._csg_stub:
             raise NotImplementedError
 
+        csg = textwrap.dedent("""\
+            algebraic3d
+            {}
+            """).format(self._csg_stub)
+        for name in self.names:
+            csg += "tlo {};\n".format(name)
+
+        return csg
+
+    def create_mesh(self, maxh, save_result=True, filename='', directory=''):
         if save_result == True and filename == '':
             filename = self.generic_filename(maxh)
 
-        return from_csg(self.csg_string.format(maxh=maxh), save_result=save_result, filename=filename, directory=directory)
+        return from_csg(self.csg_string().format(maxh=maxh), save_result=save_result, filename=filename, directory=directory)
 
 
 class Sphere(MeshPrototype):
-    def __init__(self, r, center=(0, 0, 0)):
+    def __init__(self, r, center=(0, 0, 0), name='Sphere'):
         self.r = r
         self.center = center
-        self.csg_string = textwrap.dedent("""\
-            algebraic3d
-            solid main = sphere ( {center}; {r} ) -maxh = {{maxh}};
-            tlo main;
-            """).format(center=vec2str(center, delims=''), r=r)
+        self.names = [name]
+        self._csg_stub = "solid {name} = sphere ( {center}; {r} ) -maxh = {{maxh}};\n".format(name=name, center=vec2str(center, delims=''), r=r)
 
     def generic_filename(self, maxh):
         return "sphere__center_{}__r_{:.1f}__maxh_{:.1f}".format(
-                   vec2str(self.center, fmt='{:.1f}', delims='', sep='_'), self.r, maxh).replace(".", "_")
+            vec2str(self.center, fmt='{:.1f}', delims='', sep='_'), self.r, maxh).replace(".", "_")
--- src/finmag/util/mesh_prototypes_test.py
+++ src/finmag/util/mesh_prototypes_test.py
@@ -4,13 +4,14 @@ import pytest
 import os
 import numpy as np
 from math import pi
+from meshes import mesh_volume
 from mesh_prototypes import *
 
 TOL = 1e-2  # relative tolerance for mesh volumes
 
 
-def check_mesh_volume(mesh, vol, atol=0.0 rtol=TOL):
-    assert(np.allclose(mesh_volume(mesh), vol, atol=atol, rtol=rtol
+def check_mesh_volume(mesh, vol, atol=0.0, rtol=TOL):
+    assert(np.allclose(mesh_volume(mesh), vol, atol=atol, rtol=rtol))
 
 
 def test_mesh_prototypes(tmpdir):
@@ -31,5 +32,33 @@ def test_sphere(tmpdir):
     assert(os.path.exists('foo/sphere__center_2_0_3_0_-4_0__r_20_0__maxh_8_0.xml.gz'))
     assert(os.path.exists('bar/sphere.xml.gz'))
 
-    mesh = sphere.create_mesh(maxh=2.0, save_result=False)
+    mesh = sphere.create_mesh(maxh=2.5, save_result=False)
     check_mesh_volume(mesh, 4./3 * pi * r**3)
+
+
+def test_combining_meshes(tmpdir):
+    os.chdir(str(tmpdir))
+    r1 = 10.0
+    r2 = 20.0
+    r3 = 15.0
+
+    # This should raise an error because the two spheres have the same name (which is given automatically)
+    sphere1 = Sphere(r1, center=(-30, 0, 0))
+    sphere2 = Sphere(r2, center=(+30, 0, 0))
+    with pytest.raises(ValueError):
+        _ = sphere1 + sphere2
+
+    # Same again, but with different names
+    sphere1 = Sphere(r1, center=(-30, 0, 0), name='sphere_1')
+    sphere2 = Sphere(r2, center=(+30, 0, 0), name='sphere_2')
+    sphere3 = Sphere(r3, center=(0, 10, 0), name='sphere_3')
+    three_spheres = sphere1 + sphere2 + sphere3
+
+    #mesh = three_spheres.create_mesh(maxh=2.5, save_result=False)
+    mesh = three_spheres.create_mesh(maxh=2.0, save_result=True, directory=str(tmpdir))
+    assert(os.path.exists(os.path.join(str(tmpdir), 'generic_mesh_sphere_1_sphere_2_sphere_3.xml.gz')))
+
+    vol1 = 4./3 * pi * r1**3
+    vol2 = 4./3 * pi * r2**3
+    vol3 = 4./3 * pi * r3**3
+    check_mesh_volume(mesh, vol1 + vol2 + vol3)
