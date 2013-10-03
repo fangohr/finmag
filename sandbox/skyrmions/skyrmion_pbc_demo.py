40dbea51afbd6561645feb27bab471ab571e0c44
Marijan Beg <mb4e10@soton.ac.uk>
2013-10-03 15:02:49 +0100

    The old DMI class deleted. Rotor computation for 1d mesh added. Rotor computation for 3d mesh is always computed using dolfin. Spatially varying DMI constant enabled. Documentation changed. ---MAJOR CHANGE---: The wrong computation of DMI_factor corrected - multiplication by mu0*Ms deleted which should affect the simulations since this product is not approximatelly 1 for skyrmion materials.

--- sandbox/skyrmions/skyrmion_pbc_demo.py
+++ sandbox/skyrmions/skyrmion_pbc_demo.py
@@ -1,37 +1,28 @@
 import numpy as np
 import dolfin as df
 from finmag import Simulation
-from finmag.energies import Exchange, DMI, Demag, Zeeman
+from finmag.energies import Exchange, DMI, Zeeman
 
-
-
-R=150
-N=50
-#mesh = df.RectangleMesh(0,0,R,R,20,20)
-mesh = df.RectangleMesh(0,0,R,R,N,N)
+R1=460
+R2=400
+N=100
+mesh = df.RectangleMesh(0,0,R1,R2,184,160)
 
 def m_init_fun(pos):
     return np.random.random(3)-0.5
-
-
-
 
 Ms = 8.6e5
 sim = Simulation(mesh, Ms, pbc='2d',unit_length=1e-9)
 sim.set_m(m_init_fun)
 
-
-#A = 3.57e-13
-#D = 2.78e-3
-
 A = 1.3e-11
 D = 4e-3
 sim.add(Exchange(A))
 sim.add(DMI(D))
-sim.add(Zeeman((0,0,0.35*Ms)))
-#sim.add(Demag())
+sim.add(Zeeman((0,0,0.45*Ms)))
 
-def loop(final_time, steps=100):
+sim.alpha = 1
+def loop(final_time, steps=500):
     t = np.linspace(sim.t + 1e-12, final_time, steps)
     for i in t:
         sim.run_until(i)
@@ -41,7 +32,7 @@ def loop(final_time, steps=100):
     df.interactive()
     
     
-loop(1e-9)
+loop(100e-9)
 
 
 
