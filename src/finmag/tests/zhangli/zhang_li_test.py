47410645e6ad58b9a61ffce8a92788dd30a000c0
Weiwei Wang <w.wang@soton.ac.uk>
2013-10-29 23:26:55 +0000

    add sllg for stt

--- src/finmag/tests/zhangli/zhang_li_test.py
+++ src/finmag/tests/zhangli/zhang_li_test.py
@@ -58,6 +58,30 @@ def test_zhangli():
     assert p1[0] < p0[0]
     assert abs(p0[0])<1e-15
     assert abs(p1[0])>1e-3
+    
+def test_zhangli_sllg():
+    
+    mesh = df.BoxMesh(0, 0, 0, 100, 1, 1, 50, 1, 1)
+    
+    sim = Sim(mesh, Ms=8.6e5, unit_length=1e-9, kernel='sllg')
+    sim.set_m(init_m)
+    
+    sim.add(UniaxialAnisotropy(K1=520e3, axis=[1, 0, 0]))
+    sim.add(Exchange(A=13e-12))
+    sim.alpha = 0.01
+    
+    sim.set_zhangli(init_J, 0.5,0.01)
+    
+    p0=sim.m_average
+    
+    sim.run_until(1e-11)
+    p1=sim.m_average
+    
+    print p0, p1
+
+    assert p1[0] < p0[0]
+    assert abs(p0[0])<1e-15
+    assert abs(p1[0])>1e-3
 
 def init_J_x(pos):
     return (1,0,0)
@@ -156,6 +180,7 @@ def compare_gradient_field2():
 
 if __name__ == "__main__":
     test_zhangli()
+    test_zhangli_sllg()
     #compare_gradient_field1()
     #compare_gradient_field2()
 
