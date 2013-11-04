daa0a78458e86db56a0e1065124e1b20d8083db1
Hans Fangohr <fangohr@soton.ac.uk>
2013-11-04 12:53:05 +0000

    Adding script to install the developer fenics packages for Ubuntu. This is required to run some of the recent code that Max has been working on. (Actual installation fails for me at the moment, but this could be due to a dodgy Internet connection in the Hotel.)

---
+++ install/install-fenics-dev-packages-in-ubuntu.sh
@@ -0,0 +1,9 @@
+# Following instructions from 
+# https://launchpad.net/~fenics-packages/+archive/fenics-dev
+
+sudo add-apt-repository ppa:fenics-packages/fenics && \
+sudo add-apt-repository ppa:fenics-packages/fenics-dev && \
+sudo apt-get update && \
+sudo apt-get -y install fenics && \
+sudo apt-get -y dist-upgrade
+
