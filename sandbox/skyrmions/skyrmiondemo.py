96e6f0b0fe57e5f986c6b674ffe51b6a43ee9537
Hans Fangohr <fangohr@soton.ac.uk>
2013-11-09 13:51:18 +0000

    Modifying example with Denys Makarov; no major change.

--- sandbox/skyrmions/skyrmiondemo.py
+++ sandbox/skyrmions/skyrmiondemo.py
@@ -5,18 +5,18 @@ from finmag.energies import Exchange, DMI, Demag, Zeeman
 from finmag.util.meshes import cylinder
 
 #mesh = df.BoxMesh(0,0,0,30,30,3,10,10,1)
-mesh = cylinder(200,3,3)
+mesh = cylinder(60, 3, 3)
 
 
 Ms = 8.6e5
-sim = Simulation(mesh, Ms,unit_length=1e-9)
+sim = Simulation(mesh, Ms, unit_length=1e-9)
 sim.set_m((1, 1, 1))
 
 A = 1.3e-11
 D = 4e-3
 sim.add(Exchange(A))
 sim.add(DMI(D))
-sim.add(Zeeman((0, 0, 0.1*Ms)))
+sim.add(Zeeman((0, 0, 0.25*Ms)))
 
 #sim.add(Demag())
 
