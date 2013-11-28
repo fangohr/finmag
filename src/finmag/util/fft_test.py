3ed14acf92e30b29981689477961e3f8f1f49f26
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-11-28 18:16:17 +0000

    Enable restriction of the computation of the power spectrum to a subset of mesh nodes.

--- src/finmag/util/fft_test.py
+++ src/finmag/util/fft_test.py
@@ -117,7 +117,7 @@ def test_power_spectral_density_from_averaged_magnetisation(tmpdir):
     t_step = 1e-11
     t_ini = 0
     t_end = 10e-9
-    ndt_filename = fft_test_helpers.create_test_ndt_file(str(tmpdir), omega, alpha, t_step, t_ini, t_end)
+    ndt_filename = fft_test_helpers.create_test_ndt_file(str(tmpdir), t_step, t_ini, t_end, omega, alpha)
 
     ##
     ## Step 2: compute the PSDs of a resampled time series, both by
@@ -179,7 +179,7 @@ def test_power_spectral_density_from_spatially_resolved_magnetisation(tmpdir):
     t_end = 10e-9
 
     num_vertices = 42 # in a real application this would be the number of mesh vertices
-    fft_test_helpers.create_test_npy_files(str(tmpdir), omega, alpha, t_step, t_ini, t_end, num_vertices)
+    fft_test_helpers.create_test_npy_files(str(tmpdir), t_step, t_ini, t_end, omega, alpha, num_vertices)
 
     ##
     ## Step 2: compute the FFT of a resampled time series, both by
@@ -212,6 +212,89 @@ def test_power_spectral_density_from_spatially_resolved_magnetisation(tmpdir):
     # Compute Fourier transform of resampled time series using FFT_m
     freqs_computed, psd_mx_computed, psd_my_computed, psd_mz_computed = \
         power_spectral_density('m_ringdown*.npy', t_step_res, t_ini=t_ini_res, t_end=t_end_res, subtract_values=None)
+
+    # Check that the analytically determined power spectra are the same as the computed ones.
+    assert(np.allclose(psd_mx_expected, psd_mx_computed, atol=0, rtol=RTOL))
+    assert(np.allclose(psd_my_expected, psd_my_computed, atol=0, rtol=RTOL))
+    assert(np.allclose(psd_mz_expected, psd_mz_computed, atol=0, rtol=RTOL))
+
+    # Plot the spectra for debugging
+    fig = plt.figure(figsize=(20, 5))
+    ax = fig.gca()
+    ax.plot(freqs_computed, psd_mx_expected, label='psd_mx_expected')
+    ax.plot(freqs_computed, psd_my_expected, label='psd_my_expected')
+    ax.plot(freqs_computed, psd_mz_expected, label='psd_mz_expected')
+    ax.plot(freqs_computed, psd_mx_computed, label='psd_mx_computed')
+    ax.plot(freqs_computed, psd_my_computed, label='psd_my_computed')
+    ax.plot(freqs_computed, psd_mz_computed, label='psd_mz_computed')
+    ax.legend(loc='best')
+    fig.savefig('psd_m_McMichaelStiles.png')
+
+
+def test_power_spectral_density_from_spatially_resolved_magnetisation_confined_to_mesh_region(tmpdir):
+    """
+    First we write some 'forged' spatially resolved magnetisation
+    dynamics to a bunch of .npy files (representing the time series).
+    we use two mesh regions with different oscillations, but we
+    compute the spectrum for the first region only and compare with an
+    analytical solution as in the previous tests.
+
+    """
+    os.chdir(str(tmpdir))
+    RTOL = 1e-10
+
+    H1 = 1e6  # external field in A/m
+    alpha1 = 0.5  # some sort of damping constant
+    omega1 = gamma * H1  # precession frequency
+
+    H2 = 2.8e4  # external field in A/m
+    alpha2 = 0.3  # some sort of damping constant
+    omega2 = gamma * H2  # precession frequency
+
+    ##
+    ## Step 1: Construct a time series of artificial magnetisation
+    ## data and save it to a bunch of .npy files.
+    ##
+    t_step = 1e-11
+    t_ini = 0
+    t_end = 10e-9
+
+    N1 = 42 # in a real application this would be the number of mesh vertices
+    N2 = 23 # in a real application this would be the number of mesh vertices
+    fft_test_helpers.create_test_npy_files_with_two_regions(
+        str(tmpdir), t_step, t_ini, t_end, omega1, alpha1, N1, omega2, alpha2, N2)
+
+    ##
+    ## Step 2: compute the FFT of a resampled time series, both by
+    ## hand and using FFT_m.
+    ##
+    ## XXX TODO: Resampling timesteps is not supported when using .npy
+    ## files. Either simplify the code below, or implement saving to
+    ## .h5 files so that it's easier to implement resampling for
+    ## spatially resolved data, too.
+    ##
+    t_step_res = t_step
+    t_ini_res = t_ini
+    t_end_res = t_end
+    ts_resampled = np.arange(t_ini_res, t_end_res, t_step_res)
+
+    # Compute time series based on resampled timesteps
+    mx_res = exp(-ts_resampled * 1e8 / alpha1) * sin(omega1 * ts_resampled)
+    my_res = exp(-ts_resampled * 1e8 / alpha1) * cos(omega1 * ts_resampled)
+    mz_res = 1 - sqrt(mx_res**2 + my_res**2)
+
+    # Compute 'analytical' Fourier transform of resampled time series and
+    # determine the power of the spectrum for each component. We also need
+    # to multiply by the number of mesh nodes because the numerical algorithm
+    # sums up all contributions at the individual nodes (but we can just
+    # multiply because they are all identical by construction).
+    psd_mx_expected = N1 * np.absolute(np.fft.rfft(mx_res))**2
+    psd_my_expected = N1 * np.absolute(np.fft.rfft(my_res))**2
+    psd_mz_expected = N1 * np.absolute(np.fft.rfft(mz_res))**2
+
+    # Compute Fourier transform of resampled time series using FFT_m
+    freqs_computed, psd_mx_computed, psd_my_computed, psd_mz_computed = \
+        power_spectral_density('m_ringdown*.npy', t_step_res, t_ini=t_ini_res, t_end=t_end_res, subtract_values=None, restrict_to_vertices=xrange(N1))
 
     # Check that the analytically determined power spectra are the same as the computed ones.
     assert(np.allclose(psd_mx_expected, psd_mx_computed, atol=0, rtol=RTOL))
@@ -294,10 +377,10 @@ def test_plot_power_spectral_density(tmpdir):
     num_vertices = 42 # in a real application this would be the number of mesh vertices
 
     # Write a sample .ndt file with some artifical magnetisation data
-    ndt_filename = fft_test_helpers.create_test_ndt_file(str(tmpdir), omega, alpha, t_step, t_ini, t_end)
+    ndt_filename = fft_test_helpers.create_test_ndt_file(str(tmpdir), t_step, t_ini, t_end, omega, alpha)
 
     # Write a sample .ndt file with some artifical magnetisation data
-    npy_filenames = fft_test_helpers.create_test_npy_files(str(tmpdir), omega, alpha, t_step, t_ini, t_end, num_vertices)
+    npy_filenames = fft_test_helpers.create_test_npy_files(str(tmpdir), t_step, t_ini, t_end, omega, alpha, num_vertices)
 
     kwargs = dict(t_step=t_step, t_ini=t_ini, t_end=t_end,
                   subtract_values=None, components="xy", figsize=(5, 4),
--- src/finmag/util/fft_test_helpers.py
+++ src/finmag/util/fft_test_helpers.py
@@ -5,7 +5,7 @@ import subprocess as sp
 from numpy import sqrt, sin, cos, pi, exp, real, conj
 
 
-def create_test_ndt_file(dirname, omega, alpha, t_step, t_ini, t_end, debug=True):
+def create_test_ndt_file(dirname, t_step, t_ini, t_end, omega, alpha, debug=True):
     """
     Create a .ndt file with a time series of artificial magnetisation
     data for testing purposes.
@@ -41,7 +41,7 @@ def create_test_ndt_file(dirname, omega, alpha, t_step, t_ini, t_end, debug=True
     return ndt_filename
 
 
-def create_test_npy_files(dirname, omega, alpha, t_step, t_ini, t_end, num_vertices):
+def create_test_npy_files(dirname, t_step, t_ini, t_end, omega, alpha, num_vertices):
     """
     Construct a time series of artificial magnetisation data and save
     it to a bunch of .npy files.
@@ -63,6 +63,48 @@ def create_test_npy_files(dirname, omega, alpha, t_step, t_ini, t_end, num_verti
         a[0, :] = mx[i]
         a[1, :] = my[i]
         a[2, :] = mz[i]
+        filename = os.path.join(dirname, 'm_ringdown_{:06d}.npy'.format(i))
+        np.save(filename, a.ravel())
+
+    npy_files = os.path.join(dirname, 'm_ringdown_*.npy')
+    return npy_files
+
+
+def create_test_npy_files_with_two_regions(dirname, t_step, t_ini, t_end, omega1, alpha1, num_vertices1, omega2, alpha2, num_vertices2):
+    """
+    Construct a time series of artificial magnetisation data and save
+    it to a bunch of .npy files.
+    """
+    print "Precessional frequency in region 1: {} GHz".format(omega1 / 1e9)
+    print "Precessional frequency in region 2: {} GHz".format(omega2 / 1e9)
+
+    ts = np.arange(t_ini, t_end, t_step)
+    num_timesteps = len(ts)
+    print "Number of timesteps: {}".format(num_timesteps)
+
+    # Use damped harmonic oscillator to create fake magnetisation dynamics
+    mx1 = exp(-ts * 1e8 / alpha1) * sin(omega1 * ts)
+    my1 = exp(-ts * 1e8 / alpha1) * cos(omega1 * ts)
+    mz1 = 1 - sqrt(mx1**2 + my1**2)
+
+    mx2 = exp(-ts * 1e8 / alpha2) * sin(omega2 * ts)
+    my2 = exp(-ts * 1e8 / alpha2) * cos(omega2 * ts)
+    mz2 = 1 - sqrt(mx2**2 + my2**2)
+
+    # Write the data to a series of .npy files
+    N = num_vertices1 + num_vertices2
+    a = np.zeros((3, N))
+    for i in xrange(num_timesteps):
+        # Write values in region 1
+        a[0, :num_vertices1] = mx1[i]
+        a[1, :num_vertices1] = my1[i]
+        a[2, :num_vertices1] = mz1[i]
+
+        # Write values in region 2
+        a[0, num_vertices1:] = mx2[i]
+        a[1, num_vertices1:] = my2[i]
+        a[2, num_vertices1:] = mz2[i]
+
         filename = os.path.join(dirname, 'm_ringdown_{:06d}.npy'.format(i))
         np.save(filename, a.ravel())
 
