56d7f205505ada85cc29762342f59099af29a5eb
Hans Fangohr <fangohr@soton.ac.uk>
2013-12-04 14:58:12 +0000

    Added diff-match-patch to required libraries to install.

--- install/on_ubuntu_13.04.sh
+++ install/on_ubuntu_13.04.sh
@@ -13,7 +13,7 @@ fi
 
 required="fenics libboost-python-dev libboost-thread-dev libsundials-serial-dev
     libboost-test-dev python-matplotlib python-visual python-scipy python-pip
-    python-setuptools python-progressbar paraview-python cython"
+    python-setuptools python-progressbar paraview-python cython "
 
 building_doc="texlive-latex-extra texlive-latex-recommended python-pygments
     texlive-fonts-recommended"
@@ -30,4 +30,5 @@ sudo add-apt-repository ppa:fenics-packages/fenics
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install $packages
-sudo pip install -U sphinx pytest aeon sh
+sudo pip install -U sphinx pytest aeon sh diff-match-patch
+
