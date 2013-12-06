9273f01f9543c356f69fecf647ae0c785a6012ee
Weiwei Wang <w.wang@soton.ac.uk>
2013-12-06 18:22:25 +0000

    add the tolerence setting method to simulation class

--- src/finmag/tests/test_sim_ode.py
+++ src/finmag/tests/test_sim_ode.py
@@ -15,6 +15,8 @@ def test_sim_ode(do_plot=False):
     sim = Sim(mesh, 8.6e5, unit_length=1e-9, pbc='2d')
     sim.alpha = alpha
     sim.set_m((1, 0, 0))
+    
+    sim.set_tol(1e-10, 1e-14)
 
     H0 = 1e5
     sim.add(Zeeman((0, 0, H0)))
@@ -25,14 +27,21 @@ def test_sim_ode(do_plot=False):
     precession_coeff = sim.gamma / (1 + alpha ** 2)
     mz_ref = np.tanh(precession_coeff * alpha * H0 * ts)
 
-    mz = []
+    mzs = []
+    length_error=[]
     for t in ts:
         sim.advance_time(t)
-        mz.append(sim.m[-1]) # same as m_average for this macrospin problem
+        mm = sim.m.copy()
+
+        mm.shape=(3,-1)
+        mx,my,mz = mm[:,0] # same as m_average for this macrospin problem
+        mzs.append(mz)
+        length = np.sqrt(mx**2+my**2+mz**2)
+        length_error.append(abs(length-1.0))
 
     if do_plot:
         ts_ns = ts * 1e9
-        plt.plot(ts_ns, mz, "b.", label="computed")
+        plt.plot(ts_ns, mzs, "b.", label="computed")
         plt.plot(ts_ns, mz_ref, "r-", label="analytical")
         plt.xlabel("time (ns)")
         plt.ylabel("mz")
@@ -41,10 +50,11 @@ def test_sim_ode(do_plot=False):
         plt.savefig(os.path.join(MODULE_DIR, "test_sim_ode.png"))
 
     print("Deviation = {}, total value={}".format(
-            np.max(np.abs(mz - mz_ref)),
+            np.max(np.abs(mzs - mz_ref)),
             mz_ref))
 
-    assert np.max(np.abs(mz - mz_ref)) < 3e-5
+    assert np.max(np.abs(mzs - mz_ref)) < 1e-9
+    assert np.max(length_error) < 1e-9
 
 if __name__ == "__main__":
     test_sim_ode(do_plot=True)
