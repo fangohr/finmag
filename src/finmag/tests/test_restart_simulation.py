48677f62998d51a8159bc90550e4b3bae7b22daf
Maximilian Albert <maximilian.albert@soton.ac.uk>
2013-12-13 10:44:25 +0000

    Execute test in temporary directory so that the saved files don't end up in the .deb package. Also tidied up the comments that were automatically saved from the original IPython notebook in which this was written.

--- src/finmag/tests/test_restart_simulation.py
+++ src/finmag/tests/test_restart_simulation.py
@@ -1,19 +1,20 @@
 import finmag
+import os
 
-def test_restart_same_simulation():
+# XXX TODO: This test was adapted from an IPython notebook, and some
+# of the commands (e.g. debugging print statements) don't make sense
+# in an automated test. These should be cleaned up.
+
+
+def test_restart_same_simulation(tmpdir):
+    os.chdir(str(tmpdir))
 
     sim = finmag.example.barmini()
     sim.run_until(10e-12)
 
-    # <markdowncell>
-
     # To be able to restart the simulation from a particular point, we need to save the magnetisation at that time before:
 
-    # <codecell>
-
     sim.save_restart_data()
-
-    # <markdowncell>
 
     # We can see from the message that the filename ``barmini-restart.npz`` has been chosen. This is the *canonical* filename, composed of
     # 
@@ -23,67 +24,39 @@ def test_restart_same_simulation():
     # 
     # For completeness the simulation name is:
 
-    # <codecell>
-
     print(sim.name)
-
-    # <markdowncell>
 
     # Let us also save the magnetisation at this point in time.
 
-    # <codecell>
-
     m_10em12 = sim.m
-
-    # <markdowncell>
 
     # We can also choose any filename we like (although we need to stick to the ``.npz`` extension), for example
 
-    # <codecell>
-
     sim.save_restart_data(filename="my-special-state.npz")
-
-    # <markdowncell>
 
     # And show the average component values for future reference
 
-    # <codecell>
-
     print("t=%s, <m>=%s" % (sim.t, sim.m_average))
 
-    # <markdowncell>
-
     # Then carry on with the time integration:
-
-    # <codecell>
 
     sim.run_until(100e-12)
     print("t=%s, <m>=%s" % (sim.t, sim.m_average))
 
     assert sim.t == 100e-12
-    # <markdowncell>
 
     # We know imagine that we need to restart this run, or create another simulation that continues at the point of t=1e-12 where we have saved our restart snapshot:
 
-    # <markdowncell>
-
     # ### Restart 
-
-    # <markdowncell>
 
     # Imagine we need to go back to t=10e-12 and the corresponding magnetisation configuration. We can use:
 
-    # <codecell>
-
     sim.restart()
     assert sim.t == 10e-12
-    # <markdowncell>
 
     # If the ``restart`` method is not given any filename, it will look for the canonical restart name of its simulation object.
     # 
     # And just to convince us:
-
-    # <codecell>
 
     print("time = %s " % sim.t)
     print("<m> = %s" % sim.m_average)
@@ -92,11 +65,8 @@ def test_restart_same_simulation():
 
     # integrate a little so that we change time and status
     sim.run_until(20e-12)
-    # <markdowncell>
 
     # If we want to restart from a different configuration (i.e. not from the canonical filename, we need to provide a restart file name):
-
-    # <codecell>
 
     sim.restart('my-special-state.npz')
     print("time = %s " % sim.t)
@@ -104,14 +74,10 @@ def test_restart_same_simulation():
     assert sim.t == 10e-12
     assert (sim.m == m_10em12).all  # check that this identical to before saving
 
-
     sim.run_until(24e-12)
     assert sim.t == 24e-12
-    # <markdowncell>
 
     # If we want to use the same magnetisation, but change the point in time at which we start the integration, we can use the optional ``t0`` parameter:
-
-    # <codecell>
 
     sim.restart('my-special-state.npz', t0=0.42e-12)
     print("time = %s " % sim.t)
@@ -119,25 +85,14 @@ def test_restart_same_simulation():
     assert sim.t == 0.42e-12
     assert (sim.m == m_10em12).all  # check that this identical to before saving
 
-    # <codecell>
-
     print("t=%s, <m>=%s" % (sim.t, sim.m_average))
-
-    # <markdowncell>
 
     # ## Creating a new simulation from saved restart file
 
-    # <markdowncell>
-
     # To create a new simulation that starts from a saved configurtion, we need to create the simulation object (and we have to use exactly the same mesh -- there is no check for this at the moment), and can then use the restart method as before:
 
-    # <codecell>
-
-def test_restart_new_simulation():
-    import finmag
     sim2 = finmag.example.barmini()
     sim2.restart('my-special-state.npz')
-    # <codecell>
 
     print("t=%s, <m>=%s" % (sim2.t, sim2.m_average))
     assert sim2.t == 10e-12
