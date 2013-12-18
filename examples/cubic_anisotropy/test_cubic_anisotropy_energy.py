f8ee7a2c72c32bcea43949a8b89e54d523536d8e
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2013-12-18 16:38:22 +0000

    finished test for cubic anisotropy energy, shows wrong result

--- examples/cubic_anisotropy/test_cubic_anisotropy_energy.py
+++ examples/cubic_anisotropy/test_cubic_anisotropy_energy.py
@@ -1,4 +1,5 @@
 import dolfin as df
+from finmag import Simulation
 from finmag.energies import CubicAnisotropy
 
 Ms = 876626  # A/m
@@ -8,21 +9,20 @@ K2 = -13744132
 K3 =  1100269
 u1 = (0, -0.7071, 0.7071)
 u2 = (0,  0.7071, 0.7071)
-u3 = (-1, 0, 0)  # perpendicular to u1 and u2
 
 
 def test_cubic_anisotropy_energy():
     mesh = df.BoxMesh(0, 0, 0, 1, 1, 40, 1, 1, 40)
-    S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1)
-
-    m = df.Function(S3)
-    m.assign(df.Constant((0, 0, 1)))
+    sim = Simulation(mesh, Ms, unit_length=1e-9)
+    sim.set_m((0, 0, 1))
 
     ca = CubicAnisotropy(u1, u2, K1, K2, K3)
-    ca.setup(S3, m, Ms, unit_length=1e-9)
+    sim.add(ca)
 
     energy = ca.compute_energy()
+    print "Computed cubic anisotropy energy. E = {}.".format(energy)
     energy_expected = 8.3e-20  # oommf cubicEight_100pc.mif -> ErFe2.odt
+    print "Expected E = {}.".format(energy_expected)
 
     rel_diff = abs(energy - energy_expected) / abs(energy_expected)
     assert rel_diff < 1e-2
