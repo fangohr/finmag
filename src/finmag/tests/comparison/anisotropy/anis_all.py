5b18ac00dffffabbf0f9ac5b2bf0ac74d667eaa5
Weiwei Wang <w.wang@soton.ac.uk>
2014-01-01 22:56:51 +0000

    clean the tests in all_anis.py

--- src/finmag/tests/comparison/anisotropy/anis_all.py
+++ src/finmag/tests/comparison/anisotropy/anis_all.py
@@ -1,10 +1,14 @@
 import conftest as test
 
-from test_anis_magpar import test_against_magpar
-from test_anis_nmag import test_against_nmag
-from test_anis_oommf import test_against_oommf
+
+
+
 
 if __name__ == "__main__":
+    from test_anis_magpar import test_against_magpar
+    from test_anis_nmag import test_against_nmag
+    from test_anis_oommf import test_against_oommf
+
     finmag = test.setup(K2=0)
 
     test_against_magpar(finmag)
