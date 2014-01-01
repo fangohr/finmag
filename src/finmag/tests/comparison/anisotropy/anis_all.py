8f9bad43209a4cb6d24db58bb6b89c549fd1db77
Weiwei Wang <w.wang@soton.ac.uk>
2014-01-01 22:52:40 +0000

    fix the tests

--- src/finmag/tests/comparison/anisotropy/anis_all.py
+++ src/finmag/tests/comparison/anisotropy/anis_all.py
@@ -5,11 +5,12 @@ from test_anis_nmag import test_against_nmag
 from test_anis_oommf import test_against_oommf
 
 if __name__ == "__main__":
-    finmag = test.setup()
+    finmag = test.setup(K2=0)
 
     test_against_magpar(finmag)
-    test_against_nmag(finmag)
     test_against_oommf(finmag)
 
+    finmag = test.setup()
+    test_against_nmag(finmag)
     test.teardown(finmag)
 
