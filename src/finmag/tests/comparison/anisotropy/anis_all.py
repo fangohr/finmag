5af367803a51b15d627d07be0d72206f8cda61d9
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-01-04 22:42:07 +0000

    Reapply Weiwei's changes that somehow got lost in the merge.

--- src/finmag/tests/comparison/anisotropy/anis_all.py
+++ src/finmag/tests/comparison/anisotropy/anis_all.py
@@ -1,15 +1,20 @@
 import conftest as test
 
-from test_anis_magpar import test_against_magpar
-from test_anis_nmag import test_against_nmag
-from test_anis_oommf import test_against_oommf
+
+
+
 
 if __name__ == "__main__":
-    finmag = test.setup()
+    from test_anis_magpar import test_against_magpar
+    from test_anis_nmag import test_against_nmag
+    from test_anis_oommf import test_against_oommf
+
+    finmag = test.setup(K2=0)
 
     test_against_magpar(finmag)
-    test_against_nmag(finmag)
     test_against_oommf(finmag)
 
+    finmag = test.setup()
+    test_against_nmag(finmag)
     test.teardown(finmag)
 
