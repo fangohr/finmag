40446368109df622aaa7a185990e70e9a305c7d5
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-01-05 09:20:26 +0000

    Print sys.path for debugging.

--- bin/reproduce_ipython_notebooks.py
+++ bin/reproduce_ipython_notebooks.py
@@ -4,6 +4,7 @@ from IPython.nbformat.current import reads
 from ipynbdoctest import test_notebook as reproduce_notebook
 from glob import glob
 import os
+import sys
 import pytest
 
 #ipynb_files = ['doc/ipython_notebooks_src/tutorial-using-ipython-notebook.ipynb',  # passes
@@ -11,6 +12,8 @@ import pytest
 #              ]
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+print("sys.path: {}".format(sys.path))
 
 ipynb_files = sorted(glob(os.path.join(MODULE_DIR, '../doc/ipython_notebooks_src/*.ipynb')))
 print "============================================================"
@@ -23,7 +26,8 @@ print "============================================================"
 # within Jenkins.
 @pytest.mark.parametrize("ipynb", ipynb_files)
 def test_reproduce_ipython_notebook(ipynb):
-    print "testing %s" % ipynb
+    print "=====   =====   =====   =====   =====   =====   =====   ====="
+    print "Testing notebook: '{}'".format(ipynb)
     with open(ipynb) as f:
         nb = reads(f.read(), 'json')
     reproduce_notebook(nb)
