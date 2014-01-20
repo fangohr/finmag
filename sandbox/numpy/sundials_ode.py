ccbd308940f6eff2107749492cb7fde7455f3a54
Weiwei Wang <w.wang@soton.ac.uk>
2014-01-20 22:27:50 +0000

    add another example, maybe obvious but sometimes it is important for the fixed spin case

---
+++ sandbox/numpy/sundials_ode.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+from finmag.native import sundials
+
+
+def call_back(t, y):
+    return y[0]**2-y[0]**3
+    
+class Test_Sundials(object):
+    
+    def __init__(self, call_back, x0):
+        
+        self.sim = call_back
+        self.x0 = x0
+        self.x = x0.copy()
+        self.ode_count = 0
+        
+        self.create_integrator()
+    
+    def create_integrator(self, reltol=1e-6, abstol=1e-6, nsteps=10000):
+
+        integrator = sundials.cvode(sundials.CV_BDF, sundials.CV_NEWTON)
+        integrator.init(self.sundials_rhs, 0, self.x0)
+        integrator.set_linear_solver_sp_gmr(sundials.PREC_NONE)
+        integrator.set_scalar_tolerances(reltol, abstol)
+        integrator.set_max_num_steps(nsteps)
+        
+        self.integrator = integrator
+    
+    
+    def sundials_rhs(self, t, y, ydot):
+
+        self.ode_count+=1
+                
+        #The following line is very important!!!
+        ydot[:] = 0
+        ydot[0] = self.sim(t, y)
+        
+        return 0
+    
+    def run_step(self, steps, min_dt=1e-10):
+        
+        self.t = 0
+        self.ts=[]
+        self.ys=[]
+
+        for i in range(steps):
+            cvode_dt = self.integrator.get_current_step()
+            if cvode_dt < min_dt:
+                cvode_dt = min_dt
+            self.t += cvode_dt
+            self.integrator.advance_time(self.t, self.x)
+            self.ts.append(self.t)
+            self.ys.append(self.x[0])
+            #print i
+            
+    
+    
+    def print_info(self):
+        print 'sim t=%0.15g'%self.t
+        print 'x=%0.15g'%self.x[0]
+        print 'rhs=%d'%self.ode_count
+
+
+    
---
+++ sandbox/numpy/test_ode.py
@@ -0,0 +1,43 @@
+import os
+import matplotlib as mpl
+mpl.use("Agg")
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sundials_ode import Test_Sundials
+from sundials_ode import call_back
+
+def plot_data(ts,ys):
+    
+    fig=plt.figure()
+    
+    plt.plot(ts, ys, '.-')
+        
+    #plt.legend()
+    plt.grid()
+    
+    fig.savefig('ts_y.pdf')
+    
+
+
+def test1():
+    y0 = np.array([1e-4,0,0,0,0,0,0,0,0,0])
+    ts = Test_Sundials(call_back, y0)
+    ts.run_step(50, min_dt=1e-5)
+    
+    plot_data(ts.ts,ts.ys)
+    
+    ts.print_info()
+
+
+if __name__ == '__main__':
+    
+    test1()
+
+
+    
+
+    
+    
+
+    
