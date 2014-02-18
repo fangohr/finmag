167383e93216cce4dbfd5d8e27b84f86757815a2
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-02-18 22:12:43 +0000

    update "nmag example 2" to newer finmag API

--- examples/nmag_example_2/run_finmag.py
+++ examples/nmag_example_2/run_finmag.py
@@ -1,9 +1,7 @@
 from aeon import default_timer
 from finmag import Simulation
-from finmag.integrators.llg_integrator import llg_integrator
 from finmag.energies import Exchange, Demag
 from finmag.util.meshes import from_geofile
-import dolfin as df
 
 
 def run_simulation():
@@ -11,19 +9,19 @@ def run_simulation():
 
     sim = Simulation(mesh, Ms=0.86e6, unit_length=1e-9, name="finmag_bar")
     sim.set_m((1, 0, 1))
-    sim.integrator = llg_integrator(sim.llg, sim.llg.m, backend=sim.integrator_backend, reltol=1e-6, abstol=1e-6)
+    sim.set_tol(1e-6, 1e-6)
     sim.alpha = 0.5
 
     sim.add(Exchange(13.0e-12))
     sim.add(Demag())
 
-    sim.schedule(Simulation.save_averages, every=5e-12)
+    sim.schedule('save_averages', every=5e-12)
     sim.run_until(3e-10)
 
     print default_timer
-    #print "The RHS was evaluated {} times, while the Jacobian was computed {} times.".format(
-    #        sim.integrator.stats()['nfevals'],
-    #        default_timer._timings['LLG::sundials_jtimes'].calls)
+    print "The RHS was evaluated {} times, while the Jacobian was computed {} times.".format(
+            sim.integrator.stats()['nfevals'],
+            default_timer.get("sundials_jtimes", "LLG").calls)
 
 if __name__ == "__main__":
     run_simulation()
