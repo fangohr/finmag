a0a986c1fb4d2ba980376fc4a6a3d9c89a916cf7
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-02-24 21:42:17 +0000

    callgraph uses grouper
    needs pycallgraph from github to use grouping. The grouper feature has
    been merged, but not released AFAIK

--- examples/nmag_example_2/callgraph.py
+++ examples/nmag_example_2/callgraph.py
@@ -4,6 +4,7 @@ try:
     from pycallgraph import PyCallGraph
     from pycallgraph import Config
     from pycallgraph import GlobbingFilter
+    from pycallgraph import Grouper
     from pycallgraph.output import GraphvizOutput
 except ImportError:
     print "You need to install pycallgraph (for instance with `pip install pycallgraph`)."
@@ -12,11 +13,16 @@ except ImportError:
 from run_finmag import run_simulation
 
 config = Config()
+config.trace_grouper = Grouper(groups=[
+    "finmag.util.*",
+    "finmag.integrators.*",
+    "finmag.energies.*",
+    "finmag.sim.*",
+])
+
 # `max_depth`=15 is the level that would account for all calls to
 # Exchange.__compute_field_petsc; 14 would miss those from TableWriter.save
-config.trace_filter = GlobbingFilter(exclude=[
-    'finmag.util.*',
-], include=[
+config.trace_filter = GlobbingFilter(include=[
     'finmag.*',
     'run_finmag.*',
 ])
