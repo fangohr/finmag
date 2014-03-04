82dd55d5c27b700089bf6170cb1bed0196016ca9
Hans Fangohr <fangohr@soton.ac.uk>
2014-03-04 15:13:06 +0000

    Updated tailored tutorial creation (code didn't keep up to date with nbconvert now being part of the ipython).

--- doc/tailored_tutorials/make-tailored-tutorial-tarball.py
+++ doc/tailored_tutorials/make-tailored-tutorial-tarball.py
@@ -75,15 +75,15 @@ def create_index_html(conf):
 def assemble_rst(conf):
 
     rstrootfilenames = conf['tutorials']
-    targetdirname = targetdirectoryname(conf) 
-    
+    targetdirname = targetdirectoryname(conf)
+
     for fileroot in rstrootfilenames:
         # copy all rst files (and where we have images also the corresponding _files directories)
-        # to the target location  
+        # to the target location
         shutil.copy("../ipython_notebooks_dest/%s.rst" % fileroot, os.path.join(targetdirname,fileroot+".rst"))
         if os.path.exists("../ipython_notebooks_dest/%s_files" % fileroot):
             targetdirnamefiles = os.path.join(targetdirname,fileroot+"_files")
-            if os.path.exists(targetdirnamefiles):
+            if os.path.exiscompile_ipynb2htmlts(targetdirnamefiles):
                 shutil.rmtree(targetdirnamefiles)
             shutil.copytree("../ipython_notebooks_dest/%s_files" % fileroot, targetdirnamefiles)
 
@@ -92,34 +92,34 @@ def assemble_rst(conf):
         if not os.path.exists(ipynbdir):
             os.mkdir(ipynbdir)
         shutil.copy("../ipython_notebooks_src/%s.ipynb" % fileroot, ipynbdir)
-        
-    
+
+
     # Create index file
     fout = open(os.path.join(targetdirname,'index.rst'),'w')
-    
+
     title = "Finmag Manual for %s" % (", ".join(conf['names']))
     fout.write("%s\n" % title)
     fout.write("=" * len(title) + '\n\n')
 
     fout.write("Tutorial compiled at %s.\n\n" % time.asctime())
-    
+
     fout.write(conf['header'])
-    
+
     fout.write(conf['ipynbdir'])
-  
-   
+
+
     # Assemble the index file
     for fileroot in rstrootfilenames:
-        f = open(os.path.join(targetdirname,fileroot + '.rst'),'r')
+        f = open(os.path.jocompile_ipynb2htmlin(targetdirname,fileroot + '.rst'),'r')
         for line in f:
             fout.write(line)
         fout.write('\n\n')  # need white space before next section to have valid rst
-    
+
     fout.close()
-    
+
 
 def compile_rst2html(conf):
-    targetdirname = targetdirectoryname(conf) 
+    targetdirname = targetdirectoryname(conf)
     cmd = "cd %s; rst2html.py --stylesheet-path=../../../css/voidspace.css index.rst index.html" % targetdirname
     logging.debug("Running cmd '%s'" % cmd)
     output = subprocess.check_output(cmd, shell=True)
@@ -127,12 +127,12 @@ def compile_rst2html(conf):
 
 
 def compile_ipynb2html(conf):
-    try:
-        NBCONVERT = os.environ['NBCONVERT']
-    except KeyError:
-        print("Please set the environment variable NBCONVERT so that it "
-              "points to the location of the script nbconvert.py.")
-        sys.exit(1)
+    #try:
+    #    NBCONVERT = os.environ['NBCONVERT']
+    #except KeyError:
+    #    print("Please set the environment variable NBCONVERT so that it "
+    #          "points to the location of the script nbconvert.py.")
+    #    sys.exit(1)
     # XXX TODO: It would be much nicer to use the python functions in
     #           nbconvert directly rather than calling it via
     #           'subprocess'. Once the --output-dest flag (or a
@@ -145,15 +145,16 @@ def compile_ipynb2html(conf):
         os.mkdir(ipynbdir)
     for fileroot in conf['tutorials']:
         shutil.copy("../ipython_notebooks_src/{}.ipynb".format(fileroot), ipynbdir)
-        cmd = "cd {}; python {} --format=html {}.ipynb ../{}.html".format(
-            ipynbdir, NBCONVERT, fileroot, fileroot)
+        NBCONVERT='NBCONVERT'
+        cmd = "cd {}/..; ipython nbconvert --to html ipynb/{}.ipynb".format(
+            ipynbdir, fileroot, fileroot)
         logging.debug("Running cmd '%s'" % cmd)
         output = subprocess.check_output(cmd, shell=True)
         logging.debug("Output was %s" % output)
 
 
 def assemble_tarballs(conf):
-    targetdirname = targetdirectoryname(conf) 
+    targetdirname = targetdirectoryname(conf)
     cmd = "cd %s; tar cfvz finmag-tutorial-%s.tgz finmag-tutorial-%s" % \
        (os.path.join(targetdirname, '..'), conf['nameshort'], conf['nameshort'])
     logging.debug("Running cmd '%s'" % cmd)
@@ -163,7 +164,7 @@ def assemble_tarballs(conf):
 
 
 def remove_builddir(conf):
-    targetdirname = targetdirectoryname(conf) 
+    targetdirname = targetdirectoryname(conf)
     cmd = shutil.rmtree(targetdirname)
     logging.debug("Removing %s" % targetdirname)
 
@@ -176,11 +177,11 @@ if __name__ == '__main__':
                          help='partner[s] to process (names of subdirectories)')
     parser.add_argument('--keepbuild', action='store_true', default=False, \
                          help='Keep directory in which tar ball is built (for debugging).')
-    parser.add_argument('--use-ipynb2html', action='store_true', default=False, \
+    parser.add_argument('--use-ipynb2html', action='store_true', default=True, \
                          help='Use nbconvert directly to convert .ipynb files to .html.')
 
     args = vars(parser.parse_args())
-    
+
     if isinstance(args['partner'],list):
         partners = args['partner']
         logging.info("About to process %s" % ", ".join(partners))
@@ -194,7 +195,7 @@ if __name__ == '__main__':
         conf = conffile.conf
         #add defaults
         conf['header'] = ".. contents::\n\n"
-        conf['ipynbdir']  = "The raw ipython notebooks are `available here <ipynb>`__.\n\n" 
+        conf['ipynbdir']  = "The raw ipython notebooks are `available here <ipynb>`__.\n\n"
 
         # Do the work
         if args['use_ipynb2html']:
