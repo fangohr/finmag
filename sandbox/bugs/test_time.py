2f593949d3fc27218a3087926a097d50f18fe349
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-04-14 17:21:49 +0200

    add minimal test case for time bug Weiwei found

---
+++ sandbox/bugs/test_time.py
@@ -0,0 +1,40 @@
+import os
+import dolfin as df
+import numpy as np
+from finmag import Simulation as Sim
+from finmag.energies import Zeeman
+
+alpha = 0.01
+Ms = 8.6e5
+
+def test_time_run_until():
+    mesh = df.BoxMesh(0, 0, 0, 2, 2, 2, 1, 1, 1)
+    sim = Sim(mesh, Ms, unit_length=1e-9, pbc='2d')
+    sim.alpha = alpha
+    sim.set_m((1, 0, 0))
+    sim.set_tol(1e-10, 1e-14)
+    sim.add(Zeeman((0, 0, 1e5)))
+
+    dt = 1e-9; 
+    ts = np.linspace(0, 50 * dt, 4)
+
+    for t in ts:
+        sim.run_until(t)
+        print "Asked to run until t = {} s, internal time is now t = {} s.".format(t, sim.t)
+        assert abs(sim.t - t) < 1e-13
+
+def test_time_advance_time():
+    mesh = df.BoxMesh(0, 0, 0, 2, 2, 2, 1, 1, 1)
+    sim = Sim(mesh, Ms, unit_length=1e-9, pbc='2d')
+    sim.alpha = alpha
+    sim.set_m((1, 0, 0))
+    sim.set_tol(1e-10, 1e-14)
+    sim.add(Zeeman((0, 0, 1e5)))
+
+    dt = 1e-9; 
+    ts = np.linspace(0, 50 * dt, 4)
+
+    for t in ts:
+        sim.advance_time(t)
+        print "Asked to run until t = {} s, internal time is now t = {} s.".format(t, sim.t)
+        assert abs(sim.t - t) < 1e-13
