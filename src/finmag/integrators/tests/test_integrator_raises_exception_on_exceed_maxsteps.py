e63df9de0a21a23ef24d20040707d456260932cb
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-04-15 16:28:30 +0200

    replace get/set_max_steps with more python max_steps attribute

--- src/finmag/integrators/tests/test_integrator_raises_exception_on_exceed_maxsteps.py
+++ src/finmag/integrators/tests/test_integrator_raises_exception_on_exceed_maxsteps.py
@@ -2,20 +2,19 @@ import os
 import pytest
 import finmag
 
+steps = 10
 
 def test_can_change_maxsteps():
     sim = finmag.example.barmini()
     sim.create_integrator()
-    steps = 10
-    sim.integrator.set_max_steps(steps)
-    assert sim.integrator.get_max_steps() == steps
+    sim.integrator.max_steps = steps
+    assert sim.integrator.max_steps == steps
 
 
 def test_time_run_until():
     sim = finmag.example.barmini()
     sim.create_integrator()
-    steps = 10
-    sim.integrator.set_max_steps(steps)
+    sim.integrator.max_steps = steps
     t = 1e-9;
     with pytest.raises(RuntimeError) as exc_info:
         sim.run_until(t)
@@ -29,14 +28,14 @@ def test_time_run_until():
 def test_time_advance_time():
     sim = finmag.example.barmini()
     sim.create_integrator()
-    steps = 10
-    sim.integrator.set_max_steps(steps)
+    sim.integrator.max_steps = steps
     t = 1e-9
     with pytest.raises(RuntimeError) as exc_info:
         sim.advance_time(t)
     assert exc_info.value.message.split('\n')[0] \
        == "The integrator has reached its maximum of {} steps.".format(steps)
     assert sim.t < t  # check that integration was aborted
+
 
 @pytest.mark.slow
 def test_time_default_max_steps():
@@ -48,5 +47,5 @@ def test_time_default_max_steps():
     # (This test will need updating whenever we change the message.)
     assert exc_info.value.message.split('\n')[0] \
        == "The integrator has reached its maximum of {} steps.".format(
-               sim.integrator.get_max_steps())
+               sim.integrator.max_steps)
     assert sim.t < t
