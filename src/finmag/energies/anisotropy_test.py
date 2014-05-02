b7ff518aa6254f346e80e340a68ce91b79430f79
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-05-02 17:43:00 +0100

    remove debugging code that was not meant to be committed

--- src/finmag/energies/anisotropy_test.py
+++ src/finmag/energies/anisotropy_test.py
@@ -77,10 +77,7 @@ def test_anisotropy_field(fixt):
 
     """
     TOLERANCE = 1e-14
-    print fixt["m"]
-    print fixt["m"].vector().array()
     fixt["m"].assign(df.Constant((1/np.sqrt(2), 0, 1/np.sqrt(2))))
-    print "fails before this"
     H = fixt["anis"].compute_field()
 
     v = df.TestFunction(fixt["S3"])
