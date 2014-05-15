b8818fc1efee04f439d8c11d3bce7acffdf40288
Weiwei Wang <w.wang@soton.ac.uk>
2014-05-15 21:35:21 +0100

    add the doc and rename the exercise2

--- sandbox/basics-dolfin/parallel/exercises/cells_distribution.py
+++ sandbox/basics-dolfin/parallel/exercises/cells_distribution.py
@@ -1,13 +1,29 @@
+#!/usr/bin/env python
+
+"""
+A short example to show how the nodes are distributed on processes
+without the knowledge of the data details. To run this script,
+
+    mpirun -n 8 python cells_distribution.py
+    
+"""
+
 import dolfin as df
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 size = comm.Get_size()
 
-def fun(x):
-    rank = comm.Get_rank()
-    return rank*0.1
 
+def fun(x):
+    return 1.0*rank/size
+
+"""
+A wrapper class that could be called by dolfin in parallel 
+for a normal python function. In this way, we don't have to
+know the details of the data.
+"""
 class HelperExpression(df.Expression):
     def __init__(self,value):
         super(HelperExpression, self).__init__()
@@ -20,7 +36,7 @@ class HelperExpression(df.Expression):
 
 if __name__=="__main__":
 
-    mesh = df.RectangleMesh(0, 0, 20, 20, 20, 20)
+    mesh = df.RectangleMesh(0, 0, 20, 20, 100, 100)
     V = df.FunctionSpace(mesh, 'DG', 0)
     hexp = HelperExpression(fun)
     u = df.interpolate(hexp, V)
