2c2a75c5c5a20598b9e712caac64b86259157ec3
Hans Fangohr <fangohr@soton.ac.uk>
2014-06-08 09:00:15 +0100

    Adding script to only install slepc4py. I had trouble getting this installed on my virtual machine, although it worked fine on osiris. This is what made it happen onthe virtual machine in the end.

---
+++ install/slepc4py.sh
@@ -0,0 +1,15 @@
+# For Hans' virtual machine, the following commands need to be run as root.
+# I.e. something like "sudo su - ", and the execute what is in this script
+# (8 June 2014):
+
+#root@xubu1404:~# export PETSC_DIR=/usr/lib/petsc
+#root@xubu1404:~# export SLEPC_DIR=/usr/lib/slepc
+#root@xubu1404:~# pip install slepc4py --allow-external slepc4py --allow-unverified slepc4py 
+
+export PETSC_DIR=/usr/lib/petsc
+export SLEPC_DIR=/usr/lib/slepc
+pip install slepc4py --allow-external slepc4py --allow-unverified slepc4py 
+
+
+
+
