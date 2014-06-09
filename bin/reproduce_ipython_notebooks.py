633eff28b877309f7e89871e78666d9be9320a31
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-06-09 03:50:49 +0100

    Remove obsolete commented lines.

--- bin/reproduce_ipython_notebooks.py
+++ bin/reproduce_ipython_notebooks.py
@@ -7,10 +7,6 @@ import os
 import sys
 import pytest
 
-#ipynb_files = ['doc/ipython_notebooks_src/tutorial-using-ipython-notebook.ipynb',  # passes
-#               #'doc/ipython_notebooks_src/tutorial-use-of-logging.ipynb',  # fails
-#              ]
-
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 print("sys.path: {}".format(sys.path))
@@ -21,9 +17,8 @@ print "Found {} .ipynb files:".format(len(ipynb_files))
 print "\n".join(ipynb_files)
 print "============================================================"
 
-# This parameterization checks each .ipynb file in a
-# separate test, which is nicer for debugging from
-# within Jenkins.
+# This parameterization checks each .ipynb file in a separate test, which is nice
+# for debugging in Jenkins.
 @pytest.mark.parametrize("ipynb", ipynb_files)
 def test_reproduce_ipython_notebook(ipynb):
     print "\n=====   =====   =====   =====   =====   =====   =====   ====="
