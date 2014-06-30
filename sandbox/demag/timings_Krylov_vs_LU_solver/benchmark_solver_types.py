1261465b711ac25de1410cdb906606d59707629c
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-06-30 12:44:22 +0100

    Small script to benchmark Krylov vs. LU solver for the barmini and bar examples.

---
+++ sandbox/demag/timings_Krylov_vs_LU_solver/benchmark_solver_types.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import dolfin as df
+import numpy as np
+import sys
+
+try:
+    solver_type = sys.argv[1]
+except IndexError:
+    print("Usage: time_solver_types.py SOLVER_TYPE [N]")
+    sys.exit()
+
+try:
+    N = int(sys.argv[2])
+except IndexError:
+    N = 3
+
+from finmag.example import barmini, bar
+
+timings = []
+for i in xrange(N):
+    print("[DDD] Starting run #{}".format(i))
+    sim = barmini(demag_solver_type=solver_type)
+    df.tic()
+    sim.relax()
+    timings.append(df.toc())
+    print("Latest run took {:.2f} seconds.".format(timings[-1]))
+print("Timings (in seconds): {}".format(['{:.2f}'.format(t) for t in timings]))
+print("Mean: {:.2f}".format(np.mean(timings)))
+print("Median: {:.2f}".format(np.median(timings)))
+print("Fastest: {:.2f}".format(np.min(timings)))
