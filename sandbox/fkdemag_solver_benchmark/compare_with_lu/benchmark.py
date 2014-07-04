f54c2390a22e6c3c46ecca99b5eaa4203a4dbf71
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-07-04 14:49:16 +0100

    refactored benchmark script

--- sandbox/fkdemag_solver_benchmark/compare_with_lu/benchmark.py
+++ sandbox/fkdemag_solver_benchmark/compare_with_lu/benchmark.py
@@ -8,16 +8,33 @@ from finmag.energies import Demag
 from finmag.util.meshes import sphere
 
 radius = 5.0
-maxhs = [0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0]
+maxhs = [0.9, 1.0] #[0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0]
 unit_length = 1e-9
 m_0 = (1, 0, 0)
 Ms = 1
 H_ref = np.array((- Ms / 3.0, 0, 0))
 
+def setup_demag(S3, m, Ms, unit_length, method, *args, **kwargs):
+    demag = Demag(method, *args, **kwargs)
+    demag.setup(S3, m, Ms, unit_length)
+    return demag
+
+def benchmark(demag, H_ref):
+    REPETITIONS = 10
+    start = time.time()
+    for j in xrange(REPETITIONS):
+        H = demag.compute_field()
+    elapsed = (time.time() - start) / REPETITIONS
+
+    H = H.reshape((3, -1))
+    spread = abs(H[0].max() - H[0].min()) 
+    error = abs(H.mean(axis=1)[0] - H_ref[0]) / abs(H_ref[0])
+
+    return elapsed, spread, error
+
 vertices = []
-solvers_label = ["FK", "FK opt.", "GCR", "Treecode", "FK LU"]
-timings = [[], [], [], [], []]
-errors = [[], [], [], [], []]
+methods = ("FK default", "FK opt.", "FK LU", "GCR", "Treecode")
+results = []
 
 for maxh in maxhs:
     mesh = sphere(r=radius, maxh=maxh, directory="meshes")
@@ -26,57 +43,50 @@ for maxh in maxhs:
     m = df.Function(S3)
     m.assign(df.Constant(m_0))
 
-    for i in xrange(5):
-        if i == 0:
+    results_for_this_mesh = []
+    for method in methods:
+        if method == "FK default":
             parameters = {}
             parameters["phi_1_solver"] = "default"
             parameters["phi_1_preconditioner"] = "default"
             parameters["phi_2_solver"] = "default"
             parameters["phi_2_preconditioner"] = "default"
-            solver_type = "Krylov" 
-            demag = Demag("FK", solver_type=solver_type, parameters=parameters)
-        elif i == 1:
+            demag = Demag("FK", solver_type="FK", parameters=parameters)
+        elif method == "FK opt.":
             parameters = {}
             parameters["phi_1_solver"] = "cg"
             parameters["phi_1_preconditioner"] = "ilu"
             parameters["phi_2_solver"] = "cg"
             parameters["phi_2_preconditioner"] = "ilu"
-            solver_type = "Krylov" 
-            demag = Demag("FK", solver_type=solver_type, parameters=parameters)
-        elif i == 2:
-            solver = "GCR"
-            demag = Demag(solver)
-        elif i == 3:
-            solver = "Treecode"
-            demag = Demag(solver)
-        elif i == 4:
-            solver = "FK"
-            demag = Demag(solver, solver_type="LU")
-        demag.setup(S3, m, Ms, unit_length)
+            demag = Demag("FK", solver_type="FK", parameters=parameters)
+        elif method == "FK LU":
+            demag = Demag("FK", solver_type="LU")
+        elif method == "GCR":
+            demag = Demag("GCR")
+        elif method == "Treecode":
+            demag = Demag("Treecode")
+        else:
+            import sys
+            print "What are you doing?"
+            sys.exit(1)
 
-        start = time.time()
-        for j in xrange(10):
-            H = demag.compute_field()
-        elapsed = (time.time() - start) / 10.0
-
-        H = H.reshape((3, -1)).mean(axis=1)
-        error = abs(H[0] - H_ref[0]) / abs(H_ref[0])
-        timings[i].append(elapsed)
-        errors[i].append(error)
-
-print solvers_label, timings, errors
+        results_for_this_mesh.append(benchmark(demag, H_ref))
+    results.append(results_for_this_mesh)
 
 fig = plt.figure()
+
 ax = fig.add_subplot(211)
-for i, label in enumerate(solvers_label):
-    ax.plot(vertices, timings[i], label=label)
+for i, method in enumerate(methods):
+    timings = [results_per_mesh[i][0] for results_per_mesh in results]
+    ax.plot(vertices, timings, label=method)
 ax.legend(loc=2)
 ax.set_xlabel("vertices")
 ax.set_ylabel("time (s)")
 
 ax = fig.add_subplot(212)
-for i, label in enumerate(solvers_label):
-    ax.plot(vertices, errors[i], label=label)
+for i, method in enumerate(methods):
+    errors = [results_per_mesh[i][2] for results_per_mesh in results]
+    ax.plot(vertices, errors, label=method)
 ax.legend(loc=2)
 ax.set_xlabel("vertices")
 ax.set_ylabel("relative error (%)")
