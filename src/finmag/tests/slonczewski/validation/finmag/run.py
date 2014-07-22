a65472a2e0d7cc43a0fd7f234c3fe3af2fecb296
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-07-22 13:47:01 +0100

    excellent agreement of slonczewski in finmag compared to oommf/nmag

--- src/finmag/tests/slonczewski/validation/finmag/run.py
+++ src/finmag/tests/slonczewski/validation/finmag/run.py
@@ -5,16 +5,15 @@ from math import pi, sin, cos
 from finmag import Simulation as Sim
 from finmag.energies import Exchange, UniaxialAnisotropy, Zeeman
 from finmag.util.consts import mu0
-import logging
-logger=logging.getLogger('finmag')
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 averages_file = os.path.join(MODULE_DIR, "averages.txt")
 
-def run_simulation(t_max = 10e-9):
-    L = W = 12.5e-9; H = 5e-9;
+def run_simulation():
+    L = W = 12.5e-9
+    H = 5e-9
     mesh = df.BoxMesh(0, 0, 0, L, W, H, 5, 5, 2)
-    sim = Sim(mesh, Ms=8.6e5)
+    sim = Sim(mesh, Ms=860e3, name="finmag_validation")
     sim.set_m((1, 0.01, 0.01))
     sim.alpha = 0.014
     sim.gamma = 221017
@@ -22,24 +21,19 @@ def run_simulation(t_max = 10e-9):
     H_app_mT = np.array([0.2, 0.2, 10.0])
     H_app_SI = H_app_mT / (1000 * mu0)
     sim.add(Zeeman(tuple(H_app_SI)))
-
     sim.add(Exchange(1.3e-11))
     sim.add(UniaxialAnisotropy(-1e5, (0, 0, 1)))
 
-    I = 5e-5 # current in A
-    J = I / (L * W) # current density in A/m^2
-    theta = 40.0 * pi/180; phi = pi/2 # polarisation direction
+    I = 5e-5  # current in A
+    J = I / (L * W)  # current density in A/m^2
+    theta = 40.0 * pi/180  # polarisation direction
+    phi = pi/2
     p = (sin(theta)*cos(phi), sin(theta)*sin(phi), cos(theta))
-    sim.llg.use_slonczewski(J=J, P=0.4, d=H, p=p)
+    sim.set_stt(current_density=J, polarisation=0.4, thickness=H, direction=p)
 
-    with open(averages_file, "w") as f:
-        dt = 5e-12; 
-        for t in np.arange(0, t_max, dt):
-            sim.run_until(t)
-            logger.info("integrated to time = %gs / %gs" % (t, t_max))
-            f.write("{} {} {} {}\n".format(t, *sim.m_average))
-    return len(np.arange(0, t_max, dt))
+    sim.schedule("save_averages", every=5e-12)
+    sim.run_until(10e-9)
 
 
 if __name__ == "__main__":
-    run_simulation(short=True)
+    run_simulation()
---
+++ src/finmag/tests/slonczewski/validation/finmag/test_finmag_validation.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+from finmag.util.fileio import Tablereader
+
+FINMAG_DYNAMICS_FILE = "finmag_validation_light.ndt"
+NMAG_FILE = "../nmag/averages_nmag5.txt"
+OOMMF_FILE = "../oommf/averages_oommf.txt"
+EPSILON = 1e-16
+TOLERANCE = 1e-4
+
+
+def extract_magnetisation_dynamics():
+    reader = Tablereader("finmag_validation.ndt")
+    dynamics = np.array(reader["time", "m_x", "m_y", "m_z"]).T
+    np.savetxt(FINMAG_DYNAMICS_FILE, dynamics, header="time mx my mz")
+
+
+def test_against_nmag():
+    if not os.path.isfile(FINMAG_DYNAMICS_FILE):
+        extract_magnetisation_dynamics()
+    finmag_dynamics = np.loadtxt(FINMAG_DYNAMICS_FILE)
+    nmag_dynamics = np.loadtxt(NMAG_FILE)
+
+    diff = np.abs(np.array(finmag_dynamics) - np.array(nmag_dynamics))
+    print("Deviation is %s" % (np.max(diff)))
+    assert np.max(diff[:, 0]) < EPSILON # compare timesteps
+    assert np.max(diff[:, 1:]) < TOLERANCE
+
+
+def plot_dynamics():
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure(figsize=(15, 6))
+    ax = fig.add_subplot(111)
+
+    if os.path.isfile(NMAG_FILE):
+        nmag = np.loadtxt(NMAG_FILE)
+        nmag[:,0] *= 1e9
+        ax.plot(nmag[:,0], nmag[:,1], "rx", label="nmag m_x")
+        ax.plot(nmag[:,0], nmag[:,2], "bx", label="nmag m_y")
+        ax.plot(nmag[:,0], nmag[:,3], "gx", label="nmag m_z")
+    else:
+        print "Missing nmag file."
+
+    if os.path.isfile(OOMMF_FILE):
+        oommf = np.loadtxt(OOMMF_FILE)
+        oommf[:,0] *= 1e9
+        ax.plot(oommf[:,0], oommf[:, 1], "r+", label="oommf m_x")
+        ax.plot(oommf[:,0], oommf[:, 2], "b+", label="oommf m_y")
+        ax.plot(oommf[:,0], oommf[:, 3], "g+", label="oommf m_z")
+    else:
+        print "Missing oommf file."
+
+    finmag = np.loadtxt(FINMAG_DYNAMICS_FILE)
+    finmag[:,0] *= 1e9
+    ax.plot(finmag[:,0], finmag[:,1], "k", label="finmag m_x")
+    ax.plot(finmag[:,0], finmag[:,2], "k", label="finmag m_y")
+    ax.plot(finmag[:,0], finmag[:,3], "k", label="finmag m_z")
+
+    ax.set_xlim((0, 2))
+    ax.set_xlabel("time (ns)")
+    ax.legend()
+    plt.savefig("dynamics.png")
+
+if __name__ == "__main__":
+    plot_dynamics()
+
+
+
