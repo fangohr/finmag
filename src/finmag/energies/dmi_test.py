eaa1e58919d9152b15147f0ea07c66dcba24b5fb
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-07-25 19:58:17 +0100

    Use higher-level Field interface in dmi_test.

--- src/finmag/energies/dmi_test.py
+++ src/finmag/energies/dmi_test.py
@@ -107,10 +107,9 @@ def test_dmi_pbc2d_1D(plot=False):
     sim.relax(stopping_dmdt=0.001)
 
     if plot:
-        df.plot(sim._m)
-        df.interactive()
+        sim.m_field.plot_with_dolfin()
 
-    mx=[sim._m(x+0.5,1)[0] for x in range(20)]
+    mx = [sim.m_field.probe([x + 0.5, 1])[0] for x in range(20)]
     
     assert np.max(np.abs(mx)) < 1e-6
 
