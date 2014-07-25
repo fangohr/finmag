93f0c1e27ac29a93c91a91a7cad5de34727c7f48
Hans Fangohr <fangohr@soton.ac.uk>
2014-07-25 13:22:16 +0100

    Simplify code with Becky and David.

--- sandbox/basics-dolfin/parallel/exercises/cells_distribution.py
+++ sandbox/basics-dolfin/parallel/exercises/cells_distribution.py
@@ -17,28 +17,32 @@ size = comm.Get_size()
 
 
 def fun(x):
-    return 1.0*rank/size
+    return rank 
 
-"""
-A wrapper class that could be called by dolfin in parallel 
-for a normal python function. In this way, we don't have to
-know the details of the data.
-"""
-class HelperExpression(df.Expression):
-    def __init__(self,value):
-        super(HelperExpression, self).__init__()
-        self.fun = value
 
-    def eval(self, value, x):
-        value[0] = self.fun(x)
-
+## David, Becky, Hans 25 July 14: the next few lines are potentially
+## useful, but not needed here.
+## """
+## A wrapper class that could be called by dolfin in parallel 
+## for a normal python function. In this way, we don't have to
+## know the details of the data.
+## """
+## class HelperExpression(df.Expression):
+##     def __init__(self,value):
+##         super(HelperExpression, self).__init__()
+##         self.fun = value
+## 
+##     def eval(self, value, x):
+##         value[0] = self.fun(x)
+## 
 
 
 if __name__=="__main__":
 
     mesh = df.RectangleMesh(0, 0, 20, 20, 100, 100)
     V = df.FunctionSpace(mesh, 'DG', 0)
-    hexp = HelperExpression(fun)
+    #hexp = HelperExpression(fun)       # if we want to use the HelperExpression
+    hexp = df.Expression("alpha", alpha=rank)
     u = df.interpolate(hexp, V)
     
     file = df.File('color_map.pvd')
