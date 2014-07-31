ba6887ccaee476f9383a4f08f6357f60025a84ff
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-07-31 22:23:27 +0200

    move misplaced files

--- src/finmag/physics/native/terms.h
+++ src/finmag/physics/native/terms.h
@@ -1,1 +1,5 @@
-#pragma once
+/* Compute the damping for one node. */
+void damping(double const& alpha, double const& gamma,
+             double const& m0, double const& m1, double const& m2,
+             double const& H0, double const& H1, double const& H2,
+             double& dm0, double& dm1, double& dm2);
