3b5c738f84bde797117efa6aacb47b8105cf370b
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-08-01 18:28:33 +0100

    add alpha to new C++ code, with test

--- sandbox/new_native_code/test_equation.py
+++ sandbox/new_native_code/test_equation.py
@@ -6,32 +6,35 @@ from equation import equation_module as eq
 @pytest.fixture
 def setup():
     mesh = df.UnitIntervalMesh(2)
-    V = df.VectorFunctionSpace(mesh, "CG", 1, dim=3)
-    m = df.Function(V)
+    V = df.FunctionSpace(mesh, "CG", 1)
+    alpha = df.Function(V)
+    alpha.assign(df.Constant(1))
+    W = df.VectorFunctionSpace(mesh, "CG", 1, dim=3)
+    m = df.Function(W)
     m.assign(df.Constant((1, 0, 0)))
-    H = df.Function(V)
+    H = df.Function(W)
     H.assign(df.Constant((0, 1, 0)))
-    dmdt = df.Function(V)
-    return mesh, V, m, H, dmdt
+    dmdt = df.Function(W)
+    return mesh, V, alpha, W, m, H, dmdt
 
 
-def same(f, g):
+def same(v, w):
     """
-    Returns True if the functions `f` and `g` have the same vector-entries.
+    Returns True if the vectors `v` and `w` have the same entries.
 
     """
-    diff = f.vector() - g.vector()
+    diff = v - w
     diff.abs()
     return diff.sum() == 0
 
 
 def test_new_equation(setup):
-    mesh, V, m, H, dmdt = setup
+    mesh, V, alpha, W, m, H, dmdt = setup
     equation = eq.Equation(m.vector(), H.vector(), dmdt.vector())
 
 
 def test_new_equation_wrong_size(setup):
-    mesh, V, m, H, dmdt = setup
+    mesh, V, alpha, W, m, H, dmdt = setup
     W = df.VectorFunctionSpace(mesh, "CG", 2, dim=3)  # W like Wrong
     H_W = df.Function(W)
     with pytest.raises(StandardError):
@@ -39,7 +42,7 @@ def test_new_equation_wrong_size(setup):
 
 
 def test_regression_vector_wrong_state(setup):
-    mesh, V, m, H, dmdt = setup
+    mesh, V, alpha, W, m, H, dmdt = setup
     equation = eq.Equation(m.vector(), H.vector(), dmdt.vector())
     equation.solve()
     # the following operation would fail with PETSc error code 73
@@ -49,10 +52,21 @@ def test_regression_vector_wrong_state(setup):
 
 
 def test_damping(setup):
-    mesh, V, m, H, dmdt = setup
+    mesh, V, alpha, W, m, H, dmdt = setup
     equation = eq.Equation(m.vector(), H.vector(), dmdt.vector())
     equation.solve()
-
-    dmdt_expected = df.Function(V)
+    dmdt_expected = df.Function(W)
     dmdt_expected.assign(df.Constant((0, 1, 0)))
-    assert same(dmdt, dmdt_expected)
+    assert same(dmdt.vector(), dmdt_expected.vector())
+
+
+def test_alpha(setup):
+    mesh, V, alpha, W, m, H, dmdt = setup
+    equation = eq.Equation(m.vector(), H.vector(), dmdt.vector())
+    assert equation.get_alpha() is None  # does not crash
+    equation.set_alpha(alpha.vector())
+    assert same(alpha.vector(), equation.get_alpha())
+    # since alpha and Equation::alpha are fundamentally the same object
+    # changing one should change the other, which is what we test next
+    alpha.assign(df.Constant(2))
+    assert same(alpha.vector(), equation.get_alpha())
