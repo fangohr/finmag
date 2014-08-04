f3ea8f9c1e65d7d69f875a5089d51f57769f0e6a
Hans Fangohr <fangohr@soton.ac.uk>
2014-08-04 12:54:00 +0100

    Worked throuh xdmf file writing and reading. Works great for writing, but not for reading. Paraview can read meshes and functions saved with xdmf. (With david).

--- sandbox/xdmf_and_hdf5/parallel-xdmf-mesh-and-vector/output.py
+++ sandbox/xdmf_and_hdf5/parallel-xdmf-mesh-and-vector/output.py
@@ -1,15 +1,21 @@
+
 from dolfin import *
 
 mesh = UnitSquareMesh(5, 5)
 V = FunctionSpace(mesh, 'CG', 1)
 u = interpolate(Expression('x[0]'), V)
 
-f = HDF5File(mesh.mpi_comm(), 'u.h5', 'w')    # works
+f = File('mesh.xdmf')
 
-f.write(mesh, 'mesh')
-print "mesh we have written: {}".format(mesh.coordinates().shape)
-f.write(u, 'u')
-print "vector we have written: {}".format(u.vector().array().shape)
+f << mesh
+print("mesh we have written: {}".format(mesh.coordinates().shape))
 del f
+
+
+fu = File('u.xdmf')
+fu << u
+print("u we have written: {}".format(u.vector().array().shape))
+
+del fu
 
 
