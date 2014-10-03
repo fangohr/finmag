2d859984fb0a8e941d34f3172d93af292aa2416e
Marijan Beg <mb4e10@soton.ac.uk>
2014-10-03 12:48:23 +0100

    Length scales and its tests added, so that exchange length can be removed from other modules and tests.

---
+++ src/finmag/util/length_scales.py
@@ -0,0 +1,46 @@
+import dolfin as df
+from finmag.field import Field
+from finmag.util.consts import mu0
+from numpy import pi
+
+def exchange_length(A, Ms):
+    """
+    Computes the exchange length, when given the exchange constant A
+    and the saturation magnetisation Ms. Both Ms and A are Field objects.
+
+    """
+    dg_functionspace = df.FunctionSpace(A.mesh(), 'DG', 0)
+    exchange_length = Field(dg_functionspace)
+    
+    function = df.project(df.sqrt(2*A.f/(mu0*Ms.f**2)), dg_functionspace)
+    exchange_length.set(function)
+
+    return exchange_length
+
+def bloch_parameter(A, K1):
+    """
+    Computes the Bloch parameter, when given the exchange constant A
+    and the anisotropy energy density K1. Both A and K1 are Field objects.
+
+    """
+    dg_functionspace = df.FunctionSpace(A.mesh(), 'DG', 0)
+    bloch_parameter = Field(dg_functionspace)
+    
+    function = df.project(df.sqrt(A.f/K1.f), dg_functionspace)
+    bloch_parameter.set(function)
+
+    return bloch_parameter
+
+
+def helical_period(A, D):
+    """
+    Computes the Helical period of a Skyrmion, when given exchange
+    constant, A and the DMI strength,D. 
+    """
+    dg_functionspace = df.FunctionSpace(A.mesh(), 'DG', 0)
+    helical_period = Field(dg_functionspace)
+    
+    function = df.project(df.sqrt(4*pi*A.f/D.f), dg_functionspace)
+    helical_period.set(function)
+
+    return helical_period
---
+++ src/finmag/util/length_scales_test.py
@@ -0,0 +1,77 @@
+import dolfin as df
+import numpy as np
+import length_scales as ls
+from finmag.field import Field
+from finmag.util.consts import mu0
+
+
+class TestLengthScales(object):
+    def setup(self):
+        # Create a 3d mesh.
+        self.mesh3d = df.UnitCubeMesh(11, 10, 10)
+
+        # Function space is DG (Discontinuous Galerkin)
+        # with degree=1 unless named explicitly.
+
+        # Create scalar function spaces.
+        self.functionspace = df.FunctionSpace(self.mesh3d,
+                                            family="DG", degree=1)
+
+    def test_exchange_length_constant(self):
+        A = Field(self.functionspace, 2/mu0)
+        Ms = Field(self.functionspace, 1/mu0)
+
+        lex = ls.exchange_length(A, Ms)
+
+        assert np.allclose(lex.get_numpy_array_debug(), 2)
+
+    def test_exchange_length_varying(self):
+        A_expression = df.Expression('4/mu0*x[0] + 1e-100', mu0=mu0)
+        Ms_expression = df.Expression('2/mu0*x[0] + 1e-100', mu0=mu0)
+        
+        A = Field(self.functionspace, A_expression)
+        Ms = Field(self.functionspace, Ms_expression)
+
+        lex = ls.exchange_length(A, Ms)
+
+        assert abs(lex.probe((0.5, 0.5, 0.5)) - 2) < 0.05
+
+    def test_bloch_parameter_constant(self):
+        A = Field(self.functionspace, 2)
+        K1 = Field(self.functionspace, 0.5)
+
+        bloch_parameter = ls.bloch_parameter(A, K1)
+
+        assert np.allclose(bloch_parameter.get_numpy_array_debug(), 2)
+
+    def test_bloch_parameter_varying(self):
+        A_expression = df.Expression('4*x[0] + 1e-100')
+        K1_expression = df.Expression('x[0] + 1e-100')
+        
+        A = Field(self.functionspace, A_expression)
+        K1 = Field(self.functionspace, K1_expression)
+
+        bloch_parameter = ls.bloch_parameter(A, K1)
+
+        assert abs(bloch_parameter.probe((0.5, 0.5, 0.5)) - 2) < 0.05
+
+    def test_helical_period_constant(self):
+        A = Field(self.functionspace, 1/np.pi)
+        D = Field(self.functionspace, 4)
+
+        helical_period = ls.helical_period(A, D)
+
+        assert np.allclose(helical_period.get_numpy_array_debug(), 1)
+
+    def test_helical_period_varying(self):
+        A_expression = df.Expression('2/pi*x[0] + 1e-100', pi=np.pi)
+        D_expression = df.Expression('8*x[0] + 1e-100')
+        
+        A = Field(self.functionspace, A_expression)
+        D = Field(self.functionspace, D_expression)
+
+        helical_period = ls.helical_period(A, D)
+
+        assert abs(helical_period.probe((0.5, 0.5, 0.5)) - 1) < 0.05
+        
+        
