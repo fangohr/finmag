2dd4c1873e84b079133262b2adbcf5f5fc86daeb
Maximilian Albert <maximilian.albert@soton.ac.uk>
2014-10-04 01:27:34 +0100

    Fix another documentation script.

--- examples/exchange_demag/simple_1D_finmag.py
+++ examples/exchange_demag/simple_1D_finmag.py
@@ -16,10 +16,12 @@ nd = np.load(os.path.join(MODULE_DIR, "nmag_hansconf.npy"))
 # run finmag
 mesh = df.IntervalMesh(100, 0, 10e-9)
 S3 = df.VectorFunctionSpace(mesh, "Lagrange", 1, dim=3)
+DG0 = df.FunctionSpace(mesh, "DG", 0)
 m = Field(S3, df.Expression(("cos(x[0]*pi/10e-9)", "sin(x[0]*pi/10e-9)", "0")))
+Ms = Field(DG0, 1.0)
 
 exchange = Exchange(1.3e-11)
-exchange.setup(m, Ms=1)
+exchange.setup(m, Ms)
 
 fd = exchange.energy_density()
 
