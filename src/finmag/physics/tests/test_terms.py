0bef884a46f71f65bd3d4c3758b58db5405cc64f
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-10-16 15:36:48 +0100

    in test_terms.py move compilation of extension module to test time (not init time)

--- src/finmag/physics/tests/test_terms.py
+++ src/finmag/physics/tests/test_terms.py
@@ -1,21 +1,28 @@
+import pytest
 from os import path
 import dolfin as df
-from finmag.physics.equation import NATIVE_DIR
-
-with open(path.join(NATIVE_DIR, "terms.h"), "r") as header:
-    code = header.read()
-
-terms_module = df.compile_extension_module(
-    code=code,
-    source_directory=NATIVE_DIR,
-    sources=["terms.cpp"],
-    # declare dm_x, dm_y and dm_z as input/output parameters
-    # they will turn up in Python as return values
-    additional_declarations="%apply double& INOUT { double& dm_x, double& dm_y, double& dm_z };",
-    include_dirs=[NATIVE_DIR],)
 
 
-def test_damping():
+@pytest.fixture
+def terms_module():
+    MODULE_DIR = path.dirname(path.abspath(__file__))
+    SOURCE_DIR = path.join(MODULE_DIR, "..", "native")
+
+    with open(path.join(SOURCE_DIR, "terms.h"), "r") as header:
+        code = header.read()
+
+    extension_module = df.compile_extension_module(
+        code=code,
+        source_directory=SOURCE_DIR,
+        sources=["terms.cpp"],
+        # declare dm_x, dm_y and dm_z as input/output parameters
+        # they will turn up in Python as return values
+        additional_declarations="%apply double& INOUT { double& dm_x, double& dm_y, double& dm_z };",
+        include_dirs=[SOURCE_DIR],)
+    return extension_module
+
+
+def test_damping(terms_module):
     alpha, gamma = 1, 1
     mx, my, mz = 1, 0, 0
     Hx, Hy, Hz = 0, 1, 0
@@ -23,7 +30,7 @@ def test_damping():
     assert (dmx, dmy, dmz) == (0, 0.5, 0)
 
 
-def test_precession():
+def test_precession(terms_module):
     alpha, gamma = 1, 1
     mx, my, mz = 1, 0, 0
     Hx, Hy, Hz = 0, 1, 0
@@ -31,7 +38,7 @@ def test_precession():
     assert (dmx, dmy, dmz) == (0, 0, -0.5)
 
 
-def test_relaxation():
+def test_relaxation(terms_module):
     c = 1.0
     mx, my, mz = 2, 0, 0
     dmx, dmy, dmz = terms_module.relaxation(c, mx, my, mz, 0, 0, 0)
