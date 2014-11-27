6f39dd1f16bafbc27f5e5ed63ad11b20cbec804d
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-11-27 16:41:54 +0000

    update example spatially varying anisotropy to use field, refactor a little

--- examples/spatially-varying-anisotropy/run.py
+++ examples/spatially-varying-anisotropy/run.py
@@ -1,12 +1,3 @@
-import os
-import numpy as np
-import pylab
-import dolfin as df
-
-from finmag import Simulation
-from finmag.energies import UniaxialAnisotropy, Exchange, Demag
-
-MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 """
 Demonstrating spatially varying anisotropy. Example with anisotropy vectors as follows:
 
@@ -18,97 +9,76 @@ Demonstrating spatially varying anisotropy. Example with anisotropy vectors as f
 
 -----------------------------------
 
-^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^ 
-|  |  |  |  |  |  |  |  |  |  |  | 
-|  |  |  |  |  |  |  |  |  |  |  | 
+^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
+|  |  |  |  |  |  |  |  |  |  |  |
+|  |  |  |  |  |  |  |  |  |  |  |
 
 -----------------------------------
 """
+import os
+import numpy as np
+import pylab
+import dolfin as df
+import matplotlib.pyplot as plt
+from finmag import Simulation
+from finmag.field import Field
+from finmag.energies import UniaxialAnisotropy, Exchange
+
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def run_simulation():
+def run_simulation(plot=False):
+    mu0 = 4.0 * np.pi * 10**-7  # vacuum permeability N/A^2
+    Ms = 1.0e6  # saturation magnetisation A/m
+    A = 13.0e-12  # exchange coupling strength J/m
+    Km = 0.5 * mu0 * Ms**2  # magnetostatic energy density scale kg/ms^2
+    lexch = (A/Km)**0.5  # exchange length m
+    unit_length = 1e-9
+    K1 = Km
 
-    mu0   = 4.0 * np.pi * 10**-7  # vacuum permeability             N/A^2
-    Ms    = 1.0e6                 # saturation magnetisation        A/m
-    A     = 13.0e-12              # exchange coupling strength      J/m
-    Km    = 0.5 * mu0 * Ms**2     # magnetostatic energy density scale   kg/ms^2
-    lexch = (A/Km)**0.5           # exchange length                 m
-    K1    = Km
-
-    L = lexch # cube length in m
+    L = lexch / unit_length
     nx = 10
-    Lx = L*nx
+    Lx = nx * L
     ny = 1
-    Ly = ny*L
+    Ly = ny * L
     nz = 30
-    Lz = nz*L
+    Lz = nz * L
     mesh = df.BoxMesh(0, 0, 0, Lx, Ly, Lz, nx, ny, nz)
 
+    # Anisotropy easy axis is (0, 0, 1) in the lower half of the film and
+    # (1, 0, 0) in the upper half. This is a toy model of the exchange spring
+    # systems that Bob Stamps is working on.
+    boundary = Lz / 2.0
+    expr_a = df.Expression(("x[2] <= b ? 0 : 1", "0", "x[2] <= b ? 1 : 0"), b=boundary)
+    V = df.VectorFunctionSpace(mesh, "DG", 0, dim=3)
+    a = Field(V, expr_a)
 
-    #anisotropy direction starts is [0,0,1] in lower half of the film
-    #and [1,0,0] in upper half. This is a toy model of the exchange spring
-    #systems that Bob Stamps is working on.
-    expr_a = df.Expression(("x[2]<=Lz/2. ? 0 : 1","0", "x[2]<=Lz/2. ? 1 : 0"),Lz=Lz)
-
-    #discretise material parameter as discontinous Galerkin order 0 basis function
-    V3_DG0 = df.VectorFunctionSpace(mesh,"DG",0,dim=3)
-    a = df.interpolate(expr_a,V3_DG0)
-
-    sim = Simulation(mesh, Ms)
-    sim.set_m((1,0,1))
+    sim = Simulation(mesh, Ms, unit_length)
+    sim.set_m((1, 0, 1))
     sim.add(UniaxialAnisotropy(K1, a))
     sim.add(Exchange(A))
-    
     sim.relax()
 
-    #create simple plot
-    xpos=[]
-    Mx = []
-    Mz = []
-    ax = []
-    az = []
+    if plot:
+        points = 200
+        zs = np.linspace(0, Lz, points)
+        axis_zs = np.zeros((points, 3))  # easy axis probed along z-axis
+        m_zs = np.zeros((points, 3))  # magnetisation probed along z-axis
 
-    aS3 = df.interpolate(a,sim.S3) 
+        for i, z in enumerate(zs):
+            axis_zs[i] = a((Lx/2.0, Ly/2.0, z))
+            m_zs[i] = sim.m_field((Lx/2.0, Ly/2.0, z))
 
-    zs = np.linspace(0,Lz,200)
-    for z in zs:
-        pos = (Lx/2., Ly/2., z )
-        Mx.append(sim.m_field.probe(pos)[0])
-        Mz.append(sim.m_field.probe(pos)[2])
-        ax.append(a(pos)[0])
-        az.append(a(pos)[2])
-        
-    pylab.plot(Mx,zs/1e-9,'-',label='Magnetisation Mx')
-    pylab.plot(Mz,zs/1e-9,'--',label='Magnetisation Mz')
-    pylab.plot(ax,zs/1e-9,'-o',label='Anisotropy vector ax')
-    pylab.plot(az,zs/1e-9,'-x',label='Anisotropy vector az')
-    pylab.ylabel('position z in film [nm]')
-    pylab.legend(loc='upper center')
-    pylab.savefig(os.path.join(MODULE_DIR,'profile.png'))
-    #pylab.show()
-
-    ##this only works with an X-display, so comment out for jenkins
-    #v = df.plot(sim._m, 
-    #            title='exchange spring across layers with different anisotropy directions',
-    #            axes=True)
-    #
-    #v.elevate(-90) 
-    #v.update(sim._m)    # bring settings above into action
-    ##v.write_png(os.path.join(MODULE_DIR,'exchangespring.png')) #this is broken in my dolfin, HF
-    #                                                            #the bitmap looks random  
-    #v.write_ps(os.path.join(MODULE_DIR,'exchangespring'))       #will write exchangespring.eps
-    #os.system("ps2png exchangespring.eps exchangespring.png")   #create the png file for documentation
-
-    sim.m_field.save_pvd(os.path.join(MODULE_DIR,'exchangespring.pvd'))    #same more data for paraview
-
-    print("Written plots and data to %s" % (os.path.join(MODULE_DIR,'exchangespring.*')))
-        
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(zs, axis_zs[:, 0], "-o", label="a_x")
+        ax.plot(zs, axis_zs[:, 2], "-x", label="a_z")
+        ax.plot(zs, m_zs[:, 0], "-", label="m_x")
+        ax.plot(zs, m_zs[:, 2], "-", label="m_z")
+        ax.set_xlabel("z (nm)")
+        ax.legend(loc="upper left")
+        plt.savefig(os.path.join(MODULE_DIR, "profile.png"))
+        sim.m_field.save_pvd(os.path.join(MODULE_DIR, 'exchangespring.pvd'))
 
 if __name__ == "__main__":
-    run_simulation()
-
-
-
-
-
-
+    run_simulation(plot=True)
