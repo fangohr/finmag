e16625ed6159797c45d8a64c87c5721a7dc422e4
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-11-28 14:31:46 +0000

    remove nativeboostdemo, which was just a hello world

--- src/finmag/tests/nativeboostdemo/test_hello.py
+++
@@ -1,8 +0,0 @@
-import finmag.native as n
-
-print n.llg.demo_hello("x")
-
-
-def test_demo_hello():
-    assert n.llg.demo_hello("x") == "Hellox"
-    assert n.llg.demo_hello("y z 1") == "Helloy z 1"
