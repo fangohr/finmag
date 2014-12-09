b84e77a977500be4f6bbf94c135360c2d64e09e9
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2014-12-09 15:12:16 +0100

    TimeZeemanPython now importable from finmag.energies

--- src/finmag/energies/init.py
+++ src/finmag/energies/init.py
@@ -4,7 +4,7 @@ from energy_base import EnergyBase
 from exchange import Exchange
 from anisotropy import UniaxialAnisotropy
 from cubic_anisotropy import CubicAnisotropy
-from zeeman import Zeeman, TimeZeeman, DiscreteTimeZeeman, OscillatingZeeman
+from zeeman import Zeeman, TimeZeeman, DiscreteTimeZeeman, OscillatingZeeman, TimeZeemanPython
 from dmi import DMI, DMI_interfacial
 from thin_film_demag import ThinFilmDemag
 from dw_fixed_energy import FixedEnergyDW
