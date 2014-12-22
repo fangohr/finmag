f0d06dfe2405e4e4defbca35ceb16a5085b628fe
Hans Fangohr <fangohr@soton.ac.uk>
2014-12-22 14:21:39 +0000

    updated for Hagen Fuchs.

---
+++ doc/tailored_tutorials/2015-Hagen-Fuchs.py
@@ -0,0 +1,12 @@
+conf = {'nameshort': 'HagenFuchs',
+        'names': ('Hagen Fuchs', 'Ulrich Roessler', 'Denys Makarov'),
+        'tutorials' : ['tutorial-using-ipython-notebook',
+                       'tutorial-example2',
+                       'tutorial-saving-averages-demo',
+                       'tutorial-use-of-logging',
+                       'tutorial-running-simulations-with-dmi',
+                       'tutorial-sampling-m-at-arbitrary-positions',
+                       'ref-restarting-simulations',
+                       'tutorial-domain-wall-relaxation-example'
+                       ],
+        'creationdate': "22-12-2014"}
