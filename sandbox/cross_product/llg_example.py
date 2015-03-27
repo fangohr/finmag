dac148b9bf774202a16af749bc9c6fd0fe8b529b
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2015-03-27 16:50:20 +0000

    some more investigation

--- sandbox/cross_product/llg_example.py
+++ sandbox/cross_product/llg_example.py
@@ -1,10 +1,12 @@
 import dolfin as df
 import time
 
-mesh = df.UnitSquareMesh(100, 100)
+mesh = df.UnitSquareMesh(200, 200)
 print mesh
 S1 = df.FunctionSpace(mesh, 'CG', 1)
 S3 = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
+u = df.TrialFunction(S3)
+v = df.TestFunction(S3)
 
 m = df.Function(S3)  # magnetisation
 Heff = df.Function(S3)  # effective field
@@ -17,18 +19,29 @@ Heff.assign(df.Constant((0, 0, 1)))
 alpha.assign(df.Constant(1))
 Ms.assign(df.Constant(1))
 
-dmdt_expression = -gamma/(1+alpha*alpha)*df.cross(m, Heff) - alpha*gamma/(1+alpha*alpha)*df.cross(m, df.cross(m, Heff))
-dmdt_function = df.Function(S3)
-dmdt = dmdt_function.vector()
+# just assembling it
+LLG = -gamma/(1+alpha*alpha)*df.cross(m, Heff) - alpha*gamma/(1+alpha*alpha)*df.cross(m, df.cross(m, Heff))
+L = df.dot(LLG, df.TestFunction(S3)) * df.dP
 
+dmdt = df.Function(S3)
 start = time.time()
-for i in xrange(10000):
-    df.assemble(df.dot(dmdt_expression, df.TestFunction(S3)) * df.dP, tensor=dmdt)
-    if i % 100 == 0:
-        print i
+for i in xrange(1000):
+    df.assemble(L, tensor=dmdt.vector())
 stop = time.time()
 print "delta = ", stop - start
+print dmdt.vector().array()
 
-# An expected result is unit z vector and the type is dolfin vector.
-print dmdt.array()
-print type(dmdt)
+# more linear algebra, same problem... still need to assemble the cross product
+# we're doing even more work than before
+a = df.dot(u, v) * df.dP
+A = df.assemble(a)
+b = df.Function(S3)
+
+dmdt = df.Function(S3)
+start = time.time()
+for i in xrange(1000):
+    df.assemble(L, tensor=b.vector())  # this is what should go out of the loop
+    df.solve(A, dmdt.vector(), b.vector())  # some variation of this could stay in
+stop = time.time()
+print "delta = ", stop - start
+print dmdt.vector().array()
