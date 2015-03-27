665c1b7e6c8554054683031048f89c5eb57a7005
Marc-Antonio Bisotti <mb8g11@soton.ac.uk>
2015-03-27 12:49:28 +0000

    commit benchmark (with Marijan)

--- sandbox/cross_product/llg_example.py
+++ sandbox/cross_product/llg_example.py
@@ -1,6 +1,8 @@
 import dolfin as df
+import time
 
-mesh = df.IntervalMesh(10, 0, 1)
+mesh = df.UnitSquareMesh(100, 100)
+print mesh
 S1 = df.FunctionSpace(mesh, 'CG', 1)
 S3 = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
 
@@ -16,7 +18,16 @@ alpha.assign(df.Constant(1))
 Ms.assign(df.Constant(1))
 
 dmdt_expression = -gamma/(1+alpha*alpha)*df.cross(m, Heff) - alpha*gamma/(1+alpha*alpha)*df.cross(m, df.cross(m, Heff))
-dmdt = df.assemble(df.dot(dmdt_expression, df.TestFunction(S3)) * df.dP)
+dmdt_function = df.Function(S3)
+dmdt = dmdt_function.vector()
+
+start = time.time()
+for i in xrange(10000):
+    df.assemble(df.dot(dmdt_expression, df.TestFunction(S3)) * df.dP, tensor=dmdt)
+    if i % 100 == 0:
+        print i
+stop = time.time()
+print "delta = ", stop - start
 
 # An expected result is unit z vector and the type is dolfin vector.
 print dmdt.array()
