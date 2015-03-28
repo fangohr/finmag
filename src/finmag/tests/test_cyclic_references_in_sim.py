96bcb8c6efd204d383ca60e73652e088189c0de9
Hans Fangohr <fangohr@soton.ac.uk>
2015-03-28 22:31:09 +0000

    First steps towards understanding (undesired) persistence of simulation objects

--- src/finmag/tests/test_cyclic_references_in_sim.py
+++ src/finmag/tests/test_cyclic_references_in_sim.py
@@ -5,7 +5,7 @@ def test_cyclic_refs_in_simulation_object_basic():
     import dolfin as df
     mesh = df.UnitIntervalMesh(1)
     s = finmag.Simulation(mesh, Ms=1, unit_length=1e-9, name='simple')
-    refcount = s.delete()
+    refcount = s.shutdown()
     # The number 4 is emperical. If it increases, we
     # have introduced an extra cyclic reference.
     assert refcount == 4
@@ -17,7 +17,7 @@ def test_cyclic_refs_in_simulation_object_barmini():
     mesh = df.UnitIntervalMesh(1)
     s = finmag.example.barmini()
     s.run_until(1e-12)
-    refcount = s.delete()
+    refcount = s.shutdown()
     # The number 4 is emperical. If it increases, we
     # have introduced an extra cyclic reference.
     assert refcount == 4
