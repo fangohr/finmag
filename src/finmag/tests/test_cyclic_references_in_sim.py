2c13697b977507847972ea7abdbe4442fe34ffef
Hans Fangohr <fangohr@soton.ac.uk>
2015-03-31 09:43:28 +0100

    In cythonised code, refcount seems to be only 3, not 4. Updated tests accordingly. (Is emperical only anyway.)

--- src/finmag/tests/test_cyclic_references_in_sim.py
+++ src/finmag/tests/test_cyclic_references_in_sim.py
@@ -8,7 +8,9 @@ def test_cyclic_refs_in_simulation_object_basic():
     refcount = s.shutdown()
     # The number 4 is emperical. If it increases, we
     # have introduced an extra cyclic reference.
-    assert refcount == 4
+    # Update: the cythonised code seems to have only 3 references at his point. Updated
+    # to smaller than 4 to allow binary build tests to pass.
+    assert refcount <= 4
 
 def test_cyclic_refs_in_simulation_object_barmini():
 
