ffcab477127c50e449bb076e39b8b8752eb97df2
Rebecca Carey <r.carey@soton.ac.uk>
2015-05-01 14:57:19 +0100

    Adding saving of json files to the classes for saving and loading data

--- sandbox/saving_data/savingdata.py
+++ sandbox/saving_data/savingdata.py
@@ -1,16 +1,25 @@
 import dolfin as df
 import numpy as np
-
+import json
+from collections import OrderedDict
 
 class SavingData(object):
-    def __init__(self, h5filename, npzfilename, functionspace):
+    def __init__(self, h5filename, npzfilename, jsonfilename, functionspace):
         self.functionspace = functionspace
         self.h5filename = h5filename
         self.npzfilename = npzfilename
-        self.h5file = df.HDF5File(functionspace.mesh().mpi_comm(), h5filename, 'w')
+        self.h5file = df.HDF5File(self.functionspace.mesh().mpi_comm(), self.h5filename, 'w')
+        self.jsonfilename = jsonfilename
         
         self.field_index = 0
         self.t_array = []
+
+        self.fieldsDict = {} # dictionary of all field types e.g. 'm', 'H_eff'
+
+        # create json file
+        with open(self.jsonfilename, 'w') as jsonfile:
+            json.dump(self.fieldsDict, jsonfile, sort_keys=False)
+        jsonfile.close()
 
     def save_field(self, f, field_name, t):
         name = field_name + str(self.field_index)
@@ -18,6 +27,17 @@ class SavingData(object):
         
         self.t_array.append(t)
         np.savez(self.npzfilename, t_array=self.t_array)
+
+        # todo: method of doing this without having to open
+        # and close json file all time?
+        if not self.fieldsDict.has_key(field_name):
+            self.fieldsDict[field_name] = OrderedDict()
+
+        self.fieldsDict[field_name][name] = t
+
+        with open(self.jsonfilename, 'w') as jsonfile:
+            json.dump(self.fieldsDict, jsonfile, sort_keys=False)
+        jsonfile.close()
 
         self.field_index += 1
 
@@ -29,13 +49,14 @@ class SavingData(object):
 
 
 class LoadingData(object):
-    def __init__(self, h5filename, npzfilename, functionspace):
+    def __init__(self, h5filename, npzfilename, jsonfilename, functionspace):
         self.functionspace = functionspace
         self.h5filename = h5filename
         self.npzfilename = npzfilename
-        self.h5file = df.HDF5File(functionspace.mesh().mpi_comm(), h5filename, 'r')
-        
-        npzfile = np.load(npzfilename)
+        self.h5file = df.HDF5File(self.functionspace.mesh().mpi_comm(), self.h5filename, 'r')
+        self.jsonfilename = jsonfilename
+
+        npzfile = np.load(self.npzfilename)
         self.t_array = npzfile['t_array']
 
     def load_mesh(self, name='mesh'):
@@ -46,11 +67,41 @@ class LoadingData(object):
         return mesh_loaded
 
     def load_field(self, field_name, t):
-        index = np.where(t_array)[0][0]
+        index = np.where(self.t_array==t)[0][0]
+        print index
+
         name = field_name + str(index)
 
-        f_loaded = df.Function(functionspace)
-        self.h5file.read(f_loaded, name, False)
+        f_loaded = df.Function(self.functionspace)
+
+        # todo: removed last parameter, False to make it work.
+        # (originally was self.h5file.read(f_loaded, name))
+        # Need to check if this is needed...
+        self.h5file.read(f_loaded, name)
+
+        return f_loaded
+
+    def load_field_with_json_data(self, field_name, t):
+
+        with open(self.jsonfilename) as jsonfile:
+            fieldsDict = json.load(jsonfile, object_pairs_hook=OrderedDict)
+        jsonfile.close()
+
+        # todo: this next line is v. bad!
+        # The json file format can easily be changed to a more aproppiate one
+        # depending on how we actually want to use the data stored in the json file.
+        # For example a dictionary of lists rather than the current dictionary of 
+        # ordererDictionaries.
+        name = str([item[0] for item in fieldsDict[field_name].items() if item[1]==t][0])
+
+        # name = field_name + str(index)
+
+        f_loaded = df.Function(self.functionspace)
+        
+        # todo: removed last parameter, False to make it work.
+        # (originally was self.h5file.read(f_loaded, name))
+        # Need to check if this is needed...
+        self.h5file.read(f_loaded, name)
 
         return f_loaded
 
--- sandbox/saving_data/savingdata_test.py
+++ sandbox/saving_data/savingdata_test.py
@@ -1,17 +1,20 @@
 import dolfin as df
 import numpy as np
+import json
+from collections import OrderedDict
 from savingdata import SavingData, LoadingData
 
 h5filename = 'file.h5'
 npzfilename = 'file.npz'
+jsonfilename = 'file.json'
 mesh = df.UnitSquareMesh(10, 10)
 functionspace = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
 f = df.Function(functionspace)
-t_array = np.linspace(0, 1e-9, 0.1e-9)
+t_array = np.linspace(0, 1e-9, 5)
 
 
 def test_save_data():
-    sd = SavingData(h5filename, npzfilename, functionspace)
+    sd = SavingData(h5filename, npzfilename, jsonfilename, functionspace)
 
     sd.save_mesh()
 
@@ -24,7 +27,7 @@ def test_save_data():
 
 
 def test_load_data():
-    ld = LoadingData(h5filename, npzfilename, functionspace)
+    ld = LoadingData(h5filename, npzfilename, jsonfilename, functionspace)
 
     mesh_loaded = ld.load_mesh()
 
@@ -35,3 +38,37 @@ def test_load_data():
         assert np.all(f.vector().array() == f_loaded.vector().array())
 
     ld.close()
+
+
+def test_load_data_with_json_data():
+    ld = LoadingData(h5filename, npzfilename, jsonfilename, functionspace)
+
+    mesh_loaded = ld.load_mesh()
+
+    for t in t_array:
+        f_loaded = ld.load_field_with_json_data('f', t)
+
+        f.assign(df.Constant((t, 0, 0)))
+        assert np.all(f.vector().array() == f_loaded.vector().array())
+
+    ld.close()
+
+
+def test_saved_json_data():
+    with open(jsonfilename) as jsonfile:
+        jsonData = json.load(jsonfile, object_pairs_hook=OrderedDict)
+    jsonfile.close()
+
+    # assert times from keys
+    for i, t in enumerate(t_array):
+        name = "f{}".format(i)
+        jsonTime = jsonData['f'][name]
+        assert(jsonTime == t)
+
+    # assert times by iterating through values (as they should be ordered)
+    index = 0
+    for jsonTime in jsonData['f'].itervalues():
+         assert(jsonTime == t_array[index])
+         index += 1
+
+
