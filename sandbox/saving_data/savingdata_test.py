84c0782c61b750d69568a308864d600962b9588d
Marijan Beg <mb4e10@soton.ac.uk>
2015-05-01 11:14:21 +0100

    Add testing script.

---
+++ sandbox/saving_data/savingdata_test.py
@@ -0,0 +1,37 @@
+import dolfin as df
+import numpy as np
+from savingdata import SavingData, LoadingData
+
+h5filename = 'file.h5'
+npzfilename = 'file.npz'
+mesh = df.UnitSquareMesh(10, 10)
+functionspace = df.VectorFunctionSpace(mesh, 'CG', 1, 3)
+f = df.Function(functionspace)
+t_array = np.linspace(0, 1e-9, 0.1e-9)
+
+
+def test_save_data():
+    sd = SavingData(h5filename, npzfilename, functionspace)
+
+    sd.save_mesh()
+
+    for i in range(len(t_array)):
+        f.assign(df.Constant((t_array[i], 0, 0)))
+    
+        sd.save_field(f, 'f', t_array[i])
+
+    sd.close()
+
+
+def test_load_data():
+    ld = LoadingData(h5filename, npzfilename, functionspace)
+
+    mesh_loaded = ld.load_mesh()
+
+    for t in t_array:
+        f_loaded = ld.load_field('f', t)
+
+        f.assign(df.Constant((t, 0, 0)))
+        assert np.all(f.vector().array() == f_loaded.vector().array())
+
+    ld.close()
