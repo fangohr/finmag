563d7c791c99c6289c354a4468f925174fb81ce8
Hagen Fuchs <code@hfuchs.net>
2015-05-16 17:56:56 +0200

    energies/dmi.py: Introduce new lifshitz invariant terms.

--- src/finmag/energies/dmi.py
+++ src/finmag/energies/dmi.py
@@ -98,11 +98,80 @@ class DMI(EnergyBase):
         if self.dmi_type is 'interfacial':
             E_integrand = DMI_interfacial(m, self.dmi_factor*self.D.f,
                                           dim=dmi_dim)
+        elif self.dmi_type in ['C_nv', 'D_n', 'D_2d', 'C_n', 'S_4']:
+            E_integrand = LI_assembler(m, self.dmi_factor*self.D.f, self.dmi_type)
         else:
             E_integrand = self.dmi_factor*self.D.f*times_curl(m.f, dmi_dim)
 
         super(DMI, self).setup(E_integrand, m, Ms, unit_length)
 
+def LI_assembler(m, D, crystalsym):
+    """
+    This is the LI assembler for the terms as listed in ANB89.
+
+    Remember: "To stabilize vortex states, an additional
+    uniaxial anisotropy must be present." (Bogdanov 1994)
+
+    Also: Note the comments below on the exclusion of certain lifshitz
+    invariants.
+    """
+    #logger.info("Running LI assembler with type ", crystalsym)
+    gradm = df.grad(m.f)
+
+    # TODO How costly are these assignments?
+    dmxdx = gradm[0, 0]
+    dmydx = gradm[1, 0]
+    dmzdx = gradm[2, 0]
+    dmxdy = gradm[0, 1]
+    dmydy = gradm[1, 1]
+    dmzdy = gradm[2, 1]
+    # Not in use currently (see below):
+    #dmxdz = gradm[0, 2]
+    #dmydz = gradm[1, 2]
+    #dmzdz = gradm[2, 2]
+
+    mx = m.f[0]
+    my = m.f[1]
+    mz = m.f[2]
+
+    # TODO (Bogdanov 1989) or (1994) notation?
+    w_1 = mz*dmxdx - mx*dmzdx + mz*dmydy - my*dmzdy
+    w_2 = mz*dmxdy - mx*dmzdy - mz*dmydx + my*dmzdx
+    # (Note: ANB89 uses w_1' and w_2' instead.)
+    w_3 = mz*dmxdx - mx*dmzdx - mz*dmydy + my*dmzdy
+    w_4 = mz*dmxdy - mx*dmzdy + mz*dmydx - my*dmzdx
+    #w_z = mx*dmydz - my*dmxdz  # w_3 in (Bogdanov 1994).
+
+    # Note: All terms w_z are left out!  ANB89: "We note also that the
+    # classes D_n and C_n admit the invariants [w_z] which can lead to
+    # the formation of a spiral structure with propagation vector along
+    # the z axis." (Bogdanov, Hubert 1994) says the same thing.
+
+    # Nota bene, the DMI_interfacial() below implements -w_1 (C_nv).
+
+    # TODO w_1...4 use actually only 4 distinct LIs:
+    # LIs, spelled out: m_i delta_j m_k - m_k delta_j m_i
+    #
+    #w_1 = w_11 + w_12    w_2 = w_21 + w_22
+    #w_3 = w_11 - w_12    w_4 = w_21 - w_22
+
+    # For now, I'll equate all different dmi constants.
+    a = a_1 = a_2 = a_3 = a_4 = a_5 = a_6 = D
+    # Note: At least C_nv and D_n admit w_z terms (see above)!
+    C_nv = a * w_1
+    D_n  = a_1 * w_2
+    D_2d = a_2 * w_4
+    C_n  = a_3 * w_1 + a_4 * w_2
+    S_4  = a_5 * w_3 + a_6 * w_4
+
+    # TODO I'd really like a more elegant dispatch table here.  I want
+    # many other pretty things, too.
+    if crystalsym is 'C_nv':   return C_nv
+    elif crystalsym is 'D_n':  return D_n
+    elif crystalsym is 'D_2d': return D_2d
+    elif crystalsym is 'C_n':  return C_n
+    elif crystalsym is 'S_4':  return S_4
+    else: True # TODO Die?  Do nothing?  What?
 
 def DMI_interfacial(m, D, dim):
     """
