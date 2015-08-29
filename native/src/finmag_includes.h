ddbdf4e1c970fbd00d9d33c730e8be08a9643c97
Maximilian Albert <maximilian.albert@gmail.com>
2015-08-29 14:12:11 +0100

    Include boost/python.hpp before everything else to avoid a compilation error due to the warning: '"_POSIX_C_SOURCE" redefined [-Werror]'

--- native/src/finmag_includes.h
+++ native/src/finmag_includes.h
@@ -10,6 +10,17 @@
 #ifndef __FINMAG_INCLUDES_H
 #define __FINMAG_INCLUDES_H
 
+// Boost includes
+#ifndef IDE_ERROR_BLOCK
+#include <boost/python.hpp>
+#include <boost/mpl/map.hpp>
+#include <boost/mpl/transform.hpp>
+#include <boost/mpl/range_c.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/typeof/typeof.hpp>
+#include <boost/thread.hpp>
+#endif
+
 // Standard C/C++ includes
 #include <cstring>
 #include <cmath>
@@ -17,17 +28,6 @@
 
 // OpenMP
 #include <omp.h>
-
-// Boost includes
-#ifndef IDE_ERROR_BLOCK
-#include <boost/mpl/map.hpp>
-#include <boost/mpl/transform.hpp>
-#include <boost/mpl/range_c.hpp>
-#include <boost/mpl/list.hpp>
-#include <boost/python.hpp>
-#include <boost/typeof/typeof.hpp>
-#include <boost/thread.hpp>
-#endif
 
 // CVODE/Sundials
 #include <cvode/cvode.h>
