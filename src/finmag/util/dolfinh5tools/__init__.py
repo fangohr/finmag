7fe6b17605b4c75a87904196c5aaf458fa4ebbb4
Rebecca <r.carey@soton.ac.uk>
2015-10-16 15:48:07 +0100

    Deleting the dolfinh5tools files originally used in the finmag source as dolfinh5tools is now assumed to be installed.

--- src/finmag/util/dolfinh5tools/__init__.py
+++
@@ -1,7 +0,0 @@
-# FinMag - a thin layer on top of FEniCS to enable micromagnetic multi-physics simulations
-# Copyright (C) 2012 University of Southampton
-# Do not distribute
-#
-# CONTACT: h.fangohr@soton.ac.uk
-#
-# AUTHOR(S) OF THIS FILE: Gabriel Balaban gabrib@math.uio.no
