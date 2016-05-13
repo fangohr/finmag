62c2bcb18cc42f0b3185f38f5e09bccb524cc14e
Hans Fangohr osiris <fangohr@soton.ac.uk>
2016-05-13 11:52:31 +0100

    updated for dolfin 1.6

--- sandbox/basics-dolfin/parallel/local-arrays/parallel.py
+++ sandbox/basics-dolfin/parallel/local-arrays/parallel.py
@@ -1,10 +1,10 @@
-print "Hello World"
+print("Hello World")
 import dolfin as df
 #df.parameters.reorder_dofs_serial = False
 nx = ny = 10
-mesh = df.RectangleMesh(0, 0, 1, 1, nx, ny)
+mesh = df.RectangleMesh(df.Point(0, 0), df.Point(1, 1), nx, ny)
 V = df.VectorFunctionSpace(mesh, 'CG', 1, dim=3)
-zero = df.Constant(list([0,0,1]))
-zero_v = df.interpolate(zero, V)
-print("My vector is of shape %s." % zero_v.vector().array().shape)
-print zero_v.vector().array()
+vector_value_constant = df.Constant(list([0,1,2]))
+our_function = df.interpolate(vector_value_constant, V)
+print("My vector is of shape %s." % our_function.vector().array().shape)
+print(our_function.vector().array())
