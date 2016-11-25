4115acc86c93139e4946cc2d2f7076894d06e202
mvousden <mark.vousden@soton.ac.uk>
2016-11-25 12:23:37 +0000

    CI-7: Improve the stop script.

--- stop.sh
+++ stop.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
-BUILDBOT_EXEC_PREFIX="$HOME/miniconda2/bin"
+
+# Script that stops the master, and attempts to tear down the workers.
+
+. common.lib
+
+# Function to append the name of this script to output messages.
+# $1: Message (in quotes)
+# Returns nothing, but prints to stdout.
+function echo_stop {
+    echo "[stop.sh] $1"
+}
+
+# Find buildbot path. Optionally, user can define environment variable
+# $BUILDBOT_EXEC_PREFIX themselves.
+if [ -z "$BUILDBOT_EXEC_PREFIX" ]; then
+    if [ -z "$(which buildbot)" ]; then
+        echo_stop "Error: command 'buildbot' not found."
+    else
+        BUILDBOT_EXEC_PREFIX="$(dirname "$(which buildbot)")"
+    fi
+fi
+
+echo_stop "Stopping master..."
 $BUILDBOT_EXEC_PREFIX/buildbot stop master/
-$BUILDBOT_EXEC_PREFIX/buildbot-worker stop worker_1/
-$BUILDBOT_EXEC_PREFIX/buildbot-worker stop worker_2/
-rm -rf worker_1/ worker_2/
+echo_stop "Master stopped"
+
+# Stopping and destroying workers.
+for zI in $(seq 1 2); do
+    if [ -d worker_$zI ]; then
+        echo_stop "Worker worker_$zI not found. Not destroying."
+    fi
+    echo_stop "Stopping worker_$zI..."
+    $BUILDBOT_EXEC_PREFIX/buildbot-worker stop worker_$zI
+    echo_stop "Destroying worker_$zI..."
+    rm -rf worker_$zI
+done
