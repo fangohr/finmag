fbbd1acf9f8c7dcf7b3ff2384cbe146d0863d60b
Hans Fangohr <fangohr@soton.ac.uk>
2016-11-27 07:44:17 +0000

    Tool to warn if we haven't got enough memory to build native modules

---
+++ install/memory-warning.py
@@ -0,0 +1,18 @@
+import sys
+from psutil import virtual_memory
+
+mem = virtual_memory()
+mem.total  # total physical virtual_memory
+
+GB = 1024**3
+
+if mem.total / GB < 4:
+    print("Warning: building native modules may fail due to not enough physical memory.")
+    print("You have {} GB available.\n".format(mem.total/GB))
+    print("\tContext: The C++ compiler needs lots of RAM. 4GB seem sufficient (Nov 2016)\n")
+    print("\tIf you are on OSX / Windows, and using Docker, try this")
+    print("""\t\tdocker-machine stop
+\t\tVBoxManage modifyvm default --memory 4096
+\t\tdocker-machine start)\n""")
+    print("\tSource: http://stackoverflow.com/questions/32834082/how-to-increase-docker-machine-memory-mac")
+    sys.exit(1)
