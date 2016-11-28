4e8b1f263b6b3380cc3fea881a17bb75f445f53f
mb1a15 <mb4e10@soton.ac.uk>
2016-11-28 10:07:48 +0000

    Remove GCR demag from init.py.

--- src/finmag/energies/demag/__init__.py
+++ src/finmag/energies/demag/__init__.py
@@ -2,7 +2,6 @@ import logging
 from fk_demag import FKDemag
 from fk_demag_pbc import MacroGeometry
 from fk_demag_2d import Demag2D
-from solver_gcr import FemBemGCRSolver
 from treecode_bem import TreecodeBEM
 
 log = logging.getLogger("finmag")
