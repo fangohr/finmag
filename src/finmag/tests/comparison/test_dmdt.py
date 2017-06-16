fd2310772878cfa6199f0ad6a0044a7506e0a551
Thomas Kluyver <thomas@kluyver.me.uk>
2017-06-16 16:19:37 +0100

    Comparison test can now run without oommf
    Using stored result

--- src/finmag/tests/comparison/test_dmdt.py
+++ src/finmag/tests/comparison/test_dmdt.py
@@ -20,7 +20,6 @@ S1 = df.FunctionSpace(msh, "Lagrange", 1)
 S3 = df.VectorFunctionSpace(msh, "Lagrange", 1)
 
 
-@pytest.mark.skipif('subprocess.call(["which", "oommf"]) != 0')
 def test_dmdt_computation_with_oommf():
     # set up finmag
     llg = LLG(S1, S3)
